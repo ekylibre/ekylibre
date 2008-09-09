e5685647a1c779e6e1db7bb2159754147782cf7e
Vincent Mauget <vincent.mauget@ekylibre.org>
2008-09-09 12:24:24 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@2 67a09383-3dfa-4221-8551-890bac9c277c

--- vendor/plugins/pdf_report/lib/pdf_report.rb
+++ vendor/plugins/pdf_report/lib/pdf_report.rb
@@ -3,58 +3,47 @@
 module PdfReport
 
 
+
   
   def self.included (base)
     base.extend(ClassMethods)
   end
 
   module ClassMethods
-
-
-
-    # this function looks for a method render_report_template and calls analyse_template if not.
-    def render_report(template, id)
-      begin
-        template = Template.find(template) if template.is_a? Integer or template.is_a? Template  
-      rescue		
-        raise ArgumentTypeError, "the template has not been found in the database, you must create before."    
-      end		
-      analyze_template(template) unless ActionController::Base.methods.include? 'render_report_#{template.id}'   	
-      pdf = self.send('render_report_#{template.id}',id)
-    end
-
-    # this function 
-    def analyze_template(template)
-      doc = Document.new(File.new("#{template.name}"))
-      root = doc.root
-      template = root.elements['template']						 	
-      if template.attributes['query-standard'] =~ 'sql'	
-        code = 'def render_report_\'#{template.id}\'' + 'pdf = FPDF.new (\"#{template.attributes[\'orientation\']}\",\" 		    	      #{template.attributes[\'unit\']}\", \"#{template.attributes[\'format\']}\") + pdf.SetSubject(template.attributes[\'title\'])'
-        
-        code += analyze_infos(template, root.elements['infos']) if root.elements['infos']
-	
-
-        code += 'pdf.Close(); pdf.Output(); end'
-        
-      elsif
-        raise	'You must specify sql for query-standard.'
-      end
+    
+    require 'rexml/document'
+    include REXML
+    require 'digest/md5'
+    
       
 
+    # this function begins to analyse the template extracting the main characteristics of
+    # the Pdf document as the title, the orientation, the format, the unit ... 
+    def analyze_template(template, name, id)
+      document = Document.new(template)#File.new(template))
+      document_root = document.root
+      raise Exception.new "Only SQL" unless document_root.attributes['query-standard']||'sql' == 'sql'
+      code = 'def render_report_'+name+'(id)'+"\n"
       
+      code += 'pdf = FPDF.new("#{document_root.attributes[\'orientation\']}","#{document_root.attributes[\'unit\']}","#{document_root.attributes[\'format\']}", "#{document_root.attributes[\'size\']}")' + "\n end"
+   
+     # code += analyze_infos(template,document_root.elements['infos']) if document_root.elements['infos']
       
+      #code += analyze_loop(template, root.elements['loop']) if root.elements['loop']
+      # code += ' end;'
+      #code += 'pdf.Close(); pdf.Output(); end'
       
-      
-      ActionController::Base.logger.error(code)
+      # ActionView::Base.logger.error(code)
       module_eval(code)
+      # render_report_1
     end
-
-    # this function test if the balise info exist in the template and add it in the code	
+    
+    # this function test if the balise info exists in the template and add it in the code	
     def analyze_infos(template, infos)
       infos.each_element('info') do |info|
         case info.attributes['type']
-        when "created-on"
-          code += 'pdf.Set(\"#{info.text}\")'
+        #when "created-on"
+         # code += 'pdf.Set(\"#{info.text}\")'
         when "written-by"
           code += 'pdf.SetAuthor(\"#{info.text}\")'
         when "created-by"
@@ -70,12 +59,26 @@ module PdfReport
   end
 
   # insertion of the module in the Actioncontroller
-  ActionController::Base.send :include, PdfReport
+  ActionView::Base.send :include, PdfReport
 
         
 end
 
+module ActionView
+  class Base
+    # this function looks for a method render_report_template and calls analyse_template if not.
+    def render_report(template, id)
+      raise Exception.new "Your argument template must be a string" unless template.is_a? String
+      digest = Digest::MD5.hexdigest(template)
+      self.class.analyze_template(template, digest, id) unless self.methods.include? 'render_report_#{digest}'
 
+      pdf = self.send('render_report_'+digest,id)
+    
+    
+    
+    end
+  end
+end
 
 
 
