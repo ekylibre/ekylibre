dc44bf104ddc5d5ea126c33d264eb9147a2cf149
Vincent Mauget <vincent.mauget@ekylibre.org>
2008-10-23 14:48:06 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@89 67a09383-3dfa-4221-8551-890bac9c277c

--- app/models/template.rb
+++ app/models/template.rb
@@ -16,9 +16,6 @@
 #
 
 class Template < ActiveRecord::Base
-  # the column md5 is fulled with the MD5 of the content template.
-  def before_save
-    self.md5=Digest::MD5.hexdigest(self.content)
-   
-  end
+ 
+ 
 end
