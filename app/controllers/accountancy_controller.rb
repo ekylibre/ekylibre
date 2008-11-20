05a808e419e4e9679e23ece4f20d27c899e23ace
Vincent Mauget <vincent.mauget@ekylibre.org>
2008-11-20 16:24:54 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@115 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/accountancy_controller.rb
+++ app/controllers/accountancy_controller.rb
@@ -11,14 +11,58 @@ class AccountancyController < ApplicationController
 
 
   def accounts
+    accounts = Account.find(:all)
+    results = Hash.new
+    
+    accounts.each do |account|
+      entries = Entry.find_by_id(account.id)
+      results[account.name.to_sym] = Hash.new
+      result = results[account.name.to_sym]
+      entries.each do |entrie|
+        result[:debit] += entrie[debit]
+        result[:credit] += entrie[credit]
+      end
+      result[:solde] = result[:debit] - result[:credit]
+     results[account.name.to_sym] = result
+    end
+    
     @accounts = @current_company.accounts
   end
   
+
+      
   def entries
-    
   end
 
+
   def journals
+    nature_id = Journal.find_by_nature_id(params[id]) 
+    nature_journal = JournalNature.find_by_name(nature_id)
+    results = Hash.new
+    
+    periods = JournalPeriod.find(:all,:conditions=>["journal_id = ?", params[id]])
+    periods.each do |period| 
+      results[period.started_on.to_sym] = Hash.new
+      result=results[period.started_on.to_sym]
+
+      records = JournalRecord.find(:all,:conditions=>["period_id = ?", period[id]])
+      records.each do |record|
+        result[record.id.to_sym] = Hash.new
+        result = result[record.id.to_sym]
+        result[:save_on] = record[created_on]
+        entries = Entry.find(:all,:conditions=>["record_id = ?", record[id]])
+        entries.each do |entrie|
+          result[entrie.account_id.to_sym] = Hash.new
+          result[entrie.account_id.to_sym][:debit] = entrie[debit]
+          result[entrie.account_id.to_sym][:credit] = entrie[credit]
+          result[entrie.account_id.to_sym][:solde] = entrie[solde]
+          account = Account.find(entrie.account_id)
+          result[entrie.account_id.to_sym][:number] = account[number]
+          result[entrie.account_id.to_sym][:name] = account[name]
+        end
+        results[period.started_on.to_sym] = results
+      end
+    end
     journals_list params
 #    @journals = @current_company.journals
   end
