8188da09e6180c650bcb827d7faf4aae29108d6f
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2008-11-27 11:26:10 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@138 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -16,31 +16,33 @@ class HelpController < ApplicationController
     code += content_tag(:br, '')
     code += content_tag(:a ,  '/app/languages/fr/help/exemple.txt')
 
-    file = "app/languages/fr/help/+file_name.to_s+.txt"
+    file_text  = "app/languages/fr/help/"+file_name+".txt"
+    file_cache = "app/languages/fr/help/cache/"+file_name+".html"
        
-    if File.exists?(file)
-      code  += content_tag(:h1 , 'TestOK')   
-    else
-       code += content_tag(:h1 ,'TestNOk')
-    
-     # code += content_tag (:h1 , 'fichier existant')
-  
+    if File.exists?(file_cache)  # the file exists in the cache
+      then 
+      code  += content_tag(:h1 , 'TestOKdans le cache') 
 
+      i = 1
+      file = File.open('app/languages/fr/help/cache/'+file_name+'.html' , 'r')
+     # file = File.open('app/languages/fr/help/'+file_name+'.txt', 'r') # text file, not cache file
+      file.each_line { |lign|
+          line = "#{i} - #{lign}"        
+          code  += content_tag(:h2, line)
+          i += 1
+      }
 
-  # fichier = File.open ("/app/languages/fr/help/exemple.txt", "r")
-  #  i = 1
-   # fichier.each_line { |ligne|
-    #  lign = "#{i} - #{ligne}"
-     # code += content_tag(:b , lign )
-      #i += 1 
+      else
+      code += content_tag(:h1 ,'TestNOk')
+    end
+   
+    #file.close 
 
-   # }
-   # fichier.close
 
     code  = content_tag(:div, code, :id=>:help, :flex=>1)
     render :text=>code
   
-    end
+  end
 
 
 end
