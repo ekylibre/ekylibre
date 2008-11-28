ae99733d0435ee31866b61f9fda245cfb83895e3
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2008-11-28 11:30:20 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@144 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -28,14 +28,19 @@ class HelpController < ApplicationController
     else
       file = File.open('app/languages/fr/help/'+file_name+'.txt' , 'r') # text file 
 
-      #gsub(/^<<[a-z]|[A-Z]|[0-9][|][a-z]|[A-Z]|[0-9]>>$/ , "&bull;") 
+     
       content = file.read
-      #code  += content_tag(:h4, content.gsub(/[A-Z]/ , 'e'))
-      code  += content_tag(:h4, content.gsub(/^</ , '&bull;'))
+      ltr = link_to_remote('\4', :url => { :action => "search", :id => '\1' }, :update => :help)
+      content  =  content.gsub(/<<(\w+)((\|)(\w+))>>/ , ltr )
+
+      
+      content = textilize(content)
+      code  += content_tag(:div, content)
+      #FileUtils.cp ( content, 'app/languages/fr/help/cache/')
       
       code += content_tag(:h1 ,'TestOk hors cache')
-
     end
+    
    
     #file.close 
 
