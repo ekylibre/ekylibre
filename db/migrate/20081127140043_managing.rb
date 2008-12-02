f8be78e2fa6b2d8556c4d606001106a9a8849d62
Brice Texier <brice.texier@ekylibre.org>
2008-12-02 11:31:57 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@152 67a09383-3dfa-4221-8551-890bac9c277c

--- db/migrate/20081127140043_managing.rb
+++ db/migrate/20081127140043_managing.rb
@@ -1,6 +1,15 @@
 class Managing < ActiveRecord::Migration
   def self.up
 
+    # Unit (of measure)
+    create_table :units do |t|      
+      t.column :name,                   :string,   :null=>false, :limit=>8
+      t.column :full_name,              :string,   :null=>false
+      t.column :company_id,             :integer,  :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    add_index :units, [:name, :company_id], :unique=>true
+    add_index :units, :company_id
+    
     # Product
     create_table :products do |t| 
       t.column :to_purchase,            :boolean,  :null=>false
@@ -23,14 +32,6 @@ class Managing < ActiveRecord::Migration
     add_index :products, :company_id
 
 
-    # Unit (of measure)
-    create_table :units do |t|      
-      t.column :name,                   :string,   :null=>false
-      t.column :company_id,             :integer,  :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
-    end
-    add_index :units, [:name, :company_id], :unique=>true
-    add_index :units, :company_id
-    
     # Shelf
     create_table :shelves do |t|
       t.column :name,                   :string,   :null=>false
@@ -65,7 +66,7 @@ class Managing < ActiveRecord::Migration
     create_table :pricelist_items do |t|
       t.column :price,                  :decimal,  :null=>false, :precision=>16, :scale=>4
       t.column :price_with_taxes,       :decimal,  :null=>false, :precision=>16, :scale=>4
-      t.column :is_active,              :boolean,  :null=>false, :default=>true
+      t.column :active,                 :boolean,  :null=>false, :default=>true
       t.column :use_range,              :boolean,  :null=>false, :default=>false
       t.column :quantity_min,           :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
       t.column :quantity_max,           :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
@@ -76,13 +77,13 @@ class Managing < ActiveRecord::Migration
     add_index :pricelist_items, :product_id
     add_index :pricelist_items, :pricelist_id
     add_index :pricelist_items, [:quantity_min, :product_id, :pricelist_id, :company_id], :unique=>true
-    add_index :, :company_id
+    add_index :pricelist_items, :company_id
     
 
     # Tax
     create_table :taxes do |t|
       t.column :name,                   :string,  :null=>false
-      t.column :is_reductible,          :boolean, :null=>false, :default=>true
+      t.column :reductible,             :boolean, :null=>false, :default=>true # for the eco-particpation
       t.column :amount,                 :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
       t.column :rate,                   :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
       t.column :description,            :text
@@ -90,7 +91,7 @@ class Managing < ActiveRecord::Migration
       t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
     end
     add_index :taxes, [:name, :company_id], :unique=>true
-    add_index :, :company_id
+    add_index :taxes, :company_id
         
     # ProductTax
     create_table :product_taxes do |t|
@@ -100,11 +101,160 @@ class Managing < ActiveRecord::Migration
       t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
     end
     add_index :product_taxes, [:product_id, :tax_id], :unique=>true
-    add_index :, :company_id
+    add_index :product_taxes, :company_id
 
+
+    # StockLocation
+    create_table :stock_locations do |t|
+      t.column :name,                   :string,  :null=>false
+      t.column :x,                      :string
+      t.column :y,                      :string
+      t.column :z,                      :string
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    add_index :stock_locations, :company_id
+
+    # Stock
+    create_table :stocks do |t|
+      t.column :name,                   :string,  :null=>false
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    add_index :stock_warehouses, :company_id
+
+    # StockMove
+    create_table :stock_moves do |t|
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    add_index :stock_warehouses, :company_id
+
+
+
+
+    
+
+
+
+
+
+
+    # EstimateNature
+    create_table :estimate_natures do |t|
+      t.column :code,                   :string,   :limit=>16, :null=>false
+      t.column :name,                   :string,   :null=>false
+      t.column :expiration_id,          :integer,  :null=>false, :references=>:delays
+      t.column :active,                 :boolean,  :null=>false, :default=>true
+      t.column :payment_delay_id,       :integer,  :null=>false, :references=>:delays
+      t.column :downpayment_rate,       :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :note,                   :text
+      t.column :company_id,             :integer,  :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end    
+  
+    # Estimate
+    create_table :estimates do |t|
+      t.column :number,                 :string,   :limit=>64, :null=>false
+      t.column :nature_id,              :integer,  :null=>false, :references=>:estimate_natures
+      t.column :price,                  :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :taxed_price,            :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :state,                  :string,   :limit=>1, :null=>false, :default=>'O'
+      t.column :expired_on,             :date,     :null=>false
+      t.column :expiration_id,          :integer,  :null=>false, :references=>:delays
+      t.column :payment_delay_id,       :integer,  :null=>false, :references=>:delays
+      t.column :has_downpayment,        :boolean,  :null=>false, :default=>false
+      t.column :downpayment_price,      :decimal,  :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :client_id,              :integer,  :null=>false, :references=>:entities
+      t.column :contact_id,             :integer,  :null=>false, :references=>:entity_contacts
+      t.column :contact_version_id,     :integer,  :null=>false, :references=>:entity_contact_versions
+      t.column :invoice_contact_id,     :integer,  :null=>false, :references=>:entity_contacts
+      t.column :delivery_contact_id,    :integer,  :null=>false, :references=>:entity_contacts
+      t.column :object,                 :string
+      t.column :function_title,         :string
+      t.column :introduction,           :text
+      t.column :conclusion,             :text
+      t.column :note,                   :text
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+  
+    # EstimateLine
+    create_table :estimate_lines do |t|
+      t.column :estimate_id,            :integer, :null=>false, :references=>:estimates
+      t.column :product_id,             :integer, :null=>false, :references=>:products
+      t.column :pricelist_id,           :integer, :null=>false, :references=>:pricelists
+      t.column :price_id,               :integer, :null=>false, :references=>:pricelist_items
+      t.column :price_version_id,       :integer, :null=>false, :references=>:pricelist_item_versions
+      t.column :number,                 :integer, :null=>false
+      t.column :quantity,               :decimal, :null=>false, :precision=>16, :scale=>2, :default=>1.0.to_d
+      t.column :price,                  :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :taxed_price,            :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      # From product
+      t.column :code,                   :string,  :limit=>32, :null=>false
+      t.column :ean13,                  :string,  :limit=>13
+      t.column :catalog_name,           :string,  :null=>false
+      t.column :catalog_description,    :text
+      t.column :description,            :text
+      t.column :account_id,             :integer, :null=>false, :references=>:accounts, :on_delete=>:cascade, :on_update=>:cascade
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    
+    # Invoice
+    create_table :invoices do |t|
+      t.column :nature,                 :string,  :limit=>1,  :null=>false # S Standard R Replacement C Credit(Avoir)
+      t.column :number,                 :string,  :limit=>64, :null=>false
+      t.column :price,                  :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :taxed_price,            :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :payment_delay_id,       :integer, :null=>false, :references=>:delays
+      t.column :payment_on,             :date,          :null=>false
+      t.column :has_downpayment,        :boolean, :null=>false, :default=>false
+      t.column :downpayment_price,      :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :client_id,              :integer, :null=>false, :references=>:entities
+      t.column :contact_id,             :integer, :null=>false, :references=>:entity_contacts
+      t.column :contact_version_id,     :integer, :null=>false, :references=>:entity_contact_versions
+      t.column :delivery_contact_id,    :integer, :null=>false, :references=>:entity_contacts
+      t.column :delivery_contact_version_id, :integer, :null=>false, :references=>:entity_contact_versions
+      t.column :object,                 :string
+      t.column :function_title,         :string
+      t.column :introduction,           :text
+      t.column :conclusion,             :text
+      t.column :note,                   :text
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    
+    # Delivery
+    create_table :deliveries do |t|
+      t.column :estimate_id,            :integer, :null=>false, :references=>:estimates
+      t.column :invoice_id,             :integer, :null=>false, :references=>:invoices
+      t.column :delivered_on,           :date,    :null=>false
+      t.column :price,                  :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :taxed_price,            :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+    
+    # DeliveryLine
+    create_table :delivery_lines do |t|
+      t.column :delivery_id,            :integer, :null=>false, :references=>:deliveries
+      t.column :estimate_line_id,       :integer, :null=>false, :references=>:estimate_lines
+      t.column :product_id,             :integer, :null=>false, :references=>:products
+      t.column :pricelist_id,           :integer, :null=>false, :references=>:pricelists
+      t.column :price_id,               :integer, :null=>false, :references=>:pricelist_items
+      t.column :price_version_id,       :integer, :null=>false, :references=>:pricelist_item_versions
+      t.column :quantity,               :decimal, :null=>false, :precision=>16, :scale=>2, :default=>1.0.to_d
+      t.column :price,                  :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :taxed_price,            :decimal, :null=>false, :precision=>16, :scale=>2, :default=>0.0.to_d
+      t.column :company_id,             :integer, :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+
+
+
+  
   end
 
   def self.down
+    drop_table :delivery_lines
+    drop_table :deliveries
+    drop_table :invoices
+    drop_table :estimate_lines
+    drop_table :estimates
+    drop_table :estimate_natures
+
     drop_table :product_taxes
     drop_table :taxes
     drop_table :pricelist_items
