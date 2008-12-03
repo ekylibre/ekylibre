bc99178d73b1be294718e800118b5d37aa52a51a
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2008-12-03 16:16:06 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@167 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,21 +1,30 @@
 class HelpController < ApplicationController
   include ActionView::Helpers::TagHelper
-
-
+  
+  
   def search
-    @id = params[:id]
+    @id = params[:id]    #.gsub('-',"//")
+    if request.get?
+      session[:help_history] = [] if session[:help_history].nil?
+      if session[:help_history][0] != @id
+        10.times{|i| session[:help_history][i+1] = session[:help_history][i]}
+        session[:help_history][0] = @id
+      end
+    end
+    
     if session[:help_history] 
       @previous = session[:help_history][1] if session[:help_history].size>1
+      
     end
     session[:help]=true
     render :partial=>'search'
   end
-
+  
   def close    
     session[:help]=false
     session[:help_history] = []
     render :text=>''
   end
-
-
+  
+  
 end
