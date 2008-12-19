154f0512516282aca0452add776ca3ac43272bc4
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2008-12-19 08:49:03 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@210 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/accountancy_controller.rb
+++ app/controllers/accountancy_controller.rb
@@ -30,14 +30,14 @@ class AccountancyController < ApplicationController
   end
 
  
-  # this method finds the account with the matching number and the company_id.
+  #
   def find_accounts
     @search = params[:account].gsub('*','%')
     @find_accounts = Account.find(:all,:conditions=>["company_id = ? AND number LIKE ?", @current_company.id, @search])
     render :partial => 'find_accounts'
   end
   
-  # this method is used to load datas such as accounts, financialyears ... 
+  #  
   def load_data
    # creation of a financial year.
     @current_company.accounts.create!(:number=>'6', :name=>'charge', :label=>'charge', :parent_id=>1)
@@ -70,28 +70,44 @@ class AccountancyController < ApplicationController
         @record.reload        
       end
 
+    if request.post?
+      params[:record][:created_on] = convert_date(params[:record][:created_on]) 
+      params[:record][:printed_on] = convert_date(params[:record][:printed_on])  
+      @record = Journal.find(session[:journal]).create_record(params[:record])
+      account = Account.find_by_number(params[:account][:number])
+      currency = Currency.find(:first, :conditions=>["company_id = ?", @current_company.id])
+      @entry = Entry.create!(params[:entry].merge({:account_id => account.id, :record_id => @record.id, 
+                                :currency_id => currency.id, :company_id => @current_company.id}))
+      @record.reload
       @records = Journal.find(session[:journal]).last_records(session[:records_number].to_i)
-      @record = JournalRecord.find(:first, :conditions => ["journal_id=? and debit!=credit",session[:journal]], :order=>:id) if @record.balanced
-      @record = JournalRecord.new if @record.nil?
-      @entry = Entry.new
-    end
+      @number = @record.balanced ? @records.first.number.succ : @record.number
+      @created_on = @record.balanced ? convert_date(Date.today) : convert_date(@record.created_on) 
+      @printed_on = @record.balanced ? convert_date(Date.today) : convert_date(@record.printed_on) 
+      render :action => "entries.rjs" if request.xhr?
+    
+    else
+      journals_list params        
+      unless session[:journal].nil? and session[:records_number].nil?
+        @records = Journal.find(session[:journal]).last_records(session[:records_number].to_i)
+        @entries = Entry.find(:all, :conditions => ["record_id IN (?)", @records ])
+        @record_unbalanced = JournalRecord.find(:last, :conditions => ["debit != credit AND id IN (?)", @records])
+        if @records.size > 0
+          @number = @record_unbalanced ? @record_unbalanced.number : @records.first.number.succ
+          @created_on = @record_unbalanced ? convert_date(@record_unbalanced.created_on) : convert_date(Date.today)
+          @printed_on = @record_unbalanced ? convert_date(@record_unbalanced.printed_on) : convert_date(Date.today)
+        end
+      end
+    end   
 
-    if @record.new_record? # or (request.post? and @record.balanced)
-      @record.number = @records.size>0 ? @records.first.number.succ : 1
-      @record.created_on = @record.printed_on = Date.today
-    end
-
-    render :action => "entries.rjs" if request.xhr?    
   end
   
   # this method converts a date into a new date with a specific format or a string into a date.
   def convert_date(date)
     if date.is_a? Date
-      return date.strftime("%e-%m-%Y") 
+      return date.strftime("%e/%m/%Y") 
     else 
-      d=date.split('-') 
-     # raise Exception.new d.inspect
-      return Date.civil(d[0].to_i,d[1].to_i,d[2].to_i) 
+      d=date.split('/') 
+      return Date.civil(d[2].to_i,d[1].to_i,d[0].to_i) 
     end
   end
   
@@ -146,14 +162,16 @@ class AccountancyController < ApplicationController
     render :action => 'print_balance'
     #raise Exception.new date + " test"
     #raise Exception.new @current_company.id
-    # raise Exception.new [:test][:current_company]
     if request.post?
+      date = params[:test][:date]
+      year = date.to_i
+      params[:test][:year_begin] = Date.new(year,01,01)
+      params[:test][:year_end] = Date.new(year,12,31)
       params[:test][:current_company] = @current_company.id
+      #raise Exception.new 
       render_xil('/home/thibaud/ekylibre2/trunk/ekylibre/app/balance.xml',:locals=>params[:test])
     end
   end
-  
-
 end
 
 
