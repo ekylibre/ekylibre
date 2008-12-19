60d45291b3266d8a21e8b31b3ca5ae06468463d8
Brice Texier <brice.texier@ekylibre.org>
2008-12-19 08:36:28 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@206 67a09383-3dfa-4221-8551-890bac9c277c

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -4,16 +4,11 @@ module HelpHelper
     content = ''
     file_text  = "app/languages/fr/help/"+file_name+".txt"
     file_cache = "app/languages/fr/help/cache/"+file_name+".html"
-    
-    
+        
     if File.exists?(file_cache)    # the file exists in the cache 
-      
       file = File.open('app/languages/fr/help/cache/'+file_name+'.html' , 'r')
       content =  file.read
-                
-      
     elsif File.exists?(file_text)  # the file doesn't exist in the cache, but exits as a text file
-      
       file = File.open('app/languages/fr/help/'+file_name+'.txt' , 'r')
       content = file.read
       ltr = link_to_remote('\4', :url => { :controller=>:help , :action => "search", :id => '\1' }, :update => :help).gsub('%5C',"\\")
@@ -25,13 +20,11 @@ module HelpHelper
       file_new = File.open('app/languages/fr/help/cache/'+file_name+'.html' , 'wb')
       file_new.write(content )
       file_new.close
-      file.close
-      
+      file.close      
     elsif !default.blank? 
       content =  retrieve(default)# no help file for this section
-      
     else
-      content = lc :error_no_file
+      content = content_tag(:h2, lc(:error_no_file))
     end
     content
     
