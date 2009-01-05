ee0a985c14ecb2d060065c4581cdd45eddab089f
Brice Texier <brice.texier@ekylibre.org>
2009-01-05 14:37:10 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@218 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,14 +1,40 @@
 class ManagementController < ApplicationController
 
-  def new_sale
-    render :action=>:sale
+  def index
   end
 
-  def sale
-  end
- 
   def sales
   end
+
+  # Step 1
+  def sales_new
+  end
+
+  # Step 2
+  def sales_general
+  end
+
+  # Step 3
+  def sales_products
+  end
+
+  # Step 4
+  def sales_deliveries
+  end
+
+  # Step 5
+  def sales_invoices
+  end
+
+  # Step 6
+  def sales_payments
+  end
+
+  # Step 7
+  def sales_print
+  end
+  
+
   
   def purchases
   end
