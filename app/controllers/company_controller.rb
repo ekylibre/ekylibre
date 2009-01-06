8080b73558716e60fc159e9a638d8e24f7961f96
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2009-01-06 13:47:42 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@226 67a09383-3dfa-4221-8551-890bac9c277c

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -11,7 +11,7 @@ class CompanyController < ApplicationController
     @company = @current_company
     if request.post?
       if @company.update_attributes(params[:company])
-        redirect_to :action=>:company
+        redirect_to :action=>:index  
       end
     end
   end
@@ -21,11 +21,16 @@ class CompanyController < ApplicationController
     @user = @current_user
   end
 
-  dyta(:users, :conditions=>{:company_id=>['@current_company.id']}) do |t|
+  dyta(:users, :conditions=>{:company_id=>['@current_company.id'],:deleted=>false}) do |t|
     t.column :name
     t.column :first_name
     t.column :last_name
     t.column :email
+   # t.column :locked
+    t.procedure :users_create, :action=>:users_create
+    t.action :users_update, :image=>:update 
+    t.action :users_delete, :image=>:delete , :method=>:post , :confirm=>:sure
+
   end
 
   def users
@@ -37,6 +42,9 @@ class CompanyController < ApplicationController
     t.column :nic
     t.column :siret
     t.column :note
+    t.procedure :establishments_create, :action=>:establishments_create
+    t.action :establishments_update, :image=>:update
+    t.action :establishments_delete, :image=>:delete , :method=>:post , :confirm=>:sure
   end
 
   def establishments
@@ -46,10 +54,71 @@ class CompanyController < ApplicationController
   dyta(:departments, :conditions=>{:company_id=>['@current_company.id']}) do |t| 
     t.column :name
     t.column :desc
+    t.procedure :departments_create, :action=>:departments_create
+    t.action :departments_update, :image=>:update
+    t.action :departments_delete, :image=>:delete , :method=>:post , :confirm=>:sure
   end
 
   def departments
     departments_list params
+  end
+
+  def establishments_create
+    access :establishments
+    if request.post?
+      @establishment = Establishment.new(params[:establishment])
+      @establishment.company_id = session[:company_id]
+      redirect_to :action=>:establishments if @establishment.save
+    else
+      @establishment = Establishment.new
+    end
+    render_form
+  end
+ 
+  def establishments_update
+    access :establishments
+    @establishment = Establishment.find(params[:id])
+    if request.post?
+      if @establishment.update_attributes(params[:establishment])
+        redirect_to :action=>:establishments
+      end
+    end
+    render_form
+  end
+
+  def establishments_delete
+    access :establishments
+    Establishment.delete(params[:id])
+    redirect_to :action=>:establishments
+  end
+
+  def departments_create
+    access :departments
+    if request.post?
+      @department = Department.new(params[:department])
+      @department.company_id = session[:company_id]
+      redirect_to :action=>:departments if @department.save
+    else
+      @department = Department.new
+    end
+    render_form
+  end
+
+  def departments_update
+    access :departments
+    @department = Department.find(params[:id])
+    if request.post?
+      if @department.update_attributes(params[:department])
+        redirect_to :action=>:departments
+      end
+    end
+    render_form
+  end
+
+  def departments_delete
+    access :departments
+    Department.delete(params[:id])
+    redirect_to :action=>:departments
   end
 
   def users_create
@@ -57,7 +126,8 @@ class CompanyController < ApplicationController
     if request.post?
       @user = User.new(params[:user])
       @user.company_id = session[:company_id]
-      redirect_to session[:url][1] if @user.save
+      @user.role_id = params[:user][:role_id]
+      redirect_to :action=>:users if @user.save
     else
       @user = User.new
     end
@@ -65,7 +135,28 @@ class CompanyController < ApplicationController
   end
 
   def users_update
+    access :users
+    @user= User.find(params[:id])
+    if request.post?
+      if @user.update_attributes(params[:user]) 
+        redirect_to :action=>:users
+      end
+    end
+    render_form
   end
   
+  def users_delete
+    access :users
+    User.delete(params[:id])
+    redirect_to :action=>:users
+   # @user.id = params[:id]
+   #  @user.deleted = true
+  end
+
+  def users_lock
+    @user.id = param[:id]
+    @user.locked = true
+  end
+
 end
 
