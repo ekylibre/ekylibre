4a6117b3615822b7068f6191f5008102558c10bc
Vincent Mauget <vincent.mauget@ekylibre.org>
2009-03-16 13:59:44 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@546 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20090311094745_mar2v1.rb
@@ -0,0 +1,24 @@
+class Mar2v1 < ActiveRecord::Migration
+  
+  def self.up
+    create_table :professions do |t|
+      t.column :name,                   :string, :null=>false
+      t.column :code,                   :string
+      t.column :rome,                   :string   
+      t.column :commercial,             :boolean
+      t.column :company_id,             :integer,  :null=>false, :references=>:companies, :on_delete=>:restrict, :on_update=>:restrict
+    end
+    
+    add_column :employees, :profession_id, :integer, :null=>false, :references=>:professions, :on_delete=>:restrict, :on_update=>:restrict
+
+    add_column :employees, :commercial, :boolean, :null=>false, :default=>false
+    
+  end
+  
+  def self.down
+    remove_column :employees, :employee_id
+    
+    drop table :professions
+  end
+  
+end
