cb65a866acf03e283085d23a27cd8aaadb601c66
Brice Texier <brice.texier@ekylibre.org>
2009-04-10 09:46:21 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@634 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20090407073247_avr2b1.rb
@@ -0,0 +1,15 @@
+class Avr2b1 < ActiveRecord::Migration
+  def self.up
+    add_column :entities, :proposer_id, :integer, :references=>:entities
+    add_column :entities, :payment_mode_id, :integer, :references=>:payment_modes
+    add_column :entities, :payment_delay_id, :integer, :references=>:delays
+    add_column :entities, :invoices_count, :integer
+  end
+
+  def self.down
+    remove_column :entities, :invoices_count
+    remove_column :entities, :payment_delay_id
+    remove_column :entities, :payment_mode_id
+    remove_column :entities, :proposer_id
+  end
+end
