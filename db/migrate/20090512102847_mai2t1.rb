28144bf4effc09ce532fa17af52d2bfc516a0072
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2009-05-14 07:10:09 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@705 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20090512102847_mai2t1.rb
@@ -0,0 +1,36 @@
+class Mai2t1 < ActiveRecord::Migration
+  def self.up
+    remove_column :contacts, :name
+    remove_column :products, :without_stocks
+    add_column :products,    :manage_stocks, :boolean, :null=>false, :default=>false
+    
+    create_table :delivery_modes do |t|
+      t.column :name,           :string, :null=>false
+      t.column :code,           :string, :null=>false,   :limit=>3  ## "exw" "cip" "cpt"
+      t.column :comment,        :text
+      t.column :company_id,     :integer,  :null=>false, :references=>:companies, :on_delete=>:restrict, :on_update=>:restrict
+    end
+    
+    add_column :deliveries,   :mode_id,  :integer,  :references=>:delivery_modes, :on_delete=>:restrict, :on_update=>:restrict
+    
+    execute "INSERT INTO delivery_modes(company_id, name, code, comment, created_at, updated_at) SELECT companies.id , 'Remise en main propre', 'exw', '',current_timestamp, current_timestamp FROM companies"
+
+    execute "INSERT INTO delivery_modes(company_id, name, code, comment, created_at, updated_at) SELECT companies.id , 'Livraison avec assurance', 'cip', '',current_timestamp, current_timestamp FROM companies"
+
+    execute "INSERT INTO delivery_modes(company_id, name, code, comment, created_at, updated_at) SELECT companies.id , 'Livraison sans assurance', 'cpt', '', current_timestamp, current_timestamp FROM companies"
+
+    execute "UPDATE deliveries SET mode_id = m.id FROM delivery_modes m WHERE m.code = deliveries.nature AND m.company_id = deliveries.company_id"
+    
+    remove_column :deliveries,  :nature
+    
+  end
+
+  def self.down
+    add_column :deliveries, :nature, :string, :limit=>3
+    remove_column :deliveries, :mode_id
+    drop_table :delivery_modes
+    remove_column :products, :manage_stocks
+    add_column :products, :without_stocks, :boolean, :null=>false, :default=>false
+    add_column :contacts, :name, :string
+  end
+end
