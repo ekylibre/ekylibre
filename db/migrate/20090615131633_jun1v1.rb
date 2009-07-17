13145c59f4257856e07f4d4160eeac6e77fde209
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2009-07-17 08:43:03 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@908 67a09383-3dfa-4221-8551-890bac9c277c

--- db/migrate/20090615131633_jun1v1.rb
+++ db/migrate/20090615131633_jun1v1.rb
@@ -2,8 +2,8 @@ class Jun1v1 < ActiveRecord::Migration
   def self.up
     add_column :bank_accounts, :default, :boolean, :null => false, :default => false
 
-    Entity.find(:all).each do |entity|
-      bank_account = entity.bank_accounts.find :first
+    Entity.find_all_by_id(BankAccount.all.collect{|b| b.entity_id}).each do |entity|
+      bank_account = entity.bank_accounts.find(:first, :order=>:id)
       bank_account.update_attribute(:default, true) if bank_account
     end
   end
