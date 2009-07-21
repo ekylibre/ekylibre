57507168558f4001023cc4eaaf0487b46ad5346e
Brice Texier <brice.texier@ekylibre.org>
2009-07-21 14:55:52 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@932 67a09383-3dfa-4221-8551-890bac9c277c

--- vendor/plugins/stamp/lib/migration.rb
+++ vendor/plugins/stamp/lib/migration.rb
@@ -63,8 +63,8 @@ module Stamp
             table_defintion.column :created_at,   :datetime,  :null => false
             table_defintion.column :updated_at,   :datetime,  :null => false
             unless options[:userstamp] == false
-              table_defintion.column :created_by,   :integer,  :references=>:users, :on_delete=>:restrict, :on_update=>:cascade
-              table_defintion.column :updated_by,   :integer,  :references=>:users, :on_delete=>:restrict, :on_update=>:cascade
+              table_defintion.column :creator_id,   :integer,  :references=>:users, :on_delete=>:restrict, :on_update=>:cascade
+              table_defintion.column :updater_id,   :integer,  :references=>:users, :on_delete=>:restrict, :on_update=>:cascade
             end
             table_defintion.column :lock_version, :integer,   :null => false, :default => 0
           end
@@ -73,8 +73,8 @@ module Stamp
           add_index name.to_sym, :created_at
           add_index name.to_sym, :updated_at
           unless options[:userstamp] == false
-            add_index name.to_sym, :created_by
-            add_index name.to_sym, :updated_by
+            add_index name.to_sym, :creator_id
+            add_index name.to_sym, :updater_id
           end
         end
       end
