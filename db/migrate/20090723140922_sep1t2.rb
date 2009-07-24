d9ad1cd653a4d674ec6f08d276ee6a80f762ab18
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2009-07-24 15:11:51 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@964 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20090723140922_sep1t2.rb
@@ -0,0 +1,17 @@
+class Sep1t2 < ActiveRecord::Migration
+  def self.up
+
+    create_table :observations do |t|
+      t.column :importance,       :string,   :null=>false, :limit=>10
+      t.column :description,     :text,     :null=>false
+      t.column :entity_id,       :integer,  :null=>false, :references=>:entities, :on_delete=>:cascade, :on_update=>:cascade
+      t.column :company_id,      :integer,  :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+ 
+  end
+
+  def self.down
+    drop_table :observations
+
+  end
+end
