c32751c0bc4d5c0722532571dfe332677f2b21b8
Brice Texier <brice.texier@ekylibre.org>
2009-10-19 08:48:29 +0000

    Add utility to checks bad documents
    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@1233 67a09383-3dfa-4221-8551-890bac9c277c

--- app/models/document.rb
+++ app/models/document.rb
@@ -56,40 +56,30 @@ class Document < ActiveRecord::Base
     file_data
   end
 
-  def path
-    "#{DIRECTORY}/#{self.company.code}/#{self.nature_code}/#{self.subdir}"
+  def path(strict=true)
+    code = self.nature_code
+    code.gsub!(/\*/, '') unless strict
+    "#{DIRECTORY}/#{self.company.code}/#{code}/#{self.subdir}"
   end
 
   def file_path
     File.join(self.path, self.filename)
   end
 
-#   def self.archive(owner, data, attributes={})
-#     attrs = attributes.merge(:company_id=>owner.company_id, :owner_id=>owner.id, :owner_type=>owner.class.name)
-#     raise Exception.new attrs.inspect
-#     document = Document.new(attrs)
-#     method_name = [:print_name, :number, :code, :name, :id].detect{|x| owner.respond_to?(x)}
-#     document.printed_at = Time.now
-#     document.extension ||= 'bin'
-#     document.subdir = Date.today.strftime('%Y-%m')
-#     document.original_name = owner.send(method_name).to_s.simpleize+'.'+document.extension.to_s
-#     document.filename = owner.send(method_name).to_s.codeize+'-'+document.printed_at.to_i.to_s(36).upper+'-'+Document.generate_key+'.'+document.extension.to_s
-#     document.filesize = data.length
-#     document.sha256 = Digest::SHA256.hexdigest(data)
-#     document.crypt_mode = 'none'
-#     if document.save
-#       directory = document.path
-#       File.makedirs(directory)
-#       File.open(File.join(directory, document.filename), 'wb') do |file|
-#         file.write(data)
-#       end
-#     else
-#       puts document.errors.inspect
-#     end
-#     return document
-#   end
 
-  
+  def self.missing_files(update=false)
+    count = 0
+    for document in Document.all
+      unless File.exists? document.file_path(false)
+        if update
+          document.nature_code += '*'
+          document.save(false)
+        end
+        count += 1
+      end
+    end
+    return count
+  end
 
   def self.generate_key(password_length=8)
     letters = %w(A B C D E F G H I J K L M N O P Q R S T U V W Y X Z)
