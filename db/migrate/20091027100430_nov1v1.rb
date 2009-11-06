f50e85cb667547f4b1c9ff007eac54a18ad9e75c
Vincent Mauget <vincent.mauget@ekylibre.org>
2009-11-06 10:58:32 +0000

    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@1249 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20091027100430_nov1v1.rb
@@ -0,0 +1,21 @@
+class Nov1v1 < ActiveRecord::Migration
+  def self.up
+    add_column :entries, :draft, :boolean, :null=>false, :default=>false
+
+    [:invoices, :sale_orders, :payments, :purchase_orders, :transfers].each do |model|
+      add_column model, :accounted, :boolean, :null=>false, :default=>false
+    end
+
+  end
+  
+  def self.down
+    
+    [:invoices, :sale_orders, :payments, :purchase_orders, :transfers].each do |model|
+      remove_column model, :accounted
+    end
+
+    remove_column :entries, :draft
+
+
+  end
+end
