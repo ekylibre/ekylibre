79609863a2410b281ae4a7df239af592f17d441b
Brice Texier <brice.texier@ekylibre.org>
2009-12-01 09:02:58 +0000

    Fixes bug on migration due to ActionMailer
    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@1302 67a09383-3dfa-4221-8551-890bac9c277c

--- db/migrate/20090720100733_sep1b3.rb
+++ db/migrate/20090720100733_sep1b3.rb
@@ -6,15 +6,19 @@ class Sep1b3 < ActiveRecord::Migration
   @@models = @@models.collect{|m| m.sub(/\.rb$/,'')}.sort
   def self.up
     for model_name in @@models
-      table = model_name.pluralize.to_sym
+      # table = model_name.pluralize.to_sym
       model = model_name.camelcase.constantize
-      if model.table_exists?
-        add_column table, :creator_id, :integer unless model.columns_hash.keys.include? "creator_id"
-        add_column table, :updater_id, :integer unless model.columns_hash.keys.include? "updater_id"
-        model.update_all('creator_id=created_by') if model.columns_hash.keys.include?("creator_id") and model.columns_hash.keys.include?("created_by")
-        model.update_all('updater_id=updated_by') if model.columns_hash.keys.include?("updater_id") and model.columns_hash.keys.include?("updated_by")
-        remove_column table, :created_by if model.columns_hash.keys.include? "created_by"
-        remove_column table, :updated_by if model.columns_hash.keys.include? "updated_by"
+      begin
+        if model.table_exists?
+          table = model.table_name
+          add_column table, :creator_id, :integer unless model.columns_hash.keys.include? "creator_id"
+          add_column table, :updater_id, :integer unless model.columns_hash.keys.include? "updater_id"
+          model.update_all('creator_id=created_by') if model.columns_hash.keys.include?("creator_id") and model.columns_hash.keys.include?("created_by")
+          model.update_all('updater_id=updated_by') if model.columns_hash.keys.include?("updater_id") and model.columns_hash.keys.include?("updated_by")
+          remove_column table, :created_by if model.columns_hash.keys.include? "created_by"
+          remove_column table, :updated_by if model.columns_hash.keys.include? "updated_by"
+        end
+      rescue
       end
     end
     add_column :languages, :created_at, :timestamp
@@ -25,15 +29,19 @@ class Sep1b3 < ActiveRecord::Migration
     remove_column :languages, :updated_at
     remove_column :languages, :created_at
     for model_name in @@models
-      table = model_name.pluralize.to_sym
+      # table = model_name.pluralize.to_sym
       model = model_name.camelcase.constantize
-      if model.table_exists?
-        add_column table, :created_by, :integer unless model.columns_hash.keys.include? "created_by"
-        add_column table, :updated_by, :integer unless model.columns_hash.keys.include? "updated_by"
-        model.update_all('created_by=creator_id') if model.columns_hash.keys.include?("creator_id") and model.columns_hash.keys.include?("created_by")
-        model.update_all('updated_by=updater_id') if model.columns_hash.keys.include?("updater_id") and model.columns_hash.keys.include?("updated_by")
-        remove_column table, :creator_id if model.columns_hash.keys.include?("creator_id")
-        remove_column table, :updater_id if model.columns_hash.keys.include?("updater_id")
+      begin
+        if model.table_exists?
+          table = model.table_name
+          add_column table, :created_by, :integer unless model.columns_hash.keys.include? "created_by"
+          add_column table, :updated_by, :integer unless model.columns_hash.keys.include? "updated_by"
+          model.update_all('created_by=creator_id') if model.columns_hash.keys.include?("creator_id") and model.columns_hash.keys.include?("created_by")
+          model.update_all('updated_by=updater_id') if model.columns_hash.keys.include?("updater_id") and model.columns_hash.keys.include?("updated_by")
+          remove_column table, :creator_id if model.columns_hash.keys.include?("creator_id")
+          remove_column table, :updater_id if model.columns_hash.keys.include?("updater_id")
+        end
+      rescue
       end
     end
     remove_column :languages, :updated_by
