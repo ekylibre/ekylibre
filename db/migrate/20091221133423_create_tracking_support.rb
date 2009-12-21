8d0e0aaf1ded978dac1e8ab9f984da704256e59e
Thibaud Mérigon <thibaud.merigon@ekylibre.org>
2009-12-21 14:20:00 +0000

    add create_tracking_support migration
    git-svn-id: https://www.ekylibre.org/svn/trunk/ekylibre@1340 67a09383-3dfa-4221-8551-890bac9c277c

---
+++ db/migrate/20091221133423_create_tracking_support.rb
@@ -0,0 +1,49 @@
+class Tracking < ActiveRecord::Migration
+  def self.up
+    add_column :stock_trackings, :product_id, :integer, :references=>:products, :on_update=>:cascade, :on_delete=>:cascade
+    add_column :stock_trackings, :producer_id, :integer, :references=>:entities, :on_update=>:cascade, :on_delete=>:cascade
+
+    add_column :product_stocks,  :origin_id, :integer
+    add_column :product_stocks,  :origin_type, :string   ## productions || purchase_orders || transfers
+
+    add_column :productions, :shape_id, :integer, :references=>:shapes, :on_update=>:cascade, :on_delete=>:cascade
+    
+    add_column :products, :to_product, :boolean, :null=>false, :default=>false
+
+    add_column :shapes, :number, :string
+
+    add_column :sale_order_lines,     :tracking_id,     :integer,  :references=>:stock_trackings, :on_delete=>:cascade, :on_update=>:cascade
+    add_column :purchase_order_lines, :tracking_id,     :integer,  :references=>:stock_trackings, :on_delete=>:cascade, :on_update=>:cascade
+    add_column :product_stocks,       :tracking_id,     :integer,  :references=>:stock_trackings, :on_update=>:cascade, :on_delete=>:cascade 
+    add_column :stock_transfers,      :tracking_id,     :integer,  :references=>:stock_trackings, :on_update=>:cascade, :on_delete=>:cascade 
+    add_column :inventory_lines,      :tracking_id,     :integer,  :references=>:stock_trackings, :on_update=>:cascade, :on_delete=>:cascade 
+    add_column :delivery_lines,       :tracking_id,     :integer,  :references=>:stock_trackings, :on_update=>:cascade, :on_delete=>:cascade 
+
+    
+    create_table :shape_operation_lines do |t|
+      t.column :shape_operation_id,     :integer,   :null=>false, :references=>:shape_operations, :on_delete=>:cascade, :on_update=>:cascade
+      t.column :product_id,             :integer,   :references=>:products, :on_delete=>:cascade, :on_update=>:cascade
+      t.column :quantity,               :decimal
+      t.column :surface_unit_id,        
+      t.column :company_id,             :integer,   :null=>false, :references=>:companies, :on_delete=>:cascade, :on_update=>:cascade
+    end
+
+  end
+
+  def self.down
+    drop_table :shape_operation_lines
+    remove_column :delivery_lines, :tracking_id
+    remove_column :inventory_lines, :tracking_id
+    remove_column :stock_transfers, :tracking_id
+    remove_column :product_stocks, :tracking_id
+    remove_column :purchase_order_lines, :tracking_id
+    remove_column :sale_order_lines, :tracking_id
+    remove_column :shapes, :number
+    remove_column :products, :to_product
+    remove_column :productions, :shape_id
+    remove_column :product_stocks, :origin_type
+    remove_column :product_stocks, :origin_id
+    remove_column :stock_trackings, :producer_id
+    remove_column :stock_trackings, :product_id
+  end
+end
