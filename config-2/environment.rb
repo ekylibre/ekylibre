a3b4dab3706eaaf4b84a0a2578b257b66fa7e694
Brice Texier <brice.texier@ekylibre.org>
2011-01-24 22:16:13 +0000

    Fixes a bug for state_machine
    git-svn-id: https://www.ekylibre.org/svn/ekylibre/trunk@1720 67a09383-3dfa-4221-8551-890bac9c277c

--- config-2/environment.rb
+++ config-2/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "rubyzip", :lib=>"zip/zip"
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "will_paginate", :version=>'2.3.15'
-  config.gem "state_machine", :source => 'http://gemcutter.org' # , :version=>'0.9.4', :source => "http://gems.github.com" # , :require=>'state_machine/machine'
+  config.gem "state_machine" # , :source => 'http://gemcutter.org' # , :version=>'0.9.4', :source => "http://gems.github.com" # , :require=>'state_machine/machine'
   # config.gem "exception_notification", :gitsource # , :version=>'2.3.15'
   # config.gem "measure"
 
