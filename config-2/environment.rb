04f4ed3c2acc6c15a94df2ba75757f3fb212783b
Brice Texier <brice.texier@ekylibre.org>
2011-01-25 07:25:00 +0000

    Returns to Rails 2.3.2 TODO: Try bundler to solve i18n problems
    git-svn-id: https://www.ekylibre.org/svn/ekylibre/trunk@1721 67a09383-3dfa-4221-8551-890bac9c277c

--- config-2/environment.rb
+++ config-2/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "will_paginate", :version=>'2.3.15'
   config.gem "state_machine" # , :source => 'http://gemcutter.org' # , :version=>'0.9.4', :source => "http://gems.github.com" # , :require=>'state_machine/machine'
+  config.gem "i18n", "0.4.2"
   # config.gem "exception_notification", :gitsource # , :version=>'2.3.15'
   # config.gem "measure"
 
