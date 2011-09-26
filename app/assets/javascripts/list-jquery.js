444e3dc66ff2314c754fd72cb3049d9981dcb0e6
Brice Texier <brice.texier@ekylibre.org>
2011-09-26 17:04:34 +0200

    Adds jQuery support for List plugin

---
+++ app/assets/javascripts/list-jquery.js
@@ -0,0 +1,62 @@
+/* -*- Mode: Javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; coding: latin-1 -*- */
+/*jslint browser: true */
+/* List Javascript Inobtrusive Support for jQuery */
+
+(function($) {
+
+    $.behave("a[data-toggle-column]", "click", function(event) {
+        var element = $(this), columnId = element.data('toggle-column');
+        var column = $(columnId);
+        var className = column.data("cells-class");
+        if (className === null) { className = columnId; }
+        var search = '.'+className;
+        var visibility = '';
+        if (column.hasClass("hidden")) {
+            $(search).each(function(item) { item.removeClass("hidden"); });
+            column.removeClass("hidden");
+            element.removeClass("im-unchecked");
+            element.addClass("im-checked");
+            visibility = 'shown';
+        } else {
+            $(search).each(function(item) { item.addClass("hidden"); });
+            column.addClass("hidden");
+            element.removeClass("im-checked");
+            element.addClass("im-unchecked");
+            visibility = 'hidden';
+        }
+        var url = element.attr("href")
+        if (url !== null) {
+            $.ajax(url, {data: {visibility: visibility} });
+        }
+        return false;
+    });
+
+
+    $.behave("select[data-per-page]", "change", function(event) {
+        var element = $(this), url = element.data('per-page');
+        if (url !== null) {
+            var update = element.data('update'), options = {data: {per_page: element.value}};
+            if (update !== null) {
+                options.success = function (data, status, xhr) {
+                    $('#'+update).html(data);
+                }
+            }
+            $.ajax(url, options);
+        }
+        return false;
+    });
+
+    $.behave("a[data-remote-update]", "click", function(event, element) {
+        var url = element.attr('href');
+        var method = element.data('method') || 'get';
+        var update = element.data('remote-update');
+        $.ajax(url, {
+            method: method,
+            success: function (data, status, xhr) {
+                $('#'+update).html(data); 
+            }
+        });
+        return false;
+    });
+
+})(jQuery);
