b13d653058ddb30fb58e4393c53012adc412da53
Brice Texier <brice.texier@ekylibre.org>
2011-10-06 22:09:11 +0200

    Adds format definition for new importer

---
+++ lib/exchanges/svf.rb
@@ -0,0 +1,6 @@
+# Spaced Values Format
+module SVF
+end
+
+dir = File.join(File.dirname(__FILE__), 'svl')
+require File.join(dir, 'loader')
---
+++ lib/exchanges/svf/loader.rb
@@ -0,0 +1,13 @@
+
+
+module SVF
+
+  # Build submodule to handle format defined in the file
+  # Format must be defined in YAML
+  def load(name, file)
+    
+  end
+
+
+
+end
