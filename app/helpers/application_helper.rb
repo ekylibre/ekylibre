919c294c9e5b24c8caa237bc00984a4512276cfb
Brice Texier <brice.texier@ekylibre.org>
2011-10-24 12:16:40 +0200

    Force encoding for non-UTF8 flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -455,6 +455,7 @@ module ApplicationHelper
     code = ''
     if flash[:notifications].is_a?(Hash) and flash[:notifications][mode].is_a?(Array)
       for message in flash[:notifications][mode]
+        message.force_encoding('UTF-8') if message.respond_to? :force_encoding
         code << "<div class='flash #{mode}'><h3>#{tg('notifications.' << mode.to_s)}</h3><p>#{h(message).gsub(/\n/, '<br/>')}</p></div>"
       end
     end
