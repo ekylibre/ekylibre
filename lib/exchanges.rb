ac82305535ab22374e2f103d94d76041ade963ba
Brice Texier <brice.texier@ekylibre.org>
2011-10-27 09:10:17 +0200

    Adds task to import accountancy data with rake

--- lib/exchanges.rb
+++ lib/exchanges.rb
@@ -1,4 +1,4 @@
-
+# encoding: UTF-8
 module Exchanges
 
   class NotSupportedFormatError < StandardError
@@ -11,12 +11,62 @@ module Exchanges
   end
 
   class Exchanger
+    @@verbose = false
+
     def self.import(*args)
       raise NotImplementedError.new
     end
     def self.export(*args)
       raise NotImplementedError.new
     end
+
+    def self.benchmark(text, &block)
+      if @@verbose
+        @@depth ||= -1 
+        @@depth += 1
+        prefix = "| " * @@depth
+        puts(prefix + "/ " + text)
+        start = Time.now
+        yield
+        duration = (Time.now - start).to_f
+        minutes = (duration/60).to_i
+        seconds = (duration - 60*minutes.to_f).round(3)
+        puts(prefix + "\\ #{minutes.to_s.rjust(2, '0')}:#{seconds}")
+        @@depth -= 1
+      else
+        yield
+      end
+    end
+
+    def self.print_jauge(count, total_count, options={})
+      status = ["#{(100*count.to_f/total_count).to_i.to_s.rjust(3)}% [", '', "]"]
+      if options[:start]
+        stop = options[:stop]||Time.now
+        if options[:time] == :elapsed
+          duration = (stop - options[:start]).to_f
+          minutes = (duration/60).to_i
+          seconds = (duration - 60*minutes).to_i
+          status[2] += " #{minutes}m#{seconds.to_s.rjust(2,'0')}s "
+        else
+          duration = (stop - options[:start]).to_f * (total_count - count) / count
+          minutes = (duration/60).to_i
+          seconds = (duration - 60*minutes).to_i
+          status[2] += " rem. #{minutes}m#{seconds.to_s.rjust(2,'0')}s "
+        end
+      end
+      size = `stty size`.split[1].to_i - status.join.length - 1
+      done = (size*count.to_f/total_count).to_i
+      status[1] = "="*done + ">" + " "*(size - done)
+      status = status.join
+      if options[:replace]
+        print("\r"*options[:replace].size + status)
+        puts "" if options[:new_line]
+      else
+        puts status
+      end
+      return status
+    end
+
   end
 
   def self.import(company, format, file, options={})
--- lib/exchanges/isa_compta.rb
+++ lib/exchanges/isa_compta.rb
@@ -15,80 +15,159 @@ module Exchanges
 
     def self.import(company, file, options={})
       version = nil
-      File.open(file, "rb:CP1252") do |f|
-        version = f.readline.to_s[13..16].to_i
-      end
-      used_versions = [8550]
-      version = used_versions.select{|x| x <= version}.sort[-1]
-      if version == 8550
-        isa = SVF::Isa8550.parse(file)
-        if isa_fy = isa.folder.financial_year
-          # Find or create financial year
-          unless fy = company.financial_years.find_by_started_on_and_stopped_on(isa_fy.started_on, isa_fy.stopped_on)
-            if company.financial_years.where("? BETWEEN started_on AND stopped_on OR ? BETWEEN started_on AND stopped_on", isa_fy.started_on, isa_fy.stopped_on).count > 0
-              raise ImcompatibleDataError.new("Financial year dates overlaps existing financial years")
-            else
-              raise ImcompatibleDataError.new("Accountancy must be in Euro (EUR) not in '#{isa_fy.currency}'") if isa_fy.currency != "EUR"
-              fy = company.financial_years.create!(:started_on=>isa_fy.started_on, :stopped_on=>isa_fy.stopped_on)
-            end
-          end
-
-          # Ignore Analytics data
-
-          # Adds missing accounts
-          all_accounts = {}
-          for isa_account in isa_fy.accounts
-            unless account = company.accounts.find_by_number(isa_account.number)
-              account = company.accounts.create!(:name=>(isa_account.label.blank? ? isa_account.number : isa_account.label), :number=>isa_account.number, :reconcilable=>isa_account.reconcilable, :last_letter=>isa_account.letter, :is_debit=>(isa_account.input_direction=='de' ? true : false), :comment=>isa_account.to_s)
-            end
-            all_accounts[isa_account.number] = account.id
-          end
-
-          # Ignore taxes
-
-          # Add journals
-          # Find used journals
-          used_journals = isa_fy.entries.collect{|e| e.journal}.uniq
-          all_journals = {}
-          for isa_journal in isa_fy.journals.select{|j| used_journals.include?(j.code)}
-            journal = nil
-            journals = company.journals.find_all_by_code(isa_journal.code)
-            journal = journals[0] if journals.size == 1
-            unless journal
-              journals = company.journals.where("LOWER(name) LIKE ? ", isa_journal.label.mb_chars.downcase)
-              journal = journals[0] if journals.size == 1
-            end
-            unless journal
-              journals = company.journals.where("TRANSLATE(LOWER(name), 'àâäéèêëìîïòôöùûüỳŷÿ', 'aaaeeeeiiiooouuuyyy') LIKE ? ", '%'+isa_journal.label.mb_chars.downcase.gsub(/\s+/, '%')+'%')
-              journal = journals[0] if journals.size == 1
-            end
-            journal ||= company.journals.create!(:code=>isa_journal.code, :name=>(isa_journal.label.blank? ? "[#{isa_journal.code}]" : isa_journal.label), :nature=>@@journal_natures[isa_journal.type]||:various) # , :closed_on=>isa_journal.last_close_on
-            all_journals[isa_journal.code] = journal.id
-          end
-          
-          for isa_entry in isa_fy.entries
-            # number = (isa_entry.number.blank? ? 'NIL'+isa_entry.position.to_s : isa_entry.number)
-            number = "#{isa_entry.code}"
-            unless entry = company.journal_entries.find_by_number_and_journal_id_and_printed_on(number, all_journals[isa_entry.journal], isa_entry.printed_on)
-              entry = company.journal_entries.create(:number=>number, :journal_id=>all_journals[isa_entry.journal], :printed_on=>isa_entry.printed_on, :created_on=>isa_entry.created_on, :updated_at=>isa_entry.updated_on, :lock_version=>isa_entry.version_number) # , :state=>(isa_entry.unupdateable? ? :confirmed : :draft) 
-              raise isa_entry.inspect+"\n"+entry.errors.full_messages.to_sentence unless entry.valid?
-            end
-            entry.lines.clear
-            for isa_line in isa_entry.lines
-              if isa_line.debit < 0 or isa_line.credit < 0
-                debit = isa_line.debit
-                isa_line.debit = isa_line.credit.abs
-                isa_line.credit = debit.abs
-              end
-              line =  entry.lines.create(:account_id=>all_accounts[isa_line.account], :name=>"#{isa_line.label} (#{isa_entry.label})", :currency_debit=>isa_line.debit, :currency_credit=>isa_line.credit, :letter=>(isa_line.lettering > 0 ? isa_line.letter : nil), :comment=>isa_line.to_s)
-              raise isa_line.to_s+"\n"+line.errors.full_messages.to_sentence unless line.valid?
-            end
-          end
-          
-          
-
+      verbose = @@verbose = options.delete(:verbose)
+      benchmark "Check version of #{file}" do
+        File.open(file, "rb:CP1252") do |f|
+          version = f.readline.to_s[13..16].to_i
         end
+        used_versions = [8550]
+        version = used_versions.select{|x| x <= version}.sort[-1]
+      end
+      if version == 8550
+        benchmark "Import #{file}" do
+          isa = nil
+          benchmark "Parse file #{file}" do
+            isa = SVF::Isa8550.parse(file) 
+          end
+          
+          if isa_fy = isa.folder.financial_year
+            benchmark "Import financial year #{isa_fy.started_on}/#{isa_fy.stopped_on}" do
+              # Find or create financial year
+              unless fy = company.financial_years.find_by_started_on_and_stopped_on(isa_fy.started_on, isa_fy.stopped_on)
+                if company.financial_years.where("? BETWEEN started_on AND stopped_on OR ? BETWEEN started_on AND stopped_on", isa_fy.started_on, isa_fy.stopped_on).count > 0
+                  raise ImcompatibleDataError.new("Financial year dates overlaps existing financial years")
+                else
+                  raise ImcompatibleDataError.new("Accountancy must be in Euro (EUR) not in '#{isa_fy.currency}'") if isa_fy.currency != "EUR"
+                  fy = company.financial_years.create!(:started_on=>isa_fy.started_on, :stopped_on=>isa_fy.stopped_on)
+                end
+              end
 
+              # Ignore Analytics data
+
+              # Adds missing accounts
+              all_accounts = {}
+              benchmark "Import accounts" do
+                for isa_account in isa_fy.accounts
+                  unless account = company.accounts.find_by_number(isa_account.number)
+                    account = company.accounts.create!(:name=>(isa_account.label.blank? ? isa_account.number : isa_account.label), :number=>isa_account.number, :reconcilable=>isa_account.reconcilable, :last_letter=>isa_account.letter, :is_debit=>(isa_account.input_direction=='de' ? true : false), :comment=>isa_account.to_s)
+                  end
+                  all_accounts[isa_account.number] = account.id
+                end
+              end
+
+              # Ignore taxes
+
+              # Add journals
+              # Find used journals
+              used_journals = isa_fy.entries.collect{|e| e.journal}.uniq
+              all_journals = {}
+              benchmark "Import journals" do
+                for isa_journal in isa_fy.journals.select{|j| used_journals.include?(j.code)}
+                  journal = nil
+                  journals = company.journals.find_all_by_code(isa_journal.code)
+                  journal = journals[0] if journals.size == 1
+                  unless journal
+                    journals = company.journals.where("LOWER(name) LIKE ? ", isa_journal.label.mb_chars.downcase)
+                    journal = journals[0] if journals.size == 1
+                  end
+                  unless journal
+                    journals = company.journals.where("TRANSLATE(LOWER(name), 'àâäéèêëìîïòôöùûüỳŷÿ', 'aaaeeeeiiiooouuuyyy') LIKE ? ", '%'+isa_journal.label.mb_chars.downcase.gsub(/\s+/, '%')+'%')
+                    journal = journals[0] if journals.size == 1
+                  end
+                  journal ||= company.journals.create!(:code=>isa_journal.code, :name=>(isa_journal.label.blank? ? "[#{isa_journal.code}]" : isa_journal.label), :nature=>@@journal_natures[isa_journal.type]||:various) # , :closed_on=>isa_journal.last_close_on
+                  all_journals[isa_journal.code] = journal.id
+                end
+              end
+              
+              entries_to_import = isa_fy.entries
+              total_count = entries_to_import.size
+              benchmark "Import #{total_count} entries" do
+                unused_entries = fy.journal_entries.collect{|je| je.id}.uniq.sort
+                status, start, count, interval = "", Time.now, 0, 1.00
+                next_start = start+interval
+                
+                # Determine which search filter is the best
+                # filters = {}
+                # filters[:number] = entries_to_import.collect{|e| e.number}
+                # filters[:code] = entries_to_import.collect{|e| "#{e.code}"}
+                # filters[:journal_and_code] = entries_to_import.collect{|e| "#{e.journal}-#{e.code}"}
+                # for k, v in filters
+                #   if v.uniq.sort == v.sort
+                #     puts "Filter #{k} works"
+                #   else
+                #     puts "Filter #{k} cannot work!"
+                #   end
+                # end
+                # raise "Stop"
+
+                for isa_entry in entries_to_import
+                  entry, journal_id = nil, all_journals[isa_entry.journal]
+                  unless isa_entry.number.blank?
+                    entries = company.journal_entries.where("journal_id=? AND printed_on=? AND number = ?", journal_id, isa_entry.printed_on, isa_entry.number)
+                    if entries.size == 1
+                      entry = entries.first
+                    else
+                      entries = company.journal_entries.where("journal_id=? AND printed_on=? AND SUBSTR(number,1,2)||SUBSTR(number,LENGTH(number)-5,6) = ?", journal_id, isa_entry.printed_on, isa_entry.number)
+                      entry = entries.first if entries.size == 1
+                    end
+                  end
+                  unless entry
+                    number = "#{isa_entry.journal}#{isa_entry.code.to_s.rjust(6,'0')}"
+                    entries = company.journal_entries.find_all_by_number_and_journal_id_and_printed_on(number, journal_id, isa_entry.printed_on)
+                    if entries.size == 1
+                      entry = entries.first
+                    elsif entries.size > 1
+                      number += rand.to_s[2..-1].to_i.to_s(36).upcase
+                      number = number[0..255]
+                    end
+                    unless entry
+                      entry = company.journal_entries.create(:number=>number, :journal_id=>all_journals[isa_entry.journal], :printed_on=>isa_entry.printed_on, :created_on=>isa_entry.created_on, :updated_at=>isa_entry.updated_on, :lock_version=>isa_entry.version_number)  # , :state=>(isa_entry.unupdateable? ? :confirmed : :draft) 
+                      raise isa_entry.inspect+"\n"+entry.errors.full_messages.to_sentence unless entry.valid?
+                    end
+                  end
+
+                  unused_entries.delete(entry.id)
+
+                  entry.lines.clear
+                  for isa_line in isa_entry.lines
+                    if isa_line.debit < 0 or isa_line.credit < 0
+                      debit = isa_line.debit
+                      isa_line.debit = isa_line.credit.abs
+                      isa_line.credit = debit.abs
+                    end
+                    line =  entry.lines.create(:account_id=>all_accounts[isa_line.account], :name=>"#{isa_line.label} (#{isa_entry.label})", :currency_debit=>isa_line.debit, :currency_credit=>isa_line.credit, :letter=>(isa_line.lettering > 0 ? isa_line.letter : nil), :comment=>isa_line.to_s)
+                    raise isa_line.to_s+"\n"+line.errors.full_messages.to_sentence unless line.valid?
+                  end
+
+                  count += 1
+                  if Time.now > next_start
+                    status = print_jauge(count, total_count, :replace=>status, :start=>start)
+                    next_start = Time.now + interval
+                  end
+                end
+
+
+
+                print_jauge(count, total_count, :replace=>status, :start=>start, :time=>:elapsed, :new_line=>true)
+                if unused_entries.size > 0
+                  puts "#{unused_entries.size} destroyed entries (on #{total_count})"
+                  JournalEntry.destroy(unused_entries)
+                end
+              end
+              
+              # Check all lines line-per-line
+              benchmark "Check all entries" do
+                found, expected = fy.journal_entries.size, isa_fy.entries.size
+                raise Exception.new("The count of entries is different: #{found} in database and #{expected} in file") if found != expected
+                found, expected = company.journal_entry_lines_between(fy.started_on, fy.stopped_on).size, isa_fy.entries.inject(0){|s, e| s += e.lines.size}
+                raise Exception.new("The count of entry lines is different: #{found} in database and #{expected} in file") if found != expected
+                for entry in fy.journal_entries
+                end
+              end
+
+            end
+          end
+        end
 
       else
         raise NotWellFormedFileError.new("Version does not seems to be supported")
