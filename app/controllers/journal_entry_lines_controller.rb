f1c78df61e077fffc32801631041a45ed6f3841e
burisu <burisu@oneiros.fr>
2012-03-11 20:40:00 +0100

    Adapts journal entry writing

--- app/controllers/journal_entry_lines_controller.rb
+++ app/controllers/journal_entry_lines_controller.rb
@@ -22,21 +22,24 @@ class JournalEntryLinesController < ApplicationController
   def new
     @journal_entry_line = JournalEntryLine.new
     @journal_entry_line.name = params[:name] if params[:name]
-    if params["entry-debit"] and params["entry-credit"]
-      debit, credit = params["entry-debit"].to_f, params["entry-credit"].to_f
+    if params["entry-original-debit"] and params["entry-original-credit"]
+      debit, credit = params["entry-original-debit"].to_f, params["entry-original-credit"].to_f
       if debit > credit
         @journal_entry_line.original_credit = debit - credit
       else
         @journal_entry_line.original_debit  = credit - debit
       end
     end
-    if params[:journal_id] and journal = @current_company.journals.find_by_id(params[:journal_id])
-      if journal.cashes.size == 1
-        @journal_entry_line.account = journal.cashes.first.account
+    if params[:journal_id] and @journal = @current_company.journals.find_by_id(params[:journal_id])
+      if @journal.cashes.size == 1
+        @journal_entry_line.account = @journal.cashes.first.account
       end
     end
+    params[:printed_on] = params[:printed_on].to_date rescue nil
+    if params[:printed_on]
+      @financial_year = @current_company.financial_year_at(params[:printed_on])
+    end
     if request.xhr?
-      
       render :partial=>"journal_entry_lines/row_form", :object=>@journal_entry_line
     else
       redirect_to_back
