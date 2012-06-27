e7f165a7d93203331fa430c4effec71b7c1cd33e
Brice Texier <brice.texier@ekylibre.org>
2012-06-27 14:40:45 +0200

    Adds tests for models file

---
+++ test/lib/models_test.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+require 'test_helper'
+
+class ModelsTest < Test::Unit::TestCase
+
+  # Checks the validity of references files for models
+  def test_ekylibre_models
+    for k, v in Ekylibre.references
+      for c, t in v
+        assert(!t.blank?, "#{k}.#{c} foreign key is not determined.")
+        if t.is_a? Symbol
+          assert_nothing_raised do
+            t.to_s.classify.constantize
+          end
+        end
+      end
+    end
+  end
+
+end
