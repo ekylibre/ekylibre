c7f19cbb34c7e1be8e9c8393bd8f3089b981af6c
Brice Texier <burisu@oneiros.fr>
2012-08-25 00:02:37 +0200

    Fixes bug on precompilation due to bad name for sprites.
    Names (black, blue and green) were replaced with color value (#000, #00F...)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Ekylibre::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Defaults to Rails.root.join("public/assets")
+  # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
@@ -46,7 +46,8 @@ Ekylibre::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( themes/tekyla/screen.css themes/tekyla/print.css )
+  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += [ "themes/tekyla/screen.css", "themes/tekyla/print.css" ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
