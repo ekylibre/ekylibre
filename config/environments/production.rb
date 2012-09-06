c2ffacc48fd421302b6dc32aecb0e9c3e1b3c2c3
Brice Texier <burisu@oneiros.fr>
2012-09-06 10:52:09 +0200

    Adds assets to precompile. Fixes #50. Fixes #47.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Ekylibre::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,6 +47,7 @@ Ekylibre::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += [ "themes/tekyla/screen.css", "themes/tekyla/print.css" ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
