ed0083c7c73761979375bc41586ca3ddb9b40eb6
Brice Texier <burisu@oneiros.fr>
2013-07-29 21:59:31 +0200

    Removes useless simple_form inputs

--- app/inputs/choice_input.rb
+++
@@ -1,2 +0,0 @@
-class ChoiceInput < SimpleForm::Inputs::CollectionSelectInput
-end
--- app/inputs/unroll_input.rb
+++
@@ -1,9 +0,0 @@
-class UnrollInput < SimpleForm::Inputs::Base
-  enable :placeholder, :maxlength
-
-  def input
-    add_size!
-    @builder.text_field(attribute_name, input_html_options)
-  end
-
-end
