ffad3553f9bc606c7ea9570dd2c90ae71bdbc19f
David JOULIN <ionosphere.consulting@gmail.com>
2013-08-22 12:02:38 +0200

    adapt production dashboard

--- app/controllers/backend/cells/last_milk_result_cells_controller.rb
+++ app/controllers/backend/cells/last_milk_result_cells_controller.rb
@@ -1,6 +1,11 @@
 class Backend::Cells::LastMilkResultCellsController < Backend::CellsController
 
-  list(:model => :product_indicator_data, :conditions => ["product_id = 96"], :order => "measured_at DESC", :per_page => 10) do |t|
+  list(:model => :product_indicator_data,
+       :joins => {:product => :nature},
+       :conditions => ["nomen = ?", 'milk'],
+       :order => "measured_at DESC, id DESC",
+       :per_page => 10
+       ) do |t|
     t.column :indicator#, :url => true
     t.column :name, :through => :product, :url => {:controller => "'/backend/products'"}
     t.column :value
