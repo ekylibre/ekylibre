2ef62ff8af3b69a00cd25b268f14cef5bb233e68
Brice Texier <burisu@oneiros.fr>
2013-09-13 17:51:29 +0200

    Removes useless code

--- app/controllers/backend/legal_entities_controller.rb
+++ app/controllers/backend/legal_entities_controller.rb
@@ -7,23 +7,4 @@ class Backend::LegalEntitiesController < Backend::EntitiesController
     t.column :name, :url => true
   end
 
-  # Displays the main page with the list of legal_entities.
-  def index
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => LegalEntity.all }
-      format.json { render :json => LegalEntity.all }
-    end
-  end
-
-  # Displays the page for one legal_entity.
-  def show
-    return unless @legal_entity = find_and_check
-    respond_to do |format|
-      format.html { t3e(@legal_entity) }
-      format.xml  { render :xml => @legal_entity }
-      format.json { render :json => @legal_entity }
-    end
-  end
-
 end
