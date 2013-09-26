32808ccfa7f888c19c7d11824570d9e72cc3a5ec
David JOULIN <ionosphere.consulting@gmail.com>
2013-09-26 11:58:33 +0200

    update cropping plan controller

--- app/controllers/backend/cells/cropping_plan_on_cultivable_land_parcels_cells_controller.rb
+++ app/controllers/backend/cells/cropping_plan_on_cultivable_land_parcels_cells_controller.rb
@@ -3,7 +3,7 @@ class Backend::Cells::CroppingPlanOnCultivableLandParcelsCellsController < Backe
   def show
     # GET DATA
     # for last campaign, show each production with product support and area
-    @campaigns = Campaign.currents.limit(1)
+    @campaigns = Campaign.currents.last
   end
 
 end
