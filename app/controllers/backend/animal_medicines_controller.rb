9fc3f7e61ddbe4868f3abe8f5e8075f62912f8bb
David JOULIN <ionosphere.consulting@gmail.com>
2013-10-03 20:51:50 +0200

    remove useless AnimalMedicine code

--- app/controllers/backend/animal_medicines_controller.rb
+++ app/controllers/backend/animal_medicines_controller.rb
@@ -8,22 +8,4 @@ class Backend::AnimalMedicinesController < BackendController
     t.column :name, url: true
   end
 
-  # Displays the main page with the list of animal_medicines.
-  def index
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => AnimalMedicine.all }
-      format.json { render :json => AnimalMedicine.all }
-    end
-  end
-
-  # Displays the page for one animal_medicine.
-  def show
-    return unless @animal_medicine = find_and_check
-    respond_to do |format|
-      format.html { t3e(@animal_medicine) }
-      format.xml  { render :xml => @animal_medicine }
-      format.json { render :json => @animal_medicine }
-    end
-  end
 end
