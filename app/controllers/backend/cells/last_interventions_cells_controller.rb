03db709da70423adcc8aaf7afd709b2c85bbb59e
David JOULIN <ionosphere.consulting@gmail.com>
2013-10-12 11:42:15 +0200

    fix bug on last interventions cells

--- app/controllers/backend/cells/last_interventions_cells_controller.rb
+++ app/controllers/backend/cells/last_interventions_cells_controller.rb
@@ -3,14 +3,21 @@ class Backend::Cells::LastInterventionsCellsController < Backend::CellsControlle
   def show
     @intervention = Intervention.last
     production = Production.find(@intervention.production_id)
-    if target = @intervention.casts.of_role(:target).first.actor_id
-      container = Product.find(target)
-      # if container.is_a?(CultivableLandParcel)
-      #   @container = container.class.find(container.id)
-      if container.is_a?(Plant)
-        @container = CultivableLandParcel.find(container.current_place_id)
-      else
-        @container = container
+    target = @intervention.casts.of_role(:target) || @intervention.casts.of_role(:input)
+    puts target
+    if target.first
+      actor = target.first.actor
+      if actor
+        container = Product.find(actor.id)
+        # if container.is_a?(CultivableLandParcel)
+        #   @container = container.class.find(container.id)
+        if container.is_a?(Plant)
+          @container = CultivableLandParcel.find(container.current_place_id)
+        elsif container.is_a?(Animal)
+          @container = BuildingDivision.find(container.current_place_id)
+        else
+          @container = container
+        end
       end
     end
   end
