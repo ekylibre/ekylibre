8761100749bff6f122e12219f57605c57750290d
Brice Texier <burisu@oneiros.fr>
2013-11-17 18:30:48 +0100

    Fixes bug on account links with ProductNature in place of ProductNatureCategory

--- app/models/account.rb
+++ app/models/account.rb
@@ -45,19 +45,19 @@ class Account < Ekylibre::Record::Base
   # has_many :attorneys, class_name: "Entity", foreign_key: :attorney_account_id
   has_many :balances, class_name: "AccountBalance"
   has_many :cashes
-  has_many :clients, class_name: "Entity", foreign_key: :client_account_id
-  has_many :collected_taxes, class_name: "Tax", foreign_key: :collect_account_id
+  has_many :clients,             class_name: "Entity", foreign_key: :client_account_id
+  has_many :collected_taxes,     class_name: "Tax", foreign_key: :collect_account_id
   has_many :commissioned_incoming_payment_modes, class_name: "IncomingPaymentMode", foreign_key: :commission_account_id
   has_many :depositables_incoming_payment_modes, class_name: "IncomingPaymentMode", foreign_key: :depositables_account_id
-  has_many :immobilizations_products, class_name: "ProductNature", foreign_key: :asset_account_id
-  has_many :journal_entry_items, class_name: "JournalEntryItem"
-  has_many :paid_taxes, class_name: "Tax", foreign_key: :deduction_account_id
-  has_many :purchases_products, class_name: "ProductNature", foreign_key: :charge_account_id
-  has_many :purchase_items, class_name: "PurchaseItem"
-  has_many :sale_items, class_name: "SaleItem"
-  has_many :sales_products, class_name: "ProductNatureCategory", foreign_key: :product_account_id
-  has_many :stocks_products, class_name: "ProductNatureCategory", foreign_key: :stock_account_id
-  has_many :suppliers, class_name: "Entity", foreign_key: :supplier_account_id
+  has_many :assets_categories,    class_name: "ProductNatureCategory", foreign_key: :asset_account_id
+  has_many :journal_entry_items,  class_name: "JournalEntryItem"
+  has_many :paid_taxes,           class_name: "Tax", foreign_key: :deduction_account_id
+  has_many :charges_categories,   class_name: "ProductNatureCategory", foreign_key: :charge_account_id
+  has_many :purchase_items,       class_name: "PurchaseItem"
+  has_many :sale_items,           class_name: "SaleItem"
+  has_many :products_categories,  class_name: "ProductNatureCategory", foreign_key: :product_account_id
+  has_many :stocks_categories,    class_name: "ProductNatureCategory", foreign_key: :stock_account_id
+  has_many :suppliers,            class_name: "Entity", foreign_key: :supplier_account_id
   #[VALIDATORS[ Do not edit these lines directly. Use `rake clean:validations`.
   validates_length_of :last_letter, allow_nil: true, maximum: 10
   validates_length_of :number, allow_nil: true, maximum: 20
