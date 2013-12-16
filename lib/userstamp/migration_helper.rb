bc09e1959bfeef8b4ec6a5614987dedf6d9e1ea4
Brice Texier <burisu@oneiros.fr>
2013-12-16 10:05:47 +0100

    Normalizes Userstamp

--- lib/userstamp/migration_helper.rb
+++ lib/userstamp/migration_helper.rb
@@ -1,22 +1,53 @@
-ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
+module Userstamp
 
-  def stamps(options = {})
-    groups = [:time, :user, :lock]
-    groups &= [options.delete(:only)].flatten if options[:only]
-    groups -= [options.delete(:except)].flatten if options[:except]
-    if groups.include?(:time)
-      self.datetime(:created_at, null: false)
-      self.datetime(:updated_at, null: false)
-      self.index(:created_at)
-      self.index(:updated_at)
+  module MigrationHelper
+
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
     end
-    if groups.include?(:user)
-      self.references(:creator, index: true)
-      self.references(:updater, index: true)
-    end
-    if groups.include?(:lock)
-      self.integer(:lock_version, null: false, default: 0)
+
+    module InstanceMethods
+      def stamps(options = {})
+        groups = [:time, :user, :lock]
+        groups &= [options.delete(:only)].flatten if options[:only]
+        groups -= [options.delete(:except)].flatten if options[:except]
+        if groups.include?(:time)
+          self.datetime(:created_at, null: false)
+          self.datetime(:updated_at, null: false)
+          self.index(:created_at)
+          self.index(:updated_at)
+        end
+        if groups.include?(:user)
+          self.references(:creator, index: true)
+          self.references(:updater, index: true)
+        end
+        if groups.include?(:lock)
+          self.integer(:lock_version, null: false, default: 0)
+        end
+      end
     end
   end
-
 end
+
+# ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
+
+#   def stamps(options = {})
+#     groups = [:time, :user, :lock]
+#     groups &= [options.delete(:only)].flatten if options[:only]
+#     groups -= [options.delete(:except)].flatten if options[:except]
+#     if groups.include?(:time)
+#       self.datetime(:created_at, null: false)
+#       self.datetime(:updated_at, null: false)
+#       self.index(:created_at)
+#       self.index(:updated_at)
+#     end
+#     if groups.include?(:user)
+#       self.references(:creator, index: true)
+#       self.references(:updater, index: true)
+#     end
+#     if groups.include?(:lock)
+#       self.integer(:lock_version, null: false, default: 0)
+#     end
+#   end
+
+# end
