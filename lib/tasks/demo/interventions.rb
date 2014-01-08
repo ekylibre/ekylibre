8df17ae81b71fd484cb4a529b61e91b567293f67
Brice Texier <burisu@oneiros.fr>
2014-01-08 14:09:34 +0100

    Fixes bug in intervention demo

--- lib/tasks/demo/interventions.rb
+++ lib/tasks/demo/interventions.rb
@@ -95,8 +95,8 @@ demo :interventions do
         # Run interventions
         intervention = nil
         for period in periods
+          stopped_at = period[:started_at] + period[:duration]
           if stopped_at < Time.now
-            stopped_at = period[:started_at] + period[:duration]
             intervention = Intervention.create!(reference_name: procedure_name, production: Booker.production, production_support: options[:support], started_at: period[:started_at], stopped_at: stopped_at)
             for cast in booker.casts
               intervention.add_cast!(cast)
