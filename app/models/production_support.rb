0ebc5a77cdbc8dfa6aad3566d82c91a13eb74b9a
David JOULIN <ionosphere.consulting@gmail.com>
2014-01-23 22:04:12 +0100

    introduce active activity for PPF

--- app/models/production_support.rb
+++ app/models/production_support.rb
@@ -87,7 +87,14 @@ class ProductionSupport < Ekylibre::Record::Base
     started_at = options[:at] || Time.now
     return self.markers.where(indicator_name: indicator.name).where("started_at <= ?", started_at).reorder(started_at: :desc).first
   end
-
+  
+  def active?
+    if self.activity.fallow_land?
+      return false
+    else
+      return true
+    end
+  end
 
   def cost(role=:input)
     cost = []
