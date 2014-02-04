84284ee417f1e37d91ab977fb4eb114fb0ba0817
Brice Texier <burisu@oneiros.fr>
2014-02-04 18:49:38 +0100

    Removes v0 migrations

--- db/previous/v0/20091106135405_fill_document_templates_nature.rb
+++
@@ -1,8 +0,0 @@
-class FillDocumentTemplatesNature < ActiveRecord::Migration
-  def self.up
-    execute "UPDATE #{quoted_table_name(:document_templates)} SET nature = code WHERE nature IS NULL"
-  end
-
-  def self.down
-  end
-end
--- db/previous/v0/20120730093920_remove_default_journal_closed_on.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDefaultJournalClosedOn < ActiveRecord::Migration
-  def up
-    change_column_default :journals, :closed_on, nil
-  end
-
-  def down
-    change_column_default :journals, :closed_on, Date.civil(1970,12,31)
-  end
-end
