2cf3441aca2e34d8751ab9e9e2ecff4cc5e6ddcf
David JOULIN <ionosphere.consulting@gmail.com>
2014-02-09 14:00:12 +0100

    refactorize building,building_divisions first_run to zones, adapt first_run and clean code.

--- lib/tasks/first_run/buildings.rb
+++ lib/tasks/first_run/buildings.rb
@@ -1,101 +1,58 @@
 # -*- coding: utf-8 -*-
 load_data :buildings do |loader|
 
-  file = loader.path("buildings.shp")
-  if file.exist?
-    standard_place_variant = ProductNatureVariant.import_from_nomenclature(:building)
+  # create base land parcel
+  land_parcel_variant = ProductNatureVariant.import_from_nomenclature(:land_parcel)
+  LandParcel.create!(:variant_id => land_parcel_variant.id, :work_number => "LP00",
+                                 :name => "Parcelle par défault", :initial_born_at => Time.new(1995, 1, 1, 10, 0, 0, "+00:00"), :initial_owner => Entity.of_company, :default_storage => nil)
 
-    loader.count :buildings do |w|
-      #############################################################################
-      # Import (from nomenclature) a default product_nature to place animal
-      animal_place_variant = ProductNatureVariant.import_from_nomenclature(:animal_building)
-      settlement_place_variant = ProductNatureVariant.import_from_nomenclature(:screed_building)
-      office_place_variant = ProductNatureVariant.import_from_nomenclature(:office_building)
-      born_at = Time.new(1995, 1, 1, 10, 0, 0, "+00:00")
+  path = loader.path("zones.csv")
+  if path.exist?
+    born_at = Time.new(1995, 1, 1, 10, 0, 0, "+00:00")
+    default_place = LandParcel.first
+    loader.count :zone_groups do |w|
+      CSV.foreach(path, headers: true) do |row|
+        r = OpenStruct.new(name: row[0].to_s,
+                           nature: (row[1].blank? ? nil : row[1].to_sym),
+                           code: (row[2].blank? ? nil : row[2].to_s),
+                           group_code: (row[3].blank? ? nil : row[3].to_s),
+                           place_code: (row[4].blank? ? nil : row[4].to_s),
+                           description: (row[5].blank? ? nil : row[5].to_s)
+                          )
 
-      # TODO Localize or externalize localized data
-      for building in [{variant_id: standard_place_variant.id,   name: "Bâtiment historique", :work_number => "B05", :identification_number => "STABULATION_05"},
-                       {variant_id: settlement_place_variant.id, name: "Aire bétonnée", :work_number => "B06", :identification_number => "STABULATION_06"},
-                       {variant_id: animal_place_variant.id,     name: "Stabulation principale", :work_number => "B07", :identification_number => "STABULATION_07"},
-                       {variant_id: animal_place_variant.id,     name: "Bâtiment Taurillons Bois", :work_number => "B04", :identification_number => "BAT_TAURILLON"},
-                       {variant_id: animal_place_variant.id,     name: "Bâtiment Bouquet en L Genisse", :work_number => "B03", :identification_number => "BAT_GEN"},
-                       {variant_id: animal_place_variant.id,     name: "Poulailler 1 (côté Jardin)", :work_number => "B09", :identification_number => "BAT_POULAILLER_1"},
-                       {variant_id: office_place_variant.id,     name: "Bureau", :work_number => "B08", :identification_number => "BUREAU"},
-                       {variant_id: settlement_place_variant.id, name: "Silo bas", :work_number => "B01", :identification_number => "SILO_BAS"},
-                       {variant_id: settlement_place_variant.id, name: "Fosse eaux brunes", :work_number => "B02", :identification_number => "FOSSE"},
-                       {variant_id: animal_place_variant.id,     name: "Poulailler 2 (côté Forêt)", :work_number => "B10", :identification_number => "BAT_POULAILLER_2"}
-                      ]
-        unless Building.find_by_work_number(building[:work_number])
-          Building.create!({initial_owner: Entity.of_company, initial_born_at: born_at}.merge(building)) # , default_storage: Building.first
-          w.check_point
-        end
-      end
+        unless zone = Product.find_by_work_number(r.code)
+          zone_variant = ProductNatureVariant.find_by(:reference_name => r.nature) || ProductNatureVariant.import_from_nomenclature(r.nature)
+          pmodel = zone_variant.nature.matching_model
+          zone = pmodel.create!(:variant_id => zone_variant.id, :work_number => r.code,
+                                 :name => r.name, :initial_born_at => born_at, :initial_owner => Entity.of_company)
 
-    end
-  end
-
-  file = loader.path("buildings.shp")
-  if file.exist?
-    loader.count :buildings_shapes do |w|
-      #############################################################################
-      born_at = Time.new(1995, 1, 1, 10, 0, 0, "+00:00")
-      RGeo::Shapefile::Reader.open(file.to_s, :srid => 2154) do |file|
-        # puts "File contains #{file.num_records} records."
-        file.each do |record|
-          building = Building.find_by_work_number(record.attributes['WORK_NUMBE'])
-          building ||= Building.create!(variant_id: standard_place_variant.id,
-                                        name: record.attributes['DESCRIPTION'].to_s,
-                                        :work_number => record.attributes['WORK_NUMBE'].to_s,
-                                        initial_born_at: born_at,
-                                        # default_storage: Building.first,
-                                        :identification_number => record.attributes['NUMERO'].to_s)
-          building.read!(:shape, record.geometry, at: born_at, force: true)
-          building.read!(:net_surface_area, building.shape_area, at: born_at)
-          w.check_point
-        end
-      end
-    end
-  end
-
-  file = loader.path("building_divisions.shp")
-  if file.exist?
-    loader.count :building_divisions do |w|
-
-      RGeo::Shapefile::Reader.open(file.to_s, :srid => 2154) do |file|
-        # puts "File contains #{file.num_records} records."
-        born_at = Time.new(1995, 1, 1, 10, 0, 0, "+00:00")
-        file.each do |record|
-          # find the building_division if exist
-          building_division   = BuildingDivision.find_by_work_number(record.attributes['WORK_NUMBE'])
-          # import the correct product_nature_variant with the NOMEN attributes in shp file
-          building_division_variant   = ProductNatureVariant.import_from_nomenclature(record.attributes['NOMEN'].to_sym)
-          building_division_variant ||= ProductNatureVariant.import_from_nomenclature(:building_division)
-          #  create the building_division
-          building_division ||= BuildingDivision.create!(variant_id: building_division_variant.id,
-                                                         name: record.attributes['DECRIPTION'].to_s,
-                                                         :work_number => record.attributes['WORK_NUMBE'].to_s,
-                                                         :initial_born_at => born_at,
-                                                         :identification_number => record.attributes['NUMERO'].to_s)
-          building_division.read!(:shape, record.geometry, at: born_at)
-          ind_area = building_division.shape_area
-          building_division.read!(:net_surface_area, ind_area.in_square_meter, at: born_at)
-
-          if record.attributes['CONTAINER'].to_s
-            if building = Building.find_by_work_number(record.attributes['CONTAINER'].to_s)
-              building.add(building_division, born_at)
-              building_division.update_attributes(initial_container: building)
-              building_division.save!
+            if container = Product.find_by_work_number(r.place_code)
+              # container.add(zone, born_at)
+              zone.update_attributes(initial_container: container)
+              zone.save!
             end
-          end
+        end
+        w.check_point
+     end
+   end
+  end
 
-          # puts "Record number #{record.index}:"
-          # puts "  Geometry: #{record.geometry.as_text}"
-          # puts "  Attributes: #{record.attributes.inspect}"
+  path = loader.path("zones.shp")
+  if path.exist?
+    loader.count :zones_shapes do |w|
+      #############################################################################
+      born_at = Time.new(1995, 1, 1, 10, 0, 0, "+00:00")
+      RGeo::Shapefile::Reader.open(path.to_s, :srid => 4326) do |file|
+        # puts "File contains #{file.num_records} records."
+        file.each do |record|
+          if zone = Product.find_by_work_number(record.attributes['WORK_NUMBE'])
+            zone.read!(:shape, record.geometry, at: born_at, force: true)
+            zone.read!(:net_surface_area, zone.shape_area, at: born_at)
+          end
           w.check_point
         end
       end
     end
   end
-
 
 end
