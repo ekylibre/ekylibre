a2915599f36b62c19d764d33a98cbd5f491508d2
Brice Texier <burisu@oneiros.fr>
2014-04-02 00:03:08 +0200

    Fixes demo

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -49,7 +49,7 @@ end
 
 class Intervention < Ekylibre::Record::Base
   attr_readonly :reference_name, :production_id
-  belongs_to :ressource , :polymorphic => true
+  belongs_to :ressource , polymorphic: true
   belongs_to :production, inverse_of: :interventions
   belongs_to :production_support
   belongs_to :issue
@@ -228,7 +228,7 @@ class Intervention < Ekylibre::Record::Base
         if variable.parted?
           # Parted from
           variant = producer.variant
-          produced.actor = variant.matching_model.new(variant: variant, initial_born_at: stopped_at, initial_owner: producer.actor.owner, initial_container: producer.actor.container, initial_population: produced.population, initial_shape: produced.shape, name: producer.name, extjuncted: true, tracking: producer.tracking)
+          produced.actor = variant.matching_model.new(variant: variant, initial_born_at: stopped_at, initial_owner: producer.actor.owner, initial_container: producer.actor.container, initial_population: produced.population, initial_shape: produced.shape, name: producer.name, extjuncted: true, tracking: producer.actor.tracking)
           unless produced.actor.save
             puts "*" * 80 + variant.matching_model.name
             puts produced.actor.inspect
