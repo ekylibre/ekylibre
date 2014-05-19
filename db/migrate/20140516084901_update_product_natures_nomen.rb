973beef1595b237be3c957fb61e4333460731a19
David JOULIN <ionosphere.consulting@gmail.com>
2014-05-19 23:07:05 +0200

    continue on nomenclature migration

--- db/migrate/20140516084901_update_product_natures_nomen.rb
+++ db/migrate/20140516084901_update_product_natures_nomen.rb
@@ -23,7 +23,7 @@ class UpdateProductNaturesNomen < ActiveRecord::Migration
     
   end
 
-  ITEMS = [
+  PRODUCT_NATURE_ITEMS = [
        {table: 'product_natures', column: 'abilities_list', old: 'spread(mineral_matter)', new: 'spread(preparation)'},
        {table: 'product_natures', column: 'abilities_list', old: 'spread(organic_matter)', new: 'spread(excrement)'},
        {table: 'product_natures', column: 'abilities_list', old: 'store(organic_matter)', new: 'store(excrement)'},
@@ -83,13 +83,53 @@ class UpdateProductNaturesNomen < ActiveRecord::Migration
        {table: 'product_natures', column: 'category_id', linked_table: 'product_nature_categories', reference_name: 'animal_food_building_division', new_linked_reference_name: 'building_division', old_linked_reference_name: 'equipment'},
        {table: 'product_natures', column: 'category_id', linked_table: 'product_nature_categories', reference_name: 'silage_division', new_linked_reference_name: 'building_division', old_linked_reference_name: 'equipment'}
      ]
+     
+   PRODUCT_NATURE_VARIANT_ITEMS = [
+       {table: 'product_nature_variants', column: 'reference_name', old: 'alfalfa_crop', new: 'lucerne_crop'},
+       {table: 'product_nature_variants', column: 'derivative_of', old: 'organic_matter', new: 'raw_matter', reference_name: 'wine_vinasse'},
+       {table: 'product_nature_variants', column: 'reference_name', old: 'coop:ammonitre_33,5%_vr', new: 'coop:ammonitre_33,5__vr'},
+       {table: 'product_nature_variants', column: 'reference_name', old: 'coop:super_46%_gr._vr', new: 'coop:super_46__gr__vr'},
+       {table: 'product_nature_variants', column: 'reference_name', old: 'coop:chlorure_60%_gr_vr', new: 'coop:chlorure_60__gr_vr'},
+       {table: 'product_nature_variants', column: 'unit_name', old: 'bag', new: '20kg_bag', reference_name:  'coop:metarex_rg_tds_20kg'}
+     ]  
+       
+   PRODUCT_NATURE_VARIANT_READING_ITEMS = [
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'lucerne_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'apple_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'walnut_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'hazel_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'rose_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'raspberry_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'strawberry_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'gooseberry_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'blackcurrant_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'pear_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'peach_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'fig_crop'},
+       {table: 'product_nature_variant_readings', new_frozen_indicator_name: 'net_surface_area', new_frozen_indicator_datatype: 'measure', new_value: '1.00', new_unit: 'hectare', new_absolute_value: '10000.00', new_absolute_unit: 'square_meter', reference_name: 'vine_grape_crop'}      
+       
+     ]
+     
+      INDICATOR_ITEMS = [
+        # for sulfur_dioxide_concentration
+       {table: 'product_readings', column: 'indicator_name', old: 'sulfur_dioxyde_concentration', new: 'sulfur_dioxide_concentration'},
+       {table: 'product_nature_variant_readings', column: 'indicator_name', new: 'sulfur_dioxide_concentration', old: 'sulfur_dioxyde_concentration'},
+       {table: 'productions_support_markers', column: 'indicator_name', new: 'sulfur_dioxide_concentration', old: 'sulfur_dioxyde_concentration'},
+       {table: 'analysis_items', column: 'indicator_name', new: 'sulfur_dioxide_concentration', old: 'sulfur_dioxyde_concentration'},
+       
+       # for plants_count
+       {table: 'product_readings', column: 'indicator_name', old: 'plants_density', new: 'plants_count'},
+       {table: 'product_nature_variant_readings', column: 'indicator_name', new: 'plants_count', old: 'plants_density'},
+       {table: 'productions_support_markers', column: 'indicator_name', new: 'plants_count', old: 'plants_density'},
+       {table: 'analysis_items', column: 'indicator_name', new: 'plants_count', old: 'plants_density'}
+             
+       ]
 
   def up
     # check if product_nature is present in DB and update it with new values
-    for item in ITEMS       
+    for item in PRODUCT_NATURE_ITEMS       
       replace_items_in_array(item[:table], item[:column], item)
     end
-    
     
     for item in CHANGING_CATEGORIES
       if connection.select_value("SELECT count(*) FROM #{item[:linked_table]} WHERE reference_name = '#{item[:new_linked_reference_name]}'").to_i > 0
@@ -97,6 +137,27 @@ class UpdateProductNaturesNomen < ActiveRecord::Migration
       end
     end
     
+    for item in PRODUCT_NATURE_VARIANT_ITEMS
+     replace_items_in_array(item[:table], item[:column], item)
+    end
+    
+    for item in PRODUCT_NATURE_VARIANT_READING_ITEMS
+      if variant_id = connection.select_value("SELECT min(id) FROM product_nature_variants WHERE reference_name = '#{item[:reference_name]}'").to_i
+        if item[:old_frozen_indicator_name] and item[:new_frozen_indicator_name]
+          
+        elsif item[:new_frozen_indicator_name]
+          if item[:new_frozen_indicator_datatype] == 'measure'
+            execute("INSERT INTO #{item[:table]} (variant_id, indicator_name, indicator_datatype, measure_value_value, measure_value_unit, absolute_measure_value_value, absolute_measure_value_unit, created_at, updated_at) VALUES ('#{variant_id}', '#{item[:new_frozen_indicator_name]}', '#{item[:new_frozen_indicator_datatype]}', '#{item[:new_value]}', '#{item[:new_unit]}', '#{item[:new_absolute_value]}', '#{item[:new_absolute_unit]}', DEFAULT, DEFAULT)")
+          end
+        elsif item[:new_value] and item[:old_value]
+          execute("UPDATE #{item[:table]} SET #{item[:column]} = '#{item[:new_value]}' WHERE #{item[:column]} = '#{item[:old_value]}'")
+        end
+      end
+    end
+    
+    for item in INDICATOR_ITEMS
+     replace_items_in_array(item[:table], item[:column], item)
+    end
     
     # add column for maximum_nitrogen_input in mmp
     add_column :manure_management_plan_zones, :maximum_nitrogen_input, :decimal, precision: 19, scale: 4    
@@ -105,6 +166,26 @@ class UpdateProductNaturesNomen < ActiveRecord::Migration
   def down
     # remove column for maximum_nitrogen_input in mmp
     remove_column :manure_management_plan_zones, :maximum_nitrogen_input
+    
+    for item in INDICATOR_ITEMS
+     replace_items_in_array(item[:table], item[:column], new: item[:old], old: item[:new])
+    end
+    
+    for item in PRODUCT_NATURE_VARIANT_READING_ITEMS
+      if variant_id = connection.select_value("SELECT min(id) FROM product_nature_variants WHERE reference_name = '#{item[:reference_name]}'").to_i
+        if item[:old_frozen_indicator_name] and item[:new_frozen_indicator_name]
+          
+        elsif item[:new_frozen_indicator_name]
+          execute("DELETE FROM #{item[:table]} WHERE variant_id = '#{variant_id}' AND indicator_name = '#{item[:new_frozen_indicator_name]}'")
+        elsif item[:new_value] and item[:old_value]
+          execute("UPDATE #{item[:table]} SET #{item[:column]} = '#{item[:old_value]}' WHERE #{item[:column]} = '#{item[:new_value]}'")
+        end
+      end
+    end
+    
+    for item in PRODUCT_NATURE_VARIANT_ITEMS
+      replace_items_in_array(item[:table], item[:column], new: item[:old], old: item[:new])
+    end
     
     for item in CHANGING_CATEGORIES
       if connection.select_value("SELECT count(*) FROM #{item[:linked_table]} WHERE reference_name = '#{item[:old_linked_reference_name]}'").to_i > 0
@@ -115,7 +196,7 @@ class UpdateProductNaturesNomen < ActiveRecord::Migration
     # check if product_nature is present in DB and update it with old abilities
 # check if product_nature is present in DB and update it with new abilities
 
-    for item in ITEMS       
+    for item in PRODUCT_NATURE_ITEMS       
       replace_items_in_array(item[:table], item[:column], new: item[:old], old: item[:new])
     end  
   end
