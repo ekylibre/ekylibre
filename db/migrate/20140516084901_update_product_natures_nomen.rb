038f07e9a52fcaa9431c5a738c8ee71a69b93825
Brice Texier <burisu@oneiros.fr>
2014-05-19 10:37:37 +0200

    Refactorizes last migration

--- db/migrate/20140516084901_update_product_natures_nomen.rb
+++ db/migrate/20140516084901_update_product_natures_nomen.rb
@@ -1,48 +1,35 @@
 class UpdateProductNaturesNomen < ActiveRecord::Migration
   
   # TODO need to be refactorized by a more powerfull system
-  
+  ABILITIES = {
+    'complete_sower' => {old: 'sow, spray, spread(mineral_matter)', new: 'sow, spray, spread(preparation)'},
+    'spreader' => {old: 'spread(mineral_matter)', new: 'spread(preparation)'}
+  }
   
   def up
-    #
-    # ABILITITES
-    #
-    # for product_natures
-    product_natures = []
-    product_natures << {item: 'complete_sower', old_abilities: 'sow, spray, spread(mineral_matter)', new_abilities: 'sow, spray, spread(preparation)'}
-    product_natures << {item: 'spreader', old_abilities: 'spread(mineral_matter)', new_abilities: 'spread(preparation)'}
     # check if product_nature is present in DB and update it with new abilities
-    for product_nature in product_natures       
-      if connection.select_value("SELECT count(*) FROM product_natures WHERE reference_name = '#{product_nature[:item]}'").to_i > 0
-        say "update " + product_nature[:item].inspect.yellow + " with " + product_nature[:new_abilities].inspect.green
-        execute "UPDATE product_natures SET abilities_list = '#{product_nature[:new_abilities]}' WHERE reference_name = '#{product_nature[:item]}' AND abilities_list = '#{product_nature[:old_abilities]}'"
+    for product_nature, abilities in ABILITIES       
+      if connection.select_value("SELECT count(*) FROM product_natures WHERE reference_name = '#{product_nature}'").to_i > 0
+        say "update " + product_nature.inspect.yellow + " with " + abilities[:new].inspect.green
+        execute "UPDATE product_natures SET abilities_list = '#{abilities[:new]}' WHERE reference_name = '#{product_nature}' AND abilities_list = '#{abilities[:old]}'"
       end
     end
     
-    
     # add column for maximum_nitrogen_input in mmp
-    add_column :manure_management_plan_zones, :maximum_nitrogen_input, :decimal, precision: 19, scale: 4
-    
+    add_column :manure_management_plan_zones, :maximum_nitrogen_input, :decimal, precision: 19, scale: 4    
   end
   
   def down
-    #
-    # ABILITITES
-    #
-    # for product_natures
-    product_natures = []
-    product_natures << {item: 'complete_sower', old_abilities: 'sow, spray, spread(mineral_matter)', new_abilities: 'sow, spray, spread(preparation)'}
-    product_natures << {item: 'spreader', old_abilities: 'spread(mineral_matter)', new_abilities: 'spread(preparation)'}
-    # check if product_nature is present in DB and update it with old abilities
-    for product_nature in product_natures       
-      if connection.select_value("SELECT count(*) FROM product_natures WHERE reference_name = '#{product_nature[:item]}'").to_i > 0
-        say "restore " + product_nature[:item].inspect.yellow + " with " + product_nature[:old_abilities].inspect.red
-        execute "UPDATE product_natures SET abilities_list = '#{product_nature[:old_abilities]}' WHERE reference_name = '#{product_nature[:item]}' AND abilities_list = '#{product_nature[:new_abilities]}'"
-      end
-    end
-  
     # remove column for maximum_nitrogen_input in mmp
     remove_column :manure_management_plan_zones, :maximum_nitrogen_input
-  
+
+    # check if product_nature is present in DB and update it with old abilities
+    for product_nature, abilities in ABILITIES.reverse
+      if connection.select_value("SELECT count(*) FROM product_natures WHERE reference_name = '#{product_nature}'").to_i > 0
+        say "restore " + product_nature.inspect.yellow + " with " + abilities[:old].inspect.red
+        execute "UPDATE product_natures SET abilities_list = '#{abilities[:old]}' WHERE reference_name = '#{product_nature}' AND abilities_list = '#{abilities[:new]}'"
+      end
+    end  
   end
+
 end
