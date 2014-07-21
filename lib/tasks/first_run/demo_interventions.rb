e198bda81b69f7073e7008fca295616c4e14803e
David JOULIN <ionosphere.consulting@gmail.com>
2014-07-21 21:27:35 +0200

    adapt demo data for rei data qualifying

--- lib/tasks/first_run/demo_interventions.rb
+++ lib/tasks/first_run/demo_interventions.rb
@@ -381,7 +381,7 @@ load_data :demo_interventions do |loader|
     if path.exist?
       loader.count :trip_simulation do |w|
         #############################################################################
-        read_at = Time.new(2014, 1, 1, 10, 0, 0, "+00:00")
+        read_at = Time.new(2014, 5, 5, 10, 0, 0, "+00:00")
         user = User.first
         RGeo::Shapefile::Reader.open(path.to_s, :srid => 4326) do |file|
           file.each do |record|
@@ -389,7 +389,7 @@ load_data :demo_interventions do |loader|
                           geolocation: record.geometry,
                           metadata: record.attributes['metadata'],
                           nature: record.attributes['nature'].to_sym,
-                          read_at: read_at,
+                          read_at: read_at + record.attributes['id'].to_i * 15,
                           user_id: user.id
                           )
             w.check_point
