c600429dce4014d30c38d8526e6afa4f65534e8b
Sébastien Gauvrit <sebastien.gauvrit@etu.u-bordeaux1.fr>
2014-08-01 15:19:01 +0200

    groups crumbs by intervention

--- app/controllers/backend/crumbs_controller.rb
+++ app/controllers/backend/crumbs_controller.rb
@@ -2,34 +2,31 @@ class Backend::CrumbsController < BackendController
 
   @actors = []
   
-  # array of crumbs ready to be managed by VisualizationHelper
-  @interventions_crumbs = []
-  
-  # TODO: replace by the actual current user id! crumbs are personal data
-  current_user_id = User.first 
-  interventions = Crumb.interventions(current_user_id)
-  interventions.each do |intervention|
-    name = interventions.index(intervention)
-    started_at = intervention.first.read_at
-    stopped_at = intervention.last.read_at
-    doer = User.find(current_user_id)
-    
-    intervention.each do |crumb|  
-      item = {
-                name:         name,
-                nature:       crumb.nature,
-                shape:        Charta::Geometry.new(crumb.geolocation).circle(crumb.accuracy/50000)
-              }
-      @interventions_crumbs << item
-    end 
-  end
-  
-  def interventions_crumbs
-    @interventions_crumbs
-  end
-  
   def index
-    render 'index', crumbs: @interventions_crumbs
+    # array of crumbs ready to be managed by VisualizationHelper
+    @interventions_crumbs = []
+  
+    # TODO: replace by the actual current user id! crumbs are personal data
+    current_user_id = User.first 
+    interventions = Crumb.interventions(current_user_id)
+    interventions.each do |intervention|
+      name = interventions.index(intervention)
+      started_at = intervention.first.read_at
+      stopped_at = intervention.last.read_at
+      doer = User.find(current_user_id)
+    
+      intervention.each do |crumb|  
+        item =  {
+                  name:         name,
+                  nature:       crumb.nature,
+                  shape:        Charta::Geometry.new(crumb.geolocation).circle(crumb.accuracy/50000),
+                  started_at:   started_at,
+                  stopped_at:   stopped_at,
+                  doer:         doer
+                }
+        @interventions_crumbs << item
+      end 
+    end
   end
 
 end
