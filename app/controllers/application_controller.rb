a739c41628dc70e466d3710bf85cdbfb9c9589b9
Brice Texier <burisu@oneiros.fr>
2014-09-23 11:29:24 +0200

    Fixes "Preferences" error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,7 +97,7 @@ class ApplicationController < ActionController::Base
     else
       session[:locale] = nil unless ::I18n.available_locales.include?(session[:locale].to_sym)
     end
-    session[:locale] ||= Preferences[:language] || I18n.default_locale
+    session[:locale] ||= Preference[:language] || I18n.default_locale
     I18n.locale = session[:locale]
   end
 
