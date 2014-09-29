e2955c99fb03e8b32176a528deebde2aeb45b31d
Brice Texier <burisu@oneiros.fr>
2014-09-29 11:57:38 +0200

    Ignores #run action in InterventionsControllerTest

--- test/controllers/backend/interventions_controller_test.rb
+++ test/controllers/backend/interventions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Backend::InterventionsControllerTest < ActionController::TestCase
 
-  test_restfully_all_actions compute: :create, set: :show
+  test_restfully_all_actions compute: :create, set: :show, except: :run
 
 end
 
