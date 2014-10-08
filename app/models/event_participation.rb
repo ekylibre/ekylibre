b91c4be22339be78a60f11559ea7e585fd969175
Brice TEXIER <brice@ekylibre.com>
2014-10-08 09:36:26 +0200

    Adds a valid title for backend/event_participations#edit

--- app/models/event_participation.rb
+++ app/models/event_participation.rb
@@ -42,6 +42,8 @@ class EventParticipation < Ekylibre::Record::Base
   validates_presence_of :event, :participant
   #]VALIDATORS]
 
+  delegate :name, to: :participant, prefix: true
+
   def status
     {waiting: :caution, accepted: :go, refused: :stop, informative: :undefined}.with_indifferent_access[self.state]
   end
