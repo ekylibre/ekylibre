fb30c467a8c30aee8fc50da9b0990d7b9cd126d0
Brice TEXIER <brice@ekylibre.com>
2014-10-22 16:39:19 +0200

    Changes default active_list tmp dir for prod/test modes

--- config/initializers/active_list.rb
+++ config/initializers/active_list.rb
@@ -1,2 +1,6 @@
 # Sets default temporary in Rails tmp dir by default
-ActiveList.temporary_directory = -> { Rails.root.join("tmp", "active_list-exports") }
+if Rails.env.development?
+  ActiveList.temporary_directory = -> { Rails.root.join("tmp", "exports", "active_list") }
+else
+  ActiveList.temporary_directory = -> { Ekylibre::Tenant.private_directory.join("tmp") }
+end
