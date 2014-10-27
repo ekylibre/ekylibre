095d4f212e573da1e427b29bbf15746797689a5d
Brice TEXIER <brice@ekylibre.com>
2014-10-27 09:16:35 +0100

    Deactivates default test on synchronizations#run

--- test/controllers/backend/synchronizations_controller_test.rb
+++ test/controllers/backend/synchronizations_controller_test.rb
@@ -1,4 +1,4 @@
 require 'test_helper'
 class Backend::SynchronizationsControllerTest < ActionController::TestCase
-  test_restfully_all_actions except: :update
+  test_restfully_all_actions except: [:update, :run]
 end
