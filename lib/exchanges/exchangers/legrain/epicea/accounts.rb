5e73333703a2f3c4fdc0383b37ac6eb5a0c3b2f7
Sébastien GAUVRIT <sgauvrit@ekylibre.com>
2014-11-04 16:40:58 +0100

    begins to implement epicea accounts import: imports accounts that are present in nomenclature.

--- lib/exchanges/exchangers/legrain/epicea/accounts.rb
+++ lib/exchanges/exchangers/legrain/epicea/accounts.rb
@@ -1,8 +1,18 @@
 Exchanges.add_importer :legrain_epicea_accounts do |file, w|
   rows = CSV.read(file, headers: true, encoding: "cp1252", col_sep: ";")
   w.count = rows.count
+
+  # asociate usage to its account number
+  usage_by_account_number = {}
+  Nomen::Accounts.all.each do |usage|
+    usage_by_account_number[Nomen::Accounts[usage].fr_pcga] = usage
+  end
+
   rows.each do |row|
-    # puts row.inspect
+    usage = usage_by_account_number[row[0]]
+    if usage.present?
+      account = Account.find_or_create_in_chart(usage)
+    end
     w.check_point
   end
 end
