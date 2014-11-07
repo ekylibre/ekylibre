5468a8756c55a74b489d53573c7f1230dee57628
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2014-11-07 18:10:32 +0100

    removes useless importer

--- lib/exchanges/exchangers/legrain/epicea.rb
+++ lib/exchanges/exchangers/legrain/epicea.rb
@@ -1,3 +1,2 @@
 require_relative 'epicea/accounts'
-require_relative 'epicea/general_ledger'
 require_relative 'epicea/journals'
--- lib/exchanges/exchangers/legrain/epicea/general_ledger.rb
+++
@@ -1,8 +0,0 @@
-Exchanges.add_importer :legrain_epicea_general_ledger do |file, w|
-  rows = CSV.read(file, headers: true, encoding: "cp1252", col_sep: ";", quote_char: "'")
-  w.count = rows.count
-
-  rows.each do |row|
-    w.check_point
-  end
-end
