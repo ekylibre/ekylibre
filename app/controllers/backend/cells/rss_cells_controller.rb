946ec3ade319493e9653ed948b6d0cb768d172da
coren <mloiseleur@ekylibre.com>
2014-11-07 13:06:27 +0100

    lazy load feedjira

--- app/controllers/backend/cells/rss_cells_controller.rb
+++ app/controllers/backend/cells/rss_cells_controller.rb
@@ -1,6 +1,7 @@
 class Backend::Cells::RssCellsController < Backend::CellsController
 
   def show
+    require 'feedjira' unless defined? Feedjira
     @feed = Feedjira::Feed.fetch_and_parse(params[:url])
   end
 
