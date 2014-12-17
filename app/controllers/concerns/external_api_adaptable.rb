a74cc2194ff1f9e3373da483fbd2f180ce3a4a74
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2014-12-17 14:55:17 +0100

    cleaning

--- app/controllers/concerns/external_api_adaptable.rb
+++ app/controllers/concerns/external_api_adaptable.rb
@@ -2,6 +2,8 @@
 # this module is intended to adapt easily Ekylibre
 # to external APIs just by telling the controller
 # how to translate API input/output to its Ekylibre equivalent
+# this controller actions builder assumes that the controller's name
+# is the API output name.
 
 module ExternalApiAdaptable
   extend ActiveSupport::Concern
@@ -14,14 +16,13 @@ module ExternalApiAdaptable
       actions -= [options[:except]].flatten if options[:except]
 
       name = self.controller_name
-      record_name = name.to_s.singularize
-      model = name.to_s.singularize.classify.constantize
-      records = model.name.underscore.pluralize
+      model = defaults[:model].present? ? defaults[:model].to_s.singularize.classify.constantize : name.to_s.singularize.classify.constantize
+      output_name = name
 
       methods =
         {
-          index:  lambda{@records = model.all; render template: "layouts/json/index"},
-          show:   lambda{@record = model.find(permitted_params[:id]); render template: "layouts/json/show"}
+          index:  lambda{@records = model.all; render template: 'layouts/json/index', locals:{output_name: output_name}},
+          show:   lambda{@record = model.find(permitted_params[:id]); render template: 'layouts/json/show', locals:{output_name: output_name.to_s.singularize}}
         }
 
       actions.each do |action|
