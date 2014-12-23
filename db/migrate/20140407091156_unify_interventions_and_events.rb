63832ab43472409bf8edfb96584d06d4a63a2770
Brice TEXIER <brice@ekylibre.com>
2014-12-23 14:03:29 +0100

    Restores migrations

---
+++ db/migrate/20140407091156_unify_interventions_and_events.rb
@@ -0,0 +1,31 @@
+class UnifyInterventionsAndEvents < ActiveRecord::Migration
+  def change
+    add_reference :analysis_items, :product_reading
+
+    # drop_table :product_links
+
+    add_reference :interventions, :event
+    add_column :interventions, :number, :string
+    add_column :interventions, :parameters, :text
+    execute "UPDATE interventions SET number = id"
+    add_reference :intervention_casts, :event_participation
+    add_reference :products, :person
+
+    add_column :events, :nature, :string
+    execute "UPDATE events SET nature = 'meeting'"
+    change_column_null :events, :nature, false
+    remove_column :events, :nature_id
+    drop_table :event_natures
+  end
+
+  # def down
+  #   create_table :event_natures do |t|
+  #     t.string   :name,                                   null: false
+  #     t.string   :usage,        limit: 60
+  #     t.boolean  :active,                  default: true, null: false
+  #     t.stamps
+  #     t.index    :name
+  #   end
+  # end
+
+end
---
+++ db/migrate/20140509084901_review_product_taxation.rb
@@ -0,0 +1,20 @@
+class ReviewProductTaxation < ActiveRecord::Migration
+  def change
+    create_table :product_nature_category_taxations do |t|
+      t.references :product_nature_category,    null: false
+      t.references :tax,                        null: false, index: true
+      t.string     :usage,                      null: false
+      t.stamps
+      t.index      :product_nature_category_id, name: 'index_product_nature_category_taxations_on_category_id'
+      t.index      :usage
+    end
+
+    execute "INSERT INTO product_nature_category_taxations (product_nature_category_id, tax_id, usage) SELECT product_nature_category_id, tax_id, 'sale' FROM product_nature_categories_sale_taxes"
+
+    execute "INSERT INTO product_nature_category_taxations (product_nature_category_id, tax_id, usage) SELECT product_nature_category_id, tax_id, 'purchase' FROM product_nature_categories_purchase_taxes"
+
+    drop_table :product_nature_categories_sale_taxes
+
+    drop_table :product_nature_categories_purchase_taxes
+  end
+end
