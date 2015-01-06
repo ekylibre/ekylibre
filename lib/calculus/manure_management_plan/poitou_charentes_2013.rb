6b7713d429860ae2787e69ab9cb4f9c961cece3c
ionosphere <ionosphere.consulting@gmail.com>
2015-01-06 22:18:47 +0100

    fixes #324

--- lib/calculus/manure_management_plan/poitou_charentes_2013.rb
+++ lib/calculus/manure_management_plan/poitou_charentes_2013.rb
@@ -162,8 +162,8 @@ module Calculus
           for support in campaign.production_supports.where(storage_id: @support.storage.id)
             # if an implantation intervention exist, get the plant output
             if previous_implantation_intervention = support.interventions.of_nature(:implantation).where(state: :done).order(:started_at).last
-              if previous_cultivation = previous_implantation_intervention.casts.of_generic_role(:output).actor
-                previous_variety = previous_cultivation.variety
+              if previous_cultivation = previous_implantation_intervention.casts.of_generic_role(:output).first.actor
+                previous_variety = Nomen::Varieties.find(previous_cultivation.variety)
                 previous_cultivation_dead_at = previous_cultivation.dead_at
                 break
               end
