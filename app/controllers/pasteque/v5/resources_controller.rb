a97d8341cc06246aeb95d1875e7fd7d2b1d5d239
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-09 07:58:39 +0100

    implements Pasteque ResourcesAPI

--- app/controllers/pasteque/v5/resources_controller.rb
+++ app/controllers/pasteque/v5/resources_controller.rb
@@ -1,3 +1,10 @@
 class Pasteque::V5::ResourcesController < Pasteque::V5::BaseController
-  manage_restfully only: [:index, :show]
+  #manage_restfully only: [:index, :show]
+  def index
+    render status: :not_found, json:{message: :not_found}
+  end
+
+  def show
+    render status: :not_found, json:{message: :not_found}
+  end
 end
