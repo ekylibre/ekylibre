5faa315be93099a3455e5861c498cf96acb5fa32
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-11 17:02:08 +0100

    normalizes Pasteque CurrenciesAPI

--- app/controllers/pasteque/v5/currencies_controller.rb
+++ app/controllers/pasteque/v5/currencies_controller.rb
@@ -3,21 +3,19 @@ class Pasteque::V5::CurrenciesController < Pasteque::V5::BaseController
   def index
     availables = SaleNature.pluck :currency
     @records = Nomen::Currencies.list.select{|currency| availables.include? currency.name}
-    render template: 'layouts/json/index', locals:{output_name: 'currencies', partial_path: 'currencies/currency', record: :currency}
+    render template: 'layouts/pasteque/v5/index', locals:{output_name: 'currencies', partial_path: 'currencies/currency', record: :currency}
   end
 
   def show
     if params[:id] == 'main'
-      @record = Preference[:currency]
+      @record = Nomen::Currencies[Preference[:currency]]
     else
       @record = Nomen::Currencies.find(params[:id])
-      #@record = Nomen::Currencies.all.keep_if{|currency| Nomen::Currencies[currency].number == params[:id]}.first
     end
-    render template: 'layouts/json/show', locals:{output_name: 'currency', partial_path: 'currencies/currency', record: :currency}
-  end
-
-  private
-  def permitted_params
-    params.require('currencies').permit('id')
+    if @record.present?
+      render partial: 'pasteque/v5/currencies/currency', locals:{currency: @record}
+    else
+      render status: :not_found, json: nil
+    end
   end
 end
