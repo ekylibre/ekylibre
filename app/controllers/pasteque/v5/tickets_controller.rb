e5284f26f6fa4fa7bf66bfb89469b3564913f02c
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-13 10:47:22 +0100

    begins to implement ticket#share and ticket#save

--- app/controllers/pasteque/v5/tickets_controller.rb
+++ app/controllers/pasteque/v5/tickets_controller.rb
@@ -1,3 +1,3 @@
 class Pasteque::V5::TicketsController < Pasteque::V5::BaseController
-  manage_restfully only: [:index, :show, :destroy], model: :affair, scope: :tickets
+  manage_restfully only: [:index, :show, :destroy, :update], model: :affair, scope: :tickets
 end
