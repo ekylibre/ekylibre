13451f2b32f32130d88da23a37cdbd2de6aa67af
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-13 14:14:55 +0100

    begins to implement tickets#search

--- app/controllers/pasteque/v5/tickets_controller.rb
+++ app/controllers/pasteque/v5/tickets_controller.rb
@@ -1,3 +1,15 @@
 class Pasteque::V5::TicketsController < Pasteque::V5::BaseController
   manage_restfully only: [:index, :show, :destroy, :update], model: :affair, scope: :tickets
+
+  def search
+    correspondence = {
+      ticketId: :id,
+      cashId: nil,
+      dateStart: :started_at,
+      dateStop: :stopped_at
+    }.with_indifferent_access
+    criterias = params.slice(correspondence.keys).map{|k,v|[correspondence[k], v]}.to_h
+    @records = model.where(criterias)
+    render template: "layouts/pasteque/v5/index", locals:{output_name: 'cash', partial_path: 'cashes/cash'}
+  end
 end
