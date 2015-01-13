629bb0132b9db1929373c970c649f6b7a3f7b062
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-13 15:10:34 +0100

    implements tickets#search

--- app/controllers/pasteque/v5/tickets_controller.rb
+++ app/controllers/pasteque/v5/tickets_controller.rb
@@ -4,12 +4,14 @@ class Pasteque::V5::TicketsController < Pasteque::V5::BaseController
   def search
     correspondence = {
       ticketId: :id,
-      cashId: nil,
-      dateStart: :started_at,
-      dateStop: :stopped_at
+      cashId: :cash_session_id,
+      dateStart: :created_at,
+      dateStop: :closed_on,
+      customerId: :third_id,
+      userId: :originator_id
     }.with_indifferent_access
     criterias = params.slice(correspondence.keys).map{|k,v|[correspondence[k], v]}.to_h
     @records = model.where(criterias)
-    render template: "layouts/pasteque/v5/index", locals:{output_name: 'cash', partial_path: 'cashes/cash'}
+    render template: "layouts/pasteque/v5/index", locals:{output_name: 'tickets', partial_path: 'tickets/ticket'}
   end
 end
