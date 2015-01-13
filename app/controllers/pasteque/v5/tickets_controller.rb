468a69051f9356e9f35c89d8e06a725e1e46a50b
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-13 15:55:19 +0100

    cleaning

--- app/controllers/pasteque/v5/tickets_controller.rb
+++ app/controllers/pasteque/v5/tickets_controller.rb
@@ -10,7 +10,7 @@ class Pasteque::V5::TicketsController < Pasteque::V5::BaseController
       customerId: :third_id,
       userId: :originator_id
     }.with_indifferent_access
-    criterias = params.slice(correspondence.keys).map{|k,v|[correspondence[k], v]}.to_h
+    criterias = params.slice(*correspondence.keys).map{|k,v|[correspondence[k], v]}.to_h
     @records = model.where(criterias)
     render template: "layouts/pasteque/v5/index", locals:{output_name: 'tickets', partial_path: 'tickets/ticket'}
   end
