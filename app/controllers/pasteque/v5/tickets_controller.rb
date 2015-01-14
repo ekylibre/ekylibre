f064f1e6822a8550055f1c2f829956d17d6def38
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-14 10:24:56 +0100

    keeps #search DRY

--- app/controllers/pasteque/v5/tickets_controller.rb
+++ app/controllers/pasteque/v5/tickets_controller.rb
@@ -1,19 +1,14 @@
 class Pasteque::V5::TicketsController < Pasteque::V5::BaseController
-  manage_restfully only: [:index, :show, :destroy, :update], model: :affair, scope: :tickets
-
-  def search
-    correspondence = {
+  search_filters = {
       ticketId: :id,
       cashId: :cash_session_id,
       dateStart: :created_at,
       dateStop: :closed_on,
       customerId: :third_id,
       userId: :originator_id
-    }.with_indifferent_access
-    criterias = params.slice(*correspondence.keys).map{|k,v|[correspondence[k], v]}.to_h
-    @records = model.where(criterias)
-    render template: "layouts/pasteque/v5/index", locals:{output_name: 'tickets', partial_path: 'tickets/ticket'}
-  end
+  }
+  manage_restfully only: [:index, :show, :destroy, :update, :search], model: :affair, scope: :tickets, search_filters: search_filters
+
   def open
     @records = model.tickets.open
     render template: "layouts/pasteque/v5/index", locals:{output_name: 'tickets', partial_path: 'tickets/ticket'}
