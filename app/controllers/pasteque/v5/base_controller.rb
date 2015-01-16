f228550feef277c1de9b6bee189b0590ee183f94
Sébastien Gauvrit <sgauvrit@ekylibre.com>
2015-01-16 16:42:24 +0100

    implements Pasteque way to manage errors.

--- app/controllers/pasteque/v5/base_controller.rb
+++ app/controllers/pasteque/v5/base_controller.rb
@@ -7,17 +7,17 @@ class Pasteque::V5::BaseController < ActionController::Base
     user, password = params[:user], params[:password]
 
     if user.blank? or password.blank?
-      render status: :bad_request, json: {message: "The request must contain the user email and password."}
+      render status: :bad_request, json: {status: :rej, content: ["blank user or password: #{params.inspect}"]}
       return
     end
 
     unless @user = User.find_by(email: user.downcase)
-      render status: :unauthorized, json: {message: "Invalid user or password."}
+      render status: :unauthorized, json: {status: :rej, content: ["no user found"]}
       return
     end
 
     unless @user.valid_password?(password)
-      render status: :unauthorized, json: {message: "Invalid user or password."}
+      render status: :unauthorized, json: {status: :rej, content: @user.errors.full_messages}
       return
     end
   end
