b3a80eaaf30b57958b480fe262ffad1ec709f27c
ionosphere <ionosphere.consulting@gmail.com>
2015-01-16 11:41:00 +0100

    Fix bug on loading entities and add email importing

--- lib/exchanges/exchangers/ekylibre/visuals.rb
+++ lib/exchanges/exchangers/ekylibre/visuals.rb
@@ -1,7 +1,8 @@
 # Create or updates entities
 Exchanges.add_importer :ekylibre_visuals do |file, w|
   w.count = 1
-
+  # load background
   Ekylibre::CorporateIdentity::Visual.set_default_background(file)
+
   w.check_point
 end
