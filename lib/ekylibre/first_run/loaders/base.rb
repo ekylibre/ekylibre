c842209efb7ec4d7dc4f3c6a94cb9fd1433b85c7
ionosphere <ionosphere.consulting@gmail.com>
2015-01-16 11:40:23 +0100

    re-load default logo of company

--- lib/ekylibre/first_run/loaders/base.rb
+++ lib/ekylibre/first_run/loaders/base.rb
@@ -10,4 +10,18 @@ Ekylibre::FirstRun.add_loader :base do |first_run|
     first_run.import(:ekylibre_visuals, path)
   end
 
+  # set extensions
+  extensions = ['jpeg','jpg','png']
+  # load default logo for of_company
+  if entity_of_company = Entity.of_company
+    for extension in extensions
+      picture_path = first_run.path("alamano", "logo.#{extension}")
+      f = (picture_path.exist? ? File.open(picture_path) : nil)
+      if f
+       entity_of_company.update!(picture: f)
+       f.close
+      end
+    end
+  end
+
 end
