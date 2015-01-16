b40bc66a4fbe8fb1662bde4eb4763f3e7f3f7260
ionosphere <ionosphere.consulting@gmail.com>
2015-01-16 11:41:00 +0100

    Fix bug on loading entities and add email importing

--- lib/exchanges/exchangers/ekylibre/entities.rb
+++ lib/exchanges/exchangers/ekylibre/entities.rb
@@ -9,27 +9,33 @@ Exchanges.add_importer :ekylibre_entities do |file, w|
       :first_name => row[0].blank? ? "" : row[0].to_s,
       :last_name => row[1].blank? ? "" : row[1].to_s,
       :nature => row[2].to_s.downcase,
-      :client_account_number => row[3].to_s,
-      :supplier_account_number => row[4].to_s,
-      :usages => row[5].to_s,
+      :client_account_number => row[3].blank? ? nil : row[3].to_s,
+      :supplier_account_number => row[4].blank? ? nil : row[4].to_s,
+      :usages => row[5].blank? ? nil : row[5].to_s,
       :address => row[6].to_s,
       :postal_code => row[7].blank? ? nil : row[7].to_s,
       :city => row[8].blank? ? nil : row[8].to_s,
       country: "fr",
       :phone_number => row[9].blank? ? nil : row[9].to_s,
       :link_nature => row[10].blank? ? :undefined : row[10].to_sym,
-      :code => row[11].blank? ? nil : row[11].to_s.downcase
+      :code => row[11].blank? ? nil : row[11].to_s.downcase,
+      :email => row[12].blank? ? nil : row[12].to_s
     }.to_struct
 
     klass = r.nature.camelcase.constantize
     unless person = klass.where("first_name ILIKE ? AND last_name ILIKE ?", r.first_name, r.last_name).first
       person = klass.new(first_name: r.first_name, last_name: r.last_name, nature: r.nature)
     end
-    person.client = true
-    person.client_account = Account.get(r.client_account_number, name: person.full_name)
-    person.supplier = true
-    person.supplier_account = Account.get(r.supplier_account_number, name: person.full_name)
-    person.save!
+    if r.client_account_number
+      person.client = true
+      person.client_account = Account.get(r.client_account_number, name: person.full_name)
+      person.save!
+    end
+    if r.supplier_account_number
+      person.supplier = true
+      person.supplier_account = Account.get(r.supplier_account_number, name: person.full_name)
+      person.save!
+    end
 
     # Add mail address if given
     line_6 = r.postal_code + " " + r.city
@@ -42,9 +48,14 @@ Exchanges.add_importer :ekylibre_entities do |file, w|
       person.phones.create!(coordinate: r.phone_number)
     end
 
+    # Add email number if given
+    unless r.email.blank?
+      person.emails.create!(coordinate: r.email)
+    end
+
     # Update account name
     if r.link_nature
-      if r.link_nature.to_s.downcase == "management"
+      if r.link_nature.to_s.downcase == "management" and r.client_account_number and r.usages
         associate_account = Account.find_by(number: r.client_account_number)
         associate_account.name = person.full_name
         associate_account.usages = r.usages
@@ -52,6 +63,7 @@ Exchanges.add_importer :ekylibre_entities do |file, w|
       end
       person.is_linked_to!(Entity.of_company, as: r.link_nature)
     end
+
     w.check_point
   end
 
