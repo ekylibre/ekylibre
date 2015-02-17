7606a5aa0ce01b0492748d971c625babf0dd6244
Brice TEXIER <brice@ekylibre.com>
2015-02-17 15:19:48 +0100

    Updates Quandl cell to be more precise and add optional threshold bar

--- app/controllers/backend/cells/quandl_cells_controller.rb
+++ app/controllers/backend/cells/quandl_cells_controller.rb
@@ -1,7 +1,13 @@
 class Backend::Cells::QuandlCellsController < Backend::Cells::BaseController
 
   def show
-    data = JSON.load(open("https://www.quandl.com/api/v1/datasets/CHRIS/LIFFE_EBM4.json"))
+    dataset = "CHRIS/LIFFE_EBM4"
+    start = Date.today << 12
+    finish = start >> 12 - 1
+    params[:threshold] = 183.50
+    url = "https://www.quandl.com/api/v1/datasets/#{dataset}.json?trim_start=#{start}&trim_end=#{finish}"
+    url = "https://www.quandl.com/api/v1/datasets/#{dataset}.json"
+    data = JSON.load(open(url))
     if data["errors"].any?
       # TODO Prevent ?
     else
