473d10dbb91bbcc12aa2f9fc4654af02b99d54f2
Brice TEXIER <brice@ekylibre.com>
2015-03-14 16:45:53 +0100

    Adds timeout on weather cell HTTP request. Fixes #367.

--- app/controllers/backend/cells/weather_cells_controller.rb
+++ app/controllers/backend/cells/weather_cells_controller.rb
@@ -1,36 +1,40 @@
-# -*- coding: utf-8 -*-
 class Backend::Cells::WeatherCellsController < Backend::Cells::BaseController
 
   def show
+    @forecast = nil
     if zone = (params[:id] ? CultivableZone.find_by(id: params[:id]) : CultivableZone.first)
       if reading = zone.reading(:shape)
         coordinates = Charta::Geometry.new(reading.geometry_value).centroid
-        begin
-          @forecast = JSON.load(open("http://api.openweathermap.org/data/2.5/forecast/daily?lat=#{coordinates.first}&lon=#{coordinates.second}&cnt=14&mode=json")).deep_symbolize_keys
-          @forecast[:list] = @forecast[:list].collect do |day|
-            day.deep_symbolize_keys!
-            {
-              at: Time.at(day[:dt]),
-              temperatures: [:day, :night, :min, :max, :eve, :morn].inject({}) do |hash, key|
-                hash[key] = (day[:temp][key] || 0).in_kelvin
-                hash
-              end,
-              pressure: day[:pressure].in_hectopascal,
-              humidity: (day[:humidity] || 0).in_percent,
-              wind_speed: (day[:speed] || 0).in_meter_per_second,
-              wind_direction: (day[:deg] || 0).in_degree,
-              rain: (day[:rain] || 0).in_millimeter,
-              clouds: (day[:rain] || 0).in_percent,
-              # weather: day[:weather]
-            }
-          end
+        http = Net::HTTP.new("api.openweathermap.org")
+        http.open_timeout = 3
+        http.read_timeout = 3
+        res = http.get("/data/2.5/forecast/daily?lat=#{coordinates.first}&lon=#{coordinates.second}&cnt=14&mode=json")
+        @forecast = JSON.load(res.body).deep_symbolize_keys
+        @forecast[:list] = @forecast[:list].collect do |day|
+          day.deep_symbolize_keys!
+          {
+            at: Time.at(day[:dt]),
+            temperatures: [:day, :night, :min, :max, :eve, :morn].inject({}) do |hash, key|
+              hash[key] = (day[:temp][key] || 0).in_kelvin
+              hash
+            end,
+            pressure: day[:pressure].in_hectopascal,
+            humidity: (day[:humidity] || 0).in_percent,
+            wind_speed: (day[:speed] || 0).in_meter_per_second,
+            wind_direction: (day[:deg] || 0).in_degree,
+            rain: (day[:rain] || 0).in_millimeter,
+            clouds: (day[:rain] || 0).in_percent,
+            # weather: day[:weather]
+          }
         end
       end
     end
-  rescue Exception => e
-    # Nothing
+  rescue Net::OpenTimeout => e
     @forecast = nil
-    logger.warn e.message
+    logger.warn "Net::OpenTimeout: Cannot open service OpenWeatherMap in time (#{e.message})"
+  rescue Net::ReadTimeout => e
+    @forecast = nil
+    logger.warn "Net::ReadTimeout: Cannot read service OpenWeatherMap in time (#{e.message})"
   end
 
 end
