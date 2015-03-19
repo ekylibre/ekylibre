edb70b95f2c6fdecd4f72844c09fb392562ca01b
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-03-19 17:05:40 +0100

    Fix token value in quandl cell

--- app/controllers/backend/cells/quandl_cells_controller.rb
+++ app/controllers/backend/cells/quandl_cells_controller.rb
@@ -5,7 +5,7 @@ class Backend::Cells::QuandlCellsController < Backend::Cells::BaseController
     finish = start >> 12 - 1
     #params[:threshold] = 183.50
     dataset = params[:dataset] || "CHRIS/LIFFE_EBM4"
-    token = Identifier.where(nature: :quandl_token).first || "BwQESxTYjPRj58EbvzQA"
+    token = Identifier.where(nature: :quandl_token).first.value || "BwQESxTYjPRj58EbvzQA"
     url = "https://www.quandl.com/api/v1/datasets/#{dataset}.json?auth_token=#{token}&trim_start=#{start}&trim_end=#{finish}"
     url = "https://www.quandl.com/api/v1/datasets/#{dataset}.json?auth_token=#{token}"
     data = JSON.load(open(url))
