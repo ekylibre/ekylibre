eb987d42d1b40619feb822baf6d9c82aba61df4c
Brice TEXIER <brice@ekylibre.com>
2015-03-23 18:01:28 +0100

    Fixes quantifiers tests

--- test/controllers/backend/product_nature_variants_controller_test.rb
+++ test/controllers/backend/product_nature_variants_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Backend::ProductNatureVariantsControllerTest < ActionController::TestCase
 
-  test_restfully_all_actions last_purchase_item: :show
+  test_restfully_all_actions last_purchase_item: :show, quantifiers: {format: :json, mode: :show}
 
 end
 
