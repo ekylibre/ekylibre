f6e871c8531bf3d4d6a4ec72c981bf05f26d92ac
Brice TEXIER <brice@ekylibre.com>
2015-03-23 18:07:54 +0100

    Fixes productions controller test

--- test/controllers/backend/productions_controller_test.rb
+++ test/controllers/backend/productions_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class Backend::ProductionsControllerTest < ActionController::TestCase
 
-  test_restfully_all_actions
+  test_restfully_all_actions new: {activity_id: identify(:activities_001), campaign_id: identify(:campaigns_001)},
+                             create: {production: {activity_id: identify(:activities_001), campaign_id: identify(:campaigns_001)}}
 
 end
 
