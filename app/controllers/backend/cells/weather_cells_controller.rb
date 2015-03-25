1a8a51d46b8a0b4f237f1ff8487640dba0bff932
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-03-25 11:53:15 +0100

    Fixed json parsing issues on malformed json. Fixes #378.

--- app/controllers/backend/cells/weather_cells_controller.rb
+++ app/controllers/backend/cells/weather_cells_controller.rb
@@ -9,24 +9,36 @@ class Backend::Cells::WeatherCellsController < Backend::Cells::BaseController
         http.open_timeout = 3
         http.read_timeout = 3
         res = http.get("/data/2.5/forecast/daily?lat=#{coordinates.first}&lon=#{coordinates.second}&cnt=14&mode=json")
-        @forecast = JSON.load(res.body).deep_symbolize_keys
-        @forecast[:list] = @forecast[:list].collect do |day|
-          day.deep_symbolize_keys!
-          {
-            at: Time.at(day[:dt]),
-            temperatures: [:day, :night, :min, :max, :eve, :morn].inject({}) do |hash, key|
-              hash[key] = (day[:temp][key] || 0).in_kelvin
-              hash
-            end,
-            pressure: day[:pressure].in_hectopascal,
-            humidity: (day[:humidity] || 0).in_percent,
-            wind_speed: (day[:speed] || 0).in_meter_per_second,
-            wind_direction: (day[:deg] || 0).in_degree,
-            rain: (day[:rain] || 0).in_millimeter,
-            clouds: (day[:rain] || 0).in_percent,
-            # weather: day[:weather]
-          }
+
+        json = JSON.load(res.body) rescue nil
+        unless json.nil?
+
+          @forecast = json.deep_symbolize_keys
+
+          if @forecast[:cod] == '200'
+            @forecast[:list] = @forecast[:list].collect do |day|
+              day.deep_symbolize_keys!
+              {
+                  at: Time.at(day[:dt]),
+                  temperatures: [:day, :night, :min, :max, :eve, :morn].inject({}) do |hash, key|
+                    hash[key] = (day[:temp][key] || 0).in_kelvin
+                    hash
+                  end,
+                  pressure: day[:pressure].in_hectopascal,
+                  humidity: (day[:humidity] || 0).in_percent,
+                  wind_speed: (day[:speed] || 0).in_meter_per_second,
+                  wind_direction: (day[:deg] || 0).in_degree,
+                  rain: (day[:rain] || 0).in_millimeter,
+                  clouds: (day[:rain] || 0).in_percent,
+                  # weather: day[:weather]
+              }
+            end
+          else
+            @forecast = nil
+          end
+
         end
+
       end
     end
   rescue Net::OpenTimeout => e
