7984b3c94ddfa2d4f39d8769d03f7065a1200272
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-03-27 09:46:53 +0100

    Catching exception when dashboards are editing twice at the same time. Fixes #390.

--- app/controllers/backend/beehives_controller.rb
+++ app/controllers/backend/beehives_controller.rb
@@ -32,8 +32,12 @@ class Backend::BeehivesController < Backend::BaseController
       end.compact
       cells.any? ? { cells: cells } : nil
     end.compact
-    current_user.prefer!("beehive.#{params[:id]}", {version: Backend::BeehiveHelper::FORMAT_VERSION, boxes: boxes}.deep_stringify_keys.to_yaml)
-    head :ok
+    begin
+      current_user.prefer!("beehive.#{params[:id]}", {version: Backend::BeehiveHelper::FORMAT_VERSION, boxes: boxes}.deep_stringify_keys.to_yaml)
+      head :ok
+    rescue ActiveRecord::StaleObjectError
+      head :locked
+    end
   end
 
 
