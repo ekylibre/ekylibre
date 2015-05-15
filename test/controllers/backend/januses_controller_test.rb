961b6d1abad64a1534c076a65e7150a8195dc8b0
Brice TEXIER <brice@ekylibre.com>
2015-05-15 15:19:02 +0200

    Fixes januses#toggle test

--- test/controllers/backend/januses_controller_test.rb
+++ test/controllers/backend/januses_controller_test.rb
@@ -1,4 +1,4 @@
 require 'test_helper'
 class Backend::JanusesControllerTest < ActionController::TestCase
-  test_restfully_all_actions
+  test_restfully_all_actions toggle: {mode: :soft_touch, params: {id: "backend/accounts"}}
 end
