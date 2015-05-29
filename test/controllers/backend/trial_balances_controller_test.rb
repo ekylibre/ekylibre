466657183278a48384cfed21c9516b744d3b1ca0
Brice TEXIER <brice@ekylibre.com>
2015-05-29 15:32:26 +0200

    Fixes TrialBalances test

--- test/controllers/backend/trial_balances_controller_test.rb
+++ test/controllers/backend/trial_balances_controller_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 class Backend::TrialBalancesControllerTest < ActionController::TestCase
-  test_restfully_all_actions
+  test_restfully_all_actions show: :index
+
+  test "with period" do
+    get :show, period: "2007-01-01_2015-12-31", states: [:draft, :confirmed, :closed]
+  end
+  
 end
