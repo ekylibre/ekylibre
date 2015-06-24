70362e38f1e65dca35cd66dd16c7a6c47e05c5c5
ionosphere <ionosphere.consulting@gmail.com>
2015-06-24 20:33:37 +0200

    Replace fake sales and purchases by more real one

--- lib/ekylibre/first_run/loaders/demo.rb
+++ lib/ekylibre/first_run/loaders/demo.rb
@@ -2,8 +2,9 @@
 Ekylibre::FirstRun.add_loader :demo do |first_run|
 
   if Preference.get!(:demo, false, :boolean).value
-
-    Ekylibre::FirstRun::Faker::Sales.run(max: first_run.max)
+    
+    # replace by sales.csv / purchases.csv more real.
+    # Ekylibre::FirstRun::Faker::Sales.run(max: first_run.max)
 
     # replace by interventions.csv more real.
     # Ekylibre::FirstRun::Faker::Interventions.run(max: first_run.max)
