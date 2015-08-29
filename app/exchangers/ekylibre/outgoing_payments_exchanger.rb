216bc11e3082f386197558a6413b3320e64c9bb8
ionosphere <ionosphere.consulting@gmail.com>
2015-08-29 10:38:18 +0200

    Add check to outgoing payment exchanger

--- app/exchangers/ekylibre/outgoing_payments_exchanger.rb
+++ app/exchangers/ekylibre/outgoing_payments_exchanger.rb
@@ -1,4 +1,74 @@
 class Ekylibre::OutgoingPaymentsExchanger < ActiveExchanger::Base
+  
+  def check
+    rows = CSV.read(file, headers: true).delete_if { |r| r[2].blank? }
+    valid = true
+    now = Time.now
+    w.count = rows.size
+    
+    # find a responsible
+    unless responsible = User.employees.first
+      w.error "No responsible found"
+      valid = false
+    end
+    
+    rows.each_with_index do |row, index|
+      line_number = index + 2
+      prompt = "L#{line_number.to_s.yellow}"
+      r = {
+        invoiced_at:        (row[0].blank? ? nil : Date.parse(row[0].to_s)),
+        payee_full_name:    (row[1].blank? ? nil : row[1]),
+        reference_number:   (row[2].blank? ? nil : row[2].upcase),
+        outgoing_payment_mode_name: (row[3].blank? ? nil : row[3].to_s),
+        amount: (row[4].blank? ? nil : row[4].tr(',', '.').to_d),
+        paid_on: (row[5].blank? ? nil : Date.parse(row[5].to_s)),
+        # Extra infos
+        document_reference_number: "#{Date.parse(row[0].to_s)}_#{row[1]}_#{row[2].upcase}".tr(' ', '-'),
+        description: now.l
+      }.to_struct
+      
+      # Check date
+      paid_at = nil
+      if r.paid_on
+        paid_at = r.paid_on.to_datetime
+        w.info " Date : #{r.paid_on} "
+      elsif r.invoiced_at
+        paid_at = r.invoiced_at
+        w.info " Date : #{r.invoiced_at} "
+      else
+        w.warn "No date given"
+        valid = false
+      end
+      
+      # Check outgoing payment mode
+      unless payment_mode = OutgoingPaymentMode.where(name: r.outgoing_payment_mode_name).first
+        w.error ActiveExchanger::InvalidDataError, "Cannot find outgoing payment mode #{r.outgoing_payment_mode_name} at line #{line_index.to_s.yellow}"
+        valid = false
+      end
+      
+      # Check an entity presence
+      unless entity = Entity.where('full_name ILIKE ?', r.payee_full_name).first || Entity.where('last_name ILIKE ?', r.payee_full_name).first
+        w.info " Entity will be created with #{r.payee_full_name} "
+      end
+      
+      # Check Outgoing payment presence
+      if outgoing_payment = OutgoingPayment.where(payee: entity, paid_at: paid_at, mode: payment_mode, amount: r.amount).first
+        w.info " Outgoing payment is already present with ID : #{outgoing_payment.id} "
+      end
+      
+      # Check affair presence
+      if r.reference_number && entity
+        # see if purchase exist anyway
+        if purchase = Purchase.where(supplier_id: entity.id, invoiced_at: r.invoiced_at, reference_number: r.reference_number).first
+          w.info "Purchase found with ID : #{purchase.id}"
+        else
+          w.warn "No purchase found. Outgoing payment will be in stand-by"
+        end
+      end
+      
+    end
+  end
+  
   def import
     rows = CSV.read(file, headers: true).delete_if { |r| r[2].blank? }
     w.count = rows.size
@@ -22,6 +92,7 @@ class Ekylibre::OutgoingPaymentsExchanger < ActiveExchanger::Base
       }.to_struct
 
       # set paid_at
+      paid_at = nil
       if r.paid_on
         paid_at = r.paid_on.to_datetime
       elsif r.invoiced_at
@@ -49,6 +120,7 @@ class Ekylibre::OutgoingPaymentsExchanger < ActiveExchanger::Base
                                                      payee: entity,
                                                      responsible: responsible
                                                     )
+          w.info "Outgoing payment was created with #{outgoing_payment.id}"
         end
       end
 
