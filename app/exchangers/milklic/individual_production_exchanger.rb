79c0bb9e47c8f0359631bc48f24c7a297cfc2816
ionosphere <ionosphere.consulting@gmail.com>
2015-09-06 14:33:11 +0200

    Add milklic exchanger

---
+++ app/exchangers/milklic/individual_production_exchanger.rb
@@ -0,0 +1,38 @@
+class Milklic::IndividualProductionExchanger < ActiveExchanger::Base
+  def import
+    analyser_attributes = YAML.load_file(File.join(File.dirname(__FILE__), 'entity.yml'))
+
+    unless analyser = Entity.find_by(siren: analyser_attributes[:siren])
+      analyser = Entity.create!(analyser_attributes)
+    end
+
+    begin
+      rows = CSV.read(file, encoding: 'CP1252', col_sep: ";", headers: true)
+    rescue
+      raise NotWellFormedFileError
+    end
+    w.count = rows.size
+
+    rows.each_with_index do |row, index|
+
+        r = OpenStruct.new(animal_name: row[0],
+                           animal_work_number: row[1],
+                           animal_lactation_number: row[2],
+                           animal_lactation_started_on: row[3]
+                          )
+
+        # if an animal exist
+        if animal = Animal.find_by_work_number(r.animal_work_number)
+          for i in 4..15
+            if row[i] and row.headers[i]
+              milk_daily_production_measure = (row[i].tr(',', '.').to_d).in_kilogram_per_day
+              milk_daily_production_at =  Date.strptime(row.headers[i], '%d/%m/%y').to_time
+              animal.read!(:milk_daily_production, milk_daily_production_measure, at: milk_daily_production_at, force: true) if milk_daily_production_measure && milk_daily_production_at
+            end
+          end
+        end
+      w.check_point
+    end
+  end
+
+end
--- lib/ekylibre/first_run/loaders/analyses.rb
+++ lib/ekylibre/first_run/loaders/analyses.rb
@@ -8,4 +8,7 @@ Ekylibre::FirstRun.add_loader :analyses do |first_run|
   first_run.import_file(:fiea_galactea, 'galactea3/cl_all.csv')
 
   first_run.import_file(:bovins_croissance_cattle_performance_controls, 'bovins_croissance/perf.csv')
+
+  first_run.import_file(:milklic_individual_production, 'milklic/lait_individuel.csv')
+
 end
