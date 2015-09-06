bd1a665bcbfdcf54e5dee488498801b6386bf371
Brice TEXIER <brice@ekylibre.com>
2015-09-06 14:41:34 +0200

    Fixes invalid role assignment in 20150904144552_simplify_product_junctions migration

--- db/migrate/20150904144552_simplify_product_junctions.rb
+++ db/migrate/20150904144552_simplify_product_junctions.rb
@@ -31,6 +31,25 @@ class SimplifyProductJunctions < ActiveRecord::Migration
     [:mixing, :fifth_producer, :mixed]
   ]
 
+  POLYMORPHIC_REFERENCES = [
+    [:attachments, :resource],
+    [:issues, :target],
+    [:journal_entries, :resource],
+    [:observations, :subject],
+    [:preferences, :record_value],
+    [:product_enjoyments, :originator],
+    [:product_junctions, :originator],
+    [:product_linkages, :originator],
+    [:product_links, :originator],
+    [:product_localizations, :originator],
+    [:product_memberships, :originator],
+    [:product_ownerships, :originator],
+    [:product_phases, :originator],
+    [:product_reading_tasks, :originator],
+    [:product_readings, :originator],
+    [:versions, :item]
+  ]
+
   def change
     rename_column :product_junction_ways, :road_id, :product_id
     rename_column :product_junctions, :type, :nature
@@ -38,12 +57,15 @@ class SimplifyProductJunctions < ActiveRecord::Migration
       dir.up do
         execute 'UPDATE product_junctions SET nature = CASE ' + NATURES.map { |class_name, nature| "WHEN nature = '#{class_name}' THEN '#{nature}'" }.join + " ELSE 'junction' END"
         WAYS.each do |jn, on, nn|
-          execute "UPDATE product_junction_ways SET nature = '#{nn}' FROM product_junctions AS pj WHERE pj.id = junction_id AND pj.nature = '#{jn}' AND product_junction_ways.nature = '#{on}'"
+          execute "UPDATE product_junction_ways SET role = '#{nn}' FROM product_junctions AS pj WHERE pj.id = junction_id AND pj.nature = '#{jn}' AND product_junction_ways.role = '#{on}'"
+        end
+        POLYMORPHIC_REFERENCES.each do |table, reference|
+          execute "UPDATE #{table} SET #{reference}_type = 'ProductJunction' WHERE #{reference}_type IN (" + NATURES.keys.map{ |n| "'#{n}'" }.join(', ') + ")"
         end
       end
       dir.down do
         WAYS.each do |jn, nn, on|
-          execute "UPDATE product_junction_ways SET nature = '#{nn}' FROM product_junctions AS pj WHERE pj.id = junction_id AND pj.nature = '#{jn}' AND product_junction_ways.nature = '#{on}'"
+          execute "UPDATE product_junction_ways SET role = '#{nn}' FROM product_junctions AS pj WHERE pj.id = junction_id AND pj.nature = '#{jn}' AND product_junction_ways.role = '#{on}'"
         end
         execute 'UPDATE product_junctions SET nature = CASE ' + NATURES.map { |class_name, nature| "WHEN nature = '#{nature}' THEN '#{class_name}'" }.join + " ELSE 'junction' END"
       end
