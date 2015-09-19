58c34739b43414cb24d988fb606a58cbcf2a03b4
Brice TEXIER <brice@ekylibre.com>
2015-09-19 09:00:19 +0200

    Set sensor reading as hourly frequence

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -3,4 +3,4 @@ require 'jobs/all_sensors_reading_job'
 # Clean cron jobs
 Sidekiq::Cron::Job.destroy_all!
 
-Sidekiq::Cron::Job.create(name: 'Sensor reading - every hour', cron: '* */1 * * *', klass: 'AllSensorsReadingJob')
+Sidekiq::Cron::Job.create(name: 'Sensor reading - every hour', cron: '5 * * * *', klass: 'AllSensorsReadingJob')
