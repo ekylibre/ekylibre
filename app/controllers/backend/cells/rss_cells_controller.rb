54283a69aab995e788e6beeae5c525345dd927be
Brice TEXIER <brice@ekylibre.com>
2015-11-03 10:52:17 +0100

    Ensures that a comprehensive message is shown when RSS feed is unreachable/broken. Fixes #470.

--- app/controllers/backend/cells/rss_cells_controller.rb
+++ app/controllers/backend/cells/rss_cells_controller.rb
@@ -3,5 +3,7 @@ require 'feedjira'
 class Backend::Cells::RssCellsController < Backend::Cells::BaseController
   def show
     @feed = Feedjira::Feed.fetch_and_parse(params[:url]) if params[:url]
+  rescue => e
+    @error = :informations_feed_is_unreachable.tn(url: params[:url], message: e.message)
   end
 end
