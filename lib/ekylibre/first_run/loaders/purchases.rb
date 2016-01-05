8aad42e2dde9eee50c738841e59f17938a2e00ac
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-01-05 17:13:48 +0100

    Add notice if exchanger doesn't exist. Remove unused exchangers: storages, cap_land_parcels*, original_purchase. Removed too long backup exchanger, simplify legrain_epicea_journals. Fixed edi exchanger. Add import files for activities, bank_statements, interventions, purchases, workers, zones, and galactea.

--- lib/ekylibre/first_run/loaders/purchases.rb
+++ lib/ekylibre/first_run/loaders/purchases.rb
@@ -5,8 +5,4 @@ Ekylibre::FirstRun.add_loader :purchases do |first_run|
 
   # Import outgoing_payments link to purchase
   first_run.import_file(:ekylibre_outgoing_payments, 'alamano/outgoing_payments.csv')
-
-  # Import original purchase files
-  # TODO: compress all files inside purchases.zip
-  first_run.import_file(:ekylibre_original_purchase_files, 'alamano/documents/purchases.zip')
 end
--- test/lib/active_exchanger/base_test.rb
+++ test/lib/active_exchanger/base_test.rb
@@ -5,8 +5,8 @@ class ActiveExchanger::BaseTest < ActiveSupport::TestCase
 
   # TODO: fix use of manifest.yml which used for fixtures...
   IMPORTS = {
-    ekylibre_settings: FIRST_RUN_V1.join('manifest.yml'),
-    ekylibre_visuals: fixture_files_path.join('sample_image.png')
+      ekylibre_settings: FIRST_RUN_V1.join('manifest.yml'),
+      ekylibre_visuals: fixture_files_path.join('sample_image.png')
   }
 
   setup do
@@ -36,16 +36,20 @@ class ActiveExchanger::BaseTest < ActiveSupport::TestCase
   end
 
   Nomen::ExchangeNature.find_each do |item|
-    path = fixture_files_path.join('imports', "#{item.name}.*")
-    list = Dir.glob(path)
-    if list.any?
-      list.each do |file|
-        test "#{item.name} import with #{File.basename(file)}" do
-          ActiveExchanger::Base.import(item.name, file)
-        end
-      end
+    if not ActiveExchanger::Base.importers.include?(item.name.to_sym)
+      puts "Cannot find exchanger with ExchangeNature #{item.name.to_s.yellow}"
     else
-      puts "Cannot test exchanger #{item.name.to_s.yellow} import. No file given."
+      path = fixture_files_path.join('imports', "#{item.name}.*")
+      list = Dir.glob(path)
+      if list.any?
+        list.each do |file|
+          test "#{item.name} import with #{File.basename(file)}" do
+            ActiveExchanger::Base.import(item.name, file)
+          end
+        end
+      else
+        puts "Cannot test exchanger #{item.name.to_s.yellow} import. No file given."
+      end
     end
   end
 end
