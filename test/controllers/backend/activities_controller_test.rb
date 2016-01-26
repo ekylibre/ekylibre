e14b586e5aa102ce631711194df3d9e37070a950
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-01-26 12:03:31 +0100

    Update activities controller test show

--- test/controllers/backend/activities_controller_test.rb
+++ test/controllers/backend/activities_controller_test.rb
@@ -2,6 +2,21 @@ require 'test_helper'
 
 module Backend
   class ActivitiesControllerTest < ActionController::TestCase
-    test_restfully_all_actions family: { mode: :index, name: :mussel_farming, format: :json }
+    test_restfully_all_actions family: { mode: :index, name: :mussel_farming, format: :json }, except: :show
+
+    test 'show action' do
+      get :show, {:id=>"NaID", :redirect=>root_url, :locale=>@locale}
+      assert_redirected_to root_url
+      Activity.limit(5).find_each do |record|
+        get :show, {:id=>record.id, :locale=>@locale}
+        if record.vegetal_crops?
+          assert_redirected_to backend_vegetal_activity_url(record)
+        else
+          assert_response :success
+          assert_not_nil assigns(:activity)
+        end
+      end
+    end
+
   end
 end
