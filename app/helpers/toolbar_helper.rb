a07afbc8f68107ea78c5b08a2ebd60cc55204556
Brice TEXIER <brice@ekylibre.com>
2016-01-28 12:14:08 +0100

    Removes default button group in toolbar

---
+++ app/helpers/toolbar_helper.rb
@@ -0,0 +1,108 @@
+module ToolbarHelper
+  # This class permit to register the composition of a toolbar
+  class Toolbar
+    def initialize(template)
+      @template = template
+    end
+
+    def authorized?(*args)
+      @template.authorized?(*args)
+    end
+
+    def group(options = {}, &_block)
+      fail 'Nested group are forbidden' unless @group.nil?
+      options[:class] = options[:class].to_s + ' btn-group'
+      @template.content_tag(:div, options) do
+        yield(self)
+      end
+    end
+
+    def tool(name, url, options = {})
+      @template.tool_to(name, url, options) if authorized?(url)
+    end
+
+    def mail_to(email_address, name = nil, html_options = {}, &block)
+      if html_options[:class]
+        html_options[:class] << ' btn btn-default icn btn-mail'
+      else
+        html_options[:class] = ' btn btn-default icn btn-mail'
+      end
+      @template.mail_to(email_address, name, html_options, &block)
+    end
+
+    def export(*natures)
+      options = natures.extract_options!
+      record = options[:resource] || resource
+      options[:key] ||= :number
+      key = (options[:key].is_a?(Symbol) ? record.send(options[:key]) : options[:key]).to_s
+      @template.dropdown_menu_button(:print) do |menu|
+        natures.each do |nature_name|
+          nature = Nomen::DocumentNature.find(nature_name)
+          modal_id = nature.name.to_s + '-exporting'
+          if Document.of(nature.name, key).any?
+            @template.content_for :popover, @template.render('backend/shared/export', nature: nature, key: key, modal_id: modal_id)
+            menu.item nature.human_name, '#' + modal_id, data: { toggle: 'modal' }
+          else
+            DocumentTemplate.of_nature(nature.name).each do |template|
+              menu.item(template.name, params.merge(format: :pdf, template: template.id, key: key))
+            end
+          end
+        end
+      end
+    end
+
+    def menu(name, options = {}, &block)
+      @template.dropdown_menu_button(name, options, &block)
+    end
+
+    def destroy(options = {})
+      if @template.resource.destroyable?
+        tool(options[:label] || :destroy.ta, { action: :destroy, id: @template.resource.id, redirect: options[:redirect] }, method: :delete, data: { confirm: :are_you_sure_you_want_to_delete.tl })
+      end
+    end
+
+    def action(name, record = nil, options = {})
+      url = {}
+      url.update(options.delete(:params)) if options[:params].is_a? Hash
+      url[:controller] ||= @template.controller_path
+      url[:action] ||= name
+      url[:id] = record.id if record && record.class < ActiveRecord::Base
+      action_label = options[:label] || I18n.t(name, scope: 'rest.actions')
+      if options[:variants]
+        variants = options.delete(:variants)
+        # variants ||= { action_label => url } if authorized?(url)
+        # variants ||= {}
+        menu(action_label) do |menu|
+          variants.each do |name, url_options, link_options|
+            variant_url = url.merge(url_options)
+            if authorized?(variant_url)
+              menu.item(name, variant_url, options.slice(:method, 'data-confirm').merge(link_options || {}))
+            end
+          end
+        end
+      else
+        tool(action_label, url, options)
+      end
+    end
+
+    def method_missing(method_name, *args)
+      fail ArgumentError, 'Block can not be accepted' if block_given?
+      options = args.extract_options!
+      name = method_name.to_s.gsub(/\_+$/, '').to_sym
+      record = args.shift
+      action(name, record, options)
+    end
+  end
+
+  # Build a tool bar composed of tool groups composed of tool
+  def toolbar(options = {}, &block)
+    return nil unless block_given?
+
+    html = capture(Toolbar.new(self), &block)
+
+    unless options[:wrap].is_a?(FalseClass)
+      html = content_tag(:div, html, class: 'toolbar' + (options[:class] ? ' ' << options[:class].to_s : ''))
+    end
+    html
+  end
+end
---
+++ test/helpers/toolbar_helper_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class ToolbarHelperTest < ActionView::TestCase
+  # Add tests here...
+end
