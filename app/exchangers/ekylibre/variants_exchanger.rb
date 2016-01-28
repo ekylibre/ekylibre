aab0fa957d027657f0202ac6264d42d5c3a2b7ea
Brice TEXIER <brice@ekylibre.com>
2016-01-28 16:54:46 +0100

    Permits to ekylibre/variants exchanger to import from product nature reference

--- app/exchangers/ekylibre/variants_exchanger.rb
+++ app/exchangers/ekylibre/variants_exchanger.rb
@@ -1,46 +1,59 @@
 # FIXME: Not absolute name. Rename to ProductNatureVariantsExchanger (don't forget nomenclature)
-class Ekylibre::VariantsExchanger < ActiveExchanger::Base
-  # Create or updates variants
-  def import
-    currency = Preference[:currency] || 'EUR'
+module Ekylibre
 
-    s = Roo::OpenOffice.new(file)
-    w.count = s.sheets.count
+  # Expected file is a OpenDocument spreadsheet.
+  # Columns are
+  # A: Variant name in DB
+  # B: reference_name (from ProductNatureVariant or ProductNature if not found)
+  # C: Variant code in DB
+  # D: Variety CF NOMENCLATURE
+  # E: Derivative CF NOMENCLATURE
+  # F: Purchase pretax amount price
+  # G: Sale pretax amount price
+  # H: Price unity
+  # I: Indicators - HASH
+  class VariantsExchanger < ActiveExchanger::Base
+    # Create or updates variants
+    def import
+      currency = Preference[:currency] || 'EUR'
 
-    # file format
-    # A "Variant name in DB"
-    # B "Variant reference_name CF NOMENCLATURE"
-    # C "Variant code in DB"
-    # D "Variety CF NOMENCLATURE"
-    # E "Derivative CF NOMENCLATURE"
-    # F "Purchase pretax amount price"
-    # G "Sale pretax amount price"
-    # H "Price unity"
-    # I "Indicators - HASH"
+      s = Roo::OpenOffice.new(file)
+      w.count = s.sheets.count
 
-    s.sheets.each do |sheet_name|
-      s.sheet(sheet_name)
-      # first line are headers
-      2.upto(s.last_row) do |row|
-        next if s.cell('A', row).blank?
-        r = { name: s.cell('A', row).blank? ? nil : s.cell('A', row).to_s.strip,
-              variant_reference_name: s.cell('B', row).blank? ? nil : s.cell('B', row).downcase.to_sym,
-              work_number: s.cell('C', row).blank? ? nil : s.cell('C', row).to_s.strip,
-              variety: s.cell('D', row).blank? ? nil : s.cell('D', row).to_s.strip,
-              derivative_of: s.cell('E', row).blank? ? nil : s.cell('E', row).to_s.strip,
-              purchase_unit_pretax_amount: s.cell('F', row).blank? ? nil : s.cell('F', row).to_d,
-              stock_unit_pretax_amount: s.cell('G', row).blank? ? nil : s.cell('G', row).to_d,
-              sale_unit_pretax_amount: s.cell('H', row).blank? ? nil : s.cell('H', row).to_d,
-              price_unity: s.cell('I', row).blank? ? nil : s.cell('I', row).to_s.strip.split(/[\,\.\/\\\(\)]/),
-              indicators: s.cell('J', row).blank? ? {} : s.cell('J', row).to_s.strip.split(/[[:space:]]*\;[[:space:]]*/).collect { |i| i.split(/[[:space:]]*\:[[:space:]]*/) }.inject({}) do |h, i|
-                h[i.first.strip.downcase.to_sym] = i.second
-                h
-              end
-            }.to_struct
+      s.sheets.each do |sheet_name|
+        s.sheet(sheet_name)
+        # first line are headers
+        2.upto(s.last_row) do |row|
+          next if s.cell('A', row).blank?
+          r = {
+            name: s.cell('A', row).blank? ? nil : s.cell('A', row).to_s.strip,
+            reference_name: s.cell('B', row).blank? ? nil : s.cell('B', row).downcase.to_sym,
+            work_number: s.cell('C', row).blank? ? nil : s.cell('C', row).to_s.strip,
+            variety: s.cell('D', row).blank? ? nil : s.cell('D', row).to_s.strip,
+            derivative_of: s.cell('E', row).blank? ? nil : s.cell('E', row).to_s.strip,
+            purchase_unit_pretax_amount: s.cell('F', row).blank? ? nil : s.cell('F', row).to_d,
+            stock_unit_pretax_amount: s.cell('G', row).blank? ? nil : s.cell('G', row).to_d,
+            sale_unit_pretax_amount: s.cell('H', row).blank? ? nil : s.cell('H', row).to_d,
+            price_unity: s.cell('I', row).blank? ? nil : s.cell('I', row).to_s.strip.split(/[\,\.\/\\\(\)]/),
+            indicators: s.cell('J', row).blank? ? {} : s.cell('J', row).to_s.strip.split(/[[:space:]]*\;[[:space:]]*/).collect { |i| i.split(/[[:space:]]*\:[[:space:]]*/) }.inject({}) do |h, i|
+              h[i.first.strip.downcase.to_sym] = i.second
+              h
+            end
+          }.to_struct
 
-        if r.variant_reference_name
+          unless r.reference_name
+            w.warn "Need a reference to build variant for #{r.name}"
+            next
+          end
           # force import variant from reference_nomenclature and update his attributes.
-          variant = ProductNatureVariant.import_from_nomenclature(r.variant_reference_name, true)
+          if Nomen::ProductNatureVariant.find(r.reference_name)
+            variant = ProductNatureVariant.import_from_nomenclature(r.reference_name, true)
+          elsif Nomen::ProductNature.find(r.reference_name)
+            nature = ProductNature.import_from_nomenclature(r.reference_name, true)
+            variant = nature.variants.new(name: nature.name)
+          else
+            fail 'Invalid reference name: ' + r.reference_name.inspect
+          end
           # update variant with attributes in the current row
           variant.name = r.name if r.name
           variant.number = r.work_number if r.work_number
@@ -59,7 +72,7 @@ class Ekylibre::VariantsExchanger < ActiveExchanger::Base
             if unit.present? && !Nomen::Unit[unit]
               if u = Nomen::Unit.find_by(symbol: unit)
                 unit = u.name.to_s
-                measure_unit_price = Measure.new(1.00, unit.to_sym) if unit
+                measure_unit_price = 1.00.in(unit.to_sym) if unit
               else
                 fail ActiveExchanger::NotWellFormedFileError, "Unknown unit #{unit.inspect} for variant #{variant.name.inspect}."
               end
@@ -95,24 +108,27 @@ class Ekylibre::VariantsExchanger < ActiveExchanger::Base
           end
 
           # create a purchase price if needed
-          if r.purchase_unit_pretax_amount && catalog = Catalog.where(usage: :purchase).first
+          if r.purchase_unit_pretax_amount
+            catalog = Catalog.by_default!(:purchase)
             variant.catalog_items.create!(catalog: catalog, all_taxes_included: false, amount: (r.purchase_unit_pretax_amount * ratio), currency: currency)
           end
-
           # create a stock price if needed
-          if r.stock_unit_pretax_amount && catalog = Catalog.where(usage: :stock).first and variant.catalog_items.where(catalog_id: catalog.id).empty?
-            variant.catalog_items.create!(catalog: catalog, all_taxes_included: false, amount: r.stock_unit_pretax_amount * ratio, currency: currency)
+          if r.stock_unit_pretax_amount
+            catalog = Catalog.by_default!(:stock)
+            if variant.catalog_items.where(catalog: catalog).empty?
+              variant.catalog_items.create!(catalog: catalog, all_taxes_included: false, amount: r.stock_unit_pretax_amount * ratio, currency: currency)
+            end
           end
           # create a sale price if needed
-          if r.sale_unit_pretax_amount && catalog = Catalog.where(usage: :sale).first and variant.catalog_items.where(catalog_id: catalog.id).empty?
-            variant.catalog_items.create!(catalog: catalog, all_taxes_included: false, amount: r.sale_unit_pretax_amount * ratio, currency: currency)
+          if r.sale_unit_pretax_amount
+            catalog = Catalog.by_default!(:sale)
+            if variant.catalog_items.where(catalog: catalog).empty?
+              variant.catalog_items.create!(catalog: catalog, all_taxes_included: false, amount: r.sale_unit_pretax_amount * ratio, currency: currency)
+            end
           end
-
-        else
-          w.warn "Need a Variant for #{r.name}"
         end
+        w.check_point
       end
-      w.check_point
     end
   end
 end
