1090d4c4fb6de68f814c446c2824102d838b6253
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-02-01 11:07:19 +0100

    Fix campaign open action test

--- test/controllers/backend/campaigns_controller_test.rb
+++ test/controllers/backend/campaigns_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 module Backend
   class CampaignsControllerTest < ActionController::TestCase
-    test_restfully_all_actions
+    test_restfully_all_actions except: :open
+
+    test 'open action in post mode' do
+      post :open, {:locale=>@locale, activity_id: activities(:activities_001).id, id: campaigns(:campaigns_001).id }
+      assert_redirected_to backend_campaign_path(campaigns(:campaigns_001))
+    end
   end
 end
