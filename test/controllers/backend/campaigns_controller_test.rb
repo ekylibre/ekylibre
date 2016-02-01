04fc66f11d2f99e17b3bfaf75d84ab629ca9521d
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-02-01 11:21:19 +0100

    Fix campaign current action test

--- test/controllers/backend/campaigns_controller_test.rb
+++ test/controllers/backend/campaigns_controller_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 
 module Backend
   class CampaignsControllerTest < ActionController::TestCase
-    test_restfully_all_actions except: :open
+    test_restfully_all_actions except: [:open, :current]
 
     test 'open action in post mode' do
-      post :open, {:locale=>@locale, activity_id: activities(:activities_001).id, id: campaigns(:campaigns_001).id }
+      post :open, {:locale => @locale, activity_id: activities(:activities_001).id, id: campaigns(:campaigns_001).id}
       assert_redirected_to backend_campaign_path(campaigns(:campaigns_001))
+    end
+
+    test 'current action in get mode' do
+      get :current, {:locale => @locale}
+      assert_redirected_to backend_campaign_path(@user.current_campaign)
     end
   end
 end
