121b5c2abb4cd0665a5516be7138aaf5183529ac
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-02-01 17:00:32 +0100

    Fix activity budget show action test

--- test/controllers/backend/activity_budgets_controller_test.rb
+++ test/controllers/backend/activity_budgets_controller_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 module Backend
   class ActivityBudgetsControllerTest < ActionController::TestCase
-    test_restfully_all_actions
+    test_restfully_all_actions except: :show
+
+
+    test 'show action' do
+      get :show, {:id=>"NaID", :redirect=>root_url, :locale=>@locale}
+      assert_redirected_to root_url
+      ActivityBudget.limit(5).find_each do |record|
+        get :show, {:id=>record.id, :locale=>@locale}
+        assert_redirected_to controller: :activities, action: :show, id: record.activity_id
+
+        assert_not_nil assigns(:activity_budget)
+      end
+    end
   end
 end
