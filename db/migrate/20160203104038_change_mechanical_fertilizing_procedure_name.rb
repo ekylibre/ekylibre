6d86e6863e17f88123043c70cdd31ba69bed2ee7
Brice TEXIER <brice@ekylibre.com>
2016-02-03 15:33:24 +0100

    Updates fixtures

--- db/migrate/20160203104038_change_mechanical_fertilizing_procedure_name.rb
+++ db/migrate/20160203104038_change_mechanical_fertilizing_procedure_name.rb
@@ -2,6 +2,7 @@ class ChangeMechanicalFertilizingProcedureName < ActiveRecord::Migration
   def up
     execute "UPDATE interventions SET procedure_name='fertilizing' WHERE interventions.procedure_name='mechanical_fertilizing'"
   end
+
   def down
     execute "UPDATE interventions SET procedure_name='mechanical_fertilizing' WHERE interventions.procedure_name='fertilizing'"
   end
