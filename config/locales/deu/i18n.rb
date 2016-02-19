2169bb30680e8635315f6836e2aa5f960b28bb83
Brice TEXIER <brice@ekylibre.com>
2016-02-19 11:55:29 +0100

    Adds deutsche translations

---
+++ config/locales/deu/i18n.rb
@@ -0,0 +1,15 @@
+{ deu: {
+    i18n: {
+      dir: 'ltr',
+      iso2: 'de',
+      name: 'Deutsche',
+      plural: {
+        keys: [:one, :other],
+        rule: ->(n) { n == 1 ? :one : :other }
+      }
+    },
+    date: {
+      order: [:day, :month, :year]
+    }
+  }
+}
