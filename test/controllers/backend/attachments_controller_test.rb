b48c135c793a249721d2bb3bb6e3d2bea7bc880e
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-02-24 09:58:10 +0100

    Fixes attachment controller test

--- test/controllers/backend/attachments_controller_test.rb
+++ test/controllers/backend/attachments_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 module Backend
   class AttachmentsControllerTest < ActionController::TestCase
-    test_restfully_all_actions create: { params: { subject_type: 'Entity', subject_id: 2 } }
+    test_restfully_all_actions create: { params: { subject_type: 'Entity', subject_id: 2 } }, except: :destroy
+
+
+    test 'destroy action' do
+      attachment = attachments(:attachments_002)
+      delete :destroy, {:id=>attachment.id, :locale=>@locale}
+      assert_response :ok
+      assert_equal 'deleted', JSON.parse(response.body)['attachment']
+    end
   end
 end
