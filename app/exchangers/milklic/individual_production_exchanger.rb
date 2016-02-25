8c08f380ff710eab8568e8206f611b55d5a95141
David JOULIN <ionosphere.consulting@gmail.com>
2016-02-25 19:18:11 +0100

    FIx milkclic exchanger.

--- app/exchangers/milklic/individual_production_exchanger.rb
+++ app/exchangers/milklic/individual_production_exchanger.rb
@@ -19,7 +19,7 @@ class Milklic::IndividualProductionExchanger < ActiveExchanger::Base
       r = OpenStruct.new(animal_name: row[0],
                          animal_work_number: row[1],
                          animal_lactation_number: row[2],
-                         animal_lactation_started_on: Date.strptime(row[3], '%d/%m/%y')
+                         animal_lactation_started_on: (row[3].present? ? Date.strptime(row[3], '%d/%m/%y') : nil)
                         )
 
       # if an animal exist
@@ -28,7 +28,7 @@ class Milklic::IndividualProductionExchanger < ActiveExchanger::Base
           next unless row[i] && row.headers[i]
           milk_daily_production_measure = row[i].tr(',', '.').to_d.in_kilogram_per_day
           milk_daily_production_at = Date.strptime(row.headers[i], '%d/%m/%y').to_time
-          reference_number = cattling_number + '-L' + r.animal_lactation_number.to_s + '-C' + r.animal_lactation_started_on.month.to_s
+          reference_number = cattling_number + '-L' + r.animal_lactation_number.to_s + '-C' + milk_daily_production_at.month.to_s
 
           unless analysis = Analysis.where(reference_number: reference_number, analyser: analyser).first
             analysis = Analysis.create!(reference_number: reference_number, nature: 'unitary_cow_milk_analysis',
