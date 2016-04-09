2f91adc8874887896fec55c639c171cea61e116c
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-09 17:12:35 +0200

    Update routes and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -574,6 +574,12 @@ Rails.application.routes.draw do
       end
     end
 
+    resources :map_backgrounds do
+      collection do
+        post :load_defaults
+      end
+    end
+
     resources :map_editors, only: [] do
       collection do
         post :upload
