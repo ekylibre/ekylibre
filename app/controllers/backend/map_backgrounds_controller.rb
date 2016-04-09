b490ff0fabb1bd014fe30b0e9e6ac9aa9a7c405e
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-09 17:12:01 +0200

    Add map backgrounds controller

---
+++ app/controllers/backend/map_backgrounds_controller.rb
@@ -0,0 +1,31 @@
+module Backend
+  class MapBackgroundsController < Backend::BaseController
+
+    def index
+
+      # TMP
+      @map_backgrounds = MapBackground.all
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def load_defaults
+      redirect_to params[:redirect] || { action: :index }
+    end
+  end
+end
