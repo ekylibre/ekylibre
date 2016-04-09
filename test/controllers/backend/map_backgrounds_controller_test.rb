0212cd056b77d9406237eb70b689fc35aa9e7982
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-09 17:12:01 +0200

    Add map backgrounds controller

---
+++ test/controllers/backend/map_backgrounds_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class Backend::MapBackgroundsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'loading defaults' do
+  end
+end
---
+++ test/helpers/backend/map_backgrounds_helper_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+module Backend
+  class MapBackgroundsHelperTest < ActionView::TestCase
+    # Add tests here...
+  end
+end
