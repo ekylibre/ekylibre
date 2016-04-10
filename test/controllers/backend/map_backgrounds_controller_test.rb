1c784ae01fa0487553f030820ebc7d39a4ee4b40
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-10 12:44:17 +0200

    Import map backgrounds from config file

--- test/controllers/backend/map_backgrounds_controller_test.rb
+++ test/controllers/backend/map_backgrounds_controller_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class Backend::MapBackgroundsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Ekylibre::Tenant.switch!('test')
+    @locale = ENV['LOCALE'] || I18n.default_locale
+    @user = users(:users_001)
+    sign_in(@user)
+  end
 
   test 'loading defaults' do
+    assert_difference 'MapBackground.count', MapBackgrounds::Layer.items.count do
+      post :load_defaults
+    end
+    assert_redirected_to backend_map_backgrounds_path
   end
+
+  teardown do
+    sign_out(@user)
+  end
+
 end
