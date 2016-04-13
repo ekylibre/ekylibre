7c8894c98d8781e5c5e3a9c770e0059b20c7ffb9
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-13 17:33:14 +0200

    Add by_default parameter. Add button to manage selection of map backgrounds.

--- config/routes.rb
+++ config/routes.rb
@@ -574,6 +574,9 @@ Rails.application.routes.draw do
       collection do
         post :load
       end
+      member do
+        patch :toggle_enabled
+      end
     end
 
     resources :map_editors, only: [] do
