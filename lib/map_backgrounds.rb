193348114e31444873088694def830d642b2440c
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-24 17:15:57 +0200

    Enhances map backgrounds library

--- lib/map_backgrounds.rb
+++ lib/map_backgrounds.rb
@@ -9,14 +9,16 @@ module MapBackgrounds
     attr_reader :enabled
     attr_reader :by_default
     attr_reader :attribution
+    attr_reader :options
 
-    def initialize(name, url, enabled, attribution, by_default, provider)
+    def initialize(name, url, enabled, by_default, options, provider)
       @name = name
       @url = url || provider.url
       @enabled = enabled.nil? ? false : enabled
-      @attribution = attribution || provider.attribution
       @by_default = by_default.nil? ? false : by_default
       @provider = provider
+      @options = options || provider.options
+      @options[:attribution] ||= provider.options[:attribution]
     end
 
     def label
@@ -34,15 +36,15 @@ module MapBackgrounds
         fail 'No valid config file found for MapBackgrounds::Layer' unless Pathname(config_path).exist?
         layers = YAML.load_file(config_path).deep_symbolize_keys
         layers.each do |provider, attributes|
-          provider = ::MapBackgrounds::Provider.new(provider, attributes[:url], attributes[:options])
+          provider = ::MapBackgrounds::Provider.new(provider, attributes.try(:[],:url), attributes.try(:[],:enabled), attributes.try(:[],:by_default), attributes.try(:[],:options))
 
           # A layer can be represented by a variant or by the provider itself
           if attributes.key?(:variants) and attributes[:variants].is_a? Hash
             attributes[:variants].each do |k, v|
-              items << ::MapBackgrounds::Layer.new(k, v.try(:[], :url), v.try(:[], :enabled), v.try(:[], :attribution), v.try(:[], :by_default), provider)
+              items << ::MapBackgrounds::Layer.new(k, v.try(:[], :url), v.try(:[], :enabled), v.try(:[], :by_default), v.try(:[], :options), provider)
             end
           else
-            items << ::MapBackgrounds::Layer.new(:default, provider.url, provider.enabled, provider.attribution, provider.by_default,provider)
+            items << ::MapBackgrounds::Layer.new(:default, provider.url, provider.enabled, provider.by_default, provider.options, provider)
           end
         end
       end
@@ -60,11 +62,13 @@ module MapBackgrounds
     end
   end
   class Provider
-    attr_reader :name, :url, :options
+    attr_reader :name, :url, :enabled, :by_default, :options
 
-    def initialize(name, url, options={})
+    def initialize(name, url, enabled, by_default, options={})
       @name=name
       @url=url
+      @enabled = enabled
+      @by_default = by_default
       @options=options
     end
 
@@ -72,17 +76,6 @@ module MapBackgrounds
       name.to_s.camelize
     end
 
-    def attribution
-      @options.try(:[], :attribution)
-    end
-
-    def enabled
-      @options.try(:[], :enabled)
-      end
-
-    def by_default
-      @options.try(:[], :by_default)
-    end
   end
 end
 
