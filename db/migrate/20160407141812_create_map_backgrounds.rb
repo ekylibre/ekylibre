f7a5027b987aa036eb2bbcb51e9462ec59388107
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-04-25 18:03:16 +0200

    Update mapbackground model

--- db/migrate/20160407141812_create_map_backgrounds.rb
+++ db/migrate/20160407141812_create_map_backgrounds.rb
@@ -3,8 +3,11 @@ class CreateMapBackgrounds < ActiveRecord::Migration
     create_table :map_backgrounds do |t|
       t.string :name, null: false, index: true
       t.string :url, null: false
-      t.string :base_layer
-      t.string :base_variant
+      t.string :reference_name, null: true
+      t.string :attribution, null: true
+      t.integer :min_zoom, null: true
+      t.integer :max_zoom, null: true
+      t.boolean :managed, default: false, null: false
       t.boolean :enabled, default: false, null: false
       t.boolean :by_default, default: false, null: false
 
