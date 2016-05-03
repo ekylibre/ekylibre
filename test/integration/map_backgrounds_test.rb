78a5bad8699f439d7acdbc1b3f8c9b2e28e0ec27
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-05-03 15:13:52 +0200

    Update integration tests

--- test/integration/map_backgrounds_test.rb
+++ test/integration/map_backgrounds_test.rb
@@ -2,39 +2,49 @@ require 'test_helper'
 
 class MapBackgroundsTest < CapybaraIntegrationTest
   setup do
+    #fixme: use locale param instead of user language
     I18n.locale = @locale = ENV['LOCALE'] || I18n.default_locale
-    visit("/authentication/sign_in?locale=#{@locale}")
-    login_as(users(:users_001), scope: :user)
+    login_with_user
   end
 
   teardown do
     Warden.test_reset!
   end
 
-  test 'loading defaults' do
+  def load_defaults
+    MapBackground.destroy_all
     visit('/backend/map_backgrounds')
-    assert has_no_selector?('.map-backgrounds-viewport .map-background-container')
-    click_on :load.tl
+    # assert has_no_selector?('.map-backgrounds-viewport .map-background-container')
+    click_on :load.ta
     assert_selector '.map-background-container', count: MapBackgrounds::Layer.items.count
+    save_screenshot 'map_backgrounds/load_defaults.jpg'
   end
 
-  test 'loading map backgrounds with defaults' do
-
+  def check_enabled_map_backgrounds
     visit('/backend/land_parcels')
-    assert_selector '[name=leaflet-base-layers]', count: MapBackground.availables
+    save_screenshot 'map_backgrounds/enabled_layers.jpg'
+    assert_selector '[name=leaflet-base-layers]', count: MapBackground.availables.size
+  end
+
+  test 'loading defaults' do
+    load_defaults
+    check_enabled_map_backgrounds
   end
 
   test 'enabling a map background' do
+    load_defaults
+
+    count_before = MapBackground.availables.size
 
     visit('/backend/map_backgrounds')
-    count_before = MapBackground.availables
+    save_screenshot 'map_backgrounds/enabling_background.jpg'
+
 
     first("a.map-background-display:not('.active')").click
 
-    assert_equal count_before+1, MapBackground.availables
+    assert_equal count_before+1, MapBackground.availables.size
 
-    visit('/backend/land_parcels')
-    assert_selector '[name=leaflet-base-layers]', count: MapBackground.availables
+    check_enabled_map_backgrounds
   end
 
 end
