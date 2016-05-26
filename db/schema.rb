5057db887a459f74fc3091bba7dd11bb94e1f47a
Brice TEXIER <brice@ekylibre.com>
2016-05-26 08:20:59 +0200

    Moves migration in time to prevent migration lack

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512061327) do
+ActiveRecord::Schema.define(version: 20160518061327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
