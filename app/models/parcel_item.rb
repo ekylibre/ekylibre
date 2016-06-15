2948f6097891d1183549009a26b125782e08d6b5
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-06-15 20:05:42 +0200

    Scoped parcel_item relations with ProductNatureVariant to only have deliverable variants in items.

--- app/models/parcel_item.rb
+++ app/models/parcel_item.rb
@@ -62,7 +62,7 @@ class ParcelItem < Ekylibre::Record::Base
   belongs_to :source_product, class_name: 'Product'
   belongs_to :source_product_movement, class_name: 'ProductMovement', dependent: :destroy
   belongs_to :source_product_shape_reading, class_name: 'ProductReading', dependent: :destroy
-  belongs_to :variant, class_name: 'ProductNatureVariant'
+  belongs_to :variant, -> { of_variety :matter }, class_name: 'ProductNatureVariant'
   has_one :category, through: :variant
   has_one :nature, through: :variant
   has_one :delivery, through: :parcel
