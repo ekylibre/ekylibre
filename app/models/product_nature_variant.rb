bd5df2deb249af7a4686eae76abf39ea6c221ec3
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-06-21 10:28:12 +0200

    Fixes indicators saving if datatype isn't a measure.

--- app/models/product_nature_variant.rb
+++ app/models/product_nature_variant.rb
@@ -74,7 +74,7 @@ class ProductNatureVariant < Ekylibre::Record::Base
   delegate :depreciable?, :depreciation_rate, :deliverable?, :purchasable?, :saleable?, :subscribing?, :fixed_asset_depreciation_method, :fixed_asset_depreciation_percentage, :fixed_asset_account, :fixed_asset_allocation_account, :fixed_asset_expenses_account, :product_account, :charge_account, :stock_account, to: :category
 
   accepts_nested_attributes_for :products, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :readings, reject_if: proc { |params| params['measure_value_value'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :readings, reject_if: proc { |params| params['measure_value_value'].blank? && params['integer_value'].blank? && params['boolean_value'].blank? && params['decimal_value'].blank? }, allow_destroy: true
   accepts_nested_attributes_for :catalog_items, reject_if: :all_blank, allow_destroy: true
   # acts_as_numbered
 
