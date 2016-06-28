b7582d7afa594660ca02a8fe2a21b11fbd04c5e5
Brice TEXIER <brice@ekylibre.com>
2016-06-28 15:01:44 +0200

    Fixes invalid column name in backend/parcels

--- app/controllers/backend/parcels_controller.rb
+++ app/controllers/backend/parcels_controller.rb
@@ -112,7 +112,7 @@ module Backend
       t.column :variant, url: true
       # t.column :source_product, url: true
       t.column :product_name
-      t.column :product_work_number
+      t.column :product_identification_number
       t.column :population
       t.column :unit_name, through: :variant
       t.status
