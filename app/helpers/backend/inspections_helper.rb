af2268930ae3928d1676adbc815f6134cef08bcb
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-07-04 11:41:43 +0200

    DRY-ed the chart creation in inspections through creation of an helper.

---
+++ app/helpers/backend/inspections_helper.rb
@@ -0,0 +1,8 @@
+module Backend::InspectionsHelper
+  def series(inspection, method, dataset, unit)
+    net_surface_area = inspection.product_net_surface_area_value
+    dataset.map do |calibration|
+      yield(calibration.name, calibration.send(method).round(0).to_d(unit).to_s.to_f)
+    end
+  end
+end
