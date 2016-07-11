ba19b03aa494d930495e0816b1272fe024eded4b
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-07-11 18:32:23 +0200

    Changed syntax to handle ownership declaration in parcel_items methods to ensure proper ownership on new products. Fixes #793.

--- app/models/parcel_item.rb
+++ app/models/parcel_item.rb
@@ -178,18 +178,16 @@ class ParcelItem < Ekylibre::Record::Base
   def give_incoming
     create_product_movement!(product: product, delta: population, started_at: parcel_given_at)
     create_product_localization!(product: product, nature: :interior, container: storage, started_at: parcel_given_at)
-    create_product_enjoyment!(product: product, nature: :own, started_at: parcel_given_at)
-    create_product_ownership!(product: product, nature: :own, started_at: parcel_given_at) unless parcel_remain_owner
+    create_product_enjoyment!(product: product, enjoyer: Entity.of_company, nature: :own, started_at: parcel_given_at)
+    create_product_ownership!(product: product, owner: Entity.of_company, nature: :own, started_at: parcel_given_at) unless parcel_remain_owner
   end
 
   def give_outgoing
     if self.population == source_product.population(at: parcel_given_at) && !parcel_remain_owner
-      puts "Yolo".yellow
       create_product_ownership!(product: product, owner: parcel_recipient, started_at: parcel_given_at)
       create_product_localization!(product: product, nature: :exterior, started_at: parcel_given_at)
-      create_product_enjoyment!(product: product, nature: :other, started_at: parcel_given_at, enjoyer: parcel.recipient)
+      create_product_enjoyment!(product: product, enjoyer: parcel_recipient, nature: :other, started_at: parcel_given_at, enjoyer: parcel.recipient)
     else
-      puts "Swag".blue
       create_product_movement!(product: product, delta: -1 * population, started_at: parcel_given_at)
     end
   end
