e40cb69aa802caf4cea35fffd71f16796afe0e98
Emma Dorie <edorie@ekylibre.com>
2016-07-21 18:06:36 +0200

    resolve conflicts in translations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718150935) do
+ActiveRecord::Schema.define(version: 20160721122006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
