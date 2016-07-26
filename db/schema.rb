27e8b75e0aed2a57e27e3843cb21b91c7c6b13e7
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-07-26 13:34:05 +0200

    Updates existing shape indicators to match with product born_at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725182008) do
+ActiveRecord::Schema.define(version: 20160726112542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
