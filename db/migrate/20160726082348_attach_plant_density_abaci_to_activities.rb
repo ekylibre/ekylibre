a55d7c660c006e9453839a1be4927d59996dc32d
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-07-26 17:30:29 +0200

    s/1/activities.id/g in migration.

--- db/migrate/20160726082348_attach_plant_density_abaci_to_activities.rb
+++ db/migrate/20160726082348_attach_plant_density_abaci_to_activities.rb
@@ -29,15 +29,16 @@ class AttachPlantDensityAbaciToActivities < ActiveRecord::Migration
         execute <<-SQL
           UPDATE
             plant_density_abaci AS pda
-            SET activity_id = 1
+            SET activity_id = activities.id
+            FROM activities
             WHERE pda.activity_id IS NULL;
         SQL
+        change_column :plant_density_abaci, :activity_id, :integer, null: false
       end
       dir.down do
-        raise ActiveRecord::IrreversibleMigration
+        change_column :plant_density_abaci, :activity_id, :integer, null: true
       end
 
-      change_column :plant_density_abaci, :activity_id, :integer, null: false
     end
   end
 end
