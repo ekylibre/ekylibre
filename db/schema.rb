3dfb0b3b9907bdcc49ec867d4174c19c13f7c908
Brice TEXIER <brice@ekylibre.com>
2016-07-27 22:58:55 +0200

    Updates activity productions size which have been forgotten in previous migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727094402) do
+ActiveRecord::Schema.define(version: 20160727201017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
