2c39d75ddd01f206a97d0e54a622e05ac5f52615
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-07-29 19:31:30 +0200

    Displays only available animals.

--- app/controllers/backend/animals_controller.rb
+++ app/controllers/backend/animals_controller.rb
@@ -83,7 +83,7 @@ module Backend
       @sorted = @grouped_animals
 
       without_container = []
-      Animal.of_enjoyer(Entity.of_company).select(:id, :name, :identification_number, :nature_id, :dead_at).each do |a|
+      Animal.availables(at: Time.now).of_enjoyer(Entity.of_company).select(:id, :name, :identification_number, :nature_id, :dead_at).each do |a|
         if a.container.nil? || a.memberships.empty?
           without_container << { animal: a.to_json(methods: [:picture_path, :sex_text, :status]) }
         end
