5485fc36ca96d1915d393e38362771032d6d4759
Brice TEXIER <brice@ekylibre.com>
2016-08-02 23:42:18 +0200

    Adds missing product_natures in subcription natures views

--- app/models/product_nature_variant.rb
+++ app/models/product_nature_variant.rb
@@ -52,9 +52,10 @@ class ProductNatureVariant < Ekylibre::Record::Base
   belongs_to :nature, class_name: 'ProductNature', inverse_of: :variants
   belongs_to :category, class_name: 'ProductNatureCategory', inverse_of: :variants
   has_many :catalog_items, foreign_key: :variant_id, dependent: :destroy
-  has_many :products, foreign_key: :variant_id
-  has_many :purchase_items, foreign_key: :variant_id, inverse_of: :variant
-  has_many :sale_items, foreign_key: :variant_id, inverse_of: :variant
+  has_many :parcel_items, foreign_key: :variant_id, dependent: :restrict_with_exception
+  has_many :products, foreign_key: :variant_id, dependent: :restrict_with_exception
+  has_many :purchase_items, foreign_key: :variant_id, inverse_of: :variant, dependent: :restrict_with_exception
+  has_many :sale_items, foreign_key: :variant_id, inverse_of: :variant, dependent: :restrict_with_exception
   has_many :readings, class_name: 'ProductNatureVariantReading', foreign_key: :variant_id, inverse_of: :variant
   has_picture
 
@@ -112,7 +113,7 @@ class ProductNatureVariant < Ekylibre::Record::Base
   scope :of_category, ->(category) { where(category: category) }
 
   protect(on: :destroy) do
-    products.any?
+    products.any? || sale_items.any? || purchase_items.any? || parcel_items.any?
   end
 
   before_validation on: :create do
