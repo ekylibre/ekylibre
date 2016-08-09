05c031a00c9d87f093bcc054b4faec7859256f4b
Brice TEXIER <brice@ekylibre.com>
2016-08-09 19:28:40 +0200

    Rejects empty ettachments in forms. Fixes #964.

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -4,6 +4,6 @@ module Attachable
   included do
     has_many :attachments, as: :resource, inverse_of: :resource, dependent: :destroy
 
-    accepts_nested_attributes_for :attachments, allow_destroy: true
+    accepts_nested_attributes_for :attachments, allow_destroy: true, reject_if: :all_blank
   end
 end
