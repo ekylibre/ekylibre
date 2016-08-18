fce69a15ee3c9ec8ad2b968d1eb7718108c1bb05
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-08-18 17:08:01 +0200

    Commenting + CallMessage now inherits de Ekylibre::Record::Base instead of ActiveRecord::Base.

--- app/models/call_message.rb
+++ app/models/call_message.rb
@@ -1,4 +1,5 @@
-class CallMessage < ActiveRecord::Base
+# Class representing any message linked to APIs in DB, whether ours or others.
+class CallMessage < Ekylibre::Record::Base
   belongs_to :operation, class_name: 'Call'
   enumerize :nature, in: [:incoming, :outgoing], predicates: true
 end
