1fafe05252af451283eb1715590b97c57ddc9cef
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-08-22 11:03:23 +0200

    Implemented SAVON Protocol.

--- lib/action_caller/protocols/savon.rb
+++ lib/action_caller/protocols/savon.rb
@@ -1,17 +1,36 @@
 module ActionCaller
   module Protocols
     # Methods to do calls with Savon.
-    # TODO: Actually implement it.
     module Savon
       # Sets up the format to SOAP so it's not saved as a "savon" format request.
       FORMAT = :soap
 
-      def call(operation, message, options)
+      def authenticate(options, message = {})
+        request(:authenticate, message, options)
+      end
+
+      def call(operation, options, message = {})
+        request(operation, message, options)
+      end
+
+      private
+
+      def request(operation, options, message = {})
         client = Savon.client(options)
-        response = client.call(operation, message)
-        request_log = CallRequest.create_from_savon!(operation, message, client, @format)
-        messages << resquest_log
-        messages << CallResponse.create_from_savon!(response, request_log, @format)
+
+        request_log = CallRequest.create_from_savon_httpi_request!(
+          s.build_request(operation, message: message),
+          @format
+        )
+        messages << request_log
+
+        response = client.call(operation, message: message)
+
+        messages << CallResponse.create_from_savon_httpi_response!(
+          response,
+          request_log,
+          @format
+        )
       end
     end
   end
