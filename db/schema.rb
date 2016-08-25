86f7066930f8e732ba3bbcfc925212f870fcbf8f
Brice TEXIER <brice@ekylibre.com>
2016-08-25 09:31:57 +0200

    Updates dead_at of LandParcel of ActivityProduction. Fixes #839.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822225001) do
+ActiveRecord::Schema.define(version: 20160824160125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
