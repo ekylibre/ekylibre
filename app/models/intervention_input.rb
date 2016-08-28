e8403897b72c126422622610e2d474e908d86e54
Brice TEXIER <brice@ekylibre.com>
2016-08-28 18:31:23 +0200

    Adds product presence check on InterventionInput. Fixes #1035

--- app/models/intervention_input.rb
+++ app/models/intervention_input.rb
@@ -58,7 +58,8 @@ class InterventionInput < InterventionProductParameter
   belongs_to :intervention, inverse_of: :inputs
   belongs_to :outcoming_product, class_name: 'Product'
   has_one :product_movement, as: :originator, dependent: :destroy
-  validates :quantity_population, presence: true
+  validates :quantity_population, :product, presence: true
+  # validates :component, presence: true, if: -> { reference.component_of? }
 
   scope :of_component, -> (component) { where(component: component.self_and_parents) }
 
@@ -68,8 +69,8 @@ class InterventionInput < InterventionProductParameter
 
   after_save do
     if product && intervention.record?
-      movement = product_movement
-      movement = build_product_movement(product: product) unless movement
+      movement = product_movement ||
+                 build_product_movement(product: product)
       movement.delta = -1 * quantity_population
       movement.started_at = intervention.started_at || Time.zone.now - 1.hour
       movement.stopped_at = intervention.stopped_at || movement.started_at + 1.hour
