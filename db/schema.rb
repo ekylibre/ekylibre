7ebdaed1eb04fd5d945629e9db1f085dddd0738d
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-09-09 16:48:13 +0200

    Added link to partner's site in sensors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906112630) do
+ActiveRecord::Schema.define(version: 20160909140029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3214,6 +3214,7 @@ ActiveRecord::Schema.define(version: 20160906112630) do
     t.string   "token"
     t.jsonb    "custom_fields"
     t.string   "euid"
+    t.string   "partner_url"
   end
 
   add_index "sensors", ["created_at"], name: "index_sensors_on_created_at", using: :btree
