3b6747489fcebef81af42a8610bd538f3501be7e
Brice TEXIER <brice@ekylibre.com>
2016-09-09 21:13:47 +0200

    Indents properly modules in helpers

--- app/exchangers/ekylibre/visuals_exchanger.rb
+++ app/exchangers/ekylibre/visuals_exchanger.rb
@@ -1,8 +1,10 @@
-class Ekylibre::VisualsExchanger < ActiveExchanger::Base
-  def import
-    w.count = 1
-    # load background
-    Ekylibre::CorporateIdentity::Visual.set_default_background(file)
-    w.check_point
+module Ekylibre
+  class VisualsExchanger < ActiveExchanger::Base
+    def import
+      w.count = 1
+      # load background
+      Ekylibre::CorporateIdentity::Visual.set_default_background(file)
+      w.check_point
+    end
   end
 end
