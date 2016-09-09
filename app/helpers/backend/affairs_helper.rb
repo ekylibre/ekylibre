ef4ec7ec23183b6708cd315fba685ae0329029e9
Brice TEXIER <brice@ekylibre.com>
2016-09-09 20:42:34 +0200

    Indents code properly in Backend helpers

--- app/helpers/backend/affairs_helper.rb
+++ app/helpers/backend/affairs_helper.rb
@@ -1,9 +1,11 @@
-module Backend::AffairsHelper
-  def affair_of(deal)
-    render partial: 'backend/affairs/show', object: deal.affair, locals: { affair: deal.affair, current_deal: deal }
-  end
+module Backend
+  module AffairsHelper
+    def affair_of(deal)
+      render partial: 'backend/affairs/show', object: deal.affair, locals: { affair: deal.affair, current_deal: deal }
+    end
 
-  def affair_deals(affair)
-    render partial: 'backend/affairs/show', object: affair, locals: { affair: affair, current_deal: nil }
-  end
+    def affair_deals(affair)
+      render partial: 'backend/affairs/show', object: affair, locals: { affair: affair, current_deal: nil }
+    end
+end
 end
