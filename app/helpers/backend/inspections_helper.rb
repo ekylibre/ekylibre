f0f535b252d4313f1dcdd38440c0bda6cdb4c1ff
Brice TEXIER <brice@ekylibre.com>
2016-09-09 20:42:34 +0200

    Indents code properly in Backend helpers

--- app/helpers/backend/inspections_helper.rb
+++ app/helpers/backend/inspections_helper.rb
@@ -1,8 +1,10 @@
-module Backend::InspectionsHelper
-  def data_series(inspection, method, dataset, unit)
-    net_surface_area = inspection.product_net_surface_area_value
-    dataset.map do |calibration|
-      yield(calibration.name, calibration.send(method).round(0).to_d(unit).to_s.to_f)
+module Backend
+  module InspectionsHelper
+    def data_series(inspection, method, dataset, unit)
+      net_surface_area = inspection.product_net_surface_area_value
+      dataset.map do |calibration|
+        yield(calibration.name, calibration.send(method).round(0).to_d(unit).to_s.to_f)
+      end
     end
-  end
+end
 end
