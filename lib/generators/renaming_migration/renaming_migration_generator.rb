b4ba5913788205bc788d10e3d984d7d74aa5cc2d
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-09-21 20:08:16 +0200

    Automated renaming in procedure XML temlate itself.

--- lib/generators/renaming_migration/renaming_migration_generator.rb
+++ lib/generators/renaming_migration/renaming_migration_generator.rb
@@ -5,10 +5,21 @@ class RenamingMigrationGenerator < Rails::Generators::Base
   argument :procedure_name
 
   def generate_renaming_migration
+    return puts "Procedure does not exists, stopping..." unless File.file? procedure_path(procedure_name)
     template 'migration.rb', "db/migrate/#{timestamp}_rename_#{old_name}_to_#{new_name}_in_#{procedure_name}_procedure.rb"
+    gsub_procedure(procedure_name, old_name, new_name)
   end
 
   private
+
+  def gsub_procedure(name, old_name, new_name)
+    path = procedure_path(name)
+    gsub_file path, /(<(?!procedure|handler)\w* name=)"(#{old_name})"/, "\\1\"#{new_name}\""
+  end
+
+  def procedure_path(name)
+    File.join(destination_root, 'config', 'procedures', "#{procedure_name}.xml")
+  end
 
   def timestamp
     Time.now.getutc.strftime('%Y%m%d%H%M%S')
