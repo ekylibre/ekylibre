46abd2eb55d31f6836860613639ebdd969b73881
Emma Dorie <edorie@ekylibre.com>
2016-09-22 13:02:46 +0200

    Rename parameter in hervesting procedure.

--- db/migrate/20160922102943_rename_cultivation_to_plant_in_harvesting_procedure.rb
+++ db/migrate/20160922102943_rename_cultivation_to_plant_in_harvesting_procedure.rb
@@ -1,4 +1,4 @@
-class RenameCultivationToPlantInHarvesting < ActiveRecord::Migration
+class RenameCultivationToPlantInHarvestingProcedure < ActiveRecord::Migration
   def change
     reversible do |dir|
       dir.up do
