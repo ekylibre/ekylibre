21eda01c554e3c90651688ee22e26fc2bad41164
PierreBougon <pierre.bougon@epitech.eu>
2016-10-11 11:23:28 +0200

    Live correction with Zero use. Functional.

--- app/controllers/api/v1/intervention_participations_controller.rb
+++ app/controllers/api/v1/intervention_participations_controller.rb
@@ -3,17 +3,19 @@ module Api
     # Interventions API permits to access interventions
     class InterventionParticipationsController < Api::V1::BaseController
       def create
-        params = permitted_params[:intervention]
+        params = permitted_params
         intervention = Intervention.find_or_create_by!(
           request_intervention_id:  params[:request_intervention_id],
           procedure_name:           params[:procedure_name]
         )
         participations = params[:working_periods].map do |participation_params|
-          next if interventions.participations.where('(started_at, stopped_at) OVERLAPS (?, ?)', participation_params[:started_at], participation_params[:stopped_at]).any?
-          intervention.participations.find_or_create_by!(**participation_params.merge(product_id: current_user.id))
+          part = InterventionParticipation.find_or_initialize_by(**participation_params.deep_symbolize_keys.merge(intervention_id: intervention.id, product_id: current_user.id))
+          next nil if intervention.participations.where("(started_at, stopped_at) OVERLAPS (?, ?)", part.started_at, part.stopped_at).any?
+          part.save!
+          part
         end
 
-        intervention.request_compliant &&= params[:request_compliant]
+        # intervention.request_compliant &&= params[:request_compliant]
         intervention.state = params[:state]
         intervention.save!
 
@@ -23,7 +25,7 @@ module Api
       private
 
       def permitted_params
-        super.require(:intervention).permit(:request_intervention_id, :procedure_name, { working_periods: [:started_at, :stopped_at, :nature] }, :request_compliant, :state)
+        super.permit(:request_intervention_id, :procedure_name, { working_periods: [:started_at, :stopped_at, :nature] }, :request_compliant, :state)
       end
     end
   end
