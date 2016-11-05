b78ae452d14f5844b75c2c2e45b727ac46c6c182
Brice TEXIER <brice@ekylibre.com>
2016-11-05 18:26:18 +0100

    Deactivates global search by a global preference

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -173,6 +173,7 @@ class Preference < Ekylibre::Record::Base
   prefer :bookkeep_in_draft, :boolean, true
   prefer :detail_payments_in_deposit_bookkeeping, :boolean, true
   prefer :host, :string, 'erp.example.com'
+  prefer :use_global_search, :boolean, false
   prefer :use_entity_codes_for_account_numbers, :boolean, true
   prefer :sales_conditions, :string, ''
   prefer :accounting_system, :accounting_system, Nomen::AccountingSystem.default
