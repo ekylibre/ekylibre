1651b2170a804c9cb6f8de5f5c90e786c4ebda83
Brice TEXIER <brice@ekylibre.com>
2016-11-06 20:02:05 +0100

    Adds missing dependency option for indirect_link. Related to #1308.

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -113,7 +113,7 @@ class Entity < Ekylibre::Record::Base
   has_many :issues, as: :target, dependent: :destroy
   has_many :godchildren, class_name: 'Entity', foreign_key: 'proposer_id'
   has_many :incoming_payments, foreign_key: :payer_id, inverse_of: :payer
-  has_many :indirect_links, class_name: 'EntityLink', foreign_key: :linked_id
+  has_many :indirect_links, class_name: 'EntityLink', foreign_key: :linked_id, dependent: :destroy
   has_many :outgoing_payments, foreign_key: :payee_id
   has_many :ownerships, class_name: 'ProductOwnership', foreign_key: :owner_id
   has_many :participations, class_name: 'EventParticipation', foreign_key: :participant_id, dependent: :destroy
