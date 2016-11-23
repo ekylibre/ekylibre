cae4d7d77ae38be5853db1efd39bf8841d5de57e
Brice TEXIER <brice@ekylibre.com>
2016-11-23 05:39:54 +0100

    Fixes nomenclatureless cat migration

--- db/migrate/20161122155003_add_stock_and_movement_accounts_to_storable_nomenclatureless_categories.rb
+++ db/migrate/20161122155003_add_stock_and_movement_accounts_to_storable_nomenclatureless_categories.rb
@@ -1,36 +1,36 @@
+# coding: utf-8
 class AddStockAndMovementAccountsToStorableNomenclaturelessCategories < ActiveRecord::Migration
   ACCOUNT_LABELS = YAML.load <<-YAML
-    :pt_snc:
+    pt_snc:
       :stock:
         :label: Materiais diversos
         :number: 3349
       :stock_movement:
         :label: Variação stocks diversos
         :number: 603
-    :fr_pcg82: &fr_pcg82
+    fr_pcg82: &fr_pcg82
       :stock:
         :label: Matières diverses
         :number: 329
       :stock_movement:
-        :label: 6037
-        :number: Variations de stocks diverses
-    :fr_pcga: *fr_pcg82
+        :label: Variations de stocks diverses
+        :number: 6037
+    fr_pcga: *fr_pcg82
   YAML
 
   def change
+    add_index :accounts, :number, unique: true
     reversible do |dir|
       dir.up do
         # Find out what accounting plan we're using
-        accounting_preference = execute("SELECT string_value FROM preferences WHERE name = 'accounting_system'").first
-        return unless accounting_preference.present?
-        accounting_plan = accounting_preference && accounting_preference['string_value'].to_sym
-        %w(stock stock_movement).each do |account_type|
-          @type = account_type.to_sym
-          account_number = ACCOUNT_LABELS[accounting_plan][@type][:number]
-          label = ACCOUNT_LABELS[accounting_plan][@type][:label]
-
+        accounting_system = select_value("SELECT string_value FROM preferences WHERE name = 'accounting_system'")
+        return unless accounting_system.present?
+        [:stock, :stock_movement].each do |account_type|
+          @type = account_type
+          account_number = ACCOUNT_LABELS[accounting_system][@type][:number]
+          label = ACCOUNT_LABELS[accounting_system][@type][:label]
           if concerned_categories_count.nonzero?
-            fitting_account = find_account(account_number)
+            fitting_account = find_account(account_number)['id']
             create_account(account_number, label) unless fitting_account.present?
             new_account_id = find_account(account_number)['id']
 
@@ -49,36 +49,35 @@ class AddStockAndMovementAccountsToStorableNomenclaturelessCategories < ActiveRe
   end
 
   def find_account(number)
-    execute("SELECT * FROM accounts WHERE accounts.name = '#{number}'").to_a.first
+    select_one("SELECT * FROM accounts WHERE accounts.name = '#{number}'")
   end
 
   def concerned_categories_count
-    results = execute 'SELECT COUNT(*)' \
-                      '  FROM product_nature_categories AS pnc' \
-                      "  WHERE pnc.#{@type}_account_id IS NULL" \
-                      '    AND pnc.storable = TRUE'
-    results.to_a.first['count'].to_i
+    select_value('SELECT COUNT(*)' \
+                 '  FROM product_nature_categories AS pnc' \
+                 "  WHERE COALESCE(pnc.#{@type}_account_id, 0) NOT IN (SELECT id FROM accounts)" \
+                 '    AND pnc.storable = TRUE').to_i
   end
 
   def concerned_variants_count
-    results = execute 'SELECT COUNT(*)' \
-                      '  FROM product_nature_variants AS pnv ' \
-                      '    JOIN product_nature_categories AS pnc ON pnc.id = pnv.category_id' \
-                      "  WHERE pnc.storable = TRUE AND pnv.#{@type}_account_id IS NULL"
-    results.to_a.first['count'].to_i
+    select_value('SELECT COUNT(*)' \
+                 '  FROM product_nature_variants AS pnv ' \
+                 '    JOIN product_nature_categories AS pnc ON pnc.id = pnv.category_id' \
+                 "  WHERE pnc.storable = TRUE AND COALESCE(pnv.#{@type}_account_id, 0) NOT IN (SELECT id FROM accounts)").to_i
   end
 
   def create_missing_variant_accounts
     execute 'INSERT INTO accounts (name, number, label, usages, created_at, updated_at) ' \
-            '  SELECT sa.name || \' – \' || pnv.name, ' \
-            '    sa.number || pnv.number, sa.number || pnv.number || \' – \' || pnv.name, ' \
-            '    sa.usages, sa.created_at, sa.updated_at' \
+            '  SELECT pnca.name || \' – \' || pnv.name, ' \
+            '    pnca.number || pnv.number, pnca.number || pnv.number || \' – \' || pnv.name, ' \
+            '    pnca.usages, pnca.created_at, pnca.updated_at' \
             '  FROM product_nature_variants AS pnv ' \
             '    JOIN product_nature_categories AS pnc ON (pnv.category_id = pnc.id)' \
-            "    JOIN accounts AS sa ON (pnc.#{@type}_account_id = sa.id)" \
-            '  WHERE sa.number IS NOT NULL' \
+            "    JOIN accounts AS pnca ON (pnc.#{@type}_account_id = pnca.id)" \
+            '  WHERE pnca.number IS NOT NULL' \
             '    AND pnc.storable = TRUE' \
-            "    AND pnv.#{@type}_account_id IS NULL"
+            "    AND COALESCE(pnv.#{@type}_account_id, 0) NOT IN (SELECT id FROM accounts)" \
+            "    AND pnca.number || pnv.number NOT IN (SELECT number FROM accounts)"
   end
 
   def reference_new_account_in_concerned_variants
@@ -86,7 +85,7 @@ class AddStockAndMovementAccountsToStorableNomenclaturelessCategories < ActiveRe
             "  SET #{@type}_account_id = sac.id " \
             '  FROM product_nature_categories AS pnc ' \
             "    JOIN accounts AS sa ON (pnc.#{@type}_account_id = sa.id) " \
-            '    JOIN accounts AS sac ON (sa.usages = sac.usages AND sac.number LIKE sa.number || \'%\')' \
+            "    JOIN accounts AS sac ON (sac.number = sa.number || '%')" \
             '  WHERE category_id = pnc.id AND sac.number = sa.number || pnv.number'
   end
 
@@ -100,9 +99,9 @@ class AddStockAndMovementAccountsToStorableNomenclaturelessCategories < ActiveRe
             '         CURRENT_TIMESTAMP'
   end
 
-  def reference_correct_account_in_concerned_categories(account)
+  def reference_correct_account_in_concerned_categories(account_id)
     execute 'UPDATE product_nature_categories' \
-            "  SET #{@type}_account_id = #{account}" \
+            "  SET #{@type}_account_id = #{account_id}" \
             '  FROM product_nature_categories AS pnc' \
             "  WHERE pnc.#{@type}_account_id IS NULL" \
             '    AND pnc.storable = TRUE'
