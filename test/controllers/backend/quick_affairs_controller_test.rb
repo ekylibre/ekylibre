63960f2ac1a9b4fed20896640af5b1f201628847
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-04 23:40:15 +0100

    Wrote test for quick-purchases/sales.

--- test/controllers/backend/quick_affairs_controller_test.rb
+++ test/controllers/backend/quick_affairs_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 module Backend
   class QuickAffairsControllerTest < ActionController::TestCase
-    # test_restfully_all_actions
+    test 'can\'t be accessed since there are no routes to it' do
+      assert_raise { get :new }
+      assert_raise { post :create }
+    end
   end
 end
