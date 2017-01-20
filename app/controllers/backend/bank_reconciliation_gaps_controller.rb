3a6f7ae94df7ee27e8fc9b9b173509de144131dc
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-20 16:15:02 +0100

    Small adjustments for bugs added during refacto.

--- app/controllers/backend/bank_reconciliation_gaps_controller.rb
+++ app/controllers/backend/bank_reconciliation_gaps_controller.rb
@@ -4,13 +4,13 @@ module Backend
     def create
       @bank_statement = BankStatement.find(params[:bank_statement_id])
       return head :bad_request unless @bank_statement
-
       bank_statement_items = fetch_bank_items
       journal_entry_items  = fetch_journal_items
 
       gap = sold(bank_statement_items, journal_entry_items)
 
-      new_entry = regul_entry_for(gap.abs, *accounts(gap))
+      return unless (accs = accounts(gap))
+      new_entry = regul_entry_for(gap.abs, *accs)
       return head :bad_request unless new_entry
 
       letter_and_redirect bank_statement_items,
@@ -22,7 +22,11 @@ module Backend
 
     def accounts(gap)
       bank = @bank_statement.cash.account
-      return [bank, credit_gap_account] if gap > 0
+      if gap > 0
+        return (head(:bad_request) && nil) unless Account.of_usage(:other_usual_running_profits).count.nonzero?
+        return [bank, credit_gap_account]
+      end
+      return (head(:bad_request) && nil) unless Account.of_usage(:other_usual_running_expenses).count.nonzero?
       [debit_gap_account, bank]
     end
 
@@ -37,7 +41,7 @@ module Backend
     end
 
     def regul_entry_for(amount, debit, credit)
-      return false unless debit_account && credit_account
+      return false unless debit && credit
       JournalEntry.create!(
         journal_entry_params.merge(
           currency: @bank_statement.currency,
@@ -52,24 +56,24 @@ module Backend
     end
 
     def fuse_items(source_items, with_matching_items_of: [])
-      JournalEntryItem.where(id: source_items + with_matching_items_of.items.where(account_id: statement_account.id))
+      JournalEntryItem.where(id: source_items + with_matching_items_of.items.where(account_id: @bank_statement.cash_account_id))
     end
 
     def item_for(amount, account, key)
       {
         name: "#{'rest.actions.payment_gap'.t} #{Time.zone.today.l} - #{account.name}",
         "real_#{key}": amount,
-        account_id: debit.id
+        account_id: account.id
       }
     end
 
     def letter_and_redirect(bank_items, entry_items)
       head :bad_request unless @bank_statement.letter_items(bank_items, entry_items)
-      redirect_to reconciliation_backend_bank_statement_path(@bank_statement, scroll_to: bank_items.order(transfered_on: :asc).first.id)
+      redirect_to backend_bank_statement_bank_reconciliation_items_path(@bank_statement, scroll_to: bank_items.order(transfered_on: :asc).first.id)
     end
 
     def debit_gap_account
-      shortest = Account.of_usage(:other_usual_running_profits).minimum('LENGTH(number)')
+      shortest = Account.of_usage(:other_usual_running_expenses).minimum('LENGTH(number)')
       Account.of_usage(:other_usual_running_expenses).where("LENGTH(number) = #{shortest}").first
     end
 
