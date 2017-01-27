ea7266cad1f933a96316e4b175d67a57853d31a3
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 15:51:14 +0100

    No more pre-post-merge merges.

--- app/models/product_merging.rb
+++ app/models/product_merging.rb
@@ -3,7 +3,16 @@ class ProductMerging < Ekylibre::Record::Base
   belongs_to :originator, class_name: 'InterventionProductParameter'
   belongs_to :merged_with, class_name: 'Product'
 
+  validates :product, presence: true
+  validates :merged_with, presence: true
+
   after_save do
     product.update(dead_at: merged_at)
+  end
+
+  validate do
+    unless ProductMerging.where(product: product).where.not(id: id).count.zero?
+      errors.add :product, :cannot_merge_product_thats_already_merged
+    end
   end
 end
