a193b440d116c4e688a234292d79098b65285b12
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 17:04:09 +0100

    Brings back dead_at date once Merging is destroyed.

--- app/models/product_merging.rb
+++ app/models/product_merging.rb
@@ -10,9 +10,16 @@ class ProductMerging < Ekylibre::Record::Base
     product.update(dead_at: merged_at)
   end
 
+  before_destroy do
+    dead_ats  = Issue.where(target_id: product.id).where.not(observed_at: nil).pluck(:observed_at)
+    dead_ats += InterventionTarget.where(product_id: product.id).joins(:intervention).where.not(interventions: { stopped_at: nil }).pluck('interventions.stopped_at')
+    product.dead_at = dead_ats.min
+  end
+
   validate do
     unless ProductMerging.where(product: product).where.not(id: id).count.zero?
       errors.add :product, :cannot_merge_product_thats_already_merged
     end
+    errors.add :product, :cannot_merge_dead_product if product.dead_at && product.dead_at < merged_at
   end
 end
