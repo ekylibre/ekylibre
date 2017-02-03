ff791db450e0432c1dc9f8a4f34c94af10039d23
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-03 18:28:32 +0100

    Migrated with master.

--- db/structure.sql
+++ db/structure.sql
@@ -16811,3 +16811,5 @@ INSERT INTO schema_migrations (version) VALUES ('20170124133351');
 
 INSERT INTO schema_migrations (version) VALUES ('20170125162958');
 
+INSERT INTO schema_migrations (version) VALUES ('20170203135031');
+
