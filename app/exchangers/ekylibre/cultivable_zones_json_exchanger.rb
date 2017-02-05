f807cbfa1d03cfa9487f3a2db49fd50a673735b1
David JOULIN <ionosphere.consulting@gmail.com>
2017-02-05 21:53:03 +0100

    Add cultivable zone duplication detection based on geometry in cultivable zone json exchanger.

--- app/exchangers/ekylibre/cultivable_zones_json_exchanger.rb
+++ app/exchangers/ekylibre/cultivable_zones_json_exchanger.rb
@@ -18,7 +18,18 @@ module Ekylibre
         clusters['features'].each do |feature|
           properties = feature['properties']
           shape = ::Charta.from_geojson(feature)
-          CultivableZone.create!(properties.slice('name', 'work_number').merge(shape: shape))
+          # check if current cluster cover or overlap an existing cultivable zone
+          shape_inside_cultivable_zone = CultivableZone.shape_covering(shape, 0.02)
+          unless shape_inside_cultivable_zone.any?
+            shape_inside_cultivable_zone = CultivableZone.shape_matching(shape, 0.02)
+          end
+          if shape_inside_cultivable_zone.any?
+            cultivable_zone = shape_inside_cultivable_zone.first
+            cultivable_zone.name = properties['name'] if properties['name']
+            cultivable_zone.save!
+          else
+            CultivableZone.create!(properties.slice('name', 'work_number').merge(shape: shape))
+          end
         end
       else
         raise ActiveExchanger::NotWellFormedFileError, 'File seems to be JSON but not GeoJSON.'
