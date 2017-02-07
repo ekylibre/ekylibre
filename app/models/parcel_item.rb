99ac1c1908e6ccce4b4543711b065b1fe54f02df
Nicolas PROCUREUR <nprocureur@gmail.com>
2017-02-07 13:14:07 +0100

    Catches and displays errors (if any) on a parcel giving instead of failing.

--- app/models/parcel_item.rb
+++ app/models/parcel_item.rb
@@ -192,8 +192,10 @@ class ParcelItem < Ekylibre::Record::Base
   # It takes product in stock
   def check
     checked_at = parcel_prepared_at
-    check_incoming(checked_at) if parcel_incoming?
+    state = true
+    state, msg = check_incoming(checked_at) if parcel_incoming?
     check_outgoing(checked_at) if parcel_outgoing?
+    return state, msg unless state
     save!
   end
 
@@ -221,7 +223,10 @@ class ParcelItem < Ekylibre::Record::Base
 
     self.product = existing_product_in_storage unless no_fusing || storage.blank?
 
-    self.product ||= variant.create_product!(product_params)
+    self.product ||= variant.create_product(product_params)
+
+    return false, self.product.errors if self.product.errors.any?
+    true
   end
 
   def check_outgoing(_checked_at)
