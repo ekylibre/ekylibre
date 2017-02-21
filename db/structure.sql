c083b0bf2fce59dc14e7bb99932a90bee5d83bbe
Emma Dorie <edorie@ekylibre.com>
2017-02-21 16:23:46 +0100

    rake clean

--- db/structure.sql
+++ db/structure.sql
@@ -327,7 +327,8 @@ CREATE TABLE intervention_parameters (
     assembly_id integer,
     currency character varying,
     unit_pretax_stock_amount numeric(19,4) DEFAULT 0.0 NOT NULL,
-    dead boolean DEFAULT false NOT NULL
+    dead boolean DEFAULT false NOT NULL,
+    identification_number character varying
 );
 
 
@@ -4872,7 +4873,8 @@ CREATE TABLE product_movements (
     updated_at timestamp without time zone NOT NULL,
     creator_id integer,
     updater_id integer,
-    lock_version integer DEFAULT 0 NOT NULL
+    lock_version integer DEFAULT 0 NOT NULL,
+    description character varying
 );
 
 
@@ -5382,7 +5384,22 @@ CREATE TABLE products (
     initial_movement_id integer,
     custom_fields jsonb,
     team_id integer,
-    member_variant_id integer
+    member_variant_id integer,
+    birth_date_completeness character varying,
+    birth_farm_number character varying,
+    country character varying,
+    filiation_status character varying,
+    first_calving_on timestamp without time zone,
+    mother_country character varying,
+    mother_variety character varying,
+    mother_identification_number character varying,
+    father_country character varying,
+    father_variety character varying,
+    father_identification_number character varying,
+    origin_country character varying,
+    origin_identification_number character varying,
+    end_of_life_reason character varying,
+    originator_id integer
 );
 
 
@@ -6068,7 +6085,9 @@ CREATE TABLE synchronization_operations (
     updated_at timestamp without time zone NOT NULL,
     creator_id integer,
     updater_id integer,
-    lock_version integer DEFAULT 0 NOT NULL
+    lock_version integer DEFAULT 0 NOT NULL,
+    originator_id integer,
+    originator_type character varying
 );
 
 
@@ -14838,6 +14857,13 @@ CREATE UNIQUE INDEX index_products_on_number ON products USING btree (number);
 
 
 --
+-- Name: index_products_on_originator_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_products_on_originator_id ON products USING btree (originator_id);
+
+
+--
 -- Name: index_products_on_parent_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -16874,14 +16900,6 @@ INSERT INTO schema_migrations (version) VALUES ('20170208150219');
 
 INSERT INTO schema_migrations (version) VALUES ('20170209151943');
 
-INSERT INTO schema_migrations (version) VALUES ('20170214130330');
-
-INSERT INTO schema_migrations (version) VALUES ('20170215155700');
-
-INSERT INTO schema_migrations (version) VALUES ('20170215171400');
-
-INSERT INTO schema_migrations (version) VALUES ('20170220192042');
-
 INSERT INTO schema_migrations (version) VALUES ('20170210132452');
 
 INSERT INTO schema_migrations (version) VALUES ('20170210145316');
@@ -16892,9 +16910,17 @@ INSERT INTO schema_migrations (version) VALUES ('20170210174219');
 
 INSERT INTO schema_migrations (version) VALUES ('20170210175448');
 
+INSERT INTO schema_migrations (version) VALUES ('20170214130330');
+
+INSERT INTO schema_migrations (version) VALUES ('20170215155700');
+
+INSERT INTO schema_migrations (version) VALUES ('20170215171400');
+
 INSERT INTO schema_migrations (version) VALUES ('20170220123437');
 
 INSERT INTO schema_migrations (version) VALUES ('20170220164259');
 
 INSERT INTO schema_migrations (version) VALUES ('20170220171804');
+
+INSERT INTO schema_migrations (version) VALUES ('20170220192042');
 
