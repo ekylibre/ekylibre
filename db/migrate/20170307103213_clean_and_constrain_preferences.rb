224f76b5b45a86b321c3c0346312cf2425d8333b
Brice TEXIER <brice@ekylibre.com>
2017-03-07 17:24:49 +0100

    Translates and cleans code

--- db/migrate/20170307103213_clean_and_constrain_preferences.rb
+++ db/migrate/20170307103213_clean_and_constrain_preferences.rb
@@ -1,14 +1,10 @@
 class CleanAndConstrainPreferences < ActiveRecord::Migration
   def change
-
-
     # Removes preference 'products_for_intervention' used in animal Golumn. As this is a temp preference, there is no need to handle merging.
     execute <<-SQL
       DELETE FROM preferences WHERE name='products_for_intervention'
     SQL
 
-
     add_index :preferences, [:user_id, :name], unique: true
-
   end
 end
