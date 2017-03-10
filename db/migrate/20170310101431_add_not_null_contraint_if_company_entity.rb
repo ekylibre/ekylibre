22865628a99882fee8fb9939702fe3eeaf8c8b55
Alexandre Lécuelle <alecuelle@ekylibre.com>
2017-03-10 17:22:21 +0100

    Add born at postgresql constraint

---
+++ db/migrate/20170310101431_add_not_null_contraint_if_company_entity.rb
@@ -0,0 +1,5 @@
+class AddNotNullContraintIfCompanyEntity < ActiveRecord::Migration
+  def change
+    execute "ALTER TABLE entities ADD CONSTRAINT company_born_at_not_null CHECK (( of_company = TRUE AND born_at IS NOT NULL) OR (of_company = FALSE))" 
+  end
+end
