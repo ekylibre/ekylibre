64453a381cb13accf801035d526b4b6a6d6f9d93
Brice TEXIER <brice@ekylibre.com>
2017-03-15 14:06:20 +0100

    Change content-type case for ActionIntegration

--- lib/action_integration/protocols/json.rb
+++ lib/action_integration/protocols/json.rb
@@ -5,23 +5,23 @@ module ActionIntegration
       include Protocols::RestBase
 
       def get(path, headers = {}, &block)
-        get_base(path, { 'content-type' => 'application/json' }.merge(headers), &block)
+        get_base(path, { 'Content-Type' => 'application/json' }.merge(headers), &block)
       end
 
       def post(path, data, headers = {}, &block)
-        post_base(path, data, { 'content-type' => 'application/json' }.merge(headers), &block)
+        post_base(path, data, { 'Content-Type' => 'application/json' }.merge(headers), &block)
       end
 
       def put(path, data, headers = {}, &block)
-        put_base(path, data, { 'content-type' => 'application/json' }.merge(headers), &block)
+        put_base(path, data, { 'Content-Type' => 'application/json' }.merge(headers), &block)
       end
 
       def patch(path, data, headers = {}, &block)
-        patch_base(path, data, { 'content-type' => 'application/json' }.merge(headers), &block)
+        patch_base(path, data, { 'Content-Type' => 'application/json' }.merge(headers), &block)
       end
 
       def delete(path, headers = {}, &block)
-        delete_base(path, { 'content-type' => 'application/json' }.merge(headers), &block)
+        delete_base(path, { 'Content-Type' => 'application/json' }.merge(headers), &block)
       end
     end
   end
