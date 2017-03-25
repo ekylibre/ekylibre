1d0c30aae5587a34b542e771453a5450b323dcac
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-03-25 06:13:13 +0100

    Reverted specification of DB in tests as it doesn't seem to impact tests passing, and keeps the code more generic than if DB is specified.

--- test/lib/ekylibre/tenant_test.rb
+++ test/lib/ekylibre/tenant_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 module Ekylibre
   class TenantTest < ActiveSupport::TestCase
     def test_tenant_creation
-      Ekylibre::Tenant.create('foobar', :test)
+      Ekylibre::Tenant.create('foobar')
       assert Ekylibre::Tenant.exist?('foobar')
       Ekylibre::Tenant.switch!('foobar')
       assert_equal 'foobar', Ekylibre::Tenant.current
-      Ekylibre::Tenant.create('foobarbaz', :test)
+      Ekylibre::Tenant.create('foobarbaz')
       Ekylibre::Tenant.switch('foobarbaz') do
         assert_equal 'foobarbaz', Ekylibre::Tenant.current
       end
