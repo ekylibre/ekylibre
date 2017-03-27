a4238fd4cd80e5b89e5a2dc90c5bb8cf1185bd63
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-03-27 23:45:55 +0200

    Now using the actual record saved instead of the preference's id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -313,7 +313,7 @@ class User < Ekylibre::Record::Base
   def current_financial_year
     return nil unless default_financial_year = FinancialYear.on(Date.current)
     preference = self.preference('current_financial_year', default_financial_year, :record)
-    unless financial_year = FinancialYear.find_by(id: preference.id)
+    unless financial_year = preference.value
       financial_year = default_financial_year
       prefer!('current_financial_year', financial_year)
     end
