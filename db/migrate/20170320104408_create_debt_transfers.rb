c8de32f53e22c128fbe577314a45cb813a5dc392
Nicolas PROCUREUR <nprocureur@gmail.com>
2017-03-28 17:59:57 +0200

    Updates migration to reflect journal entries dependencies.

--- db/migrate/20170320104408_create_debt_transfers.rb
+++ db/migrate/20170320104408_create_debt_transfers.rb
@@ -5,8 +5,8 @@ class CreateDebtTransfers < ActiveRecord::Migration
       t.integer :purchase_affair_id, index: true, null: false
       t.decimal :amount
       t.string :currency, null: false
-      t.integer :sale_regularization_journal_entry_id, null: false
-      t.integer :purchase_regularization_journal_entry_id, null: false
+      t.integer :sale_regularization_journal_entry_id
+      t.integer :purchase_regularization_journal_entry_id
       t.datetime :accounted_at
       t.stamps
     end
