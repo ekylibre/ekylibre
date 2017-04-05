41554106c9a4765c9e97623602f51747562513dc
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-04-05 18:14:23 +0200

    Added new dashboard cells with data. Locale TODO.

--- config/routes.rb
+++ config/routes.rb
@@ -179,6 +179,8 @@ Rails.application.routes.draw do
       resource :stewardship_cell, only: :show
       resource :stock_container_map_cell, only: :show
       resource :trade_counts_cell, only: :show
+      resource :unbalanced_clients_cell, only: :show, concerns: :list
+      resource :unbalanced_suppliers_cell, only: :show, concerns: :list
       resource :weather_cell, only: :show
       resource :working_sets_stocks_cell, only: :show
     end
