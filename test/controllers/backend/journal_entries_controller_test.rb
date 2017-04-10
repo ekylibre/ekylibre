e69896f3bc4cc92e4b1529371f589238cd979a37
Thibault Finot <thibault.finot@gmail.com>
2017-04-10 18:19:52 +0200

    add test

--- test/controllers/backend/journal_entries_controller_test.rb
+++ test/controllers/backend/journal_entries_controller_test.rb
@@ -32,5 +32,30 @@ module Backend
       },
       index: :redirected_get
     )
+
+    test "Create with With affair" do
+      sale = Sale.last
+      affair = sale.affair
+      journal = Journal.last
+      date = DateTime.now.to_date
+      user = User.where(administrator: true).last
+      sign_in(user)
+      # byebug
+      post(:create, journal_entry: {
+        journal_id: journal.id,
+        printed_on:  date,
+        real_currency_rate: 1,
+        real_currency: 'EUR',
+        number: 10,
+        affair_id: affair.id,
+        items_attributes: {
+          "1491818768866"=>{ name: "Test", account_id: Account.last.id, activity_budget_id: ActivityBudget.first.id, team_id: Team.last.id, real_debit: 10, real_credit: 0.0, _destroy: false },
+          "1491818830695"=>{ name: "Hallo", account_id: Account.last.id, activity_budget_id: ActivityBudget.first.id, team_id: Team.last.id, real_debit: 0.0, real_credit: 10, _destroy: false }
+          },
+        },
+        redirect: backend_sale_path(sale))
+        assert_response :redirect
+        assert_redirected_to backend_sale_url(sale)
+    end
   end
 end
