a3ab05fab01284e8f0cbe056a39323b987277e46
Thibault Finot <thibault.finot@gmail.com>
2017-04-11 10:40:53 +0200

    start to do test

--- test/controllers/backend/loans_controller_test.rb
+++ test/controllers/backend/loans_controller_test.rb
@@ -3,5 +3,11 @@ require 'test_helper'
 module Backend
   class LoansControllerTest < ActionController::TestCase
     test_restfully_all_actions
+
+    test "create without ongoing_at" do
+      user = User.last
+      user.sign_in
+      
+    end
   end
 end
