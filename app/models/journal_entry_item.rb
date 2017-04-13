2a8a34850d3fc360f87264db853ecd5325f5ae65
Thibault Finot <thibault.finot@gmail.com>
2017-04-13 11:03:12 +0200

    error of account.id ok, add a condition if variant is not stockable

--- app/models/journal_entry_item.rb
+++ app/models/journal_entry_item.rb
@@ -239,7 +239,7 @@ class JournalEntryItem < Ekylibre::Record::Base
     credit = options.delete(:credit) ? true : false
     credit = !credit if amount < 0
     attributes = options.merge(name: name)
-    attributes[:account_id] = account.is_a?(Integer) ? account : account.try(:id)
+    attributes[:account_id] = account.is_a?(Integer) ? account : account.id
     attributes[:activity_budget_id] = options[:activity_budget].id if options[:activity_budget]
     attributes[:team_id] = options[:team].id if options[:team]
     attributes[:tax_id] = options[:tax].id if options[:tax]
