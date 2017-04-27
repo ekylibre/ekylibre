c6b7d0b650208ae5832ff74f8a319d6c57f2ac44
Brice TEXIER <brice@ekylibre.com>
2017-04-27 05:27:44 +0200

    Cleans code with rubocop

--- test/controllers/backend/journal_entries_controller_test.rb
+++ test/controllers/backend/journal_entries_controller_test.rb
@@ -33,7 +33,7 @@ module Backend
       index: :redirected_get
     )
 
-    test "Create with affair" do
+    test 'create with affair' do
       sale = Sale.last
       affair = sale.affair
       journal = Journal.last
@@ -46,11 +46,11 @@ module Backend
              printed_on:  date,
              real_currency_rate: 1,
              real_currency: 'EUR',
-             number: 10,
+             number: 'HA0010',
              affair_id: affair.id,
              items_attributes: {
-               "1491818768866" => {
-                 name: "Test",
+               '1491818768866' => {
+                 name: 'Test',
                  account_id: Account.last.id,
                  activity_budget_id: ActivityBudget.first.id,
                  team_id: Team.last.id,
@@ -58,8 +58,8 @@ module Backend
                  real_credit: 0.0,
                  _destroy: false
                },
-               "1491818830695" => {
-                 name: "Hallo",
+               '1491818830695' => {
+                 name: 'Hallo',
                  account_id: Account.last.id,
                  activity_budget_id: ActivityBudget.first.id,
                  team_id: Team.last.id,
@@ -69,12 +69,11 @@ module Backend
                }
              }
            },
-           redirect: backend_sale_path(sale)
-          )
+           redirect: backend_sale_path(sale))
       assert_response :redirect
       assert_redirected_to backend_sale_url(sale)
     end
-    
+
     test 'duplicate' do
       get :new, duplicate_of: JournalEntry.find_by(id: JournalEntryItem.first.id).id
       assert_select '#items-table' do
