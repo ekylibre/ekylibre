11321f1b6df5a71592540585f368685d04506faf
Thibault Finot <thibault.finot@gmail.com>
2017-04-27 16:57:17 +0200

    loan calendar selector ok, accountable_repayments scope ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    # Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.add_footer = true
