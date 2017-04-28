b1d5edf54ab63edf2365aeb0eca97cde18d0a61c
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-04-28 13:32:26 +0200

    Partial lettering migration added.

--- db/migrate/20170427085417_add_partial_lettering_support.rb
+++ db/migrate/20170427085417_add_partial_lettering_support.rb
@@ -3,29 +3,77 @@ class AddPartialLetteringSupport < ActiveRecord::Migration
     reversible do |dir|
       dir.up do
         execute <<-SQL
-          CREATE OR REPLACE FUNCTION compute_outgoing_payment_list_cache() RETURNS TRIGGER AS $$
+          CREATE OR REPLACE FUNCTION compute_partial_lettering() RETURNS TRIGGER AS $$
+          DECLARE
+            new_letter varchar DEFAULT NULL;
+            old_letter varchar DEFAULT NULL;
+            new_account_id integer DEFAULT NULL;
+            old_account_id integer DEFAULT NULL;
           BEGIN
-          SELECT substring(new.letter from '[A-z]*') AS letter,
-                 account_id AS account_id,
-                 SUM(debit) - SUM(credit) AS balance
-              FROM journal_entry_items
-              WHERE account_id = new.account_id
-                AND letter ~ substring(new.letter from '[A-z]*')
-              GROUP BY account_id;
-          END;
-          $$ language plpgpsql;
+          IF TG_OP <> 'DELETE' THEN
+            new_letter := substring(NEW.letter from '[A-z]*');
+            new_account_id := NEW.account_id;
+          END IF;
 
-          CREATE TRIGGER compute_partial_lettering_status
-            AFTER INSERT OR DELETE OR UPDATE OF credit, debit, balance, letter
+          IF TG_OP <> 'INSERT' THEN
+            old_letter := substring(OLD.letter from '[A-z]*');
+            old_account_id := OLD.account_id;
+          END IF;
+
+          UPDATE journal_entry_items
+          SET letter = (CASE
+                          WHEN modified_letter_groups.balance <> 0
+                          THEN modified_letter_groups.letter || '*'
+                          ELSE modified_letter_groups.letter
+                        END)
+          FROM (SELECT new_letter AS letter,
+                       account_id AS account_id,
+                       SUM(debit) - SUM(credit) AS balance
+                    FROM journal_entry_items
+                    WHERE account_id = new_account_id
+                      AND letter ~ new_letter
+                      AND new_letter IS NOT NULL
+                      AND new_account_id IS NOT NULL
+                    GROUP BY account_id
+                UNION ALL
+                SELECT old_letter AS letter,
+                       account_id AS account_id,
+                       SUM(debit) - SUM(credit) AS balance
+                  FROM journal_entry_items
+                  WHERE account_id = old_account_id
+                    AND letter ~ old_letter
+                    AND old_letter IS NOT NULL
+                    AND old_account_id IS NOT NULL
+                  GROUP BY account_id) AS modified_letter_groups
+          WHERE modified_letter_groups.account_id = journal_entry_items.account_id
+          AND journal_entry_items.letter ~ modified_letter_groups.letter;
+
+          RETURN NEW;
+        END;
+        $$ language plpgsql;
+
+          CREATE TRIGGER compute_partial_lettering_status_insert_delete
+            AFTER INSERT OR DELETE 
             ON journal_entry_items
             FOR EACH ROW
+              EXECUTE PROCEDURE compute_partial_lettering();
+
+          CREATE TRIGGER compute_partial_lettering_status_update
+            AFTER UPDATE OF credit, debit, account_id, letter
+            ON journal_entry_items
+            FOR EACH ROW
+            WHEN (substring(OLD.letter from '[A-z]*') <> substring(NEW.letter from '[A-z]*')
+               OR OLD.account_id <> NEW.account_id
+               OR OLD.credit <> NEW.credit
+               OR OLD.debit <> NEW.debit)
               EXECUTE PROCEDURE compute_partial_lettering();
         SQL
       end
 
       dir.down do
-        execute 'DROP TRIGGER IF EXISTS compute_partial_lettering_status;'
-        execute 'DROP FUNCTION IF EXISTS compute_partial_lettering;'
+        execute 'DROP TRIGGER IF EXISTS compute_partial_lettering_status_insert_delete ON journal_entry_items;'
+        execute 'DROP TRIGGER IF EXISTS compute_partial_lettering_status_update ON journal_entry_items;'
+        execute 'DROP FUNCTION IF EXISTS compute_partial_lettering();'
       end
     end
   end
--- db/structure.sql
+++ db/structure.sql
@@ -59,6 +59,62 @@ CREATE FUNCTION compute_outgoing_payment_list_cache() RETURNS trigger
             $$;
 
 
+--
+-- Name: compute_partial_lettering(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION compute_partial_lettering() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+          DECLARE
+            new_letter varchar DEFAULT NULL;
+            old_letter varchar DEFAULT NULL;
+            new_account_id integer DEFAULT NULL;
+            old_account_id integer DEFAULT NULL;
+          BEGIN
+          IF TG_OP <> 'DELETE' THEN
+            new_letter := substring(NEW.letter from '[A-z]*');
+            new_account_id := NEW.account_id;
+          END IF;
+
+          IF TG_OP <> 'INSERT' THEN
+            old_letter := substring(OLD.letter from '[A-z]*');
+            old_account_id := OLD.account_id;
+          END IF;
+
+          UPDATE journal_entry_items
+          SET letter = (CASE
+                          WHEN modified_letter_groups.balance <> 0
+                          THEN modified_letter_groups.letter || '*'
+                          ELSE modified_letter_groups.letter
+                        END)
+          FROM (SELECT new_letter AS letter,
+                       account_id AS account_id,
+                       SUM(debit) - SUM(credit) AS balance
+                    FROM journal_entry_items
+                    WHERE account_id = new_account_id
+                      AND letter ~ new_letter
+                      AND new_letter IS NOT NULL
+                      AND new_account_id IS NOT NULL
+                    GROUP BY account_id
+                UNION ALL
+                SELECT old_letter AS letter,
+                       account_id AS account_id,
+                       SUM(debit) - SUM(credit) AS balance
+                  FROM journal_entry_items
+                  WHERE account_id = old_account_id
+                    AND letter ~ old_letter
+                    AND old_letter IS NOT NULL
+                    AND old_account_id IS NOT NULL
+                  GROUP BY account_id) AS modified_letter_groups
+          WHERE modified_letter_groups.account_id = journal_entry_items.account_id
+          AND journal_entry_items.letter ~ modified_letter_groups.letter;
+
+          RETURN NEW;
+        END;
+        $$;
+
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -16724,6 +16780,20 @@ CREATE RULE delete_product_populations AS
 
 
 --
+-- Name: compute_partial_lettering_status_insert_delete; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER compute_partial_lettering_status_insert_delete AFTER INSERT OR DELETE ON journal_entry_items FOR EACH ROW EXECUTE PROCEDURE compute_partial_lettering();
+
+
+--
+-- Name: compute_partial_lettering_status_update; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER compute_partial_lettering_status_update AFTER UPDATE OF credit, debit, account_id, letter ON journal_entry_items FOR EACH ROW WHEN ((("substring"((old.letter)::text, '[A-z]*'::text) <> "substring"((new.letter)::text, '[A-z]*'::text)) OR (old.account_id <> new.account_id) OR (old.credit <> new.credit) OR (old.debit <> new.debit))) EXECUTE PROCEDURE compute_partial_lettering();
+
+
+--
 -- Name: outgoing_payment_list_cache; Type: TRIGGER; Schema: public; Owner: -
 --
 
@@ -17359,4 +17429,6 @@ INSERT INTO schema_migrations (version) VALUES ('20170413222518');
 INSERT INTO schema_migrations (version) VALUES ('20170413222519');
 
 INSERT INTO schema_migrations (version) VALUES ('20170413222520');
+
+INSERT INTO schema_migrations (version) VALUES ('20170427085417');
 
