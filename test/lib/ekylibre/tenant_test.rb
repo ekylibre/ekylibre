aad3a2e779e8c16f30a10d7f0c1d8989a3e7a743
Brice TEXIER <brice@ekylibre.com>
2017-05-03 17:00:46 +0200

    Fixes ekylibre/tenant lib test

--- test/lib/ekylibre/tenant_test.rb
+++ test/lib/ekylibre/tenant_test.rb
@@ -2,21 +2,19 @@ require 'test_helper'
 
 module Ekylibre
   class TenantTest < ActiveSupport::TestCase
-    def test_tenant_creation
+    test 'tenant creation and destruction' do
       Ekylibre::Tenant.create('foobar')
       assert Ekylibre::Tenant.exist?('foobar')
       Ekylibre::Tenant.switch!('foobar')
       assert_equal 'foobar', Ekylibre::Tenant.current
-      Ekylibre::Tenant.create('foobarbaz')
-      Ekylibre::Tenant.switch('foobarbaz') do
-        assert_equal 'foobarbaz', Ekylibre::Tenant.current
+      Ekylibre::Tenant.switch('test') do
+        assert_equal 'test', Ekylibre::Tenant.current
       end
       assert_equal 'foobar', Ekylibre::Tenant.current
-      Ekylibre::Tenant.drop('foobarbaz')
       Ekylibre::Tenant.drop('foobar')
       Ekylibre::Tenant.switch!('test')
       assert !Ekylibre::Tenant.exist?('foobar')
-      assert !Ekylibre::Tenant.exist?('foobarbaz')
+      assert_equal 'test', Ekylibre::Tenant.current
     end
   end
 end
