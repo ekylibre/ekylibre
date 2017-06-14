96cc61b20d435e9648c470bd3025f15cae7db4e0
Brice TEXIER <brice@ekylibre.com>
2017-06-14 22:43:19 +0200

    Fixes bug introduced in previous commit

--- app/controllers/backend/interventions_controller.rb
+++ app/controllers/backend/interventions_controller.rb
@@ -381,8 +381,9 @@ module Backend
       redirect_to_back
     end
 
+    # FIXME Not linked directly to interventions
     def change_page
-      options = params.slice(:q, :procedure_name, :product_id, :cultivable_zone_id, :period_interval, :period, :page)
+      options = params.require(:interventions_taskboard).permit(:q, :procedure_name, :product_id, :cultivable_zone_id, :period_interval, :period, :page)
       options[:period_interval] ||= current_period_interval
       options[:period] ||= current_period
 
