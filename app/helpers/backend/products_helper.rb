4bb9223075e40b9fef2d6073e1c2d6edba7cbb9c
sandy <sandeepyuvasoft98@gmail.com>
2017-06-14 16:21:35 +0530

    #1856 change time graph of worker

--- app/helpers/backend/products_helper.rb
+++ app/helpers/backend/products_helper.rb
@@ -2,52 +2,35 @@ module Backend
   module ProductsHelper
     # Show a chart with working time spent between different activities
     # It can accept :cobbler option to specify inclusion.
+
     def time_spent_by_activity(resource, options = {})
       working_periods = InterventionWorkingPeriod.with_intervention_parameter(options[:as] || :tool, resource)
       working_periods = working_periods.of_campaign(current_campaign) if options[:current_campaign]
       return nil unless current_campaign && working_periods.any?
-      stopped_at = working_periods.reorder(stopped_at: :desc).first.stopped_at.to_date
-      started_at = working_periods.reorder(started_at: :asc).first.started_at.to_date
-      duration = working_periods.sum(:duration)
 
       unit = Nomen::Unit[options[:time_unit] || :hour]
 
       series = []
-      categories = {}
-
-      date = started_at
-      stopped_at = started_at + 1 if started_at >= stopped_at
-      while date < stopped_at
-        categories[date.year.to_s + date.month.to_s.rjust(3, '0')] = date.l(format: '%b %Y')
-        date = date >> 1
-      end
+      activities = []
 
       # data for bar chart times by activities and by month
       Activity.find_each do |activity|
-        activity_periods = working_periods.of_activities(activity).order(:started_at)
+        activity_periods = working_periods.of_activities(activity)
         if activity_periods.any?
-          sums = activity_periods.sums_of_periods.sort.each_with_object({}) do |period, hash|
-            hash[period.expr.to_i.to_s] = period.sum.to_i.in_second.in(unit).round(2).to_f
-            hash
-          end
-          series << { name: activity.name, data: normalize_serie(sums, categories.keys),
-                      tooltip: { value_suffix: unit.symbol }, color: activity.color }
+          activities << activity.name
+          series << { name: activity.name, y: (activity_periods.sum(:duration) / 3600), tooltip: { value_suffix: unit.symbol }, color: activity.color }
         end
       end
 
       # Without activities
-      activity_periods = working_periods.without_activity.order(:started_at)
+      activity_periods = working_periods.without_activity
       if activity_periods.any?
-        sums = activity_periods.sums_of_periods.sort.each_with_object({}) do |period, hash|
-          hash[period.expr.to_i.to_s] = period.sum.to_i.in_second.in(unit).round(2).to_f
-          hash
-        end
-        series << { name: :undefined_activity.tl, data: normalize_serie(sums, categories.keys),
-                    tooltip: { value_suffix: unit.symbol }, color: '#777777' }
+        activities << :undefined_activity.tl
+        series << { name: :undefined_activity.tl, y: (activity_periods.sum(:duration) / 3600), tooltip: { value_suffix: unit.symbol }, color: '#777777' }
       end
 
       if series.any?
-        html = column_highcharts(series, y_axis: { title: { text: :duration.tl }, stack_labels: { enabled: true, format: "{total} #{unit.symbol}" }, labels: { format: "{value} #{unit.symbol}" } }, x_axis: { categories: categories.values }, legend: true, plot_options: { column: { stacking: 'normal' } })
+        html = column_highcharts([{ name: 'Activities', data: series }], chart: { type: 'column' }, y_axis: { title: { text: :duration.tl } }, x_axis: { type: 'category' }, legend: true, plot_options: { series: { borderWidth: 0, dataLabels: { enabled: true, format: "{point.y} #{unit.symbol}" } } })
         if options[:cobbler]
           options[:cobbler].cobble(:time_spent_by_activity) do
             html
