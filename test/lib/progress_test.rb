1cb28fccac758c466181988718b94c16fde4443e
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-06-22 14:36:28 +0200

    Rewrote test to use #value= instead of #set_value and also start by clearing all potential previous Progresses.

--- test/lib/progress_test.rb
+++ test/lib/progress_test.rb
@@ -1,16 +1,20 @@
 require 'test_helper'
 
 class ProgressTest < ActiveSupport::TestCase
+  setup do
+    FileUtils.rm_rf(Dir.glob(Ekylibre::Tenant.private_directory.join('tmp', 'imports', '*.progress')))
+  end
+
   test 'progresses can be set values that can then be consulted later' do
     progress = Progress.new('Example', id: 1)
-    progress.set_value(2)
+    progress.value = 2
 
     assert_equal 2, progress.value
   end
 
   test 'progresses\' values are set as a percentage from a specifiable max value' do
     progress = Progress.new('Not 100', id: 1, max: 40)
-    progress.set_value(3)
+    progress.value = 3
 
     assert_equal 7.5, progress.value
   end
@@ -32,7 +36,7 @@ class ProgressTest < ActiveSupport::TestCase
 
     assert_not_equal first, second
 
-    first.set_value(1)
+    first.value = 1
 
     assert_not_equal 1, second.value
     assert_equal 0, second.value
@@ -44,7 +48,7 @@ class ProgressTest < ActiveSupport::TestCase
 
     assert_not_equal first, second
 
-    first.set_value(1)
+    first.value = 1
 
     assert_not_equal 1, second.value
     assert_equal 0, second.value
@@ -56,15 +60,15 @@ class ProgressTest < ActiveSupport::TestCase
 
     assert_equal initialized, fetched
 
-    Progress.new('Not stored', id: 1).set_value(2)
+    Progress.new('Not stored', id: 1).value = 2
     fetched = Progress.fetch('Not stored', id: 1)
 
     assert_equal 2, fetched.value
   end
 
-  test 'progresses aren\'t reachabloe anymore once they have been cleared' do
+  test 'progresses aren\'t reachable anymore once they have been cleared' do
     progress = Progress.new('To be cleared', id: 1)
-    progress.set_value(2)
+    progress.value = 2
     progress.clear!
 
     assert_nil Progress.fetch('To be cleared', id: 1)
@@ -72,17 +76,17 @@ class ProgressTest < ActiveSupport::TestCase
 
   test 'cleared progresses are fetchable again if they are re-used' do
     progress = Progress.new('Lazarus', id: 1)
-    progress.set_value(2)
+    progress.value = 2
     progress.clear!
 
-    progress.set_value(1) # Rise and walk
+    progress.value = 1 # Rise and walk
 
     assert_equal 1, progress.value
   end
 
   test 'cleared progresses have 0 value' do
     progress = Progress.new('Zero', id: 1)
-    progress.set_value(2)
+    progress.value = 2
     progress.clear!
 
     assert_equal 0, progress.value
