772d1e0722e7efb4c172216be1d95cc31cec94d4
Thibault Finot <thibault.finot@gmail.com>
2017-06-30 16:31:04 +0200

    edit mailer for file with multiple argument

--- app/mailers/support_mailers.rb
+++ app/mailers/support_mailers.rb
@@ -2,7 +2,7 @@ class SupportMailers < ActionMailer::Base
   def first_run_errors(message, time)
     @tenant = Ekylibre::Tenant.current
     message = message.read
-    message = message.split(',')
+    message = message.split('; ').reject { |c| c.blank? }.last.split(', ')
     error_time = DateTime.parse(message.first)
     @time = time - error_time
     @progress = message.second
