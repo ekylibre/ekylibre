7b1067656b2158d3d5c9e6298b8bb4e48bbfb130
thomasbirien <thomasbirien@gmail.com>
2017-07-20 18:13:04 +0200

    added column average_cost_amount and amount for table product_nature_variant_valuings, added comment to the model ProductNatureVariantValuings

--- db/migrate/20170720125756_create_product_nature_variant_valuings.rb
+++ db/migrate/20170720125756_create_product_nature_variant_valuings.rb
@@ -1,7 +1,8 @@
 class CreateProductNatureVariantValuings < ActiveRecord::Migration
   def change
     create_table :product_nature_variant_valuings do |t|
-
+      t.decimal :average_cost_amount, precision: 19, scale: 4, null: false
+      t.decimal :amount, precision: 19, scale: 4, null: false
       t.timestamps null: false
     end
   end
