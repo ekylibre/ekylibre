97808ae94815abf0be47033dbb8379562126ffdf
thomasbirien <thomasbirien@gmail.com>
2017-07-29 14:02:36 +0200

    comment for now average_cost_amount

--- app/models/intervention_input.rb
+++ app/models/intervention_input.rb
@@ -150,6 +150,7 @@ class InterventionInput < InterventionProductParameter
   end
 
   def calculate_average_cost_amount
+    raise
     # quantity_action = []
     # product.uk.each do |uk|
     #   quantity_action << uk.quantity_population
