803ff1855e52c264257b91cb937df12f85ffa2d4
thomasbirien <thomasbirien@gmail.com>
2017-08-02 20:35:33 +0200

    added variant.update

--- app/models/product_nature_variant_valuing.rb
+++ app/models/product_nature_variant_valuing.rb
@@ -34,6 +34,7 @@ class ProductNatureVariantValuing < Ekylibre::Record::Base
   belongs_to :variant, class_name: 'ProductNatureVariant'
 
   validates :average_cost_amount, :amount, presence: true
+  validates :average_cost_amount, :amount, numericality: true
   # some logical, verification in process
   def self.calculate_first_entrance(unitary_price, quantity_action, variant_id)
     variant = ProductNatureVariant.find(variant_id)
