2bff1296787d738572bc765ce322cdbde081b4f0
thomasbirien <thomasbirien@gmail.com>
2017-08-02 21:04:59 +0200

    deleted comment

--- db/migrate/20170801091621_add_foreign_key_to_product_nature_variant.rb
+++ db/migrate/20170801091621_add_foreign_key_to_product_nature_variant.rb
@@ -1,6 +1,5 @@
 class AddForeignKeyToProductNatureVariant < ActiveRecord::Migration
   def change
     add_reference :product_nature_variants, :valuing, index: true
-    # add_foreign_key :product_nature_variants, :product_nature_variant_valuings
   end
 end
