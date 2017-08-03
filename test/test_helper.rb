bd64ed8fafafdfc4449991b1abcd51c1096e0c84
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-08-03 12:58:54 +0200

    Added mock require + a PseudoEnv system to simulate prod env (todo: extract to lib?)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require "minitest/mock"
+
 if ENV['CI']
   require 'codacy-coverage'
   require 'coveralls'
@@ -722,3 +724,49 @@ class CapybaraIntegrationTest < ActionDispatch::IntegrationTest
     URI(URI.extract(mail_body).first).request_uri
   end
 end
+
+class PseudoEnvironment < SimpleDelegator
+  attr_reader :real_env, :current_env, :scope, :explicit_label
+
+  def initialize(caller, explicit_label=true)
+    @scope = caller
+    @real_env = Rails.env
+    @explicit_label = explicit_label
+    super(@real_env)
+  end
+
+  def set_to(new_env)
+    unset
+    @current_env = new_env
+    Rails.instance_variable_set(:@_env, self)
+
+    define_env_response(@real_env, false)
+    define_env_response(@current_env, true)
+  end
+
+  def unset
+    return unless current_env.present?
+    class << self
+      env_test = :"#{Rails.env.current_env}?"
+      undef_method env_test if defined?(env_test)
+    end
+    Rails.instance_variable_set(:@_env, real_env)
+  end
+
+  def inspect
+    main_env = @current_env || @real_env
+    return label = "#{main_env}" unless explicit_label
+    "#{label} (actual: #{real_env})"
+  end
+
+  private
+
+  def define_env_response(env, response)
+    define_singleton_method(:"#{env}?") do
+      in_caller = binding.callers.find { |binding| binding.eval('self') == scope }
+      return response if in_caller
+      real_env.send("#{env}?")
+    end
+  end
+end
+
