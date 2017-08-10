fbbd17b56a2066ea644e88e5a7bded565c16ed3c
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-08-10 11:19:46 +0200

    Uncommented previous tests for journal_entries_controller

--- test/controllers/backend/journal_entries_controller_test.rb
+++ test/controllers/backend/journal_entries_controller_test.rb
@@ -20,66 +20,66 @@ require 'test_helper'
 
 module Backend
   class JournalEntriesControllerTest < ActionController::TestCase
-    # test_restfully_all_actions(
-    #   new: { journal_id: 3 },
-    #   toggle_autocompletion: { format: :json },
-    #   currency_state: { from: 'EUR' },
-    #   update: {
-    #     items_attributes: {
-    #       0 => { account_id: 152, name: 'Test' },
-    #       1 => { account_id: 160, name: 'Test' }
-    #     }
-    #   },
-    #   index: :redirected_get
-    # )
+    test_restfully_all_actions(
+      new: { journal_id: 3 },
+      toggle_autocompletion: { format: :json },
+      currency_state: { from: 'EUR' },
+      update: {
+        items_attributes: {
+          0 => { account_id: 152, name: 'Test' },
+          1 => { account_id: 160, name: 'Test' }
+        }
+      },
+      index: :redirected_get
+    )
 
-    # test 'create with affair' do
-    #   sale = Sale.last
-    #   affair = sale.affair
-    #   journal = Journal.last
-    #   date = DateTime.now.to_date
-    #   user = User.where(administrator: true).last
-    #   sign_in(user)
-    #   post(:create,
-    #        journal_entry: {
-    #          journal_id: journal.id,
-    #          printed_on:  date,
-    #          real_currency_rate: 1,
-    #          real_currency: 'EUR',
-    #          number: 'HA0010',
-    #          affair_id: affair.id,
-    #          items_attributes: {
-    #            '1491818768866' => {
-    #              name: 'Test',
-    #              account_id: Account.last.id,
-    #              activity_budget_id: ActivityBudget.first.id,
-    #              team_id: Team.last.id,
-    #              real_debit: 10,
-    #              real_credit: 0.0,
-    #              _destroy: false
-    #            },
-    #            '1491818830695' => {
-    #              name: 'Hallo',
-    #              account_id: Account.last.id,
-    #              activity_budget_id: ActivityBudget.first.id,
-    #              team_id: Team.last.id,
-    #              real_debit: 0.0,
-    #              real_credit: 10,
-    #              _destroy: false
-    #            }
-    #          }
-    #        },
-    #        redirect: backend_sale_path(sale))
-    #   assert_response :redirect
-    #   assert_redirected_to backend_sale_url(sale)
-    # end
+    test 'create with affair' do
+      sale = Sale.last
+      affair = sale.affair
+      journal = Journal.last
+      date = DateTime.now.to_date
+      user = User.where(administrator: true).last
+      sign_in(user)
+      post(:create,
+           journal_entry: {
+             journal_id: journal.id,
+             printed_on:  date,
+             real_currency_rate: 1,
+             real_currency: 'EUR',
+             number: 'HA0010',
+             affair_id: affair.id,
+             items_attributes: {
+               '1491818768866' => {
+                 name: 'Test',
+                 account_id: Account.last.id,
+                 activity_budget_id: ActivityBudget.first.id,
+                 team_id: Team.last.id,
+                 real_debit: 10,
+                 real_credit: 0.0,
+                 _destroy: false
+               },
+               '1491818830695' => {
+                 name: 'Hallo',
+                 account_id: Account.last.id,
+                 activity_budget_id: ActivityBudget.first.id,
+                 team_id: Team.last.id,
+                 real_debit: 0.0,
+                 real_credit: 10,
+                 _destroy: false
+               }
+             }
+           },
+           redirect: backend_sale_path(sale))
+      assert_response :redirect
+      assert_redirected_to backend_sale_url(sale)
+    end
 
-    # test 'duplicate' do
-    #   get :new, duplicate_of: JournalEntry.find_by(id: JournalEntryItem.first.id).id
-    #   assert_select '#items-table' do
-    #     assert_select 'tbody.nested-fields'
-    #   end
-    # end
+    test 'duplicate' do
+      get :new, duplicate_of: JournalEntry.find_by(id: JournalEntryItem.first.id).id
+      assert_select '#items-table' do
+        assert_select 'tbody.nested-fields'
+      end
+    end
 
     test 'currency_state returns the right exchange_rate according to financial_year currency' do
       FinancialYear.delete_all
