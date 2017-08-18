df732547c0aab6753313a7528c0840d67859f52a
Brice TEXIER <brice@ekylibre.com>
2017-08-18 17:00:10 +0200

    Fixes magic FixedAsset bug

--- test/factories/fixed_asset_depreciations.rb
+++ test/factories/fixed_asset_depreciations.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :fixed_asset_depreciation do
     fixed_asset
-    amount '15.07'.to_d
-    started_on Date.parse('2020-08-01')
-    stopped_on Date.parse('2020-08-10')
+    amount 15.07
+    started_on Date.civil(2020, 8, 1)
+    stopped_on Date.civil(2020, 8, 10)
   end
 end
