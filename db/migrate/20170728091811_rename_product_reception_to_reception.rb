75483211311121ab897e2f2dc46e16956698feac
Alexandre Lécuelle <alecuelle@ekylibre.com>
2017-08-23 11:24:47 +0200

    Change layout for late_delivery field

--- db/migrate/20170728091811_rename_product_reception_to_reception.rb
+++ db/migrate/20170728091811_rename_product_reception_to_reception.rb
@@ -1,12 +1,12 @@
 class RenameProductReceptionToReception < ActiveRecord::Migration
-	def change
-	  reversible do |dir|
-	    dir.up do
-	      execute "UPDATE parcels SET type = 'Reception'"
-	    end
-	    dir.down do
-	      execute "UPDATE parcels SET type = 'ProductReception'"
-	    end
-	  end
-	end
+  def change
+    reversible do |dir|
+      dir.up do
+        execute "UPDATE parcels SET type = 'Reception'"
+      end
+      dir.down do
+        execute "UPDATE parcels SET type = 'ProductReception'"
+      end
+    end
+  end
 end
