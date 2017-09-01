024e52d9e51dace20b151a5f2ec1e33706580f23
thib44 <thibault.finot@gmail.com>
2017-09-01 12:13:55 +0200

    remove profile and change indentation

--- app/models/outgoing_payment_list.rb
+++ app/models/outgoing_payment_list.rb
@@ -135,35 +135,32 @@ class OutgoingPaymentList < Ekylibre::Record::Base
   def self.build_from_affairs(affairs, mode, responsible, initial_check_number = nil, ignore_empty_affair = false)
     thirds = Entity.where(id: affairs.select(:third_id))
     position = 0
+    purchase_payments = thirds.map do |third|
+      third_affairs = affairs.where(third: third).order(:created_at).to_a
+      first_affair = third_affairs.shift
+      third_affairs.map { |affair| first_affair.absorb!(affair) }
 
-    # Ekylibre.profile('outgoing-payment-list') do
-      purchase_payments = thirds.map do |third|
-        third_affairs = affairs.where(third: third).order(:created_at).to_a
-        first_affair = third_affairs.shift
-        third_affairs.map { |affair| first_affair.absorb!(affair) }
+      next if first_affair.balanced?
+      next if ignore_empty_affair && first_affair.third_credit_balance <= 0
 
-        next if first_affair.balanced?
-        next if ignore_empty_affair && first_affair.third_credit_balance <= 0
-
-        op = PurchasePayment.new(
-          affair: first_affair,
-          amount: first_affair.third_credit_balance,
-          cash: mode.cash,
-          currency: first_affair.currency,
-          delivered: true,
-          mode: mode,
-          paid_at: Time.zone.today,
-          payee: first_affair.third,
-          responsible: responsible,
-          to_bank_at: Time.zone.today,
-          bank_check_number: initial_check_number.blank? ? nil : initial_check_number.to_i,
-          position: position
-        )
-        initial_check_number = initial_check_number.to_i + 1 if initial_check_number.present?
-        position += 1
-        op
-      end.compact
-    # end
+      op = PurchasePayment.new(
+        affair: first_affair,
+        amount: first_affair.third_credit_balance,
+        cash: mode.cash,
+        currency: first_affair.currency,
+        delivered: true,
+        mode: mode,
+        paid_at: Time.zone.today,
+        payee: first_affair.third,
+        responsible: responsible,
+        to_bank_at: Time.zone.today,
+        bank_check_number: initial_check_number.blank? ? nil : initial_check_number.to_i,
+        position: position
+      )
+      initial_check_number = initial_check_number.to_i + 1 if initial_check_number.present?
+      position += 1
+      op
+    end.compact
     new(payments: purchase_payments, mode: mode) unless purchase_payments.empty?
   end
 end
