1b65db61def3ac24ebb3fa7bdbfdc98f98ff996b
Brice TEXIER <brice@ekylibre.com>
2017-09-16 13:33:02 +0200

    Adds test on paperclip interpolation to add more test coverage

---
+++ test/lib/config_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ConfigTest < ActiveSupport::TestCase
+  test 'paperclip interpolations' do
+    tenant = Ekylibre::Tenant.current
+    assert_equal 'test', tenant
+    assert_equal Ekylibre::Tenant.private_directory.to_s,
+                 Paperclip::Interpolations.interpolate(':private', nil, nil)
+    assert_equal Ekylibre::Tenant.private_directory.join('attachments').to_s,
+                 Paperclip::Interpolations.interpolate(':tenant', nil, nil)
+  end
+ 
+end
