1e0f70218a7bfe4a346326756f19f699976bb20f
thib44 <thibault.finot@gmail.com>
2017-09-18 18:23:32 +0200

    auto complete price for current item

---
+++ app/helpers/backend/parcels_helper.rb
@@ -0,0 +1,13 @@
+module Backend::ParcelsHelper
+
+  def purchase_item_pretax_amount(item)
+    if item.unit_pretax_amount.present? && item.unit_pretax_amount > 0
+      return item.unit_pretax_amount
+    elsif item.purchase_item.present?
+      item.purchase_item.unit_pretax_amount
+    else
+      nil
+    end
+  end
+
+end
