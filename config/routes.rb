61690bc6342a51427cc48227a74cc89a8101ecfb
nicolas PROCUREUR <nprocureur@gmail.com>
2017-09-19 12:10:30 +0200

    Adds functional tests to load land parcel and plant shapes asynchronously.

--- config/routes.rb
+++ config/routes.rb
@@ -702,7 +702,7 @@ Rails.application.routes.draw do
 
     resources :map_editors, only: [] do
       collection do
-        get :shapes
+        get :shapes, format: :json
         post :upload
       end
     end
