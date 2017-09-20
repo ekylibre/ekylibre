88479d973963e237850034a625c5df18eabaeb6f
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-09-20 14:31:18 +0200

    Replaced type and options with url as argument in async_content helper

--- app/helpers/backend/async_contents_helper.rb
+++ app/helpers/backend/async_contents_helper.rb
@@ -1,10 +1,9 @@
 module Backend
   module AsyncContentsHelper
-    def async_content(type, options = {}, html_options = {})
-      url = options[:params] || {}
+    def async_content(url, html_options = {})
       content_tag(:div, nil, html_options.merge(
                                data: {
-                                 async_content: url_for(url.merge(controller: "backend/async_contents/#{type}_async_contents", action: :show)),
+                                 async_content: url,
                                  async_content_empty_message: :no_data.tl,
                                  async_content_error_message: :internal_error.tl
                                }
