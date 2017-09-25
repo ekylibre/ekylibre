18488bd7a1426cbb6144a14916a5b17b618110b6
nicolas PROCUREUR <nprocureur@gmail.com>
2017-09-25 17:56:02 +0200

    Adds async resources map and spread on cultivable zone, building, building division, equipment, settlement

--- config/routes.rb
+++ config/routes.rb
@@ -1026,6 +1026,7 @@ Rails.application.routes.draw do
       resource :plants_visualizations, only: :show
       resource :map_cells_visualizations, only: :show
       resource :land_parcels_visualizations, only: :show
+      resource :resources_visualizations, only: :show
     end
 
     resources :wine_tanks, only: [:index], concerns: [:list]
