1db00460c3dce94d2efad8e4eb11dbdbb290adcc
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-09-29 15:29:48 +0200

    Added test for production_cost_charts

--- test/helpers/backend/activity_productions_helper_test.rb
+++ test/helpers/backend/activity_productions_helper_test.rb
@@ -2,5 +2,13 @@ require 'test_helper'
 
 module Backend
   class ActivityProductionsHelperTest < ActionView::TestCase
+    include ChartsHelper
+    include Backend::ThemeHelper
+
+    test 'production_cost_charts returns html' do
+      activity_production = create(:activity_production)
+      charts = production_cost_charts(activity_production)
+      refute_empty Nokogiri::HTML(charts).search('div[data-highcharts]')
+    end
   end
 end
