ba5f9fafed15be401e2342ab02debff10a3f6812
Brice TEXIER <brice@ekylibre.com>
2017-10-10 20:02:08 +0200

    Fixes test on test/controllers/backend/cobbles/production_cost_cobbles_controller_test.rb

--- test/controllers/backend/cobbles/production_cost_cobbles_controller_test.rb
+++ test/controllers/backend/cobbles/production_cost_cobbles_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 module Backend
   module Cobbles
     class ProductionCostCobblesControllerTest < ActionController::TestCase
-      test_restfully_all_actions
+      test_restfully_all_actions except: :show
     end
   end
 end
