fe818108dbe8ca57acdfadc3270489c13626adc4
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-10-11 17:02:46 +0200

    2.53.0

--- app/controllers/backend/cobbles/stock_in_ground_cobbles_controller.rb
+++ app/controllers/backend/cobbles/stock_in_ground_cobbles_controller.rb
@@ -8,7 +8,6 @@ module Backend
         @last_inspection = @stock_in_ground_cobble.inspections.last
         @measure_symbol  = Nomen::Unit.find(@last_inspection.user_quantity_unit(@dimension)).symbol
         @yield_symbol    = Nomen::Unit.find(@last_inspection.user_per_area_unit(@dimension)).symbol
-
       end
     end
   end
