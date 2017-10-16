76dd93ef92c13663e4b98a07819403c20e0bdbf6
Charles Duporge <cduporge@ekylibre.com>
2017-10-16 17:35:22 +0200

    Added conditional validation for variant

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -77,7 +77,7 @@ class PurchaseItem < Ekylibre::Record::Base
   # ]VALIDATORS]
   validates :currency, length: { allow_nil: true, maximum: 3 }
   validates :account, :tax, :reduction_percentage, presence: true
-  validates :variant, presence: true
+  validates :variant, presence: true, unless: Proc.new { |item| item.variant.variety.eql?("trailed_equipment") || item.variant.variety.eql?("equipment")}
   validates_associated :fixed_asset
 
   delegate :invoiced_at, :journal_entry, :number, :computation_method, :computation_method_quantity_tax?, :computation_method_tax_quantity?, :computation_method_adaptative?, :computation_method_manual?, to: :purchase
