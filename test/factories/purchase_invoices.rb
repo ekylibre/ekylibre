47d798c9603e4e13e3b6d9c5105cd19e0c028f35
Charles Duporge <cduporge@ekylibre.com>
2017-10-18 17:27:29 +0200

    Edit purchase_invoices factory and add purchase_natures factory

--- test/factories/purchase_invoices.rb
+++ test/factories/purchase_invoices.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :purchase_invoice do
+    association :affair, factory: :purchase_affair
+    association :supplier, factory: %i[entity supplier]
+    association :nature, factory: :purchase_nature
     amount 1042.40
     pretax_amount 952.0
     currency 'EUR'
