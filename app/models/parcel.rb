cd219fbf7303429b6613606ae6b63b1c03e596a6
Charles Duporge <cduporge@ekylibre.com>
2017-10-25 17:48:47 +0200

    Rake clean

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -47,6 +47,7 @@
 #  pretax_amount                        :decimal(19, 4)   default(0.0), not null
 #  purchase_id                          :integer
 #  recipient_id                         :integer
+#  reconciliation_state                 :string
 #  reference_number                     :string
 #  remain_owner                         :boolean          default(FALSE), not null
 #  responsible_id                       :integer
---
+++ test/helpers/backend/receptions_helper_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+module Backend
+  class ReceptionsHelperTest < ActionView::TestCase
+    # Add tests here...
+  end
+end
