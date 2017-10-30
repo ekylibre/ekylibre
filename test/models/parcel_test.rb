42adef4e337cf6d27a31d92720c636028a946aee
thibault <thibault.finot@gmail.com>
2017-10-30 21:44:17 +0100

    run again rake clean with good database resolved error

--- test/models/parcel_test.rb
+++ test/models/parcel_test.rb
@@ -34,9 +34,7 @@
 #  given_at                             :datetime
 #  id                                   :integer          not null, primary key
 #  in_preparation_at                    :datetime
-#  intervention_id                      :integer
 #  journal_entry_id                     :integer
-#  late_delivery                        :boolean
 #  lock_version                         :integer          default(0), not null
 #  nature                               :string           not null
 #  number                               :string           not null
@@ -47,7 +45,6 @@
 #  pretax_amount                        :decimal(19, 4)   default(0.0), not null
 #  purchase_id                          :integer
 #  recipient_id                         :integer
-#  reconciliation_state                 :string
 #  reference_number                     :string
 #  remain_owner                         :boolean          default(FALSE), not null
 #  responsible_id                       :integer
@@ -57,7 +54,6 @@
 #  state                                :string           not null
 #  storage_id                           :integer
 #  transporter_id                       :integer
-#  type                                 :string
 #  undelivered_invoice_journal_entry_id :integer
 #  updated_at                           :datetime         not null
 #  updater_id                           :integer
--- test/models/purchase_item_test.rb
+++ test/models/purchase_item_test.rb
@@ -30,7 +30,6 @@
 #  creator_id             :integer
 #  currency               :string           not null
 #  depreciable_product_id :integer
-#  equipment_id           :integer
 #  fixed                  :boolean          default(FALSE), not null
 #  fixed_asset_id         :integer
 #  id                     :integer          not null, primary key
@@ -40,16 +39,15 @@
 #  preexisting_asset      :boolean
 #  pretax_amount          :decimal(19, 4)   default(0.0), not null
 #  purchase_id            :integer          not null
-#  quantity               :decimal(19, 4)   not null
+#  quantity               :decimal(19, 4)   default(1.0), not null
 #  reduction_percentage   :decimal(19, 4)   default(0.0), not null
-#  role                   :string
 #  tax_id                 :integer          not null
 #  team_id                :integer
 #  unit_amount            :decimal(19, 4)   default(0.0), not null
 #  unit_pretax_amount     :decimal(19, 4)   not null
 #  updated_at             :datetime         not null
 #  updater_id             :integer
-#  variant_id             :integer
+#  variant_id             :integer          not null
 #
 
 require 'test_helper'
