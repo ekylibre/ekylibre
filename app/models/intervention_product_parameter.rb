eb8dc4fd383d96e6d4daa554e6411dff79398c1b
Charles Duporge <cduporge@ekylibre.com>
2017-11-13 13:49:57 +0100

    Fix circular dependency bug

--- app/models/intervention_product_parameter.rb
+++ app/models/intervention_product_parameter.rb
@@ -231,9 +231,9 @@ class InterventionProductParameter < InterventionParameter
   end
 end
 
-require_dependency 'intervention_agent'
-require_dependency 'intervention_input'
-require_dependency 'intervention_tool'
-require_dependency 'intervention_output'
-require_dependency 'intervention_doer'
-require_dependency 'intervention_target'
+# require_dependency 'intervention_agent'
+# require_dependency 'intervention_input'
+# require_dependency 'intervention_tool'
+# require_dependency 'intervention_output'
+# require_dependency 'intervention_doer'
+# require_dependency 'intervention_target'
