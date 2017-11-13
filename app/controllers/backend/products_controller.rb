0032607cb7db494061c46478dbd8dd99b382c155
Alexandre Lécuelle <alecuelle@ekylibre.com>
2017-11-13 17:18:35 +0100

    Resolve matters parcel bug

--- app/controllers/backend/products_controller.rb
+++ app/controllers/backend/products_controller.rb
@@ -175,7 +175,7 @@ module Backend
 
     # Lists parcel items of the current product
     list(:reception_items, model: :reception_items, conditions: { product_id: 'params[:id]'.c, parcels: { nature: :incoming } }, order: { created_at: :desc }) do |t|
-      t.column :parcel, url: { controller: :receptions }
+      t.column :reception, url: { controller: :receptions }
       t.column :nature, through: :reception
       t.column :given_at, through: :reception, datatype: :datetime
       t.column :population
