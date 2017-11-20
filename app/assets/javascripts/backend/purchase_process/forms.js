15023c428fb93b77bfc1abd76ebb48b23be75907
thib44 <thibault.finot@gmail.com>
2017-11-20 12:35:15 +0100

    start with vue js ok

--- app/assets/javascripts/backend/purchase_process/forms.js
+++ app/assets/javascripts/backend/purchase_process/forms.js
@@ -1,11 +1,17 @@
-(function(E, $) {
-  'use strict';
-  
-  var form = new Vue({
-    el: '#items-table',
-    data: {
-      test: 'test'
-    }
+$(document).ready(function() {
+  $('#items-table').on('cocoon:after-insert', function () {
+    var form = new Vue({
+      el: '#test',
+      data: {
+        conditionningQuantity: 0,
+        conditionning: 0,
+        // quantity: 0
+      },
+      computed: {
+        quantity: function () {
+          return this.conditionningQuantity * this.conditionning
+        }
+      }
+    })
   })
-
-})(ekylibre, jQuery);
+})
