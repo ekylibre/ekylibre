44df4cffd17b05df3f58b5a0fe7167521de73507
David JOULIN <ionosphere.consulting@gmail.com>
2017-11-29 18:29:47 +0100

    Fix bugs on XML FEC methods.

--- app/concepts/fec/exporter/xml.rb
+++ app/concepts/fec/exporter/xml.rb
@@ -4,50 +4,47 @@ module FEC
       private
 
       def build(journals)
-        builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+        builder = Nokogiri::XML::Builder.new(encoding: 'ISO-8859-15') do |xml|
           xml.comptabilite do
-            xml.exercice('DateCloture' => @financial_year.stopped_on.to_s) do
+            xml.exercice do
+              xml.DateCloture @financial_year.stopped_on.strftime('%Y-%m-%d')
               journals.each do |journal|
                 entries = journal.entries.between(@financial_year.started_on, @financial_year.stopped_on)
                 next unless entries.any?
-                xml.journal('JournalCode' => journal.code, 'JournalLib' => journal.name) do
+                xml.journal do
+                  xml.JournalCode journal.code
+                  xml.JournalLib journal.name
                   entries.includes(items: :account).references(items: :account).find_each do |entry|
                     next if entry.items.empty?
                     resource = Maybe(entry.resource)
-                    attributes = {
-                      'EcritureNum' => entry.number,
-                      'EcritureDate' => entry.printed_on.to_s,
-                      'EcritureLib' => entry.items.first.name,
-                      'PieceRef' => resource.number.or_else(''),
-                      'PieceDate' => resource.created_at.to_date.or_else(''),
-                      # 'EcritureLet' => ??? [A47a-i-VII-1]
-                      # 'DateLet'     => ??? [A47a-i-VII-1]
+                    xml.ecriture do
+                      xml.EcritureNum entry.number
+                      xml.EcritureDate entry.printed_on.strftime('%Y-%m-%d')
+                      xml.EcritureLib entry.items.first.name
+                      xml.PieceRef (resource.respond_to?(:number) ? resource.number : '')
+                      xml.PieceDate resource.created_at.or_else(entry.created_at).strftime('%Y-%m-%d')
+                      # xml.EcritureLet '' #[A47a-i-VII-1]
+                      # xml.DateLet '' #[A47a-i-VII-1]
+                      xml.ValidDate entry.printed_on.strftime('%Y-%m-%d') # TODO : replace by validated_at
                       # 'DateRglt' => ??? [A47a-i-VIII-5]
                       # 'ModeRglt' => ??? [A47a-i-VIII-5]
                       # 'NatOp'    => ??? [A47a-i-VIII-5]
                       # 'IdClient' => ??? [A47a-i-VIII-7]
-                    }
-                    attributes['ValidDate'] = entry.printed_on.to_s if entry.confirmed?
-                    if resource.respond_to?(:third)
-                      attributes['TiersNum'] = resource.third.number.or_else('')
-                      attributes['TiersLib'] = resource.third.full_name.or_else('')
-                    end
-                    xml.ecriture(attributes) do
+                      
                       entry.items.find_each do |item|
-                        attributes = {
-                          'CompteNum' => item.account.number,
-                          'CompteLib' => item.account.name,
-                          # "CompteAuxNum" => ???,
-                          # "CompteAuxLib" => ???,
-                          # "Montantdevise" => [item.real_debit, item.real_credit].max,
-                          # "Idevise" => item.real_currency
-                        }
-                        if item.debit > 0
-                          attributes['Debit'] = item.debit
-                        else
-                          attributes['Credit'] = item.credit
+                        xml.ligne do
+                            xml.CompteNum item.account.number.ljust(3, '0')
+                            xml.CompteLib item.account.name
+                            # xml.CompteAuxNum ''
+                            # xml.CompteAuxLib ''
+                            # xml.Montantdevise ''
+                            # xml.Idevise ''
+                          if item.debit > 0
+                            xml.Debit sprintf('%5.2f', item.debit) #.tr('.', ',')
+                          else
+                            xml.Credit sprintf('%5.2f', item.credit) #.to_s.tr('.', ',')
+                          end
                         end
-                        xml.ligne(attributes)
                       end
                     end
                   end
