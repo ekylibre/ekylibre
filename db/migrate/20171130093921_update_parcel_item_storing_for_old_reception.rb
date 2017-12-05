afa09f76cc78a31b1dff3497a3c6178e75b9bfa5
thib44 <thibault.finot@gmail.com>
2017-12-05 15:06:59 +0100

    start to work with sql

--- db/migrate/20171130093921_update_parcel_item_storing_for_old_reception.rb
+++ db/migrate/20171130093921_update_parcel_item_storing_for_old_reception.rb
@@ -1,26 +1,8 @@
 class UpdateParcelItemStoringForOldReception < ActiveRecord::Migration
-
-  class Product < ActiveRecord::Base
-    self.table_name = 'products'
-    has_many :localizations, foreign_key: :product_id, dependent: :destroy
-    has_many :parcel_item_storings, foreign_key: :product_id
-  end
-
-  class ReceptionItem < ActiveRecord::Base
-    self.table_name = 'parcel_items'
-    has_many :storings, class_name: 'ParcelItemStoring', foreign_key: :parcel_item_id, dependent: :destroy
-    belongs_to :product
-    belongs_to :reception, foreign_key: :parcel_id
-  end
-
-  class ParcelItemStoring < ActiveRecord::Base
-    self.table_name = 'parcel_item_storings'
-  end
-
   def change
     reversible do |dir|
       dir.up do
-        update_storing
+        execute update_storing
       end
 
       dir.down do
@@ -30,26 +12,32 @@ class UpdateParcelItemStoringForOldReception < ActiveRecord::Migration
   end
 
   def update_storing
-    # need to do it on all tenant
-    Ekylibre::Tenant.list.each do |tenant|
-      Ekylibre::Tenant.switch! 'fermes-larrere'
-      matters = Product.includes(:parcel_item_storings).where(type: 'Matter').where(parcel_item_storings: { product_id: nil })
-      matters.each do |matter|
-        reception_items = ReceptionItem.where(product: matter, type: 'ReceptionItem')
-        reception_items.each do |r_i|
-          if r_i.storings.any?
-            r_i.storings.update_all(product_id: matter.id)
-          else
-            if r_i.reception.present?
-              ParcelItemStoring.create!( product_id: matter.id,
-                                        parcel_item_id: r_i.id,
-                                        quantity: r_i.population,
-                                        storage_id: r_i.reception.storage_id)
-            end
-          end
-          puts "------#{ r_i.id }-------"
-        end
-      end
-    end
+    <<-SQL
+      -- UPDATE parcel_item_storings AS pis
+      -- SET pis.product_id = parcel_items.product.id
+      -- FROM parcel_items AS parcel_items
+      -- WHERE pis.product_id = null
+      --   AND pis.parcel_item_id = parcel_items.id
+        -- AND parcel_items.type = 'ReceptionItem'
+    SQL
   end
 end
+
+
+# need to do it on all tenant
+# matters = Product.includes(:parcel_item_storings).where(type: 'Matter').where(parcel_item_storings: { product_id: nil })
+# matters.each do |matter|
+#   reception_items = ReceptionItem.where(product: matter, type: 'ReceptionItem')
+#   reception_items.each do |r_i|
+#     if r_i.storings.any?
+#       r_i.storings.update_all(product_id: matter.id)
+#     else
+#       if r_i.reception.present?
+#         ParcelItemStoring.create!( product_id: matter.id,
+#                                   parcel_item_id: r_i.id,
+#                                   quantity: r_i.population,
+#                                   storage_id: r_i.reception.storage_id)
+#       end
+#     end
+#     puts "------#{ r_i.id }-------"
+#   end
