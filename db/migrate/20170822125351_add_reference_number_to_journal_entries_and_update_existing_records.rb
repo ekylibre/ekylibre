7e0b3f632d4e70c01c1f733ff71e7dfef4928568
David JOULIN <ionosphere.consulting@gmail.com>
2017-12-12 08:42:44 +0100

    Clean code

--- db/migrate/20170822125351_add_reference_number_to_journal_entries_and_update_existing_records.rb
+++ db/migrate/20170822125351_add_reference_number_to_journal_entries_and_update_existing_records.rb
@@ -10,7 +10,7 @@ class AddReferenceNumberToJournalEntriesAndUpdateExistingRecords < ActiveRecord:
           WHERE je.reference_number IS NULL
             AND je.resource_id = s.id AND je.resource_type = 'Sale'
         SQL
-        
+
         execute <<-SQL.strip_heredoc
           UPDATE journal_entries AS je
             SET reference_number = p.reference_number
@@ -18,11 +18,10 @@ class AddReferenceNumberToJournalEntriesAndUpdateExistingRecords < ActiveRecord:
           WHERE je.reference_number IS NULL
             AND je.resource_id = p.id AND je.resource_type = 'Purchase'
         SQL
-        
       end
 
       d.down do
-        #NOOP
+        # NOOP
       end
     end
   end
