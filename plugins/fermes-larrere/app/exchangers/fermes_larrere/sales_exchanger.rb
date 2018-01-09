d3c1778788be9958707fc06c28287c12dd0589ec
Alexandre Lécuelle <igkyab@Bender.home>
2018-01-09 16:57:46 +0100

    Restore old version of sales exchanger

--- plugins/fermes-larrere/app/exchangers/fermes_larrere/sales_exchanger.rb
+++ plugins/fermes-larrere/app/exchangers/fermes_larrere/sales_exchanger.rb
@@ -41,28 +41,27 @@ module FermesLarrere
         entity = find_or_create_entity(r)
         raise 'Cannot import sales without client informations' unless entity
 
-        #address = find_or_create_mail_address(entity, r.client_full_name, r.client_address_name, r.client_town_name, r.client_postal_code_name, r.client_country_code)
+        address = find_or_create_mail_address(entity, r.client_full_name, r.client_address_name, r.client_town_name, r.client_postal_code_name, r.client_country_code)
 
-        #delivery_address = find_or_create_mail_address(entity, r.client_invoice_full_name, r.client_invoice_address_name, r.client_invoice_town_name, r.client_invoice_postal_code_name, r.client_invoice_country_code)
+        delivery_address = find_or_create_mail_address(entity, r.client_invoice_full_name, r.client_invoice_address_name, r.client_invoice_town_name, r.client_invoice_postal_code_name, r.client_invoice_country_code)
 
         # Add phone number if given
-        #find_or_create_address(entity, :phone, r.client_phone_number)
+        find_or_create_address(entity, :phone, r.client_phone_number)
 
         # Add fax phone number if given
-        #find_or_create_address(entity, :fax, r.client_fax_number)
+        find_or_create_address(entity, :fax, r.client_fax_number)
 
         # Add email if given
-        #if r.client_email && r.client_email.to_s.match(/@/)
-        #  find_or_create_address(entity, :email, r.client_email)
-        #end
+        if r.client_email && r.client_email.to_s.match(/@/)
+          find_or_create_address(entity, :email, r.client_email)
+        end
 
         # find or import a variant
         variant = find_or_create_variant(r)
         raise "What's the fuck? " + [r.variant_gtin, r.variant_name, r.variant_nature_reference_name].to_sentence.red unless variant
 
         # find or create a sale
-        #sale = find_or_create_sale(entity, address, delivery_address, r)
-        sale = find_or_create_sale(entity, r)
+        sale = find_or_create_sale(entity, address, delivery_address, r)
         unless sale
           next
         end
@@ -83,12 +82,49 @@ module FermesLarrere
         w.info "VAT rate: #{r.origin_vat_id}".red
 
         # r.client_invoice_country_code
+        sale_item_tax = find_or_create_tax(r.origin_vat_id, Preference[:country].to_sym)
+        unless sale_item_tax
+          raise "Cannot determine VAT (id : #{r.origin_vat_id}) for sale item: #{r.invoice_line_origin_reference_number} in invoice #{r.invoice_origin_reference_number}"
+        end
+
+        w.info "Sale item tax: #{sale_item_tax.inspect}".red
 
         # find or create a sale line
-        find_or_create_sale_item(r, sale, variant, sale_line_code)
+        sale_item = nil
+        if r.unit_pretax_amount && r.quantity
+          # item
+          sale_item = sale.items.where('codes ->> ? = ?', FermesLarrere::EUROFLOW_KEY, sale_line_code).first
+          unless sale_item
+            sale_item = sale.items.new(codes: { FermesLarrere::EUROFLOW_KEY => sale_line_code })
+          end
+          sale_item.variant = variant
+          # get value to convert population from variant
+          if variant.has_indicator? :net_mass
+            sale_item.quantity = 0.0
+            unitary_net_mass_value = variant.reading(:net_mass).value.value.to_f
+            unitary_net_mass_unit = variant.reading(:net_mass).value.unit.to_sym
+            if unitary_net_mass_value.nonzero?
+              sale_item.quantity = r.quantity.in(PALLET_QUANTITY_UNIT_NAME).convert(unitary_net_mass_unit) / unitary_net_mass_value
+            end
+          else
+            sale_item.quantity = r.quantity
+          end
+          unless r.discount_percentage.blank?
+            sale_item.reduction_percentage = r.discount_percentage
+          end
+          sale_item.tax = sale_item_tax
+          sale_item.unit_pretax_amount = nil # r.unit_pretax_amount
+          sale_item.unit_amount = nil
+          sale_item.pretax_amount = r.pretax_amount
+          sale_item.amount = nil
+          sale_item.compute_from = :pretax_amount
+          sale_item.save!
+          w.info 'OK Sale item created'.green
+        end
 
-        #find_or_create_transporter(r.client_transporter_code)
+        find_or_create_transporter(r.client_transporter_code)
 
+        w.info "END - LINE REFERENCE: #{r.invoice_origin_reference_number} - #{r.invoice_line_origin_reference_number} : sale_id(#{sale.id if sale}) / sale_item_id(#{sale_item.id if sale_item})".red
 
         previous_sale = sale
         # To fix Euroflow shit computation, we add consign_amount to global
@@ -193,29 +229,29 @@ module FermesLarrere
     end
 
     # Find or create a mail with given informations
-    #def find_or_create_mail_address(entity, full_name, address_name, town_name, postal_code_name, country_code)
-      #return nil unless full_name && address_name && town_name && postal_code_name
-      #line_1 = full_name
-      #line_4 = address_name
-      #line_6 = postal_code_name + ' ' + town_name
+    def find_or_create_mail_address(entity, full_name, address_name, town_name, postal_code_name, country_code)
+      return nil unless full_name && address_name && town_name && postal_code_name
+      line_1 = full_name
+      line_4 = address_name
+      line_6 = postal_code_name + ' ' + town_name
 
-      #address = entity.mails.where("mail_line_4 ILIKE ? AND mail_line_6 ILIKE regexp_replace(?, E'\\\s+', ' ', 'g') AND mail_line_1 ILIKE ?", line_4, line_6, line_1).where(mail_country: country_code).first
-      #unless address
-        #address = entity.mails.create!(
-          #mail_line_1: line_1,
-          #mail_line_4: line_4,
-          #mail_line_6: line_6,
-          #mail_country: country_code
-        #)
-        #w.info 'OK Mail address created'.green
-      #end
-      #address
-    #end
+      address = entity.mails.where("mail_line_4 ILIKE ? AND mail_line_6 ILIKE regexp_replace(?, E'\\\s+', ' ', 'g') AND mail_line_1 ILIKE ?", line_4, line_6, line_1).where(mail_country: country_code).first
+      unless address
+        address = entity.mails.create!(
+          mail_line_1: line_1,
+          mail_line_4: line_4,
+          mail_line_6: line_6,
+          mail_country: country_code
+        )
+        w.info 'OK Mail address created'.green
+      end
+      address
+    end
 
-    #def find_or_create_address(entity, canal, coordinate)
-      #return nil if coordinate.blank?
-      #entity.addresses.find_or_create_by!(canal: canal, coordinate: coordinate)
-    #end
+    def find_or_create_address(entity, canal, coordinate)
+      return nil if coordinate.blank?
+      entity.addresses.find_or_create_by!(canal: canal, coordinate: coordinate)
+    end
 
     def find_or_create_variant(r)
       # puts '--- find or create variant ---'.yellow
@@ -287,27 +323,23 @@ module FermesLarrere
       variant
     end
 
-    #def find_or_create_sale(entity, address, delivery_address, r)
-    def find_or_create_sale(entity, r)
+    def find_or_create_sale(entity, address, delivery_address, r)
       unless r.created_at && r.invoice_origin_reference_number
         raise 'Missing basic informations on the sale invoice: created_at or number'
       end
-
       # see if sale exist anyway
       sale = Sale.where('codes ->> ? = ?', FermesLarrere::EUROFLOW_KEY, r.invoice_origin_reference_number).first
       if sale && sale.invoice?
         return nil
       end
-
       if sale
         sale.invoiced_at = r.created_at || r.updated_at
         sale.created_at = r.created_at
         sale.updated_at = r.updated_at
-        #sale.address_id = address.id if address
-        #sale.delivery_address_id = delivery_address.id if delivery_address
-        #sale.invoice_address_id = address.id if address
+        sale.address_id = address.id if address
+        sale.delivery_address_id = delivery_address.id if delivery_address
+        sale.invoice_address_id = address.id if address
         sale.save!
-
         w.info 'OK Sale updated'.green
       else
         sale = Sale.new(
@@ -323,23 +355,19 @@ module FermesLarrere
           nature: SaleNature.actives.first,
           description: "Import from ETL from N° #{r.invoice_origin_reference_number}"
         )
-
-        #sale.address_id = address.id if address
-        #sale.delivery_address_id = delivery_address.id if delivery_address
-        #sale.invoice_address_id = address.id if address
+        sale.address_id = address.id if address
+        sale.delivery_address_id = delivery_address.id if delivery_address
+        sale.invoice_address_id = address.id if address
         sale.save!
         w.info "OK Sale created on #{sale.invoiced_at}".green
-
         # charge global discount one time if present
         if r.global_discount && r.global_discount.nonzero?
           # check tax for global discount
           global_discount_item_tax = find_or_create_tax(r.global_discount_vat_rate, Preference[:country].to_sym)
-
           unless global_discount_item_tax
             raise "Cannot find tax for rate: #{r.global_discount_vat_rate}"
           end
           raise 'No variant for global discount' unless global_discount_variant
-
           item = sale.items.find_or_initialize_by(variant: global_discount_variant, quantity: 1.0)
           item.tax = global_discount_item_tax
           item.unit_pretax_amount = r.global_discount
@@ -347,18 +375,14 @@ module FermesLarrere
           item.pretax_amount = nil
           item.amount = nil
           item.save!
-
           w.info 'OK Global discount on sale created'.green
         end
-
         # charge consign one time if present
         if r.consign_amount && r.consign_amount.nonzero?
           # check consign_amount
           global_consign_item_tax = Tax.find_by(id: CONSIGN_TAX_ID)
           consign_variant = ProductNatureVariant.find_by(id: CONSIGN_VARIANT_ID)
-
           raise 'No variant for consign variant' unless consign_variant
-
           item = sale.items.find_or_initialize_by(variant: consign_variant, quantity: 1.0)
           item.tax = global_consign_item_tax
           item.unit_pretax_amount = r.consign_amount
@@ -366,7 +390,6 @@ module FermesLarrere
           item.pretax_amount = r.consign_amount
           item.amount = r.consign_amount
           item.save!
-
           w.info 'OK consign on sale created'.green
         end
       end
@@ -382,67 +405,24 @@ module FermesLarrere
       Tax.find_by(id: id_or_rate.to_i)
     end
 
-    def find_or_create_sale_item(row, sale, variant, sale_line_code)
-      return unless row.unit_pretax_amount || row.quantity
-
-      sale_item_tax = find_or_create_tax(row.origin_vat_id, Preference[:country].to_sym)
-      unless sale_item_tax
-        raise "Cannot determine VAT (id : #{row.origin_vat_id}) for sale item: #{row.invoice_line_origin_reference_number} in invoice #{row.invoice_origin_reference_number}"
+    def find_or_create_transporter(transporter_code)
+      return nil if transporter_code.blank?
+      entity = Entity.where('codes ->> ? = ?', FermesLarrere::EUROFLOW_KEY, transporter_code).first
+      unless entity
+        entity = Entity.create!(
+          nature: :organization,
+          codes: {
+            FermesLarrere::EUROFLOW_KEY => transporter_code
+          },
+          last_name: transporter_code,
+          active: true,
+          supplier: true,
+          transporter: true
+        )
+        w.info 'OK Transporter entity created'.green
       end
-
-      w.info "Sale item tax: #{sale_item_tax.inspect}".red
-
-      # item
-      sale_item = nil
-      sale_item = sale.items.where('codes ->> ? = ?', FermesLarrere::EUROFLOW_KEY, sale_line_code).first
-      sale_item ||= sale.items.new(codes: { FermesLarrere::EUROFLOW_KEY => sale_line_code })
-
-      sale_item.variant = variant
-
-      # get value to convert population from variant
-      if variant.has_indicator? :net_mass
-        sale_item.quantity = 0.0
-        unitary_net_mass_value = variant.reading(:net_mass).value.value.to_f
-        unitary_net_mass_unit = variant.reading(:net_mass).value.unit.to_sym
-
-        if unitary_net_mass_value.nonzero?
-          sale_item.quantity = row.quantity.in(PALLET_QUANTITY_UNIT_NAME).convert(unitary_net_mass_unit) / unitary_net_mass_value
-        end
-      end
-
-      sale_item.quantity = row.quantity unless variant.has_indicator? :net_mass
-      sale_item.reduction_percentage = row.discount_percentage unless row.discount_percentage.blank?
-
-      sale_item.tax = sale_item_tax
-      sale_item.unit_pretax_amount = nil # r.unit_pretax_amount
-      sale_item.unit_amount = nil
-      sale_item.pretax_amount = row.pretax_amount
-      sale_item.amount = nil
-      sale_item.compute_from = :pretax_amount
-      sale_item.save!
-
-      w.info 'OK Sale item created'.green
-      w.info "END - LINE REFERENCE: #{row.invoice_origin_reference_number} - #{row.invoice_line_origin_reference_number} : sale_id(#{sale.id if sale}) / sale_item_id(#{sale_item.id if sale_item})".red
+      entity
     end
-
-    #def find_or_create_transporter(transporter_code)
-      #return nil if transporter_code.blank?
-      #entity = Entity.where('codes ->> ? = ?', FermesLarrere::EUROFLOW_KEY, transporter_code).first
-      #unless entity
-        #entity = Entity.create!(
-          #nature: :organization,
-          #codes: {
-            #FermesLarrere::EUROFLOW_KEY => transporter_code
-          #},
-          #last_name: transporter_code,
-          #active: true,
-          #supplier: true,
-          #transporter: true
-        #)
-        #w.info 'OK Transporter entity created'.green
-      #end
-      #entity
-    #end
 
     def finish_sale(sale, pretax_amount, amount)
       adjust_global_amounts(sale, pretax_amount, amount)
@@ -456,11 +436,11 @@ module FermesLarrere
       pretax_amount = pretax_amount.round(precision)
       amount = amount.round(precision)
 
+
       if sale.pretax_amount != pretax_amount
         w.info 'Adjust pre-tax amount'
         distribute_on_sale_items(sale, :pretax_amount, pretax_amount, precision)
       end
-
       if sale.amount != amount
         w.info 'Adjust amount'
         distribute_on_sale_items(sale, :amount, amount, precision)
