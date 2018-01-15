1b3b6473ba750b14644e4d07cc292645869fd766
thib44 <thibault.finot@gmail.com>
2018-01-15 12:20:29 +0100

    add migrations

--- db/structure.sql
+++ db/structure.sql
@@ -4940,8 +4940,8 @@ CREATE TABLE parcels (
     contract_id integer,
     pretax_amount numeric(19,4) DEFAULT 0.0 NOT NULL,
     responsible_id integer,
-    type character varying,
     late_delivery boolean,
+    type character varying,
     intervention_id integer,
     reconciliation_state character varying
 );
@@ -18335,6 +18335,8 @@ INSERT INTO schema_migrations (version) VALUES ('20170608081949');
 
 INSERT INTO schema_migrations (version) VALUES ('20170628125603');
 
+INSERT INTO schema_migrations (version) VALUES ('20170629152415');
+
 INSERT INTO schema_migrations (version) VALUES ('20170704092631');
 
 INSERT INTO schema_migrations (version) VALUES ('20170706143642');
@@ -18413,15 +18415,11 @@ INSERT INTO schema_migrations (version) VALUES ('20171130144435');
 
 INSERT INTO schema_migrations (version) VALUES ('20171206145442');
 
-INSERT INTO schema_migrations (version) VALUES ('20171207134203');
-
-INSERT INTO schema_migrations (version) VALUES ('20171213134204');
-
 INSERT INTO schema_migrations (version) VALUES ('20171220162200');
 
 INSERT INTO schema_migrations (version) VALUES ('20180112142925');
 
-INSERT INTO schema_migrations (version) VALUES ('20180112150525');
-
 INSERT INTO schema_migrations (version) VALUES ('20180112151925');
+
+INSERT INTO schema_migrations (version) VALUES ('20180115111859');
 
