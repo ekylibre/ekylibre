c15aee58561de52eee4d7dbc8986994ce17aefca
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-03-01 11:20:38 +0100

    Rework historical recovery migration

--- db/migrate/20180227144608_historical_recovery_targets_distributions.rb
+++ db/migrate/20180227144608_historical_recovery_targets_distributions.rb
@@ -5,13 +5,9 @@ class HistoricalRecoveryTargetsDistributions < ActiveRecord::Migration
     ActivityProduction.all.each do |activity_production|
       language = activity_production.creator.language
 
-      computed_support_name = []
-      computed_support_name << activity_production.activity.name
-      computed_support_name << activity_production.campaign.name if activity_production.campaign
-      computed_support_name << :rank.t(number: activity_production.rank_number, locale: language)
-
       finded_land_parcel = products_without_production
-                             .where("name like ?", "%#{computed_support_name.join(' ')}%")
+                             .where("name like ?", "%#{activity_production.activity.name}%")
+                             .where("name like ?", "%#{:rank.t(number: activity_production.rank_number, locale: language)}%")
 
       if finded_land_parcel.any?
         finded_land_parcel.first.update_attribute(:activity_production_id, activity_production.id)
