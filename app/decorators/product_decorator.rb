3822d5ae4f984b7aeaa0cba150e73ae043b821c2
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-03-06 14:39:17 +0100

    Resolve hidden inputs/outputs/targets costs

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -4,4 +4,8 @@ class ProductDecorator < Draper::Decorator
   def land_parcel?
     object.is_a?(LandParcel)
   end
+
+  def not_worker_or_equipment?
+    !self.is_a?(Worker) && !product.is_a?(Equipment)
+  end
 end
