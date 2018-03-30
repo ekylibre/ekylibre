406546daca11fb6d152404d1ec941c003dc5adec
thib44 <thibault.finot@gmail.com>
2018-03-30 14:12:44 +0200

    add the migration of planning to add InterventionProposal data

---
+++ db/migrate/20180330121129_add_data_to_intervention_proposal_table.planning_engine.rb
@@ -0,0 +1,8 @@
+# This migration comes from planning_engine (originally 20180330120700)
+class AddDataToInterventionProposalTable < ActiveRecord::Migration
+  def change
+    ActivityProduction.where.not(technical_itinerary: nil).each do |activity_production|
+      activity_production.save
+    end
+  end
+end
