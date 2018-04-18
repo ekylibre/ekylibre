99d2ef26b23eeacffe2df34df10eef973ae6f18f
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-04-18 16:18:31 +0200

    Add naming formats controllers

---
+++ app/controllers/backend/naming_format_land_parcels_controller.rb
@@ -0,0 +1,35 @@
+# == License
+# Ekylibre - Simple agricultural ERP
+# Copyright (C) 2015 Brice Texier, David Joulin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Backend
+  class NamingFormatLandParcelsController < NamingFormatsController
+    def build_example
+      build_example_interactor = NamingFormats::LandParcels::BuildExampleInteractor
+                                   .call(params)
+
+      render json: { example: I18n.t('labels.example', value: build_example_interactor.example) } if build_example_interactor.success?
+      render json: { example: build_example_interactor.error } if build_example_interactor.fail?
+    end
+
+    private
+
+    def permitted_params
+      params.permit(:fields_values)
+    end
+  end
+end
