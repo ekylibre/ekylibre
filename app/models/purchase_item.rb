080a1eb0925ddf3f58757a50ad9b12712314baa2
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-04-25 17:10:23 +0200

    Resolve decimal round problems

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -331,20 +331,16 @@ class PurchaseItem < Ekylibre::Record::Base
     first_reception.id
   end
 
-  def received_quantity
+  def human_received_quantity
     return unless purchase.is_a?(PurchaseOrder) || parcels_purchase_orders_items.empty?
 
-    parcels_purchase_orders_items
-      .select{ |reception_item| reception_item.reception.state.to_sym == :given }
-      .map(&:population)
-      .sum
-      .to_f
+    received_quantity.l(precision: 3)
   end
 
   def quantity_to_receive
     return unless purchase.is_a?(PurchaseOrder) || parcels_purchase_orders_items.empty?
 
-    quantity.to_f - received_quantity
+    (quantity - received_quantity).l(precision: 3)
   end
 
   private
@@ -361,5 +357,12 @@ class PurchaseItem < Ekylibre::Record::Base
   def receptions_count
     return parcels_purchase_invoice_items.count if purchase.is_a?(PurchaseInvoice)
     return parcels_purchase_orders_items.count if purchase.is_a?(PurchaseOrder)
+  end
+
+  def received_quantity
+    parcels_purchase_orders_items
+      .select{ |reception_item| reception_item.reception.state.to_sym == :given }
+      .map(&:population)
+      .sum
   end
 end
