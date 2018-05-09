a0e18aaea9d154a77921efaf4ea1b6a4aecf82d8
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-05-09 11:44:34 +0200

    Db migrate

--- db/structure.sql
+++ db/structure.sql
@@ -19457,3 +19457,5 @@ INSERT INTO schema_migrations (version) VALUES ('20180419152918');
 
 INSERT INTO schema_migrations (version) VALUES ('20180423091042');
 
+INSERT INTO schema_migrations (version) VALUES ('20180503081248');
+
