14d4c51822e9c81b7f60abad6f81ddd25bb12a1c
Alexandre Lécuelle <alecuelle@ekylibre.com>
2018-05-14 10:40:51 +0200

    Change migration to add categories to rollback

--- db/migrate/20180503081248_add_hour_counter_to_product_categories.rb
+++ db/migrate/20180503081248_add_hour_counter_to_product_categories.rb
@@ -8,7 +8,7 @@ class AddHourCounterToProductCategories < ActiveRecord::Migration
   end
 
   def down
-    varieties = [:tractor]
+    varieties = [:tractor, :heavy_equipment, :handling_equipment]
     product_nature_names = [:dumper, :forklift, :truck, :wheel_loader]
 
     update_by_variety(varieties, remove_hour_counter: true)
