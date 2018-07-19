7def83dce46fe9284f5eeb34fdc94b9a96612e77
Charles Duporge <cduporge@ekylibre.com>
2018-07-19 17:04:32 +0200

    Add visual pagination but need js to get the behaviour of pagination

--- app/controllers/backend/invalid_journal_entries_controller.rb
+++ app/controllers/backend/invalid_journal_entries_controller.rb
@@ -1,6 +1,10 @@
 module Backend
   class InvalidJournalEntriesController < Backend::BaseController
-    def index; end
+    def index
+      @invalid_entries = JournalEntry.where.not(balance: 0.0).order(:printed_on)
+      @invalid_entries_count = @invalid_entries.count
+      @invalid_entries = @invalid_entries.page(params[:page]).per(2)
+    end
 
     def delete_all
       JournalEntry.where.not(balance: 0.0).destroy_all
