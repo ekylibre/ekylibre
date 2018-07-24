457845f2e45296f79c26b255d0eb888d45fd6a52
nicolas PROCUREUR <nprocureur@gmail.com>
2018-07-24 17:29:38 +0200

    Adds fallback to accounting system in migration

--- db/migrate/20180712091619_rename_usages_of_accounts_removed_in_prefered_accounting_system.rb
+++ db/migrate/20180712091619_rename_usages_of_accounts_removed_in_prefered_accounting_system.rb
@@ -47,8 +47,11 @@ class RenameUsagesOfAccountsRemovedInPreferedAccountingSystem < ActiveRecord::Mi
   private
 
   def preferred_accounting_system
-    execute(<<-SQL).first['string_value'].to_sym
+    accounting_system = execute(<<-SQL).first
       SELECT  "preferences".string_value FROM "preferences" WHERE "preferences"."name" = 'accounting_system' LIMIT 1
     SQL
+    return :fr_pcg82 unless accounting_system
+    
+    accounting_system['string_value'].to_sym
   end
 end
