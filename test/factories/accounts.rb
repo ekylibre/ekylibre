6ab22e2fedad7b946b523d6df8ed7b47e02009ab
Charles Duporge <cduporge@ekylibre.com>
2018-07-31 15:02:59 +0200

    Add nature and adjusted number length

--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -2,7 +2,20 @@ FactoryBot.define do
   factory :account do
     debtor false
     reconcilable false
-    sequence(:name) { |n| "801 - Compte #{n}" }
-    sequence(:number) { |n| "801000000#{n}" }
+    nature 'general'
+    sequence(:name) { |n| "801 - Compte général #{n}" }
+    sequence(:number) { |n| "8010000#{n}" }
+
+    trait :centralizing do
+      nature 'centralizing'
+      sequence(:name) { |n| "411 - Compte centralisateur #{n}" }
+      sequence(:number) { |n| "411#{n}" }
+    end
+
+    trait :auxiliary do
+      nature 'auxiliary'
+      sequence(:name) { |n| "411 - Compte auxiliaire #{n}" }
+      sequence(:number) { |n| "4110000#{n}" }
+    end
   end
 end
