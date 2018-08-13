2e4c933a252519ea1c441cad5cf3ccaf243e66dc
David JOULIN <ionosphere.consulting@gmail.com>
2018-08-13 18:56:42 +0200

    Update EDI FACT norm and exchanger

--- app/exchangers/vivescia/purchase_exchanger.rb
+++ app/exchangers/vivescia/purchase_exchanger.rb
@@ -37,7 +37,7 @@ module Vivescia
       # use edi_purchase_item.work_number on LIN segment
       catalog_transcode = {}.with_indifferent_access
       CSV.foreach(here.join('catalog.csv'), headers: true) do |row|
-        catalog_transcode[row[1]] = { name: row[4], unit: units_transcode[row[5]], account_number: row[6], account_name: row[7]}
+        catalog_transcode[row[1].to_s] = { name: row[4], unit: units_transcode[row[5]], account_number: row[6], account_name: row[7]}
       end
 
       begin
@@ -57,6 +57,10 @@ module Vivescia
                               supplier: true)
 
       edifact.purchases.each do |edi_purchase|
+
+        # only purchase for product
+        next unless edi_purchase.header.purchase_type == '381'
+
         purchase = PurchaseInvoice.create!(
           planned_at: Time.parse(edi_purchase.header.purchase_printed_on),
           invoiced_at: Time.parse(edi_purchase.header.purchase_printed_on),
@@ -71,24 +75,32 @@ module Vivescia
         edi_purchase.items.each do |edi_purchase_item|
 
           # transcode detail number in EDI into variant nomen
-          pivot = catalog_transcode[edi_purchase_item.work_number]
+          w.info "EDI work number : #{edi_purchase_item.work_number}".yellow
+          pivot = catalog_transcode[edi_purchase_item.work_number.to_s]
+          unless pivot
+            w.error "No item in pivot availables for #{edi_purchase_item.work_number}".red
+            next
+          end
+          account_number = pivot[:account_number].sub!(/0+$/, '')
+          w.info "pivot : #{pivot}".yellow
+          w.info "account_number : #{account_number}".yellow
 
           # try to find the correct variant from id of provider
           product_nature_variant = ProductNatureVariant.where('providers ->> ? = ?', sender.id, edi_purchase_item.work_number).first if edi_purchase_item.work_number.present?
           # try to find the correct variant from name
-          product_nature_variant ||= ProductNatureVariant.where('name ILIKE ?', edi_purchase_item.detail.description).first
+          product_nature_variant ||= ProductNatureVariant.where('name ILIKE ?', edi_purchase_item.detail.description).first if edi_purchase_item.detail && edi_purchase_item.detail.description.present?
           # create the variant
-          unless product_nature_variant
+          if !product_nature_variant && pivot && account_number && account_number != '2621'
             # find the correct category of product bases on pivot
-            account_number = pivot[:account_number].sub!(/0+$/, '')
             account = Account.find_or_create_by_number(account_number) do |a|
               a.name = pivot[:account_name]
             end
+            w.info "account name : #{account.name}".yellow
             # find category by charge or product account
             pnc = ProductNatureCategory.where(charge_account_id: account.id).first
             pnc ||= ProductNatureCategory.where(product_account_id: account.id).first
             unless pnc
-              puts 'No categories availables'
+              w.error "No categories availables for account : #{account.name}".red
               next
             end
             product_nature_variant = ProductNatureVariant.where(category_id: pnc.id).first
@@ -101,10 +113,30 @@ module Vivescia
           item = Nomen::Tax.find_by(country: purchase.supplier.country.to_sym, amount: edi_purchase_item.tax.rate.to_f)
           tax = Tax.import_from_nomenclature(item.name)
 
-          purchase.items.create!(quantity: edi_purchase_item.quantity,
+          if tax && product_nature_variant
+
+            w.info "quantity : #{edi_purchase_item.quantity}".yellow
+            w.info "price unit : #{edi_purchase_item.price_unit}".yellow
+
+            # set price unit to 1 if 0
+            if edi_purchase_item.quantity == 0.0
+              qty = 1.0
+            else
+              qty = edi_purchase_item.quantity
+            end
+
+            # set unit_pretax_amount to pretax_amount if 0
+            if edi_purchase_item.price_unit == 0.0
+              upta = edi_purchase_item.pretax_amount
+            else
+              upta = edi_purchase_item.price_unit
+            end
+
+            purchase.items.create!(quantity: qty,
                                  tax: tax,
-                                 unit_pretax_amount: edi_purchase_item.price_unit,
+                                 unit_pretax_amount: upta,
                                  variant: product_nature_variant)
+          end
         end
 
         w.check_point
