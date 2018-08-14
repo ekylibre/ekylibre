d2009981675b26a4aab66a939b67b8f14ef42cef
David JOULIN <ionosphere.consulting@gmail.com>
2018-08-14 18:57:24 +0200

    Update EDI FACT (in progress)

--- app/exchangers/vivescia/purchase_exchanger.rb
+++ app/exchangers/vivescia/purchase_exchanger.rb
@@ -37,7 +37,7 @@ module Vivescia
       # use edi_purchase_item.work_number on LIN segment
       catalog_transcode = {}.with_indifferent_access
       CSV.foreach(here.join('catalog.csv'), headers: true) do |row|
-        catalog_transcode[row[1].to_s] = { name: row[4], unit: units_transcode[row[5]], account_number: row[6], account_name: row[7]}
+        catalog_transcode[row[1].to_s] = { name: row[4], unit: units_transcode[row[5]], account_number: row[6], account_name: row[7] }
       end
 
       begin
@@ -47,110 +47,209 @@ module Vivescia
       end
 
       w.count = edifact.purchases.count
-      purchase_ids = []
 
       sender_name = 'Vivescia'
       sender = Entity.find_by(last_name: sender_name) ||
                Entity.where('last_name ILIKE ?', sender_name).first ||
                Entity.create!(nature: :organization,
                               last_name: sender_name,
-                              supplier: true)
+                              supplier: true, client: true)
 
       edifact.purchases.each do |edi_purchase|
+        ########################################
+        #  purchase and discount
+        ########################################
 
-        # only purchase for product
-        next unless edi_purchase.header.purchase_type == '381'
+        if %w[AFF AFA ACF].any? { |word| edi_purchase.header.purchase_code.include?(word) }
+          unless purchase = PurchaseInvoice.find_by(reference_number: edi_purchase.header.purchase_number)
+            purchase = PurchaseInvoice.create!(
+              planned_at: Time.parse(edi_purchase.header.purchase_printed_on),
+              invoiced_at: Time.parse(edi_purchase.header.purchase_printed_on),
+              reference_number: edi_purchase.header.purchase_number,
+              supplier: sender,
+              nature: PurchaseNature.actives.first,
+              description: edi_purchase.header.purchase_title
+            )
 
-        purchase = PurchaseInvoice.create!(
-          planned_at: Time.parse(edi_purchase.header.purchase_printed_on),
-          invoiced_at: Time.parse(edi_purchase.header.purchase_printed_on),
-          reference_number: edi_purchase.header.purchase_number,
-          supplier: sender,
-          nature: PurchaseNature.actives.first,
-          description: edi_purchase.header.purchase_title
-        )
+            edi_purchase.items.each do |edi_purchase_item|
+              # transcode detail number in EDI into variant nomen
+              w.info "EDI work number : #{edi_purchase_item.work_number}".yellow
+              pivot = catalog_transcode[edi_purchase_item.work_number.to_s]
+              pivot ||= catalog_transcode[edi_purchase_item.detail.description.to_s]
+              unless pivot
+                w.error "No item in pivot availables for #{edi_purchase_item.work_number}".red
+                next
+              end
+              account_number = pivot[:account_number].sub!(/0+$/, '')
+              w.info "pivot : #{pivot}".yellow
+              w.info "account_number : #{account_number}".yellow
 
-        purchase_ids << purchase.id
+              # try to find the correct variant from id of provider
+              product_nature_variant = ProductNatureVariant.where('providers ->> ? = ?', sender.id, edi_purchase_item.work_number).first if edi_purchase_item.work_number.present?
+              # try to find the correct variant from name
+              product_nature_variant ||= ProductNatureVariant.where('name ILIKE ?', edi_purchase_item.detail.description).first if edi_purchase_item.detail && edi_purchase_item.detail.description.present?
+              # create the variant
+              if !product_nature_variant && pivot
+                # find the correct category of product bases on pivot
+                account = Account.find_or_create_by_number(account_number) do |a|
+                  a.name = pivot[:account_name]
+                end
+                w.info "account name : #{account.name}".yellow
+                # find category by charge or product account
+                pnc = ProductNatureCategory.where(charge_account_id: account.id).first
+                pnc ||= ProductNatureCategory.where(product_account_id: account.id).first
+                unless pnc
+                  w.error "No categories availables for account : #{account.name}".red
+                  next
+                end
+                pnv = ProductNatureVariant.where(category_id: pnc.id).first
+                if pnv
+                  product_nature_variant = ProductNatureVariant.import_from_nomenclature(pnv.reference_name, true)
+                  product_nature_variant.providers = { sender.id => edi_purchase_item.work_number } if edi_purchase_item.work_number.present?
+                  product_nature_variant.name = pivot[:name]
+                  product_nature_variant.unit_name = Nomen::Unit[pivot[:unit]].human_name
+                  product_nature_variant.save!
+                else
+                  w.error "No variant availables for EDI work number : #{edi_purchase_item.work_number}".red
+                end
+              end
 
-        edi_purchase.items.each do |edi_purchase_item|
+              item = Nomen::Tax.find_by(country: purchase.supplier.country.to_sym, amount: edi_purchase_item.tax.rate.to_f)
+              tax = Tax.import_from_nomenclature(item.name)
 
-          # transcode detail number in EDI into variant nomen
-          w.info "EDI work number : #{edi_purchase_item.work_number}".yellow
-          pivot = catalog_transcode[edi_purchase_item.work_number.to_s]
-          unless pivot
-            w.error "No item in pivot availables for #{edi_purchase_item.work_number}".red
-            next
+              next unless tax && product_nature_variant
+
+              w.info "quantity : #{edi_purchase_item.quantity}".yellow
+              w.info "price unit : #{edi_purchase_item.price_unit}".yellow
+
+              # set price unit to 1 if 0
+
+              qty = if edi_purchase_item.quantity == 0.0
+                      if edi_purchase.header.purchase_code.include?('AFA') || edi_purchase.header.purchase_code.include?('ACF')
+                        -1.0
+                      else
+                        1.0
+                            end
+                    else
+                      if edi_purchase.header.purchase_code.include?('AFA') || edi_purchase.header.purchase_code.include?('ACF')
+                        -edi_purchase_item.quantity
+                      else
+                        edi_purchase_item.quantity
+                            end
+                    end
+
+              # set unit_pretax_amount to pretax_amount if 0
+              upta = if edi_purchase_item.price_unit == 0.0
+                       edi_purchase_item.pretax_amount
+                     else
+                       edi_purchase_item.price_unit
+                     end
+
+              purchase.items.create!(quantity: qty,
+                                     tax: tax,
+                                     unit_pretax_amount: upta,
+                                     variant: product_nature_variant)
+            end
           end
-          account_number = pivot[:account_number].sub!(/0+$/, '')
-          w.info "pivot : #{pivot}".yellow
-          w.info "account_number : #{account_number}".yellow
 
-          # try to find the correct variant from id of provider
-          product_nature_variant = ProductNatureVariant.where('providers ->> ? = ?', sender.id, edi_purchase_item.work_number).first if edi_purchase_item.work_number.present?
-          # try to find the correct variant from name
-          product_nature_variant ||= ProductNatureVariant.where('name ILIKE ?', edi_purchase_item.detail.description).first if edi_purchase_item.detail && edi_purchase_item.detail.description.present?
-          # create the variant
-          if !product_nature_variant && pivot && account_number && account_number != '2621'
-            # find the correct category of product bases on pivot
-            account = Account.find_or_create_by_number(account_number) do |a|
-              a.name = pivot[:account_name]
+        ########################################
+        #  sale
+        ########################################
+
+        elsif ['C F', 'CCF', 'CKF'].any? { |word| edi_purchase.header.purchase_code.include?(word) }
+
+          unless sale = Sale.find_by(reference_number: edi_purchase.header.purchase_number)
+
+            sale = Sale.create!(
+              invoiced_at: Time.parse(edi_purchase.header.purchase_printed_on),
+              reference_number: edi_purchase.header.purchase_number,
+              client: sender,
+              nature: SaleNature.actives.first,
+              description: edi_purchase.header.purchase_title
+            )
+
+            edi_purchase.items.each do |edi_sale_item|
+              # transcode detail number in EDI into variant nomen
+              w.info "EDI work number : #{edi_sale_item.work_number}".yellow
+              pivot = catalog_transcode[edi_sale_item.work_number.to_s]
+              pivot ||= catalog_transcode[edi_sale_item.detail.description.to_s]
+              unless pivot
+                w.error "No item in pivot availables for #{edi_sale_item.work_number}".red
+                next
+              end
+              account_number = pivot[:account_number].sub!(/0+$/, '')
+              w.info "pivot : #{pivot}".yellow
+              w.info "account_number : #{account_number}".yellow
+
+              # try to find the correct variant from id of provider
+              product_nature_variant = ProductNatureVariant.where('providers ->> ? = ?', sender.id, edi_sale_item.work_number).first if edi_sale_item.work_number.present?
+              # try to find the correct variant from name
+              product_nature_variant ||= ProductNatureVariant.where('name ILIKE ?', edi_sale_item.detail.description).first if edi_sale_item.detail && edi_sale_item.detail.description.present?
+              # create the variant
+              if !product_nature_variant && pivot
+                # find the correct category of product bases on pivot
+                account = Account.find_or_create_by_number(account_number) do |a|
+                  a.name = pivot[:account_name]
+                end
+                w.info "account name : #{account.name}".yellow
+                # find category by charge or product account
+                pnc = ProductNatureCategory.where(charge_account_id: account.id).first
+                pnc ||= ProductNatureCategory.where(product_account_id: account.id).first
+                unless pnc
+                  w.error "No categories availables for account : #{account.name}".red
+                  next
+                end
+                pnv = ProductNatureVariant.where(category_id: pnc.id).first
+                if pnv
+                  product_nature_variant = ProductNatureVariant.import_from_nomenclature(pnv.reference_name, true)
+                  product_nature_variant.providers = { sender.id => edi_sale_item.work_number } if edi_sale_item.work_number.present?
+                  product_nature_variant.name = pivot[:name]
+                  product_nature_variant.unit_name = Nomen::Unit[pivot[:unit]].human_name
+                  product_nature_variant.save!
+                else
+                  w.error "No variant availables for EDI work number : #{edi_sale_item.work_number}".red
+                end
+              end
+
+              item = Nomen::Tax.find_by(country: sale.client.country.to_sym, amount: edi_sale_item.tax.rate.to_f)
+              tax = Tax.import_from_nomenclature(item.name)
+
+              # set unit_pretax_amount in correct sign
+              upta = if edi_sale_item.sign && edi_sale_item.sign == 'A'
+                       -edi_sale_item.price_unit
+                     else
+                       edi_sale_item.price_unit
+                     end
+
+              if edi_sale_item.quantity > 0.0
+                qty = edi_sale_item.quantity
+              elsif edi_sale_item.pretax_amount > 0.0 && upta > 0.0
+                qty = (edi_sale_item.pretax_amount / upta).to_d
+              elsif edi_sale_item.pretax_amount > 0.0 && upta == 0.0 && edi_sale_item.quantity == 0.0
+                qty = 1.0
+                upta = edi_sale_item.pretax_amount
+              end
+
+              next if sale_item = SaleItem.where(
+                sale_id: sale.id,
+                pretax_amount: edi_sale_item.pretax_amount,
+                variant_id: product_nature_variant.id
+              ).first
+              sale.items.create!(
+                quantity: qty,
+                tax: tax,
+                unit_pretax_amount: upta,
+                variant: product_nature_variant
+              )
             end
-            w.info "account name : #{account.name}".yellow
-            # find category by charge or product account
-            pnc = ProductNatureCategory.where(charge_account_id: account.id).first
-            pnc ||= ProductNatureCategory.where(product_account_id: account.id).first
-            unless pnc
-              w.error "No categories availables for account : #{account.name}".red
-              next
-            end
-            product_nature_variant = ProductNatureVariant.where(category_id: pnc.id).first
-            product_nature_variant.providers = { sender.id => edi_purchase_item.work_number } if edi_purchase_item.work_number.present?
-            product_nature_variant.name = pivot[:name]
-            product_nature_variant.unit_name = Nomen::Unit[pivot[:unit]].human_name
-            product_nature_variant.save!
+
+            sale.propose if sale.draft?
+            sale.confirm
+            sale.invoice(sale.invoiced_at)
+
           end
 
-          item = Nomen::Tax.find_by(country: purchase.supplier.country.to_sym, amount: edi_purchase_item.tax.rate.to_f)
-          tax = Tax.import_from_nomenclature(item.name)
-
-          if tax && product_nature_variant
-
-            w.info "quantity : #{edi_purchase_item.quantity}".yellow
-            w.info "price unit : #{edi_purchase_item.price_unit}".yellow
-
-            # set price unit to 1 if 0
-            if edi_purchase_item.quantity == 0.0
-              qty = 1.0
-            else
-              qty = edi_purchase_item.quantity
-            end
-
-            # set unit_pretax_amount to pretax_amount if 0
-            if edi_purchase_item.price_unit == 0.0
-              upta = edi_purchase_item.pretax_amount
-            else
-              upta = edi_purchase_item.price_unit
-            end
-
-            purchase.items.create!(quantity: qty,
-                                 tax: tax,
-                                 unit_pretax_amount: upta,
-                                 variant: product_nature_variant)
-          end
         end
-
-        w.check_point
-      end
-
-      # Restart counting
-      added_purchases = PurchaseInvoice.where(id: purchase_ids)
-      w.reset! added_purchases.count, :yellow
-
-      # change status of all new added purchases
-      added_purchases.each do |purchase|
-        purchase.propose if purchase.draft?
-        purchase.confirm
-        purchase.invoice(purchase.invoiced_at)
         w.check_point
       end
     end
