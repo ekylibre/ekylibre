9b6371e62d8114d3fe1718f6d317f8cfb6222135
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-09-10 16:50:22 +0200

    Added balanced filters

--- app/controllers/backend/trial_balances_controller.rb
+++ app/controllers/backend/trial_balances_controller.rb
@@ -35,6 +35,11 @@ module Backend
         params[:stopped_on] = params[:period].split('_').last
       end
       @balance = Journal.trial_balance(params) if params[:period]
+      if @balance && params[:balance] == 'balanced'
+        @balance = @balance.select { |item| item[1].to_i < 0 || Account.find(item[1]).journal_entry_items.pluck(:real_balance).reduce(:+) == 0 }
+      elsif @balance && params[:balance] == 'unbalanced'
+        @balance = @balance.select { |item| item[1].to_i < 0 || Account.find(item[1]).journal_entry_items.pluck(:real_balance).reduce(:+) != 0 }
+      end
 
       @prev_balance = []
       if params[:previous_year] && params[:started_on] && Date.parse(params[:stopped_on]) - Date.parse(params[:started_on]) < 366
