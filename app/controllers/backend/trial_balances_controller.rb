faef0f18db28ee95714acbdcbd03c916bee2701f
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-09-11 14:40:18 +0200

    Adjusted pdf style when exporting balance

--- app/controllers/backend/trial_balances_controller.rb
+++ app/controllers/backend/trial_balances_controller.rb
@@ -109,8 +109,8 @@ module Backend
             Account.find(item[0][1]).name if item[0][1].to_i > 0
           end
           t.add_column(:debit) { |item| item[0][2].to_f }
-          t.add_column(:debit_n) { |item| item[1].any? ? item[1][2].to_f : 0 }
           t.add_column(:credit) { |item| item[0][3].to_f }
+          t.add_column(:debit_n) { |item| item[1].any? ? item[1][2].to_f : 0 }
           t.add_column(:credit_n) { |item| item[1].any? ? item[1][3].to_f : 0 }
           t.add_column(:balance) { |item| item[0][4].to_f }
           t.add_column(:balance_n) { |item| item[1].any? ? item[1][4].to_f : 0 }
