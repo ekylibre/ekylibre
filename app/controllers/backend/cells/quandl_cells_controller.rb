ba6be4276e96fe482431cb4b33d0194b47af34c0
Brice Texier <burisu@oneiros.fr>
2018-09-27 17:38:22 +0200

    Clean files

--- app/controllers/backend/cells/quandl_cells_controller.rb
+++ app/controllers/backend/cells/quandl_cells_controller.rb
@@ -6,11 +6,12 @@ module Backend
         finished = '2017-12-31'
         started = '2017-01-01'
         # params[:threshold] = 183.50
-        Quandl::ApiConfig.api_key = 'UcczgS4H7VXfcqN6-GgM'
+        api_key = ENV['QUANDL_API_KEY']
+        Quandl::ApiConfig.api_key = api_key
         # Quandl::ApiConfig.api_version = '2015-04-09'
         dataset = params[:dataset] || 'CHRIS/LIFFE_EBM4'
         identifier = Identifier.find_by(nature: :quandl_token)
-        token = (identifier ? identifier.value : 'UcczgS4H7VXfcqN6-GgM')
+        token = (identifier ? identifier.value : api_key)
         @data = Quandl::Dataset.get(dataset).data(params: { start_date: started, end_date: finished, order: 'asc' })
         @data if @data.any?
       end
