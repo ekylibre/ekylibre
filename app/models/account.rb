2726d09995724620fe89908173b7ffe513973de4
Charles Duporge <cduporge@ekylibre.com>
2018-09-28 17:10:33 +0200

    Fix wrong set of item in hash

--- app/models/account.rb
+++ app/models/account.rb
@@ -237,7 +237,7 @@ class Account < Ekylibre::Record::Base
         options[:name] ||= number.to_s
         merge_attributes = {
           number: number,
-          already_existing: (options[:already_existing] ? already_existing : false)
+          already_existing: (options[:already_existing] ? options[:already_existing] : false)
         }
         account = create!(options.merge(merge_attributes))
       end
