a06cf8485a1dbaad47e36d0bf19cea9b51dcb216
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-10-10 15:03:14 +0200

    Fixed FEC export

--- app/concepts/fec/exporter/xml.rb
+++ app/concepts/fec/exporter/xml.rb
@@ -4,8 +4,15 @@ module FEC
       private
 
       def build(journals)
+        validators = {
+                       "all" => "formatA47A-I-VII-1.xsd",
+                       "ba_bnc_ir_commercial_accountancy" => "formatA47A-I-VIII-3.xsd",
+                       "ba_ir_cash_accountancy" => "formatA47A-I-VIII-5.xsd",
+                       "bnc_ir_cash_accountancy" => "formatA47A-I-VIII-7.xsd"
+                     }
+
         builder = Nokogiri::XML::Builder.new(encoding: 'ISO-8859-15') do |xml|
-          xml.comptabilite do
+          xml.comptabilite('xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:noNamespaceSchemaLocation' => validators[fiscal_position]) do
             xml.exercice do
               xml.DateCloture @financial_year.stopped_on.strftime('%Y-%m-%d')
               journals.each do |journal|
@@ -14,8 +21,8 @@ module FEC
                 xml.journal do
                   xml.JournalCode journal.code
                   xml.JournalLib journal.name
-                  entries.includes(items: :account).references(items: :account).find_each do |entry|
-                    next if entry.items.empty?
+                  entries.includes(:incoming_payments, :purchase_payments, items: :account).references(items: :account).find_each do |entry|
+                    next if entry.items.empty? || ((fiscal_position == 'ba_ir_cash_accountancy' || fiscal_position == 'bnc_ir_cash_accountancy') && !entry.first_payment)
                     resource = Maybe(entry.resource)
                     xml.ecriture do
                       xml.EcritureNum (entry.continuous_number? ? entry.continuous_number : '')
@@ -26,10 +33,10 @@ module FEC
                       xml.EcritureLet entry.letter if entry.letter
                       # xml.DateLet
                       xml.ValidDate (entry.validated_at? ? entry.validated_at.strftime('%Y-%m-%d') : '')
-                      xml.DateRglt entry.first_payment.paid_at if entry.first_payment && fiscal_position == :ba_ir_cash_accountancy
-                      xml.ModeRglt entry.first_payment.mode.name if entry.first_payment && fiscal_position == :ba_ir_cash_accountancy
-                      xml.NatOp '' if fiscal_position == :ba_ir_cash_accountancy
-                      xml.IdClient (resource.client? ? resource.client.number : (resource.supplier? ? resource.supplier.full_name : '')) if resource && fiscal_position == :bnc_ir_cash_accountancy
+                      xml.DateRglt entry.first_payment.paid_at.strftime('%Y-%m-%d') if entry.first_payment && fiscal_position == 'ba_ir_cash_accountancy'
+                      xml.ModeRglt entry.first_payment.mode.name if entry.first_payment && fiscal_position == 'ba_ir_cash_accountancy'
+                      xml.NatOp '' if fiscal_position == 'ba_ir_cash_accountancy'
+                      xml.IdClient (resource.has_attribute?(:client_id).get && resource.client.get ? resource.client.get.number : (resource.has_attribute?(:supplier_id).get && resource.supplier.get ? resource.supplier.get.full_name : '')) if resource.is_some? && fiscal_position == 'bnc_ir_cash_accountancy'
 
                       entry.items.find_each do |item|
                         xml.ligne do
