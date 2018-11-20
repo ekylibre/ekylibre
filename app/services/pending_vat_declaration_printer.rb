144a57146e9f9dfa1d95d005c95eb9288e550e8d
David JOULIN <ionosphere.consulting@gmail.com>
2018-11-20 15:06:25 +0100

    Build dataset for pending vat declaration

--- app/services/pending_vat_declaration_printer.rb
+++ app/services/pending_vat_declaration_printer.rb
@@ -14,35 +14,63 @@ class PendingVatDeclarationPrinter
 
       vat_dataset = []
 
-      if @params[:id]
-        started_on =
-        stopped_on =
+      if @params[:id].to_i > 0
         taxe_declaration = TaxDeclaration.find(@params[:id])
+        taxes = Tax.where(id: taxe_declaration.items.pluck(:tax_id)).reorder(amount: :desc)
       end
 
-      taxe_declaration.each do |td|
-        td.items.includes(parts: { journal_entry_item: :entry }).each do |i|
-          i.parts.each do |p|
-            jei = p.journal_entry_item
-            e = jei.entry
-            item = HashWithIndifferentAccess.new
-            item[:entry_id] = e.id
-            item[:entry_number] = e.number
-            item[:entry_printed_on] = e.printed_on.l
-            item[:entry_month_name] = I18n.l(Date::MONTHNAMES[e.printed_on.month])
-            item[:entry_item_account_number] = jei.account.number
-            item[:entry_item_account_name] = jei.account.name
-            item[:entry_item_name] = jei.name
-            item[:entry_item_debit] = jei.debit.to_f
-            item[:entry_item_credit] = jei.credit.to_f
-            item[:tax_amount] = p.tax_amount.to_f
-            item[:pretax_amount] = p.pretax_amount.to_f
-            item[:tax_name] = jei.tax.name
-            item[:tax_account] = jei.vat_item_to_product_account
-            vat_dataset << item
+      columns = [:collected, :intracommunity_payable, :deductible, :fixed_asset_deductible]
+      account_transcode = { collected: :collect_account_id, deductible: :deduction_account_id, fixed_asset_deductible: :fixed_asset_deduction_account_id, intracommunity_payable: :intracommunity_payable_account_id }
+
+      columns.each do |c|
+        #section 1
+        cat = HashWithIndifferentAccess.new
+        cat[:name] = TaxDeclarationItem.human_attribute_name(c.to_sym)
+        cat[:items] = []
+        taxes.each do |t|
+          #section 2
+          cat_taxe = HashWithIndifferentAccess.new
+          cat_taxe[:name] = t.name
+          cat_taxe[:items] = []
+          taxe_total_tax = 0.0
+          taxe_total_pretax = 0.0
+          taxe_declaration.items.where(tax_id: t.id).includes(parts: { journal_entry_item: :entry }).each do |i|
+            #table 1
+            i.parts.each do |p|
+              jei = p.journal_entry_item
+              e = jei.entry
+              item = HashWithIndifferentAccess.new
+              item[:entry_number] = e.number
+              item[:entry_printed_on] = e.printed_on.l
+              item[:item_account] = jei.vat_item_to_product_account
+              item[:entry_item_name] = jei.name
+              item[:tax_amount] = p.tax_amount.to_f
+              item[:pretax_amount] = p.pretax_amount.to_f
+              item[:amount] = (p.pretax_amount.to_f + p.tax_amount.to_f).round(2)
+              cat_taxe[:items] << item
+            end
+
+            taxe_total_tax += i.send("#{c}_tax_amount")
+            taxe_total_pretax += i.send("#{c}_pretax_amount")
           end
+          cat_taxe[:pretax_amount] = taxe_total_pretax
+          cat_taxe[:tax_amount] = taxe_total_tax
+          cat[:items] << cat_taxe
         end
+        cat[:pretax_amount] = taxe_declaration.items.sum("#{c}_pretax_amount")
+        cat[:tax_amount] = taxe_declaration.items.sum("#{c}_tax_amount")
+        vat_dataset << cat
       end
+      to_pay = taxe_declaration.items.sum(:balance_tax_amount)
+      if to_pay >= 0.0
+        vat_label  = :to_pay.tl
+        vat_balance = to_pay
+      else
+        vat_label = :to_reclaim.tl
+        vat_balance = -to_pay
+      end
+      vat_dataset << vat_label
+      vat_dataset << vat_balance
       vat_dataset.compact
     end
 
@@ -56,88 +84,28 @@ class PendingVatDeclarationPrinter
 
         # build filters
         data_filters = []
-        if @params[:state]&.any?
-            content = []
-            content << :draft.tl if @params[:state].include?('draft')
-            content << :validated.tl if @params[:state].include?('validated')
-            content << :sent.tl if @params[:state].include?('sent')
-            data_filters << :tax_declarations_states.tl + ' : ' + content.to_sentence
-        end
 
         # build started and stopped
-
-        if @params[:period] == 'all'
-          started_on = FinancialYear.order(:started_on).pluck(:started_on).first.to_s
-          stopped_on = FinancialYear.order(:stopped_on).pluck(:stopped_on).last.to_s
-        else
-          started_on = @params[:period].split("_").first if @params[:period]
-          stopped_on = @params[:period].split("_").last if @params[:period]
+        if @params[:id].to_i > 0
+          taxe_declaration = TaxDeclaration.find(@params[:id])
+          started_on = taxe_declaration.started_on
+          stopped_on = taxe_declaration.stopped_on
         end
 
         r.add_field 'COMPANY_ADDRESS', company_address
         r.add_field 'DOCUMENT_NAME', @document_nature.human_name
         r.add_field 'FILE_NAME', @key
-        r.add_field 'PERIOD', @params[:period] == 'all' ? :on_all_exercises.tl : I18n.translate('labels.from_to_date', from: Date.parse(@params[:period].split('_').first).l, to: Date.parse(@params[:period].split('_').last).l)
+        r.add_field 'PERIOD', I18n.translate('labels.from_to_date', from: started_on.l, to: stopped_on.l)
         r.add_field 'DATE', Date.today.l
         r.add_field 'STARTED_ON', started_on.to_date.l
         r.add_field 'STOPPED_ON', stopped_on.to_date.l
         r.add_field 'PRINTED_AT', Time.zone.now.l(format: '%d/%m/%Y %T')
         r.add_field 'DATA_FILTERS', data_filters * ' | '
 
-        r.add_table('Tableau1', compute_dataset, header: true) do |t|
-          t.add_column(:entry_id) { |item| item[:entry_id] }
-          t.add_column(:entry_number) { |item| item[:entry_number] }
-          t.add_column(:entry_printed_on) { |item| item[:entry_printed_on] }
-          t.add_column(:entry_item_account_number) { |item| item[:entry_item_account_number] }
-          t.add_column(:entry_item_account_name) { |item| item[:entry_item_account_name] }
-          t.add_column(:entry_item_name) { |item| item[:entry_item_name] }
-          t.add_column(:entry_item_debit) { |item| item[:entry_item_debit] }
-          t.add_column(:entry_item_credit) { |item| item[:entry_item_credit] }
-          t.add_column(:tax_amount) { |item| item[:tax_amount] }
-          t.add_column(:pretax_amount) { |item| item[:pretax_amount] }
-          t.add_column(:tax_account) { |item| item[:tax_account] }
-        end
+        #TODO
 
       end
       report.file.path
-    end
-
-    def run_csv
-      csv_string = CSV.generate(headers: true) do |csv|
-        csv << [
-          JournalEntry.human_attribute_name(:id),
-          JournalEntry.human_attribute_name(:number),
-          JournalEntry.human_attribute_name(:printed_on),
-          JournalEntry.human_attribute_name(:printed_on),
-          Account.human_attribute_name(:number),
-          Account.human_attribute_name(:name),
-          JournalEntryItem.human_attribute_name(:name),
-          JournalEntryItem.human_attribute_name(:debit),
-          JournalEntryItem.human_attribute_name(:credit),
-          TaxDeclarationItemPart.human_attribute_name(:tax_amount),
-          TaxDeclarationItemPart.human_attribute_name(:pretax_amount),
-          Tax.human_attribute_name(:name),
-          Account.human_attribute_name(:label),
-        ]
-
-        compute_dataset.each do |item|
-            csv << [
-              item[:entry_id],
-              item[:entry_number],
-              item[:entry_printed_on],
-              item[:entry_month_name],
-              item[:entry_item_account_number],
-              item[:entry_item_account_name],
-              item[:entry_item_name],
-              item[:entry_item_debit],
-              item[:entry_item_credit],
-              item[:tax_amount],
-              item[:pretax_amount],
-              item[:tax_name],
-              item[:tax_account]
-            ]
-        end
-      end
     end
 
 end
