2042d349ea6b434b076428a71c000134df4d7251
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-11-27 18:07:45 +0100

    Added tests

--- test/controllers/backend/financial_years_controller_test.rb
+++ test/controllers/backend/financial_years_controller_test.rb
@@ -30,5 +30,29 @@ module Backend
       post :lock, id: financial_year.id
       assert FixedAssetDepreciation.up_to(financial_year.stopped_on).where(locked: false).empty?
     end
+
+    test 'the user in charge of a financial year closure is the only one who can keep adding entries for this period' do
+      FinancialYear.delete_all
+      financial_year = create(:financial_year, started_on: Date.today.beginning_of_year, stopped_on: Date.today.end_of_year)
+
+      sign_in User.first
+      post :prepare_for_closure, id: financial_year.id, redirect: backend_financial_years_path
+
+      assert_equal FinancialYear.first.closer, User.first
+      assert_equal FinancialYear.first.state, 'closure_in_preparation'
+
+      assert create(:journal_entry, :with_items, printed_on: Date.today, creator: User.first)
+      assert_raises ActiveRecord::RecordInvalid do
+        create(:journal_entry, :with_items, printed_on: Date.today, creator: User.second)
+      end
+
+      delete :prepare_for_closure, id: financial_year.id, redirect: backend_financial_years_path
+
+      assert_nil FinancialYear.first.closer
+      assert_equal FinancialYear.first.state, 'opened'
+
+      assert create(:journal_entry, :with_items, printed_on: Date.today, creator: User.first)
+      assert create(:journal_entry, :with_items, printed_on: Date.today, creator: User.second)
+    end
   end
 end
