0a210685a828e23a30e440159f42a3f27de6eaf4
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-12-13 11:49:36 +0100

    Added filled template file

--- app/services/centralizing_journal_printer.rb
+++ app/services/centralizing_journal_printer.rb
@@ -34,11 +34,35 @@ class CentralizingJournalPrinter
 
   def run_pdf
     dataset = compute_dataset
-    pp dataset
 
-    # report = generate_document(@document_nature, @key, @template_path) do |r|
+    report = generate_document(@document_nature, @key, @template_path) do |r|
 
-    # end
-    # report.file.path
+      e = Entity.of_company
+      company_name = e.full_name
+      company_address = e.default_mail_address&.coordinate
+
+      r.add_field 'COMPANY_ADDRESS', company_address
+      r.add_field 'DOCUMENT_NAME', I18n.translate('labels.centralizing_journal')
+      r.add_field 'FILE_NAME', @key
+      r.add_field 'STARTED_ON', @financial_year.started_on.l
+      r.add_field 'STOPPED_ON', @financial_year.stopped_on.l
+      r.add_field 'PRINTED_AT', Time.zone.now.l(format: '%d/%m/%Y %T')
+      r.add_field 'YEAR_DEBIT', dataset[:year_debit]
+      r.add_field 'YEAR_CREDIT', dataset[:year_credit]
+
+      r.add_section('Section1', dataset[:months]) do |s|
+        s.add_field(:month) { |month| month[:month] }
+        s.add_field(:month_debit) { |month| month[:month_debit] }
+        s.add_field(:month_credit) { |month| month[:month_credit] }
+
+        s.add_table('Table5', :journals) do |t|
+          t.add_column(:journal_code) { |journal| journal[:journal_code] }
+          t.add_column(:journal_name) { |journal| journal[:journal_name] }
+          t.add_column(:journal_debit) { |journal| journal[:journal_debit] }
+          t.add_column(:journal_credit) { |journal| journal[:journal_credit] }
+        end
+      end
+    end
+    report.file.path
   end
 end
