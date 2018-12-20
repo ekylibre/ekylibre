e4ea4669a44ac6a22a90cb9dae1922afb1707943
Rémi Piotaix <rpiotaix@ekylibre.com>
2018-12-20 11:52:06 +0100

    fixed translations

--- app/validators/collection_validator.rb
+++ app/validators/collection_validator.rb
@@ -1,7 +1,7 @@
 class CollectionValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.nil? || !value.kind_of?(Array)
-      record.errors.add(attribute, :should_be_set)
+      record.errors.add(attribute, :invalid)
     elsif value.any?(&:invalid?)
       record.errors.add(attribute, :invalid_child)
     end
