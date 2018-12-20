6522e225bcfd68bc56f1551868f3b4bde1aedfc6
David JOULIN <ionosphere.consulting@gmail.com>
2018-12-20 16:42:34 +0100

    Balance sheet (in progress)

--- app/services/balance_sheet_printer.rb
+++ app/services/balance_sheet_printer.rb
@@ -128,6 +128,114 @@ class BalanceSheetPrinter
     g4[:previous_net_total] = (g4[:previous_raw_total].to_d - g4[:previous_variations_total].to_d).round(2)
     actif << g4
 
+    # financial_assets - 261...
+    g5 = HashWithIndifferentAccess.new
+    g5[:group_name] = :financial_assets.tl
+    g5[:items] = []
+    items = [:financial_assets_participations, :financial_assets_participations_debts,
+             :financial_assets_others]
+    items.each do |item|
+      i = HashWithIndifferentAccess.new
+      i[:name] = item.to_s.tl
+      i[:current_raw_value] = current_compute.sum_entry_items_by_line(document_scope, item)
+      i[:current_variations] = current_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:current_net_value] = (i[:current_raw_value].to_d - i[:current_variations].to_d).round(2)
+      i[:previous_raw_value] = previous_compute.sum_entry_items_by_line(document_scope, item)
+      i[:previous_variations] = previous_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:previous_net_value] = (i[:previous_raw_value].to_d - i[:previous_variations].to_d).round(2)
+      g5[:items] << i
+      # puts g5.inspect.yellow
+    end
+    g5[:sum_name] = ''
+    g5[:current_raw_total] = current_compute.sum_entry_items_by_line(document_scope, :financial_assets_total)
+    g5[:current_variations_total] = current_compute.sum_entry_items_by_line(document_scope, :financial_assets_total_depreciations)
+    g5[:current_net_total] = (g5[:current_raw_total].to_d - g5[:current_variations_total].to_d).round(2)
+    g5[:previous_raw_total] = previous_compute.sum_entry_items_by_line(document_scope, :financial_assets_total)
+    g5[:previous_variations_total] = previous_compute.sum_entry_items_by_line(document_scope, :financial_assets_total_depreciations)
+    g5[:previous_net_total] = (g5[:previous_raw_total].to_d - g5[:previous_variations_total].to_d).round(2)
+    actif << g5
+
+    # long_cycle_alive_products - 31...
+    g6 = HashWithIndifferentAccess.new
+    g6[:group_name] = :long_cycle_alive_products.tl
+    g6[:items] = []
+    items = [:long_cycle_alive_products_animals, :long_cycle_alive_products_plant_advance,
+             :long_cycle_alive_products_plant_in_ground, :long_cycle_alive_products_wine,
+             :long_cycle_alive_products_others]
+    items.each do |item|
+      i = HashWithIndifferentAccess.new
+      i[:name] = item.to_s.tl
+      i[:current_raw_value] = current_compute.sum_entry_items_by_line(document_scope, item)
+      i[:current_variations] = current_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:current_net_value] = (i[:current_raw_value].to_d - i[:current_variations].to_d).round(2)
+      i[:previous_raw_value] = previous_compute.sum_entry_items_by_line(document_scope, item)
+      i[:previous_variations] = previous_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:previous_net_value] = (i[:previous_raw_value].to_d - i[:previous_variations].to_d).round(2)
+      g6[:items] << i
+      # puts g5.inspect.yellow
+    end
+    g6[:sum_name] = ''
+    g6[:current_raw_total] = current_compute.sum_entry_items_by_line(document_scope, :long_cycle_alive_products_total)
+    g6[:current_variations_total] = current_compute.sum_entry_items_by_line(document_scope, :long_cycle_alive_products_total_depreciations)
+    g6[:current_net_total] = (g6[:current_raw_total].to_d - g6[:current_variations_total].to_d).round(2)
+    g6[:previous_raw_total] = previous_compute.sum_entry_items_by_line(document_scope, :long_cycle_alive_products_total)
+    g6[:previous_variations_total] = previous_compute.sum_entry_items_by_line(document_scope, :long_cycle_alive_products_total_depreciations)
+    g6[:previous_net_total] = (g6[:previous_raw_total].to_d - g6[:previous_variations_total].to_d).round(2)
+    actif << g6
+
+    # short_cycle_alive_products - 32...
+    g7 = HashWithIndifferentAccess.new
+    g7[:group_name] = :short_cycle_alive_products.tl
+    g7[:items] = []
+    items = [:short_cycle_alive_products_animals, :short_cycle_alive_products_plant_advance,
+             :short_cycle_alive_products_plant_in_ground, :short_cycle_alive_products_others]
+    items.each do |item|
+      i = HashWithIndifferentAccess.new
+      i[:name] = item.to_s.tl
+      i[:current_raw_value] = current_compute.sum_entry_items_by_line(document_scope, item)
+      i[:current_variations] = current_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:current_net_value] = (i[:current_raw_value].to_d - i[:current_variations].to_d).round(2)
+      i[:previous_raw_value] = previous_compute.sum_entry_items_by_line(document_scope, item)
+      i[:previous_variations] = previous_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:previous_net_value] = (i[:previous_raw_value].to_d - i[:previous_variations].to_d).round(2)
+      g7[:items] << i
+      # puts g5.inspect.yellow
+    end
+    g7[:sum_name] = ''
+    g7[:current_raw_total] = current_compute.sum_entry_items_by_line(document_scope, :short_cycle_alive_products_total)
+    g7[:current_variations_total] = current_compute.sum_entry_items_by_line(document_scope, :short_cycle_alive_products_total_depreciations)
+    g7[:current_net_total] = (g7[:current_raw_total].to_d - g7[:current_variations_total].to_d).round(2)
+    g7[:previous_raw_total] = previous_compute.sum_entry_items_by_line(document_scope, :short_cycle_alive_products_total)
+    g7[:previous_variations_total] = previous_compute.sum_entry_items_by_line(document_scope, :short_cycle_alive_products_total_depreciations)
+    g7[:previous_net_total] = (g7[:previous_raw_total].to_d - g7[:previous_variations_total].to_d).round(2)
+    actif << g7
+
+    # Stock - 30...
+    g8 = HashWithIndifferentAccess.new
+    g8[:group_name] = :stocks.tl
+    g8[:items] = []
+    items = []
+    items.each do |item|
+      i = HashWithIndifferentAccess.new
+      i[:name] = item.to_s.tl
+      i[:current_raw_value] = current_compute.sum_entry_items_by_line(document_scope, item)
+      i[:current_variations] = current_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:current_net_value] = (i[:current_raw_value].to_d - i[:current_variations].to_d).round(2)
+      i[:previous_raw_value] = previous_compute.sum_entry_items_by_line(document_scope, item)
+      i[:previous_variations] = previous_compute.sum_entry_items_by_line(document_scope, (item.to_s + "_depreciations").to_sym)
+      i[:previous_net_value] = (i[:previous_raw_value].to_d - i[:previous_variations].to_d).round(2)
+      g8[:items] << i
+      # puts g5.inspect.yellow
+    end
+    g8[:sum_name] = ''
+    g8[:current_raw_total] = current_compute.sum_entry_items_by_line(document_scope, :stocks_total)
+    g8[:current_variations_total] = current_compute.sum_entry_items_by_line(document_scope, :stocks_total_depreciations)
+    g8[:current_net_total] = (g8[:current_raw_total].to_d - g8[:current_variations_total].to_d).round(2)
+    g8[:previous_raw_total] = previous_compute.sum_entry_items_by_line(document_scope, :stocks_total)
+    g8[:previous_variations_total] = previous_compute.sum_entry_items_by_line(document_scope, :stocks_total_depreciations)
+    g8[:previous_net_total] = (g8[:previous_raw_total].to_d - g8[:previous_variations_total].to_d).round(2)
+    actif << g8
+
     dataset << actif
 
 
