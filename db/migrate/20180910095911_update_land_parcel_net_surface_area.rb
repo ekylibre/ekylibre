fd07c0c7e0ad2d3dac78f9a1b7d421041a2a7ea5
nicolas PROCUREUR <nprocureur@gmail.com>
2019-01-10 16:07:55 +0100

    Makes sql queries readeable

--- db/migrate/20180910095911_update_land_parcel_net_surface_area.rb
+++
@@ -1,7 +0,0 @@
-class UpdateLandParcelNetSurfaceArea < ActiveRecord::Migration
-  def change
-    LandParcel.find_each do |p|
-      p.update_column(:reading_cache, {net_surface_area: p.calculate_net_surface_area})
-    end
-  end
-end
