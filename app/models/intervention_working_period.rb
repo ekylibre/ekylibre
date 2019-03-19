c1155859262bc08f4005304ed9f166391f0e7750
romain <rgrossard@ekylibre.com>
2019-03-19 16:30:59 +0100

    display error on intervention with base

--- app/models/intervention_working_period.rb
+++ app/models/intervention_working_period.rb
@@ -89,6 +89,12 @@ class InterventionWorkingPeriod < Ekylibre::Record::Base
   end
 
   validate do
+    if started_at
+      errors.add(:base, :not_opened_financial_year) if Preference[:permanent_stock_inventory] && !during_financial_year_exchange?
+    end
+  end
+
+  validate do
     errors.add(:intervention, :empty) unless intervention || intervention_participated_to || intervention_participation
     if started_at && stopped_at && stopped_at <= started_at
       errors.add(:stopped_at, :posterior, to: started_at.l)
@@ -150,6 +156,10 @@ class InterventionWorkingPeriod < Ekylibre::Record::Base
 
   def duration_gap
     (stopped_at - started_at) / 3600
+  end
+
+  def during_financial_year_exchange?
+    FinancialYearExchange.opened.where('? BETWEEN started_on AND stopped_on', started_at).any?
   end
 
   private
