5fe9c558d910d0491fc522aaee2c7f65f6845477
Romain Grossard <rgrossard@gmail.com>
2019-04-05 10:18:15 +0200

    Update tax_declarations_controller.rb

--- app/controllers/backend/tax_declarations_controller.rb
+++ app/controllers/backend/tax_declarations_controller.rb
@@ -65,9 +65,9 @@ module Backend
       respond_to do |format|
         format.html do
           no_financial_year_opened = FinancialYear.opened.empty?
-          current_financial_year_with_tax_declaration = FinancialYear.with_tax_declaration.exclude?(FinancialYear.current)
+          current_financial_year_without_tax_declaration = FinancialYear.with_tax_declaration.exclude?(FinancialYear.current)
           vat_declarations_fullfilled = FinancialYear.current && !FinancialYear.current.missing_tax_declaration?
-          render "alert_no_VAT_declaration" if not_opened_financial_year || opened_financial_year_with_tax_declaration_mode_none || vat_declarations_fullfilled
+          render "alert_no_VAT_declaration" if not_opened_financial_year || current_financial_year_without_tax_declaration || vat_declarations_fullfilled
         end
         format.pdf do
           VatExportJob.perform_later('vat_register', key, 'general', 'pdf', params, current_user)
