5e6ad50a3dc7def7ae2b1110808cf00a3743f759
Rémi Piotaix <remi@piotaix.fr>
2019-04-11 10:59:08 +0200

    Implement method_or_callable pattern for transformer

--- app/validators/exchanger_validator.rb
+++ app/validators/exchanger_validator.rb
@@ -8,7 +8,7 @@ class ExchangerValidator < ActiveModel::EachValidator
     def is_valid_exchanger?(record, value)
       transformer = options.fetch(:transform, nil)
       if transformer
-        value = transformer.call(record, value)
+        value = transformer.is_a?(Symbol) ? record.send(transformer, value) : transformer.call(record, value)
       end
 
       ActiveExchanger::Base.find_by value
