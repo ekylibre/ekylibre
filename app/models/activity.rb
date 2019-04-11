04db8cf1c3edf331ddfb2cdb55fa3b2e5e06fffd
Charles Duporge <cduporge@ekylibre.com>
2019-04-11 11:33:42 +0200

    Ensure pfi_activity doesn't return nil if conditions of calculation are not met

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -241,6 +241,7 @@ class Activity < Ekylibre::Record::Base
   end
 
   def pfi_activity_ratio(campaign)
+    pfi_activity = 0
     global_area = []
     production_pfi_per_area = []
     productions.of_campaign(campaign).each do |production|
