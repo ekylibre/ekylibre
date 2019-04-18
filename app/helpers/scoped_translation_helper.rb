c84da451d33c42ac0ad49eaaa0735cc3cfced168
Rémi Piotaix <remi@piotaix.fr>
2019-04-18 11:03:50 +0200

    Fixing ScopedTranslationHelper module temporarily to have it behave correctly with Rails 4

--- app/helpers/scoped_translation_helper.rb
+++ app/helpers/scoped_translation_helper.rb
@@ -1,33 +1,42 @@
 module ScopedTranslationHelper
-  def stl(unit, **options)
-    return unit if unit.is_a?(String) && unit == ''
-    t translation_key(i18n_scope, unit), options
-  end
-
-  def with_i18n_scope(*parts, replace: false)
-    orig = i18n_scope
-    if replace
-      i18n_scope_set parts
-    else
-      i18n_scope_set i18n_scope, parts
+  # TODO: Put this back into the module after Rails 5 upgrade
+  class << self
+    def i18n_scope
+      @i18n_scope ||= ''
     end
-    result = yield
-    i18n_scope_set orig
 
-    result
-  end
+    def i18n_scope_set(*scope)
+      @i18n_scope = translation_key *scope
+    end
 
-  def i18n_scope
-    @i18n_scope ||= ''
-  end
+    def with_i18n_scope(*parts, replace: false)
+      orig = i18n_scope
+      if replace
+        i18n_scope_set parts
+      else
+        i18n_scope_set i18n_scope, parts
+      end
 
-  def i18n_scope_set(*scope)
-    @i18n_scope = translation_key *scope
-  end
+      begin
+        result = yield
+      ensure
+        i18n_scope_set orig
+      end
 
-  private
+      result
+    end
 
     def translation_key(*parts)
       parts.flatten.map(&:to_s).join('.')
     end
+  end
+
+  def with_i18n_scope(*parts, replace: false, &block)
+    ScopedTranslationHelper.with_i18n_scope *parts, replace: replace, &block
+  end
+
+  def stl(unit, **options)
+    return unit if unit.is_a?(String) && unit == ''
+    t ScopedTranslationHelper.translation_key(ScopedTranslationHelper.i18n_scope, unit), options
+  end
 end
