c5910a1b09f9e31f9db491efd5ab48f85b939594
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2019-05-10 16:33:28 +0200

    rework based on comment (only refacto) and remove empty test files

--- app/decorators/reception_item_decorator.rb
+++ app/decorators/reception_item_decorator.rb
@@ -1,17 +1,8 @@
 class ReceptionItemDecorator < Draper::Decorator
   delegate_all
 
-  # def reception_number
-  #   return '' if object.parcels_purchase_invoice_items.nil? || object.parcels_purchase_invoice_items.first.nil?
-
-  #   reception_id = object.parcels_purchase_invoice_items.first.parcel_id
-
-  #   Reception.find_by(id: reception_id)&.reference_number || ''
-  # end
-
   def merchandise_stock_after_reception(variant_quantity)
-    quantity ||= 0
-    variant_quantity + quantity
+    variant_quantity + (quantity || 0)
   end
 
   def merchandise_stock_unit(variant_unit)
--- test/decorators/parcel_item_storing_decorator_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ParcelItemStoringDecoratorTest < Draper::TestCase
-end
