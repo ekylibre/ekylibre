8121ded2bd76032f0552a675420586e39dd3e454
romain <rgrossard@ekylibre.com>
2019-06-03 15:14:27 +0200

    Allowed fixed_asset import

--- app/exchangers/isagri/isacompta/fixed_assets_exchanger.rb
+++ app/exchangers/isagri/isacompta/fixed_assets_exchanger.rb
@@ -1,95 +1,12 @@
 module Isagri
   module Isacompta
     class FixedAssetsExchanger < ActiveExchanger::Base
-      # Check fixed assets
-      def check
-        valid = true
-        source = File.read(file)
-        detection = CharlockHolmes::EncodingDetector.detect(source)
-        rows = CSV.read(file, headers: true, col_sep: ';', encoding: detection[:encoding])
-        w.count = rows.size
-
-        # status to map
-        depreciation_method_transcode = {
-          'Linéaire' => :linear,
-          'Non amortissable' => :none,
-          'Dégressif' => :regressive
-        }
-
-        # get default allocation and expenses account
-        exchange_allocation_account = Account.find_or_import_from_nomenclature(:fixed_asset_depreciations) # 28
-        exchange_expenses_account = Account.find_or_import_from_nomenclature(:depreciations_inputations_expenses) # 68
-
-        unless exchange_allocation_account && exchange_expenses_account
-          w.error 'No way to have exchange accounts to build fixed asset'
-          valid = false
-        end
-
-        rows.each_with_index do |row, index|
-          line_number = index + 2
-          prompt = "L#{line_number.to_s.yellow}"
-
-          r = {
-            asset_account: row[0].blank? ? nil : row[0].to_s,
-            number: row[1].blank? ? '' : row[1].to_s,
-            name: row[2].blank? ? nil : row[2].to_s,
-            purchase_on: row[4].blank? ? nil : Date.strptime(row[4].to_s, '%d/%m/%y'),
-            purchase_amount: row[5].blank? ? nil : row[5].tr(',', '.').to_d,
-            depreciation_method: row[6].blank? ? nil : depreciation_method_transcode[row[6].to_s.strip],
-            in_use_on: row[7].blank? ? nil : Date.strptime(row[7].to_s, '%d/%m/%y'),
-            asset_amount: row[8].blank? ? nil : row[8].tr(',', '.').to_d,
-            duration_in_year: row[9].blank? ? nil : row[9].to_i,
-            depreciation_rate: row[10].blank? ? nil : row[10].tr(',', '.').to_f,
-            asset_sale_method: row[11].blank? ? nil : row[11].to_s,
-            net_value: row[20].blank? ? nil : row[20].tr(',', '.').to_d
-          }.to_struct
-
-          unless r.asset_account
-            w.error "No asset account exist in file on #{prompt}"
-            valid = false
-          end
-
-          unless r.name
-            w.error "No name exist in file on #{prompt}"
-            valid = false
-          end
-
-          unless r.purchase_on
-            w.error "No purchase_on date exist in file on #{prompt}"
-            valid = false
-          end
-
-          unless r.purchase_amount
-            w.error "No purchase amount date exist in file on #{prompt}"
-            valid = false
-          end
-
-          unless r.depreciation_method
-            w.error "No depreciation method date exist in file on #{prompt}. You must have one of the following ('Linéaire', 'Non amortissable', 'Dégressif')"
-            valid = false
-          end
-
-          unless r.duration_in_year
-            w.error "No valid duration exist in file on #{prompt}"
-            valid = false
-          end
-
-          # check asset account must exist in DB
-          next unless r.asset_account && r.name
-          exchange_asset_account_name = r.number + ' | ' + r.name
-          exchange_asset_account = Account.find_or_create_by_number(r.asset_account, name: exchange_asset_account_name)
-          unless exchange_asset_account
-            w.error "No way to have matching asset account in DB or Nomenclature (#{r.asset_account.inspect}) to build fixed asset on #{prompt}"
-            valid = false
-          end
-        end
-      end
 
       # Create or updates fixed assets
       def import
         source = File.read(file)
         detection = CharlockHolmes::EncodingDetector.detect(source)
-        rows = CSV.read(file, headers: true, col_sep: ';', encoding: detection[:encoding])
+        rows = CSV.read(file, headers: true, col_sep: ',', encoding: detection[:encoding])
         w.count = rows.size
         currency_preference = Preference[:currency]
 
@@ -97,19 +14,16 @@ module Isagri
         depreciation_method_transcode = {
           'Linéaire' => :linear,
           'Non amortissable' => :none,
-          'Dégressif' => :regressive
+          'Dégressif' => :regressive,
+          'Dérogatoire' => :regressive
         }
-
-        # get default allocation and expenses account
-        exchange_allocation_account = Account.find_or_import_from_nomenclature(:fixed_asset_depreciations) # 28
-        exchange_expenses_account = Account.find_or_import_from_nomenclature(:depreciations_inputations_expenses) # 68
 
         rows.each_with_index do |row, index|
           line_number = index + 2
           prompt = "L#{line_number.to_s.yellow} | "
 
           r = {
-            asset_account: row[0].blank? ? nil : row[0].to_s,
+            asset_account: row[0].blank? ? nil : normalize(row[0].to_s),
             number: row[1].blank? ? '' : row[1].to_s.strip,
             name: row[2].blank? ? nil : row[2].to_s.strip,
             purchase_on: row[4].blank? ? nil : Date.strptime(row[4].to_s, '%d/%m/%y'),
@@ -123,8 +37,12 @@ module Isagri
             net_value: row[20].blank? ? nil : row[20].tr(',', '.').to_d
           }.to_struct
 
-          description = r.number + ' | ' + r.name + ' | ' + r.purchase_on.to_s + ' | ' + r.net_value.to_s
+          # get allocation and expenses account
+          exchange_allocation_account = Account.find_or_create_by_number(to_allocation_account(r.asset_account)) # 28
+          exchange_expenses_account = Account.find_or_import_from_nomenclature(:depreciations_inputations_expenses)
 
+          description = r.number + ' | ' + r.name + ' | ' + r.purchase_on.to_s + ' | ' + r.net_value.to_s
+          
           # get or create asset account
           if r.asset_account && r.name
             exchange_asset_account_name = r.number + ' | ' + r.name
@@ -178,6 +96,25 @@ module Isagri
           w.check_point
         end
       end
+
+      # Trim account number following preferences
+      def normalize(number)
+        preference_number = Preference[:account_number_digits]
+        differential = number.size - preference_number if number.size > preference_number
+        if differential && number.last(2) == "0" * (differential)
+          number.first(preference_number)
+        elsif differential && number.last != "0" * (differential)
+          number.first(6) + number.last(preference_number - 6)
+        else
+          number
+        end
+      end
+
+      # Generate allocation account number
+      def to_allocation_account(number)
+        number.chars.insert(1, "8")[0...-1].join.to_i
+      end
+
     end
   end
 end
