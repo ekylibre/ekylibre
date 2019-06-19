ef73d3eca9bb2ec8c6c33c8fb5783e37422ae15e
Lucas Sotty <lucas.sotty@wanadoo.fr>
2019-06-19 18:19:09 +0200

    Fixed purchase_nature factory

--- test/factories/purchase_natures.rb
+++ test/factories/purchase_natures.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :purchase_nature do
     currency { 'EUR' }
     nature { 'purchase' }
+    sequence(:name) { |n| "Purchase nature #{n}" }
     with_accounting { true }
     journal
   end
