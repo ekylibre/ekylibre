1edf475bf98ea84bc83782b1329a89c812c3b456
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2019-06-19 09:37:38 +0200

    Test which reception could be reconciled to a purchase invoice and which ones appear in the modal of the purchase invoice form

--- test/controllers/backend/purchase_process/reconciliation_controller_test.rb
+++ test/controllers/backend/purchase_process/reconciliation_controller_test.rb
@@ -3,6 +3,31 @@ module Backend
   module PurchaseProcess
     class ReconciliationControllerTest < ActionController::TestCase
       test_restfully_all_actions
+
+
+      test 'reception to reconcile' do
+        Reception.delete_all
+        reception_one = create(:reception, state: :draft)
+        reception_item_one = create(:reception_item, reception: reception_one)
+
+
+        reception_two = create(:reception, state: :given, given_at: DateTime.now)
+         reception_item_two = create(:reception_item, reception: reception_two)
+
+         reception_three = create(:reception, state: :given, given_at: DateTime.now)
+         reception_item_three = create(:reception_item, reception: reception_three)
+
+        get :receptions_to_reconciliate
+        assert_response 200
+
+        parsing_response_reception = Nokogiri::HTML(response.body)
+        reception_ids = parsing_response_reception.css('input[type="checkbox"].model-checkbox').map { |k, v| k["data-id"].to_i }
+
+        assert_includes reception_ids, reception_two.id
+        assert_includes reception_ids, reception_three.id
+        refute_includes reception_ids, reception_one.id
+
+      end
     end
   end
 end
