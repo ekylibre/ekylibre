aa39d86dc9c232cc9cadba27a045dba352ea3b0d
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2019-06-20 17:08:03 +0200

    test reconciliation contorller

--- test/controllers/backend/purchase_process/reconciliation_controller_test.rb
+++ test/controllers/backend/purchase_process/reconciliation_controller_test.rb
@@ -4,16 +4,38 @@ module Backend
     class ReconciliationControllerTest < ActionController::TestCase
       test_restfully_all_actions
 
-      test 'reception to reconcile' do
+
+
+      setup do
         Reception.delete_all
-        reception_one = create(:reception, state: :draft)
-        reception_item_one = create(:reception_item, reception: reception_one)
+        PurchaseOrder.delete_all
 
-        reception_two = create(:reception, state: :given, given_at: DateTime.now)
-        reception_item_two = create(:reception_item, reception: reception_two)
+        @reception_one = create(:reception, state: :draft)
+        @reception_item_one = create(:reception_item, reception: @reception_one)
 
-        reception_three = create(:reception, state: :given, given_at: DateTime.now)
-        reception_item_three = create(:reception_item, reception: reception_three)
+        @reception_two = create(:reception, state: :given, given_at: DateTime.now)
+        @reception_item_two = create(:reception_item, purchase_invoice_item_id: nil, reception: @reception_two)
+
+        @purchase_invoice_one = create :purchase_invoice
+        @purchase_item = create :purchase_item, purchase: @purchase_invoice_one
+
+        @reception_three = create(:reception, state: :given, given_at: DateTime.now)
+        @reception_item_three = create(:reception_item, purchase_invoice_item_id: @purchase_item.id, reception: @reception_three)
+
+
+        @purchase_order_one = create(:purchase_order, state: :opened)
+        @purchase_item_one = create(:purchase_item, :of_purchase_order, purchase: @purchase_order_one)
+
+        @purchase_order_two = create(:purchase_order)
+        @purchase_order_two.close
+        @purchase_item_two = create(:purchase_item, :of_purchase_order, purchase: @purchase_order_two)
+
+        @purchase_order_three = create(:purchase_order)
+        @purchase_order_three.close
+        @purchase_item_three = create(:purchase_item, :of_purchase_order, purchase: @purchase_order_three)
+      end
+
+      test "only receptions with state 'given' appear in the purchase invoice form modal and could be reconciled" do
 
         get :receptions_to_reconciliate
         assert_response 200
@@ -21,23 +43,11 @@ module Backend
         parsing_response_reception = Nokogiri::HTML(response.body)
         reception_ids = parsing_response_reception.css('input[type="checkbox"].model-checkbox').map { |k, v| k["data-id"].to_i }
 
-        assert_includes reception_ids, reception_two.id
-        assert_includes reception_ids, reception_three.id
-        refute_includes reception_ids, reception_one.id
+        assert_includes reception_ids, @reception_two.id
+        refute_includes reception_ids, @reception_one.id
       end
 
-      test 'purchaseOrder to reconcile' do
-        PurchaseOrder.delete_all
-        purchase_order_one = create(:purchase_order, state: :opened)
-        purchase_item_one = create(:purchase_item, :of_purchase_order, purchase: purchase_order_one)
-
-        purchase_order_two = create(:purchase_order)
-        purchase_order_two.close
-        purchase_item_two = create(:purchase_item, :of_purchase_order, purchase: purchase_order_two)
-
-        purchase_order_three = create(:purchase_order)
-        purchase_order_three.close
-        purchase_item_three = create(:purchase_item, :of_purchase_order, purchase: purchase_order_three)
+      test "only purchaseOrders with state 'opened' appear in the reception form modal could be reconciled" do
 
         get :purchase_orders_to_reconciliate
         assert_response 200
@@ -45,11 +55,72 @@ module Backend
         parsing_response_purchase_order = Nokogiri::HTML(response.body)
         purchase_order_ids = parsing_response_purchase_order.css('input[type="checkbox"].model-checkbox').map { |k, v| k["data-id"].to_i }
 
-        assert_includes purchase_order_ids, purchase_order_one.id
-        refute_includes purchase_order_ids, purchase_order_two.id
-        refute_includes purchase_order_ids, purchase_order_three.id
+        assert_includes purchase_order_ids, @purchase_order_one.id
+        refute_includes purchase_order_ids, @purchase_order_two.id
+        refute_includes purchase_order_ids, @purchase_order_three.id
       end
 
+      test "only reception items not reconciled could be reconciled" do
+
+        get :receptions_to_reconciliate
+        assert_response 200
+
+        parsing_response_reception = Nokogiri::HTML(response.body)
+        reception_ids = parsing_response_reception.css('input[type="checkbox"].item-checkbox').map { |k, v| k["data-id"].to_i }
+
+        assert_includes reception_ids, @reception_item_two.id
+        refute_includes reception_ids, @reception_item_three.id
+      end
+
+      test "if the supplier is set then only receptions from this supplier appear in the purchase invoice form modal and could be reconciled" do
+
+        supplier_one = create(:entity, :supplier)
+        supplier_two = create(:entity, :supplier)
+        @reception_one.update(state: :given, given_at: DateTime.now, sender_id: supplier_two.id)
+        @reception_two.update(sender_id: supplier_one.id)
+
+        get :receptions_to_reconciliate, supplier: @reception_one.sender_id
+        assert_response 200
+
+        parsing_response_reception = Nokogiri::HTML(response.body)
+        reception_ids = parsing_response_reception.css('input[type="checkbox"].model-checkbox').map { |k, v| k["data-id"].to_i }
+
+        assert_includes reception_ids, @reception_one.id
+        refute_includes reception_ids, @reception_two.id
+        end
+
+        test "if the supplier is set then only purchaseOrders from this supplier appear in the reception form modal and could be reconciled" do
+
+        supplier_one = create(:entity, :supplier)
+        supplier_two = create(:entity, :supplier)
+        @purchase_order_one.update(supplier_id: supplier_two.id)
+        @purchase_order_two.update(supplier_id: supplier_one.id, state: :opened)
+
+        get :purchase_orders_to_reconciliate, supplier: @purchase_order_one.supplier_id
+        assert_response 200
+
+        parsing_response_purchase_order = Nokogiri::HTML(response.body)
+        purchase_order_ids = parsing_response_purchase_order.css('input[type="checkbox"].model-checkbox').map { |k, v| k["data-id"].to_i }
+
+        assert_includes purchase_order_ids, @purchase_order_one.id
+        refute_includes purchase_order_ids, @purchase_order_two.id
+        end
+
+        # test "update reconciled purchaseInvoices" do
+
+        # supplier_one = create(:entity, :supplier)
+        # supplier_two = create(:entity, :supplier)
+
+        # @reception_three.update(sender_id: supplier_two.id)
+
+        # get :receptions_to_reconciliate, supplier: @reception_three.sender_id, purchase_invoice: @purchase_invoice_one.id
+        # assert_response 200
+        # byebug
+        # parsing_response_reception = Nokogiri::HTML(response.body)
+        # reception_ids = parsing_response_reception.css('input[type="checkbox"].item-checkbox').map { |k, v| k["data-id"].to_i }
+
+
+        # end
 
     end
   end
