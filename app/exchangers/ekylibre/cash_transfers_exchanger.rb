9e1a215e38650bdde0251ea27527e580c9c3ecd4
Lucas Sotty <lucas.sotty@wanadoo.fr>
2019-07-02 14:31:01 +0200

    Removed irrelevant cash transfer exchanger

--- app/exchangers/ekylibre/cash_transfers_exchanger.rb
+++ app/exchangers/ekylibre/cash_transfers_exchanger.rb
@@ -1,46 +1,53 @@
 module Ekylibre
   class CashTransfersExchanger < ActiveExchanger::Base
+    # Create cash_transfert between internal accounts
     def import
-      rows = CSV.read(file, headers: true)
+      rows = CSV.read(file, headers: true).delete_if { |r| r[0].blank? }
       w.count = rows.size
-      now = Time.zone.now
 
-      rows.each_with_index do |row, index|
-        line_number = index + 2
-        w.check_point && next if row[0].blank?
+      rows.each do |row|
         r = {
-          invoiced_at:        (row[0].blank? ? nil : Date.parse(row[0].to_s)),
-          sender_payment_mode_name: (row[1].blank? ? nil : row[1].to_s),
-          receiver_payment_mode_name: (row[2].blank? ? nil : row[2].to_s),
-          amount: (row[3].blank? ? nil : row[3].tr(',', '.').to_d)
+          started_on: row[0].blank? ? nil : Date.parse(row[0].to_s),
+          sender_account_name: row[1].blank? ? nil : row[1].to_s,
+          receiver_account_name: row[2].blank? ? nil : row[2].to_s,
+          amount: row[3].blank? ? nil : row[3].tr(',', '.').to_d
         }.to_struct
 
-        sender_payment_mode_name = IncomingPaymentMode.find_by(name: r.sender_payment_mode_name)
-        unless sender_payment_mode_name
-          raise ActiveExchanger::InvalidDataError, "Cannot find incoming payment mode #{r.sender_payment_mode_name} at line #{line_number}"
+        unless r.started_on
+          w.error "Need a valid date for #{r.started_on}"
+          next
         end
 
-        receiver_payment_mode_name = IncomingPaymentMode.find_by(name: r.receiver_payment_mode_name)
-        unless receiver_payment_mode_name
-          raise ActiveExchanger::InvalidDataError, "Cannot find incoming payment mode #{r.receiver_payment_mode_name} at line #{line_number}"
+        # find emission cash by name or bank name or iban
+        emission_cash = Cash.where('name ILIKE ?', r.sender_account_name).first
+        emission_cash ||= Cash.where('bank_name ILIKE ?', r.sender_account_name).first
+        emission_cash ||= Cash.find_by(iban: r.sender_account_name)
+
+        unless emission_cash
+          w.error "Need a valid emission_cash for #{r.sender_account_name}"
+          next
         end
 
-        # find or create a cash transfert
-        if r.invoiced_at && sender_payment_mode_name && receiver_payment_mode_name && r.amount
-          cash_transfer = CashTransfer.find_by(
-            transfered_at: r.invoiced_at.to_time,
-            reception_cash_id: receiver_payment_mode_name.cash.id,
-            emission_cash_id: sender_payment_mode_name.cash.id,
-            emission_amount: r.amount
+        # find reception cash by name or bank name or iban
+        reception_cash = Cash.where('name ILIKE ?', r.receiver_account_name).first
+        reception_cash ||= Cash.where('bank_name ILIKE ?', r.receiver_account_name).first
+        reception_cash ||= Cash.find_by(iban: r.receiver_account_name)
 
-          )
-          cash_transfer ||= CashTransfer.create!(
-            transfered_at: r.invoiced_at.to_time,
-            reception_cash_id: receiver_payment_mode_name.cash.id,
-            emission_cash_id: sender_payment_mode_name.cash.id,
-            currency_rate: 1.0,
-            emission_amount: r.amount
-          )
+        unless reception_cash
+          w.error "Need a valid reception_cash for #{r.receiver_account_name}"
+          next
+        end
+
+        # check if cash_transfer already exist
+        unless cash_transfer = CashTransfer.find_by(emission_amount: r.amount,
+                                                    emission_cash_id: emission_cash.id,
+                                                    reception_cash_id: reception_cash.id,
+                                                    transfered_at: r.started_on.to_time)
+          CashTransfer.create!(
+                        emission_amount: r.amount,
+                        emission_cash_id: emission_cash.id,
+                        reception_cash_id: reception_cash.id,
+                        transfered_at: r.started_on.to_time)
         end
 
         w.check_point
