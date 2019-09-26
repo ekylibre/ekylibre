53be89e5bf2a0b8783a12ee8fbfdbfab20139a2d
Rémi PIOTAIX <remi@piotaix.fr>
2019-09-26 17:54:29 +0000

    Deprecate archive_report in favor of archive_report_template that requires a template in order to be able to archive the document more efficiently

--- app/controllers/concerns/pdf_printer.rb
+++ app/controllers/concerns/pdf_printer.rb
@@ -29,16 +29,11 @@ module PdfPrinter
 
   # store file in document
   # nature must a Nomen::DocumentNature object
+  # TODO refactor by extracting signing logic to a different method/class/module
   def archive_report(nature, key, data_or_path, mandatory = false, closer = nil, options = { archiving: :last })
-    data = data_or_path.is_a?(File) ? data_or_path : StringIO.new(data_or_path)
-    name = options[:name] || [nature.human_name, key].join(' ')
-    document = Document.create!(
-                 nature: nature,
-                 key: key,
-                 name: name,
-                 file: data,
-                 file_file_name: "#{key}.pdf"
-               )
+    ActiveSupport::Deprecation.warn 'archive_report is broken, use archive_report_template instead' unless mandatory
+
+    document = archive_report_template(data_or_path, nature: nature, key: key, template: nil, **options)
 
     if mandatory
       sha256 = Digest::SHA256.file document.file.path
@@ -51,6 +46,19 @@ module PdfPrinter
     document
   end
 
+  def archive_report_template(data_or_file, nature:, key:, template:, **options)
+    data = data_or_file.is_a?(File) ? data_or_file : StringIO.new(data_or_file)
+    name = options[:name] || [nature.human_name, key].join(' ')
+    Document.create!(
+      nature: nature,
+      key: key,
+      name: name,
+      file: data,
+      file_file_name: "#{key}.pdf",
+      template: template
+    )
+  end
+
   def find_open_document_template(name)
     dir = Rails.root.join('config', 'locales')
     paths = [dir.join(I18n.locale.to_s, 'reporting', "#{name}.odt"),
@@ -61,46 +69,46 @@ module PdfPrinter
 
   private
 
-  def to_template_path(name_or_path)
-    return name_or_path unless name_or_path.is_a?(String)
-    directory = self.class.name.gsub(/Printer$/, '').underscore
-    file_name = "#{name_or_path}.odt"
-    Rails.root.join('config', 'locales', I18n.locale.to_s, 'reporting', directory, file_name)
-  end
-
-  def to_pdf_data(report, options = {})
-    Dir.mktmpdir do |directory|
-      directory_path = Pathname.new(directory)
-      odf_path = directory_path.join('source.odf').to_s
-      report.generate odf_path
-      convert_to_pdf directory, odf_path
-      pdf_path = directory_path.join('source.pdf').to_s
-      remove_empty_tailing_page(pdf_path) if options[:multipage]
-      fill_checks(pdf_path, options[:checks]) if options[:checks]
-      File.read pdf_path
+    def to_template_path(name_or_path)
+      return name_or_path unless name_or_path.is_a?(String)
+      directory = self.class.name.gsub(/Printer$/, '').underscore
+      file_name = "#{name_or_path}.odt"
+      Rails.root.join('config', 'locales', I18n.locale.to_s, 'reporting', directory, file_name)
     end
-  end
 
-  def convert_to_pdf(directory, odf_path)
-    system "soffice --headless --convert-to pdf --outdir #{directory} #{odf_path}"
-  end
-
-  def remove_empty_tailing_page(pdf_path)
-    pdf = CombinePDF.load(pdf_path)
-    pdf.remove(pdf.pages.count - 1)
-    pdf.save pdf_path
-  end
-
-  def fill_checks(pdf_path, checks)
-    pdf = CombinePDF.load(pdf_path)
-    properties = { font_size: 11, height: 12, width: 250, text_align: :left }
-    checks.each_with_index do |check, index|
-      pdf.pages[index].textbox check[:amount], properties.merge({ x: 460, y: 142 })
-      pdf.pages[index].textbox check[:amount_to_letter], properties.merge({ x: 70, y: 155 })
-      pdf.pages[index].textbox check[:payee], properties.merge({ x: 78, y: 132 })
-      pdf.pages[index].textbox check[:company_town], properties.merge({ x: 450, y: 116 })
-      pdf.pages[index].textbox check[:paid_at], properties.merge({ x: 450, y: 101 })
+    def to_pdf_data(report, options = {})
+      Dir.mktmpdir do |directory|
+        directory_path = Pathname.new(directory)
+        odf_path = directory_path.join('source.odf').to_s
+        report.generate odf_path
+        convert_to_pdf directory, odf_path
+        pdf_path = directory_path.join('source.pdf').to_s
+        remove_empty_tailing_page(pdf_path) if options[:multipage]
+        fill_checks(pdf_path, options[:checks]) if options[:checks]
+        File.read pdf_path
+      end
     end
-    pdf.save pdf_path
-  end
+
+    def convert_to_pdf(directory, odf_path)
+      system "soffice --headless --convert-to pdf --outdir #{directory} #{odf_path}"
+    end
+
+    def remove_empty_tailing_page(pdf_path)
+      pdf = CombinePDF.load(pdf_path)
+      pdf.remove(pdf.pages.count - 1)
+      pdf.save pdf_path
+    end
+
+    def fill_checks(pdf_path, checks)
+      pdf = CombinePDF.load(pdf_path)
+      properties = { font_size: 11, height: 12, width: 250, text_align: :left }
+      checks.each_with_index do |check, index|
+        pdf.pages[index].textbox check[:amount], properties.merge({ x: 460, y: 142 })
+        pdf.pages[index].textbox check[:amount_to_letter], properties.merge({ x: 70, y: 155 })
+        pdf.pages[index].textbox check[:payee], properties.merge({ x: 78, y: 132 })
+        pdf.pages[index].textbox check[:company_town], properties.merge({ x: 450, y: 116 })
+        pdf.pages[index].textbox check[:paid_at], properties.merge({ x: 450, y: 101 })
+      end
+      pdf.save pdf_path
+    end
 end
