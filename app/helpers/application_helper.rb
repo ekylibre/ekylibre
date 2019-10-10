5510b75520f775f9c1f6920f5385ba548615f763
Rémi PIOTAIX <remi@piotaix.fr>
2019-10-10 15:08:56 +0000

    Refactor/flash notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -728,38 +728,6 @@ module ApplicationHelper
     nil
   end
 
-  def notification_tag(mode, messages = nil)
-    unless messages
-      if flash[:notifications].is_a?(Hash) && flash[:notifications][mode.to_s].is_a?(Array)
-        messages = flash[:notifications][mode.to_s]
-      end
-    end
-    messages = [messages] if messages.is_a?(String)
-    code = ''.html_safe
-    return code unless messages
-    messages.each do |message|
-      code << flash_message_tag(mode, h(message).gsub(/\n/, '<br/>').html_safe)
-    end
-    code
-  end
-
-  def flash_message_tag(mode, message)
-    content_tag :div, { class: "flash #{mode}", data: { alert: true } } do
-      content_tag(:a, "&times;".html_safe, { class: :close, href: '#' }) +
-        content_tag(:div, '', { class: :icon }) +
-        content_tag(:div, { class: :message }) do
-          content_tag(:h3, mode.t(scope: 'notifications.levels').html_safe) + content_tag(:p, message)
-        end
-    end
-  end
-
-  def notifications_tag
-    notification_tag(:error) <<
-      notification_tag(:warning) <<
-      notification_tag(:success) <<
-      notification_tag(:information)
-  end
-
   def table_of(array, html_options = {}, &block)
     coln = html_options.delete(:columns) || 3
     html = ''
