6d74eed178a1786ee32c80bebc7713dcbffd28b1
Rémi Piotaix <remi@piotaix.fr>
2019-10-15 19:16:34 +0200

    Fix bad migration of v2.60.2

--- db/migrate/20191010151901_remove_nature_and_with_accounting_from_purchase_nature.rb
+++ db/migrate/20191010151901_remove_nature_and_with_accounting_from_purchase_nature.rb
@@ -1,5 +1,18 @@
 class RemoveNatureAndWithAccountingFromPurchaseNature < ActiveRecord::Migration
   def change
+    res = execute <<-SQL
+      SELECT count(*) as journal_count FROM journals WHERE nature = 'purchases'
+    SQL
+    res = res.to_a.first['journal_count'].to_i
+
+    if res.zero?
+      execute <<-SQL
+        INSERT INTO journals (nature, name, code, closed_on, currency, used_for_affairs, used_for_gaps, created_at, updated_at, creator_id, updater_id, lock_version, custom_fields, used_for_permanent_stock_inventory, used_for_unbilled_payables, used_for_tax_declarations, accountant_id)
+        VALUES ('purchases', 'Achats', 'ACHA', '2011-12-31', 'EUR', false, false, now(), now(), null, null, 0, null, false, false, false, null);
+      SQL
+    end
+
+
     execute <<-SQL
       UPDATE purchase_natures
         SET journal_id = j.id
