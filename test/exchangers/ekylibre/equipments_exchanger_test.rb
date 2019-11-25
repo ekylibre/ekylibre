cc29da45bd8ae0896da7722705e30a94019c505a
Rémi Piotaix <remi@piotaix.fr>
2019-11-25 16:19:29 +0100

    Fix Ekylibre Equipment exchanger and prevent it to create unitary products with population > 1

--- test/exchangers/ekylibre/equipments_exchanger_test.rb
+++ test/exchangers/ekylibre/equipments_exchanger_test.rb
@@ -4,6 +4,16 @@ module Ekylibre
   class EquipmentsExchangerTest < ActiveExchanger::TestCase
     test 'import' do
       Ekylibre::EquipmentsExchanger.import(fixture_files_path.join('imports', 'ekylibre', 'equipments.csv'))
+
+      cuve_1 = Product.find_by name: 'Cuve n°1'
+      refute_nil cuve_1
+      assert_equal 1, cuve_1.population
+    end
+
+    test 'import fails if population is not 1 and counting_unitary' do
+      assert_raise(StandardError) do
+        Ekylibre::EquipmentsExchanger.import(fixture_files_path.join('imports', 'ekylibre', 'equipments_invalid.csv'))
+      end
     end
   end
 end
