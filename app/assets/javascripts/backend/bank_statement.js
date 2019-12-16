e1edab8a14ec345c73769e59feb640493318a049
Rémi PIOTAIX <remi@piotaix.fr>
2019-12-16 11:02:15 +0000

    Merge branch 'feature/bank-statement-item-line-improvements' into 'core'
    bank statement item line improvements
    
    See merge request ekylibre/eky!740
    
    (cherry picked from commit 9ea118351facc8e5dfd050b063178595e27670ed)
    
    627a3b69 Enable flatpickr at page first load
    afde9ff8 Avoid form input overlaping

--- app/assets/javascripts/backend/bank_statement.js
+++ app/assets/javascripts/backend/bank_statement.js
@@ -1,25 +1,28 @@
-(function(E){
-  document.addEventListener('DOMContentLoaded', function(){
+(function (E) {
+    function activateFlatpickr() {
 
-    function handleSelectedDate(date) {
-      E.bankReconciliation.createBankStatementItem(date)
-      return false;
+        function handleSelectedDate(date) {
+            E.bankReconciliation.createBankStatementItem(date);
+            return false;
+        }
+
+        const button = document.querySelector('button#new-line');
+        const ranges = JSON.parse(button.dataset.bankStatementDatesRanges);
+        const fp = flatpickr(button, {
+            enable: ranges.map(e => ({from: e.start, to: e.end})),
+            locale: I18n.locale.slice(0, 2),
+            defaultDate: moment.min(ranges.map(e => moment(e.start))).toDate(),
+            onChange: function (selectedDates, dateStr, instance) {
+                handleSelectedDate(selectedDates[0])
+            },
+            clickOpens: false
+        });
+        button.onmousedown = function () {
+            fp.open()
+        };
     }
 
-    const button = document.querySelector('button#new-line');
-    const ranges = JSON.parse(button.dataset.bankStatementDatesRanges)
-    const fp = flatpickr(button, {
-      enable: ranges.map(e =>({from: e.start, to: e.end})),
-      locale: I18n.locale.slice(0, 2),
-      defaultDate: moment.min(ranges.map(e => moment(e.start))).toDate(),
-      onChange: function(selectedDates, dateStr, instance) {
-        handleSelectedDate(selectedDates[0])
-      },
-      clickOpens: false
-    })
-    button.onmousedown = function() {
-      fp.open()
-    };
-  });
+    document.addEventListener('page:load', activateFlatpickr);
+    document.addEventListener('DOMContentLoaded', activateFlatpickr);
 
-})(ekylibre)
+})(ekylibre);
