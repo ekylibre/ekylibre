d2ff74dc09593a0f70489434860b1ce376fcaf82
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2019-12-24 15:52:22 +0100

    refacto panier local import

--- app/exchangers/panier_local/sales_exchanger.rb
+++ app/exchangers/panier_local/sales_exchanger.rb
@@ -1,5 +1,24 @@
 module PanierLocal
   class SalesExchanger < ActiveExchanger::Base
+
+    NORMALIZATION_CONFIG = [
+      {col: 1, name: :invoiced_at, type: :date, constraint: :not_nil},
+      {col: 3, name: :journal_nature, type: :string},
+      {col: 4, name: :account_number, type: :integer},
+      {col: 5, name: :entity_name, type: :string, constraint: :not_nil},
+      {col: 6, name: :entity_code, type: :integer, constraint: :not_nil},
+      {col: 7, name: :sale_reference_number, type: :integer, constraint: :not_nil},
+      {col: 8, name: :sale_description, type: :string},
+      {col: 9, name: :sale_item_amount, type: :float, constraint: :greater_or_equal_to_zero},
+      {col: 10, name: :sale_item_sens, type: :string},
+      {col: 11, name: :sale_item_pretax_amount, type: :float},
+      {col: 12, name: :vat_percentage, type: :float},
+      {col: 13, name: :quantity, type: :integer},
+    ]
+
+
+
+
     def check
       # Imports sales entries into sales to make accountancy in CSV format
       # filename example : ECRITURES.CSV
@@ -19,59 +38,34 @@ module PanierLocal
       #  12 - L: tax_rate : '20'
       #  13 - M: quantity : '104'
 
-      source = File.read(file)
-      detection = CharlockHolmes::EncodingDetector.detect(source)
-      rows = CSV.read(file, headers: true, encoding: detection[:encoding], col_sep: ';')
-      w.count = rows.size
-      valid = true
+
+      # Ouverture et décodage: CSVReader::read(file)
+      # Fournir col_sep?
+      rows = ActiveExchanger::CsvReader.new.read(file)
+      parser = ActiveExchanger::CsvParser.new(NORMALIZATION_CONFIG)
+
+      data, errors = parser.normalize(rows)
+
+      valid = errors.reject(&:empty?).empty?
 
       last_line = rows.size - 1
-
       fy_start = FinancialYear.at(Date.parse(rows.first[1].to_s))
       fy_stop = FinancialYear.at(Date.parse(rows[last_line][1].to_s))
+
       unless fy_start && fy_stop
         w.warn 'Need a FinancialYear'
         valid = false
       end
 
-      rows.each_with_index do |row, index|
-        line_number = index + 2
-        prompt = "L#{line_number.to_s.yellow}"
-        r = {
-          sale_item_line: (row[0].blank? ? nil : row[0]),
-          invoiced_at:        (row[1].blank? ? nil : Date.parse(row[1].to_s)),
-          journal_nature: (row[3].blank? ? nil : row[3].to_s),
-          account_number:   (row[4].blank? ? nil : row[4].upcase),
-          entity_name: (row[5].blank? ? nil : row[5].to_s),
-          entity_code: (row[6].blank? ? nil : row[6].to_s),
-          sale_reference_number: (row[7].blank? ? nil : row[7].to_s),
-          sale_description: (row[8].blank? ? nil : row[8].to_s),
-          sale_item_amount: (row[9].blank? ? nil : row[9].tr(',', '.').to_f),
-          sale_item_sens: (row[10].blank? ? nil : row[10].to_s),
-          sale_item_pretax_amount: (row[11].blank? ? nil : row[11].tr(',', '.').to_f),
-          vat_percentage: (row[12].blank? ? nil : row[12].tr(',', '.').to_d),
-          quantity: (row[13].blank? ? nil : row[13].tr(',', '.').to_d)
-        }.to_struct
-
-        unless r.sale_item_amount >= 0.0
-          valid = false
-        end
-
-        if r.invoiced_at.nil? || r.entity_name.nil? || r.entity_code.nil? || r.sale_reference_number.nil?
-          valid = false
-        end
-
         w.info valid.inspect.green
-
-      end
       valid
     end
 
     def import
-      source = File.read(file)
-      detection = CharlockHolmes::EncodingDetector.detect(source)
-      rows = CSV.read(file, headers: true, encoding: detection[:encoding], col_sep: ';')
+      # Ouverture et décodage
+      rows = ActiveExchanger::CsvReader.new.read(file)
       w.count = rows.size
+
       # create or find journal for sale nature
       journal = Journal.find_or_create_by(code: 'PALO', nature: 'sales', name: 'Panier Local')
       catalog = Catalog.find_or_create_by(code: 'PALO', currency: 'EUR', usage: 'sale', name: 'Panier Local')
@@ -79,7 +73,6 @@ module PanierLocal
       sale_nature = SaleNature.find_or_create_by(name: "Vente en ligne - Panier Local", catalog_id: catalog.id, currency: 'EUR', payment_delay: '30 days', journal_id: journal.id)
 
       country = Preference[:country]
-      sale_ids = []
       variant = nil
       tax = nil
       quantity = nil
@@ -87,155 +80,197 @@ module PanierLocal
       tax_amount = nil
       amount = nil
 
+      parser = ActiveExchanger::CsvParser.new(NORMALIZATION_CONFIG)
 
-      rows.each_with_index do |row, index|
-        line_number = index + 2
-        prompt = "L#{line_number.to_s.yellow}"
-        r = {
-          sale_item_line: (row[0].blank? ? nil : row[0]),
-          invoiced_at:        (row[1].blank? ? nil : Date.parse(row[1].to_s)),
-          journal_nature: (row[3].blank? ? nil : row[3].to_s),
-          account_number:   (row[4].blank? ? nil : row[4].upcase),
-          entity_name: (row[5].blank? ? nil : row[5].to_s),
-          entity_code: (row[6].blank? ? nil : row[6].to_s),
-          sale_reference_number: (row[7].blank? ? nil : row[7].to_s),
-          sale_description: (row[8].blank? ? nil : row[8].to_s),
-          sale_item_amount: (row[9].blank? ? nil : row[9].tr(',', '.').to_f),
-          sale_item_sens: (row[10].blank? ? nil : row[10].to_s),
-          sale_item_pretax_amount: (row[11].blank? ? nil : row[11].tr(',', '.').to_f),
-          vat_percentage: (row[12].blank? ? nil : row[12].tr(',', '.').to_f),
-          quantity: (row[13].blank? ? 1.0 : row[13].tr(',', '.').to_f)
-        }.to_struct
+      data, errors = parser.normalize(rows)
 
-        next if r.sale_item_amount == 0.0
+      sales_info = data.group_by { |d| d.sale_reference_number }
 
-        # find or create an entity
-        if r.entity_name && r.account_number && r.account_number.start_with?('411')
-          entity = Entity.where('codes ->> ? = ?', 'panier_local', r.entity_code).first
-          last_name = r.entity_name.mb_chars.capitalize
-          unless entity
-            # check entity account
-            acc = Account.find_or_initialize_by(number: r.account_number)
-            attributes = {name: r.entity_name}
-            attributes[:centralizing_account_name] = r.account_number.start_with?('401') ? 'suppliers' : 'clients'
-            attributes[:nature] = 'auxiliary'
-            aux_number = r.account_number[3, r.account_number.length]
-            if aux_number.match(/\A0*\z/).present?
-              w.info "We can't import auxiliary number #{aux_number} with only 0. Mass change number in your file before importing"
-              attributes[:auxiliary_number] = '00000A'
-            else
-              attributes[:auxiliary_number] = aux_number
-            end
-            acc.attributes = attributes
-            acc.save!
-            w.info "account saved ! : #{acc.label.inspect.red}"
-            # check entity
-            w.info "Create entity and link account"
-            entity = Entity.where('last_name ILIKE ?', last_name).first
-            entity ||= Entity.new
-            entity.nature = :organization
-            entity.last_name = last_name
-            entity.codes = { 'panier_local' => r.entity_code }
-            entity.active = true
-            entity.client = true
-            entity.client_account_id = acc.id
-            entity.save!
-            w.info "Entity created ! : #{entity.full_name.inspect.red}"
-          end
-        end
+      sales_info.each { |_sale_reference_number, sale_info| sale_creation(sale_info, sale_nature) }
 
-        sale = Sale.where('providers ->> ? = ?', 'panier_local', r.sale_reference_number).first
-        unless sale
-          sale = Sale.create!(
-            invoiced_at: r.invoiced_at,
-            reference_number: r.sale_reference_number,
-            client_id: entity.id,
-            nature: sale_nature,
-            description: r.sale_description,
-            providers: {'panier_local' => r.sale_reference_number}
+    end
+
+    def sale_creation(sale_info, sale_nature)
+      entity = get_or_create_entity(sale_info)
+      # get or create sale
+      sale = Sale.where('providers ->> ? = ?', 'panier_local', sale_info.first.sale_reference_number.to_s).first
+
+      if sale.nil?
+        client_sale_info = sale_info.select {|item| item.account_number.to_s.start_with?('411')}.first
+        sale = Sale.create!(
+          invoiced_at: client_sale_info.invoiced_at,
+          reference_number: client_sale_info.sale_reference_number,
+          client_id: entity.id,
+          nature: sale_nature,
+          description: client_sale_info.sale_description,
+          providers: {'panier_local' => client_sale_info.sale_reference_number}
           )
-          sale_ids << sale.id
-        end
 
-        # check vat account and amounts
-        if r.account_number.start_with?('445') && r.vat_percentage && r.invoiced_at
-          global_pretax_amount = r.sale_item_pretax_amount
-          global_tax_amount = r.sale_item_amount
-          clean_tax_account_number = r.account_number[0, Preference[:account_number_digits]]
-          tax_account = Account.find_or_create_by_number(clean_tax_account_number)
-          tax = Tax.find_by(amount: r.vat_percentage, collect_account_id: tax_account.id)
-          unless tax
-            tax = Tax.find_by(amount: r.vat_percentage, country: country.to_sym)
-            tax ||= Tax.find_on(r.invoiced_at.to_date, country: country.to_sym, amount: r.vat_percentage)
-            tax.collect_account_id = tax_account.id
-            tax.active = true
-            tax.save!
-          end
-          raise "No tax found for given #{r.vat_percentage}" unless tax
-          global_amount = global_pretax_amount + global_tax_amount
-        end
+        tax = check_or_create_vat_account_and_amount(sale_info)
 
-        # check product account, quantity and variant
-        if r.account_number.start_with?('7')
-          clean_account_number = r.account_number[0, Preference[:account_number_digits]]
-          variant = ProductNatureVariant.where('providers ->> ? = ?', 'panier_local', r.account_number).first
+        product_account_line = sale_info.select {|i| i.account_number.to_s.start_with?('7') }.first
+
+        if product_account_line.present?
+          product_account = check_or_create_product_account(product_account_line)
+          variant = ProductNatureVariant.find_by('providers ->> ? = ?', 'panier_local', product_account_line.account_number.to_s)
           unless variant
-            computed_name = "Service - Vente en ligne - #{clean_account_number}"
-            v_account = Account.find_or_create_by_number(clean_account_number)
-            pnc = ProductNatureCategory.where(product_account_id: v_account.id, name: computed_name).first
-            pnc ||= ProductNatureCategory.create!(name: computed_name, active: true, saleable: true, product_account_id: v_account.id)
-            pn = ProductNature.where(category_id: pnc.id, name: computed_name).first
-            pn ||= ProductNature.create!(active: true, name: computed_name, category_id: pnc.id, variety: 'service', population_counting: 'decimal')
-            variant = pn.variants.create!(active: true,
-                                          name: computed_name,
-                                          providers: {'panier_local' => r.account_number},
-                                          unit_name: 'unity'
-                                          )
+            variant = create_variant(product_account, product_account_line)
           end
-          raise "No variant found for given #{r.account_number}" unless variant
-          if r.sale_item_sens == 'D'
-            pretax_amount = r.sale_item_amount * -1
-          elsif r.sale_item_sens == 'C'
-            pretax_amount = r.sale_item_amount
-          end
-          quantity = r.quantity
+          pretax_amount = create_pretax_amount(product_account_line)
+          quantity = product_account_line.quantity
         end
-
-
-        if sale && quantity && pretax_amount && variant && tax
-          unless sale_item = SaleItem.where(
-            sale_id: sale.id,
-            quantity: quantity,
-            pretax_amount: pretax_amount,
-            variant_id: variant.id
-          ).first
-            sale.items.create!(
-              amount: nil,
-              pretax_amount: pretax_amount,
-              unit_pretax_amount: nil,
-              quantity: quantity,
-              tax: tax,
-              variant: variant,
-              compute_from: :pretax_amount
-            )
-          end
-          variant = nil
-          quantity = nil
-          pretax_amount = nil
-        end
-        w.check_point
       end
 
-      # Restart counting
-      added_sales = Sale.where(id: sale_ids)
-      w.reset! added_sales.count, :yellow
+      if sale && quantity && pretax_amount && variant && tax
+        unless sale_item = SaleItem.where(
+          sale_id: sale.id,
+          quantity: quantity,
+          pretax_amount: pretax_amount,
+          variant_id: variant.id
+        ).first
+          sale.items.create!(
+            amount: nil,
+            pretax_amount: pretax_amount,
+            unit_pretax_amount: nil,
+            quantity: quantity,
+            tax: tax,
+            variant: variant,
+            compute_from: :pretax_amount
+          )
+        end
+        variant = nil
+        quantity = nil
+        pretax_amount = nil
+      end
+      sale
+    end
 
-      added_sales.each do |sale|
-        sale.propose if sale.draft?
-        sale.confirm
-        sale.invoice(sale.invoiced_at)
-        w.check_point
+    def get_or_create_entity(sale_info)
+        entity = Entity.where('codes ->> ? = ?', 'panier_local', sale_info.first.entity_code.to_s)
+        if entity.any?
+          entity.first
+        else
+          account = create_entity_account(sale_info)
+          create_entity(sale_info, account)
+        end
+    end
+
+    def create_entity_account(sale_info)
+      client_sale_info = sale_info.select {|item| item.account_number.to_s.start_with?('411')}.first
+      if client_sale_info.present?
+        client_number_account = client_sale_info.account_number.to_s
+        acc = Account.find_or_initialize_by(number: client_number_account)#!
+
+
+        attributes = {
+                      name: client_sale_info.entity_name,
+                      centralizing_account_name: 'clients',
+                      nature: 'auxiliary'
+                    }
+
+        aux_number = client_number_account[3, client_number_account.length]
+        #TODO ajouter cette condition en tant que check avt l'import
+        if aux_number.match(/\A0*\z/).present?
+          w.info "We can't import auxiliary number #{aux_number} with only 0. Mass change number in your file before importing"
+          attributes[:auxiliary_number] = '00000A'
+        else
+          attributes[:auxiliary_number] = aux_number
+        end
+        acc.attributes = attributes
+        acc.save!
+        w.info "account saved ! : #{acc.label.inspect.red}"
+
+        acc
       end
     end
+
+    def create_entity(sale_info, acc)
+      client_sale_info = sale_info.select {|item| item.account_number.to_s.start_with?('411')}.first
+      # check entity
+      last_name = client_sale_info.entity_name.mb_chars.capitalize
+      w.info "Create entity and link account"
+      entity = Entity.new(
+        nature: :organization,
+        last_name: last_name,
+        codes: { 'panier_local' => client_sale_info.entity_code },
+        active: true,
+        client: true,
+        client_account_id: acc.id
+      )
+      entity.save!
+      w.info "Entity created ! : #{entity.full_name.inspect.red}"
+
+      entity
+    end
+
+    def check_or_create_vat_account_and_amount(sale_info)
+      account_numbers = sale_info.map {|i| i.account_number }
+      vat_account = account_numbers.select { |number| number.to_s.start_with?('445') }
+      vat_account_info = vat_account.any? ? sale_info.select {|item| item.account_number.to_s.start_with?('445')}.first : nil
+      if vat_account.any? && vat_account_info.present?
+        global_pretax_amount = vat_account_info.sale_item_pretax_amount
+        global_tax_amount = vat_account_info.sale_item_amount
+        clean_tax_account_number = vat_account_info.account_number.to_s[0..Preference[:account_number_digits]]
+
+        tax_account = Account.find_by(number: clean_tax_account_number)
+        tax = Tax.find_by(amount: vat_account_info.vat_percentage)
+
+        unless tax_account.present? && tax
+          tax = create_tax_account(vat_account_info, clean_tax_account_number)
+        end
+      end
+      tax
+    end
+
+    def create_tax_account(vat_account_info, clean_tax_account_number)
+      tax_account = Account.find_or_create_by_number(clean_tax_account_number)
+      tax = Tax.find_by(amount: vat_account_info.vat_percentage, collect_account_id: tax_account.id)
+
+      unless tax
+        tax = Tax.find_by(amount: vat_account_info.vat_percentage, country: Preference[:country].to_sym)
+        tax ||= Tax.find_on(vat_account_info.invoiced_at.to_date, country: Preference[:country].to_sym, amount: vat_account_info.vat_percentage)
+        tax.collect_account_id = tax_account.id
+        tax.active = true
+        tax.save!
+      end
+      tax
+    end
+
+    def check_or_create_product_account(product_account_line)
+      clean_account_number = product_account_line.account_number.to_s[0..Preference[:account_number_digits]]
+      computed_name = "Service - Vente en ligne - #{clean_account_number}"
+      product_account = Account.find_or_create_by_number(clean_account_number)
+    end
+
+    def create_variant(product_account, product_account_line)
+      clean_account_number = product_account_line.account_number.to_s[0..Preference[:account_number_digits]]
+      computed_name = "Service - Vente en ligne - #{clean_account_number}"
+
+      pnc = ProductNatureCategory.create_with(name: computed_name, active: true, saleable: true, product_account_id: product_account.id)
+          .find_or_create_by(product_account_id: product_account.id, name: computed_name)
+
+      pn = ProductNature.create_with(active: true, name: computed_name, category_id: pnc.id, variety: 'service', population_counting: 'decimal')
+          .find_or_create_by(category_id: pnc.id, name: computed_name)
+
+      if pn
+        pn.variants.create!(active: true,
+                                    name: computed_name,
+                                    providers: {'panier_local' => product_account_line.account_number},
+                                    unit_name: 'unity'
+                                    )
+      end
+    end
+
+    def create_pretax_amount(product_account_line)
+      if product_account_line.sale_item_sens == 'D'
+        product_account_line.sale_item_amount * -1
+      elsif product_account_line.sale_item_sens == 'C'
+        product_account_line.sale_item_amount
+      end
+    end
+
   end
 end
+
+
+
