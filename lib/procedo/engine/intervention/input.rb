337edbfa53544bd706b495a905eb2f78aac40fac
Rémi Piotaix <remi@piotaix.fr>
2020-02-20 18:17:37 +0100

    Take allowed_entry_factor and allowed_harvest_factor from usage instead of user supplied values

--- lib/procedo/engine/intervention/input.rb
+++ lib/procedo/engine/intervention/input.rb
@@ -4,18 +4,32 @@ module Procedo
   module Engine
     class Intervention
       class Input < Procedo::Engine::Intervention::Quantified
-        attr_reader :usage_id, :allowed_entry_factor, :allowed_harvest_factor
+        attr_reader :usage
+
+        attr_reader :allowed_entry_factor, :allowed_harvest_factor
 
         def initialize(intervention, id, attributes = {})
           super(intervention, id, attributes)
-          @usage_id = attributes[:usage_id]
-          @allowed_entry_factor = attributes[:allowed_entry_factor]
-          @allowed_harvest_factor = attributes[:allowed_harvest_factor]
+          if @attributes[:usage_id].present?
+            self.usage_id = @attributes[:usage_id]
+          end
+        end
+
+        def usage_id
+          usage&.id
+        end
+
+        def usage=(value)
+          self.usage_id = value.id
         end
 
         def usage_id=(value)
-          @usage_id = value
-          impact_dependencies!(:usage_id)
+          @usage = RegisteredPhytosanitaryUsage.find(value)
+
+          self.allowed_harvest_factor = @usage.pre_harvest_delay
+          self.allowed_entry_factor = @usage.product.in_field_reentry_delay
+
+          impact_dependencies!(:usage)
         end
 
         def allowed_harvest_factor=(value)
@@ -30,22 +44,20 @@ module Procedo
 
         def to_hash
           hash = super
-          hash[:usage_id] = @usage_id if @usage_id.present?
-          hash[:allowed_entry_factor] = @allowed_entry_factor if @allowed_entry_factor.present?
-          hash[:allowed_harvest_factor] = @allowed_harvest_factor if @allowed_harvest_factor.present?
+          hash[:usage_id] = usage_id if usage_id.present?
           hash
         end
 
         def to_attributes
           hash = super
-          hash[:usage_id] = @usage_id if @usage_id.present?
-          hash[:allowed_entry_factor] = @allowed_entry_factor if @allowed_entry_factor.present?
-          hash[:allowed_harvest_factor] = @allowed_harvest_factor if @allowed_harvest_factor.present?
+          hash[:usage_id] = usage_id if usage_id.present?
+          hash[:allowed_entry_factor] = allowed_entry_factor if allowed_entry_factor.present?
+          hash[:allowed_harvest_factor] = allowed_harvest_factor if allowed_harvest_factor.present?
           hash
         end
 
         def env
-          super.merge(usage_id: @usage_id, allowed_entry_factor: @allowed_entry_factor, allowed_harvest_factor: @allowed_harvest_factor)
+          super.merge(usage_id: usage_id, allowed_entry_factor: allowed_entry_factor, allowed_harvest_factor: allowed_harvest_factor)
         end
       end
     end
