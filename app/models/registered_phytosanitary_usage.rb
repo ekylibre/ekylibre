2e1b7564b0943b198882f02f4b7a35c2b0ada5c1
Rémi Piotaix <remi@piotaix.fr>
2020-03-23 20:21:22 +0100

    Added missing map(:name) on Variety nomen items

--- app/models/registered_phytosanitary_usage.rb
+++ app/models/registered_phytosanitary_usage.rb
@@ -64,7 +64,7 @@ class RegisteredPhytosanitaryUsage < ActiveRecord::Base
 
   # Matches at least one of the given varieties
   scope :of_variety, -> (*varieties) do
-    with_ancestors = [*varieties, *varieties.flat_map { |v| Nomen::Variety.ancestors(Nomen::Variety.find(v)) }].uniq.join('", "')
+    with_ancestors = [*varieties, *varieties.flat_map { |v| Nomen::Variety.ancestors(Nomen::Variety.find(v)).map(&:name) }].uniq.join('", "')
 
     joins('LEFT OUTER JOIN registered_phytosanitary_cropsets ON registered_phytosanitary_usages.species[1] = registered_phytosanitary_cropsets.name')
       .where("registered_phytosanitary_usages.species && '{\"#{with_ancestors}\"}' OR registered_phytosanitary_cropsets.crop_names && '{\"#{with_ancestors}\"}'")
