715c1f0fb96238db7cc1c083e226e3de9ead5175
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-04-21 10:42:05 +0000

    Merge branch 'debug/journal-nature-filter' into 'core'
    Debug/journal nature filter
    
    See merge request ekylibre/eky!976
    
    (cherry picked from commit 274082c10cb4e1e68a11945d7412e23a5a7254d3)
    
    5ca8e998 Fix missing colon un journal condition builder

--- app/services/accountancy/condition_builder/journal_condition_builder.rb
+++ app/services/accountancy/condition_builder/journal_condition_builder.rb
@@ -2,7 +2,7 @@ module Accountancy
   module ConditionBuilder
     class JournalConditionBuilder < Base
       # Build an SQL condition based on options which should contains natures
-      def nature_condition(natures, table_name)
+      def nature_condition(natures, table_name:)
         if !natures.is_a?(Hash) || natures.empty?
           connection.quoted_true
         else
