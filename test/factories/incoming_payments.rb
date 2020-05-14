4f25db6a2077285eedddb193002bb877f1065ab3
Rémi PIOTAIX <remi@piotaix.fr>
2020-05-14 16:09:37 +0000

    Refacto Panier Local exchangers

--- test/factories/incoming_payments.rb
+++ test/factories/incoming_payments.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :incoming_payment do
+    transient do
+      at { nil }
+    end
     amount { 5000.0 }
     currency { 'EUR' }
     association :mode, factory: :incoming_payment_mode
     association :payer, factory: :entity
     provider { }
+    to_bank_at { at }
+    paid_at { at }
   end
 end
