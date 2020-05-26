d2c17cd9327188fee69f384bca7713cc2bac34d3
Rémi PIOTAIX <remi@piotaix.fr>
2020-05-26 08:02:22 +0000

    Removing empty daplos exchangers tests

--- test/exchangers/panier_local/sales_exchanger_test.rb
+++ test/exchangers/panier_local/sales_exchanger_test.rb
@@ -8,10 +8,11 @@ module PanierLocal
 
       I18n.locale = :fra
     end
+
     test 'import' do
       result = PanierLocal::SalesExchanger.build(fixture_files_path.join('imports', 'panier_local', 'panier_local_sales.csv'), options: { import_id: @import.id }).run
       assert result.success?, [result.message, result.exception]
-      assert_equal 45, SaleNature.find_by(name: "Vente en ligne - Panier Local").sales.count
+      assert_equal 3, SaleNature.find_by(name: "Vente en ligne - Panier Local").sales.count
     end
 
     teardown do
