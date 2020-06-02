d99bb6f0a5c4127655417df53a2647a306aeaedc
Nicolas PROCUREUR <nprocureur@gmail.com>
2020-06-02 12:49:52 +0000

    Merge branch 'debug/panier-local-user-person' into 'core'
    Fix panier_local sale exchanger
    
    See merge request ekylibre/eky!1047
    
    (cherry picked from commit 7f620ed776c92ca630fdc8a32b0f68f4e27bc765)
    
    41991b12 Fix palo bug when creating sale (expected Entity, got User)
    c262188a Adding tests, error message on missing VAT information
    f0e98c36 Fix import test

--- test/exchangers/panier_local/sale_exchanger_test/sales_exchanger_test.rb
+++ test/exchangers/panier_local/sale_exchanger_test/sales_exchanger_test.rb
@@ -5,14 +5,14 @@ module PanierLocal
     class SalesExchangerTest < ActiveExchanger::TestCase
       setup do
         # We wan't to keep tracking of import resource
-        @import = Import.create!(nature: :panier_local_sales)
+        @import = Import.create!(nature: :panier_local_sales, creator: User.first)
 
         I18n.locale = :fra
       end
 
       test 'import' do
         result = PanierLocal::SalesExchanger.build(fixture_files_path.join('imports', 'panier_local', 'panier_local_sales.csv'), options: { import_id: @import.id }).run
-        assert result.success?, -> { [result.message, result.exception.backtrace.first] }
+        assert result.success?, -> { [result.message, result.exception&.backtrace&.first] }
 
         sales = Sale.of_provider(:panier_local, :sales, @import.id)
         assert_equal 3, sales.count
