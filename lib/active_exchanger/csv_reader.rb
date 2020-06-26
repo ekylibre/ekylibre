8368abdc393113ada2f36f20abba9c1bfcb50f8b
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-06-26 13:07:27 +0000

    Merge branch 'debug/core/odicom-fec-fix-multiple-entities-by-account' into 'core'
    Debug/core/odicom fec fix multiple entities by account
    
    See merge request ekylibre/eky!1099

--- lib/active_exchanger/csv_reader.rb
+++ lib/active_exchanger/csv_reader.rb
@@ -2,11 +2,15 @@ module ActiveExchanger
   class CsvReader
     require 'csv'
 
+    def initialize(col_sep: ',')
+      @separator = col_sep
+    end
+
     def read(filename)
       source = File.read(filename)
       detection = CharlockHolmes::EncodingDetector.detect(source)
 
-      CSV.read(filename, headers: true, encoding: detection[:encoding], col_sep: ';')
+      CSV.read(filename, headers: true, encoding: detection[:encoding], col_sep: @separator)
     end
   end
 end
