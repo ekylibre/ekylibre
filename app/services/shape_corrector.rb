e909bcf37e490fa7e5d39b13b8af1c7e4cb8d087
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-07-20 10:03:58 +0000

    Merge branch 'feature/add-ewkt-validation-in-shape-validator' into 'core'
    Add a method to be able to validate a EWKT string without parsing it first with RGeo
    
    See merge request ekylibre/eky!1167

--- app/services/shape_corrector.rb
+++ app/services/shape_corrector.rb
@@ -20,7 +20,9 @@ class ShapeCorrector
   # @param [Charta::Geometry] shape
   # @return [Maybe<Charta::Geometry>]
   def try_fix(shape)
-    new_shape = try_postgis_fix(shape).or_else { return None() }
+    new_shape = try_fix_ewkt(shape.to_ewkt)
+                  .fmap { |ewkt| Charta.new_geometry(ewkt) }
+                  .or_else { return None() }
 
     area_ratio(shape, new_shape).fmap do |ratio|
       if (ratio - 1).abs < @change_threshold
@@ -31,33 +33,39 @@ class ShapeCorrector
     end
   end
 
+  # @param [String] ewkt
+  # @return [Maybe<String>]
+  def try_fix_ewkt(ewkt)
+    try_postgis_fix(ewkt)
+  end
+
   private
 
-    # @param [Charta::Geometry] shape
-    # @return [Maybe<Charta::Geometry>]
-    def try_postgis_fix(shape)
-      res = @connection.execute(<<~SQL).to_a.first
-        SELECT ST_AsEWKT(ST_MakeValid(ST_GeomFromEWKT('#{shape.to_ewkt}'))) AS valid_shape
-      SQL
+  # @param [String] ewkt
+  # @return [Maybe<String>]
+  def try_postgis_fix(ewkt)
+    res = @connection.execute(<<~SQL).to_a.first
+      SELECT ST_AsEWKT(ST_MakeValid(ST_GeomFromEWKT('#{ewkt}'))) AS valid_shape
+    SQL
 
-      if res.present?
-        Maybe(Charta.new_geometry(res['valid_shape']))
-      else
-        None()
-      end
+    if res.present?
+      Maybe(res['valid_shape'])
+    else
+      None()
     end
+  end
 
-    # @param [Charta::Geometry] original_shape
-    # @param [Charta::Geometry] new_shape
-    # @return [Maybe<Number>]
-    def area_ratio(original_shape, new_shape)
-      original_area = original_shape.area
-      new_area = new_shape.area
+  # @param [Charta::Geometry] original_shape
+  # @param [Charta::Geometry] new_shape
+  # @return [Maybe<Number>]
+  def area_ratio(original_shape, new_shape)
+    original_area = original_shape.area
+    new_area = new_shape.area
 
-      if original_area.positive?
-        Some(new_area / original_area)
-      else
-        None()
-      end
+    if original_area.positive?
+      Some(new_area / original_area)
+    else
+      None()
     end
+  end
 end
