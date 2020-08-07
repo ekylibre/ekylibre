21aa8c02d388156e1d8f7ff2add65f9fa0373b43
Rémi PIOTAIX <remi@piotaix.fr>
2020-08-07 07:08:52 +0000

    Remove unused js and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,9 +60,6 @@
 //= require leaflet/easy-button
 //= require leaflet/modal
 //= require leaflet/label
-//= require d3
-//= require d3/tip
-//= require timeline-chart.js
 //= require autosize
 //= require plugins
 //= require_tree .
@@ -72,7 +69,6 @@
 //= require vue
 //= require sortablejs/Sortable.min.js
 //= require vuedraggable/dist/vuedraggable.js
-//= require chart.js/dist/Chart.min.js
 //= require vue-chartjs/dist/vue-chartjs.min.js
 
 //= require ext-plugins/vuejs/vue-chartjs
--- app/assets/javascripts/chart.js
+++
@@ -1,9 +0,0 @@
-// Chart library is an abstraction layer for chart rendering
-//
-//= require_self
-//= require chart/adapters
-//= require chart/widget
-//= require chart/highcharts
-//
-
-var chart = {};
