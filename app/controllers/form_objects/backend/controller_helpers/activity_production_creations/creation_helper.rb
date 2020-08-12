fd10d5439440200e81abd0ea1ceba5f28c178c78
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-08-12 08:23:52 +0000

    Merge branch 'debug/land-parcel-process' into 'core'
    Debug/land parcel process
    
    See merge request ekylibre/eky!1212

--- app/controllers/form_objects/backend/controller_helpers/activity_production_creations/creation_helper.rb
+++ app/controllers/form_objects/backend/controller_helpers/activity_production_creations/creation_helper.rb
@@ -7,6 +7,8 @@ module FormObjects
                     attr_accessor :activity
                     # @return [String]
                     attr_accessor :campaign
+                    # @return [String]
+                    attr_accessor :cultivable_zone
 
                     validates :campaign, presence: true
                     validates :activity, presence: true
@@ -16,12 +18,17 @@ module FormObjects
                         if Activity.find_by(id: activity).nil?
                           errors.add(:activity, :invalid)
                         end
+                        
                         # test if campaign exists and add :invalid error
                         if Campaign.find_by(id: campaign).nil?
                           errors.add(:campaign, :invalid)
                         end
-                    end
 
+                        # test if cultivable_zone exists and add :invalid error
+                        if cultivable_zone.present? && CultivableZone.find_by(id: cultivable_zone).nil?
+                          errors.add(:cultivable_zone, :invalid)
+                        end
+                    end
                 end
             end
         end
