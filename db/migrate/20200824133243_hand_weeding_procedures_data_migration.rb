9a02aacf11140bbb793a5719c3bbbc8b2dfade03
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-08-24 16:39:32 +0200

    Removed input from hand_weeding procedure and added data migration

---
+++ db/migrate/20200824133243_hand_weeding_procedures_data_migration.rb
@@ -0,0 +1,29 @@
+class HandWeedingProceduresDataMigration < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      UPDATE intervention_parameters AS ipd
+        SET reference_name = 'driver'
+      FROM interventions AS i
+        INNER JOIN intervention_parameters AS ipi
+          ON ipi.intervention_id = i.id
+          AND ipi.type = 'InterventionInput'
+      WHERE ipd.intervention_id = i.id
+        AND i.procedure_name = 'hand_weeding'
+        AND ipd.type = 'InterventionDoer';
+
+      UPDATE intervention_parameters AS ip
+        SET reference_name = 'plant_medicine'
+      FROM interventions AS i
+      WHERE ip.intervention_id = i.id
+        AND procedure_name = 'hand_weeding'
+        AND ip.type = 'InterventionInput';
+
+      UPDATE interventions AS i
+        SET procedure_name = 'spraying'
+      FROM intervention_parameters AS ip
+      WHERE ip.intervention_id = i.id
+        AND procedure_name = 'hand_weeding'
+        AND ip.type = 'InterventionInput'
+    SQL
+  end
+end
