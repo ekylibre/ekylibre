13b49e21a4f3d0ad4303e4dddf8f672980aa4bb0
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-08-31 11:51:36 +0200

    Removed irrelevant exchangers

--- test/exchangers/unicoque/outgoing_deliveries_exchanger_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Unicoque
-  class OutgoingDeliveriesExchangerTest < ActiveExchanger::TestCase
-    # test 'import' do
-    #   Unicoque::OutgoingDeliveriesExchanger.import(fixture_files_path.join('imports', 'unicoque', 'outgoing_deliveries.csv'))
-    # end
-  end
-end
--- test/exchangers/viniteca/interventions_exchanger_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Viniteca
-  class InterventionsExchangerTest < ActiveExchanger::TestCase
-    # test 'import' do
-    #   Viniteca::InterventionsExchanger.import(fixture_files_path.join('imports', 'viniteca', 'interventions.csv'))
-    # end
-  end
-end
