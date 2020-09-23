42dead8a71bc94b334a47641482455556beca38b
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-09-23 13:36:48 +0000

    Merge branch 'performance/trade-counts-cell' into 'develop/fusion'
    Compute values of trade_count_cell using SQL
    
    See merge request ekylibre-integration/larrere!420

--- app/controllers/backend/cells/trade_counts_cells_controller.rb
+++ app/controllers/backend/cells/trade_counts_cells_controller.rb
@@ -1,7 +1,33 @@
 module Backend
   module Cells
     class TradeCountsCellsController < Backend::Cells::BaseController
-      def show; end
+      def show
+        @unpaid_sales_amount = sales_amount
+        @unpaid_purchases_amount = purchases_amount
+
+      end
+
+      private
+
+        def sales_amount
+          ApplicationRecord.connection.execute(<<~SQL).first["balance"].to_d
+            SELECT abs(sum(affairs.credit - affairs.debit)) AS balance
+            FROM "sales"
+            LEFT JOIN "affairs" ON "affairs"."id" = "sales"."affair_id"
+            WHERE "sales"."state" IN ('order', 'invoice')
+              AND NOT "affairs"."closed"
+          SQL
+        end
+
+        def purchases_amount
+          ApplicationRecord.connection.execute(<<~SQL).first["balance"].to_d
+            SELECT abs(sum(affairs.credit - affairs.debit)) AS balance
+            FROM "purchases"
+            LEFT JOIN "affairs" ON "affairs"."id" = "purchases"."affair_id"
+            WHERE "purchases"."type" = 'PurchaseInvoice'
+              AND NOT "affairs"."closed"
+          SQL
+        end
     end
   end
 end
