54ef138d3c3bb0a36d352623b884ec9fef502550
Mateo Greil <mgreil@ekylibre.com>
2020-09-30 15:56:55 +0000

    Performance/activities#show

--- app/services/activities/net_surface_area_calculation.rb
+++ app/services/activities/net_surface_area_calculation.rb
@@ -4,16 +4,6 @@
 module Activities
   class NetSurfaceAreaCalculation
 
-    SELECT_SRID = %{
-      SELECT CASE
-        WHEN string_value = 'WGS84' THEN 4326
-        WHEN string_value = 'RGF93' THEN 2154
-        WHEN string_value ~ '_' THEN SPLIT_PART(string_value, '_', 2)::INTEGER
-        ELSE 0
-      END AS srid
-      FROM preferences WHERE name = 'map_measure_srs' LIMIT 1
-    }
-
     def compute_net_surface_area(activity, campaign)
       activity_campaign_net_surface_area = ActiveRecord::Base.connection.execute <<~SQL
         SELECT
@@ -25,9 +15,7 @@ module Activities
             activity_productions.id,
             activity_productions.activity_id AS activity_id,
             activity_productions_campaigns.campaign_id AS campaign_id,
-            ST_Area(ST_Transform(activity_productions.support_shape, (
-            #{SELECT_SRID}
-            )))/10000 as ha
+            ST_Area(activity_productions.support_shape, TRUE) / 10000 AS ha
           FROM "activity_productions"
           INNER JOIN activity_productions_campaigns
             ON activity_productions_campaigns.activity_production_id = activity_productions.id
