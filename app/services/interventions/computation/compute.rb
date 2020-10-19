ac16acb67b0e60f73ff9d908860b5d1503b9d752
Rémi Piotaix <remi@piotaix.fr>
2020-10-19 13:51:26 +0000

    Merge branch 'feature/synchro-with-63.3' into 'develop/fusion'
    Synchro with 63.3
    
    See merge request ekylibre/integration/larrere!533

--- app/services/interventions/computation/compute.rb
+++ app/services/interventions/computation/compute.rb
@@ -25,15 +25,18 @@ module Interventions
           # Iterate over engine_intervention parameters in order to add reading to parameter which depend on it for calculation
           ComputeReadings.new(engine_intervention).perform
 
-          # Enables to add the custom reading passed on the API payload and extracted before creating the procedo intervention
-          AddCustomReadings.new(engine_intervention, readings).perform if readings.any?
+          begin
+            # Enables to add the custom reading passed on the API payload and extracted before creating the procedo intervention
+            AddCustomReadings.new(engine_intervention, readings).perform if readings.any?
 
-          # Build 'html' selector which enables to recognize which part of the attributes needs to be updated
-          UpdateEngineIntervention.new(engine_intervention, @parameters).perform
+            # Build 'html' selector which enables to recognize which part of the attributes needs to be updated
+            UpdateEngineIntervention.new(engine_intervention, @parameters).perform
 
-          attributes = attributes.merge(engine_intervention.to_attributes)
-          # Readings added on references needs to be removed after merging attributes in order for the visual interface not to display additional fields
-          RemoveCustomProcedureReadings.new(engine_intervention).perform if readings.any?
+            attributes = attributes.merge(engine_intervention.to_attributes)
+          ensure
+            # Readings added on references needs to be removed after merging attributes in order for the visual interface not to display additional fields
+            RemoveCustomProcedureReadings.new(engine_intervention).perform if readings.any?
+          end
 
           attributes
         end
