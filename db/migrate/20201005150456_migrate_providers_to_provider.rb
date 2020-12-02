46f76adc23a48470c557bd840a29ca1f7d16f8fe
Lucas Sotty <lucas.sotty@wanadoo.fr>
2020-12-02 15:12:45 +0100

    Fixed providers migration

--- db/migrate/20201005150456_migrate_providers_to_provider.rb
+++ db/migrate/20201005150456_migrate_providers_to_provider.rb
@@ -3,7 +3,7 @@ class MigrateProvidersToProvider < ActiveRecord::Migration
     execute <<~SQL
       UPDATE interventions
       SET "provider" = CONCAT('{"vendor": "ekylibre", "name": "zero", "id": 0, "data": {"zero_id": ', providers->>'zero_id', ' }}')::json
-      WHERE "providers" IS NOT NULL
+      WHERE "providers" IS NOT NULL AND "providers"->>'zero_id' IS NOT NULL
     SQL
   end
 end
