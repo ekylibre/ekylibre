48ed1583c89baed513361eee42a7a2921b1b84c5
Mateo Greil <mgreil@ekylibre.com>
2020-12-03 10:34:41 +0000

    Resolve "Costing are not deleted when parent intervention is destroy"

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -81,7 +81,7 @@ class Intervention < Ekylibre::Record::Base
   belongs_to :prescription
   belongs_to :journal_entry, dependent: :destroy
   belongs_to :purchase
-  belongs_to :costing, class_name: 'InterventionCosting'
+  belongs_to :costing, class_name: 'InterventionCosting', dependent: :destroy
   belongs_to :validator, class_name: 'User', foreign_key: :validator_id
   has_many :receptions, class_name: 'Reception', dependent: :destroy
   has_many :labellings, class_name: 'InterventionLabelling', dependent: :destroy, inverse_of: :intervention
