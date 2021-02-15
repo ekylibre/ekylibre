99fd1842e840df69b10d19075829c51a66dcf453
Mateo Greil <mgreil@ekylibre.com>
2021-02-15 15:13:01 +0100

    remove raise_in_transactional_callbacks
    https://stackoverflow.com/a/37465836/12107608

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,6 @@ module Ekylibre
 
     # We want to use the structure.sql file
     config.active_record.schema_format = :sql
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
