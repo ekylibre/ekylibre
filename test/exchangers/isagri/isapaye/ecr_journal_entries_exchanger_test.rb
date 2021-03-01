6186eaaca16eed1d48a5acb969c230ff1364ea39
David JOULIN <djoulin@ekylibre.com>
2021-03-01 15:11:18 +0100

    Rename ECX to ECR. Fix translations.

--- test/exchangers/isagri/isapaye/ecr_journal_entries_exchanger_test.rb
+++ test/exchangers/isagri/isapaye/ecr_journal_entries_exchanger_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 module Isagri
   module Isapaye
-    class EcxJournalEntriesExchangerTest < ActiveExchanger::TestCase
+    class EcrJournalEntriesExchangerTest < ActiveExchanger::TestCase
       setup do
         FinancialYear.delete_all
         FinancialYear.create! started_on: '2020-01-01', stopped_on: '2020-12-31'
         FinancialYear.create! started_on: '2021-01-01', stopped_on: '2021-12-31'
         I18n.locale = :fra
         Preference.set!(:accounting_system, 'fr_pcga')
-        @import = Import.create!(nature: :isagri_isapaye_ecx_journal_entries, creator: User.first)
+        @import = Import.create!(nature: :isagri_isapaye_ecr_journal_entries, creator: User.first)
       end
 
       test 'import' do
-        result = Isagri::Isapaye::EcxJournalEntriesExchanger.build(fixture_files_path.join('imports', 'isagri', 'isapaye', 'ecx_journal_entries.ecr'), options: { import_id: @import.id }).run
+        result = Isagri::Isapaye::EcrJournalEntriesExchanger.build(fixture_files_path.join('imports', 'isagri', 'isapaye', 'ecr_journal_entries.ecr'), options: { import_id: @import.id }).run
         assert result.success?, [result.message, result.exception]
         # check data on balance sheet
         fy = FinancialYear.at(Date.parse('2021-07-31'))
