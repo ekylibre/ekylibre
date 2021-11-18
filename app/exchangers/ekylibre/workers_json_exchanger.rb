6acc83be199c0e2e329c0266344ff8ed6cba3e32
Manuel <mlabous@ekylibre.com>
2021-11-18 09:12:12 +0000

    Adding workers Json exchanger to import first-run workers

---
+++ app/exchangers/ekylibre/workers_json_exchanger.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Ekylibre
+  class WorkersJsonExchanger < ActiveExchanger::Base
+    category :plant_farming
+    vendor :ekylibre
+
+    def import
+      workers = JSON.parse(file.read)
+      workers.each do |worker|
+        entity = Entity.create!(
+          first_name: worker['first_name'],
+          last_name: worker['last_name'],
+          nature: 'contact'
+        )
+        WorkerContract.import_from_lexicon(
+          reference_name: worker['contract'],
+          entity_id: entity.id,
+          started_at: Time.now
+        )
+        Worker.create!(
+          person: entity,
+          name: entity.full_name,
+          variant: ProductNatureVariant.import_from_lexicon(worker['type'])
+        )
+      end
+    end
+  end
+end
