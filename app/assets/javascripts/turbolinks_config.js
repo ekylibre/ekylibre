5316ef02b9862b6988211ee9aacdf8e214f4ef58
Thibaut Gorioux <tgorioux@ekylibre.com>
2021-12-07 14:58:52 +0000

    Don't refresh page when %a{href='#'} is clicked

---
+++ app/assets/javascripts/turbolinks_config.js
@@ -0,0 +1,9 @@
+Turbolinks.enableTransitionCache();
+Turbolinks.enableProgressBar();
+
+// Don't refresh page when <a href='#'> is clicked
+$(document).on('turbolinks:click', function (event) {
+  if (event.target.getAttribute('href').charAt(0) === '#') {
+    event.preventDefault()
+  }
+})
