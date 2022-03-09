2a45d2dc39859999161de8556772d8bd3d8d71ff
Thibaut Gorioux <tgorioux@ekylibre.com>
2022-03-09 10:38:32 +0000

    Revert/worker groups

--- app/models/worker_group_item.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class WorkerGroupItem < ApplicationRecord
-
-  belongs_to :worker_group, class_name: "WorkerGroup"
-  belongs_to :worker, class_name: "Product"
-
-end
