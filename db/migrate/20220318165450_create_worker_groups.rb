591d2b8f9a6ce8544b8f148c77d13af7ccb96881
David JOULIN <djoulin@ekylibre.com>
2022-03-22 18:13:46 +0100

    Fix bug on sidekiq conf and rename migration.
    Former-commit-id: 89e0fa68ac75f7bbcb49e56e327cf97fa826f02b

--- db/migrate/20220318165450_create_worker_groups.rb
+++ db/migrate/20220318165450_create_worker_groups.rb
@@ -1,5 +1,8 @@
 class CreateWorkerGroups < ActiveRecord::Migration[5.0]
   def change
+    if table_exists? :worker_groups
+      drop_table :worker_groups, cascade: true
+    end
     create_table :worker_groups do |t|
       t.string :name, null: false
       t.string :work_number
@@ -7,10 +10,17 @@ class CreateWorkerGroups < ActiveRecord::Migration[5.0]
       t.string :usage
       t.stamps
     end
+    if table_exists? :worker_group_items
+      remove_reference :products, :worker_group_item
+      drop_table :worker_group_items, cascade: true
+    end
     create_table :worker_group_items do |t|
       t.references :worker, index: true
       t.references :worker_group, index: true
       t.timestamps
+    end
+    if table_exists? :worker_group_labellings
+      drop_table :worker_group_labellings, cascade: true
     end
     create_table :worker_group_labellings do |t|
       t.references :worker_group, index: true, foreign_key: true
