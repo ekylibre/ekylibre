4ad037a77a38a008dcace4c1c285d41f768db81b
AugustinC <achipault@ekylibre.com>
2022-03-22 13:52:05 +0100

    Update rights display
    Former-commit-id: a914b662be0054207fdfe40ace622de650321cd1

--- app/helpers/backend/form_builder.rb
+++ app/helpers/backend/form_builder.rb
@@ -694,9 +694,7 @@ module Backend
         data = ''.html_safe
         resources.sort { |a, b| Ekylibre::Access.human_resource_name(a.first).ascii <=> Ekylibre::Access.human_resource_name(b.first).ascii }.each do |resource, rights|
           resource_reference = reference.include?(resource.to_s) ? reference[resource.to_s] : []
-          all_access = resource.to_s.match?(/all_access/i)
           attributes = { class: "control-label" }
-          attributes[:class] << 'label-bold' if all_access
           data << @template.content_tag(:div, class: 'control-group booleans') do
             @template.content_tag(:label, attributes) do
               Ekylibre::Access.human_resource_name(resource)
