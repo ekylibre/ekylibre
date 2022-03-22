7507d6d6d260b563fe382d8c3e52502559ebfd9e
Augustin Chipault <achipault@ekylibre.com>
2022-03-22 18:06:44 +0000

    In Activity_production add a depent destroy on_intervention_proposals
    Former-commit-id: 7b5468773648fa508ab89e4e7c6fe376f700a8dc

--- app/models/activity_production.rb
+++ app/models/activity_production.rb
@@ -87,7 +87,7 @@ class ActivityProduction < ApplicationRecord
   belongs_to :technical_itinerary, class_name: TechnicalItinerary
   has_one :batch, class_name: ActivityProductionBatch, dependent: :destroy, inverse_of: :activity_production
   has_many :daily_charges, class_name: DailyCharge, dependent: :destroy, foreign_key: :activity_production_id
-  has_many :intervention_proposals, class_name: InterventionProposal, foreign_key: :activity_production_id
+  has_many :intervention_proposals, class_name: InterventionProposal, dependent: :destroy, foreign_key: :activity_production_id
 
   accepts_nested_attributes_for :batch, allow_destroy: true
 
