3a082940c5a2f36bf706211010369c5ebf511978
David JOULIN <djoulin@ekylibre.com>
2022-05-06 07:26:27 +0000

    Resolve "le jardin des peltiers - tri liste ecritures comptables"
    Former-commit-id: a2f2ee1873547307865b92710e5f894755c9cfe8

--- app/controllers/backend/journal_entries_controller.rb
+++ app/controllers/backend/journal_entries_controller.rb
@@ -42,7 +42,8 @@ module Backend
 
     list(:items, model: :journal_entry_items, conditions: { entry_id: 'params[:id]'.c }, order: :position) do |t|
       t.icon :lock, if: :currently_exchanged?
-      t.column :displayed_label_in_accountancy, label: :entry_item_label
+      t.column :position
+      t.column :name
       t.column :account, url: true
       t.column :account_number, through: :account, label_method: :number, url: true, hidden: true
       t.column :account_name, through: :account, label_method: :name, url: true, hidden: true
