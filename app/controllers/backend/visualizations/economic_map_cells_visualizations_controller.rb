06f0247a2e52af86c57f05953bbed28e14feefa7
David JOULIN <djoulin@ekylibre.com>
2022-11-29 14:25:34 +0000

    Resolve "Ajout d'une carte économique permettant de voir les couts de productions par intrants, travailleur et équipement"
    Former-commit-id: 231937352203d60369f273b37a5847bbfeac2859

--- app/controllers/backend/visualizations/economic_map_cells_visualizations_controller.rb
+++ app/controllers/backend/visualizations/economic_map_cells_visualizations_controller.rb
@@ -24,9 +24,12 @@ module Backend
 
             economic_data = ActivityProductionsInterventionsCost.of_activity_production(support)
             interventions_count = economic_data.pluck(:intervention_id).uniq.count
-            tool_cost = economic_data.sum(:tools)
-            input_cost = economic_data.sum(:inputs)
-            time_cost = economic_data.sum(:doers)
+            surface_unit_name = :hectare
+            area = support.support_shape_area.to_d(surface_unit_name)
+            tool_cost = economic_data.sum(:tools) / area
+            input_cost = economic_data.sum(:inputs) / area
+            time_cost = economic_data.sum(:doers) / area
+            global_cost = ( tool_cost + input_cost + time_cost )
 
             popup_content = []
 
@@ -36,22 +39,20 @@ module Backend
             popup_content << { label: ActivityProduction.human_attribute_name(:net_surface_area), value: support.human_support_shape_area }
             popup_content << { label: ActivityProduction.human_attribute_name(:activity), value: view_context.link_to(support.activity_name, backend_activity_path(support.activity)) }
 
-            popup_content << { label: :costs_per_hectare.tl }
+            popup_content << { label: :costs_per_hectare.tl, value: global_cost.to_s.to_f.round(2) }
             popup_content << { value: "#{:inputs.tl} : #{input_cost.to_s.to_f.round(2)}" }
             popup_content << { value: "#{:tools.tl} : #{tool_cost.to_s.to_f.round(2)}" }
             popup_content << { value: "#{:times.tl} : #{time_cost.to_s.to_f.round(2)}" }
-
-            surface_unit_name = :hectare
 
             item = {
               name: support.name,
               shape: support.support_shape,
               shape_color: support.activity.color,
               activity: support.activity.name,
+              global_cost: global_cost.to_s.to_f.round(2),
               tool_cost: tool_cost.to_s.to_f.round(2),
               input_cost: input_cost.to_s.to_f.round(2),
               time_cost: time_cost.to_s.to_f.round(2),
-              interventions_count: interventions_count,
               popup: { header: true, content: popup_content }
             }
             data << item
@@ -59,8 +60,7 @@ module Backend
 
           config = view_context.configure_visualization do |v|
             v.serie :main, data
-            v.categories :activity, :main
-            v.choropleth :interventions_count, :main
+            v.choropleth :global_cost, :main, stop_color: "#E77000"
             v.choropleth :tool_cost, :main, stop_color: "#00AA00"
             v.choropleth :input_cost, :main, stop_color: "#1122DD"
             v.choropleth :time_cost, :main, stop_color: "#E77000"
