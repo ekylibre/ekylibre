6cc50d026c1a7d7393a44aa9f5ad4b7dac84e952
David JOULIN <djoulin@ekylibre.com>
2026-06-13 22:03:32 +0200

    Add geojson on parcel api

--- db/structure.sql
+++ db/structure.sql
@@ -1,4 +1,4 @@
-\restrict s5pjKRYECT1FNWYsSf3Teb5zUlKUMh4Bc5vrUxhL7edc1Laj9b7f6HW9QHnVf6H
+\restrict CpI8qhzgEcm92MP5CzIXRCeayWAeQHmgMpzBBABokUHWlHuNjnrmZt40C1NjhZn
 
 -- Dumped from database version 13.4 (Debian 13.4-1.pgdg110+1)
 -- Dumped by pg_dump version 13.23 (Debian 13.23-1.pgdg11+1)
@@ -30342,7 +30342,7 @@ ALTER TABLE ONLY public.projects
 -- PostgreSQL database dump complete
 --
 
-\unrestrict s5pjKRYECT1FNWYsSf3Teb5zUlKUMh4Bc5vrUxhL7edc1Laj9b7f6HW9QHnVf6H
+\unrestrict CpI8qhzgEcm92MP5CzIXRCeayWAeQHmgMpzBBABokUHWlHuNjnrmZt40C1NjhZn
 
 SET search_path TO public,postgis,lexicon;
 
